5b4b59c93bffd4353c8515b825cdf35566d845eb
yasir rafique <yasir.chimp@gmail.com>
2024-10-09 01:19:33 -0700

    Update question_top_layout.blade.php

--- resources/views/web/default/question_layouts/question_top_layout.blade.php
+++ resources/views/web/default/question_layouts/question_top_layout.blade.php
@@ -2,6 +2,14 @@
 <div class="question-label">
     <span>Question Lbael HTML</span>
 </div>
+<!-- Select Option Html -->
+<div id="rureraform-element-0" class="rureraform-element-0 rureraform-element quiz-group rureraform-element-html ui-sortable-handle" data-type="inner_dropdown"></div>
+	<select id="dropdown-1">
+    <option value="Option 1">Option 1</option>
+    <option value="Option 2">Option 2</option>
+    <option value="Option 3">Option 3</option>
+  </select>
+</div>
 
 @if( $questionObj->review_required == 1 || $questionObj->developer_review_required == 1)
 <div class="question-review-required">
