c1dde1868e649a730d7cdd6c71ef7b952ed9081a
yasir rafique <yasir.chimp@gmail.com>
2024-10-09 01:15:47 -0700

    Update question_top_layout.blade.php

--- resources/views/web/default/question_layouts/question_top_layout.blade.php
+++ resources/views/web/default/question_layouts/question_top_layout.blade.php
@@ -1,4 +1,8 @@
 @if( $questionObj->review_required == 1 || $questionObj->developer_review_required == 1)
+<!-- Question Label Html -->
+<div class="question-label">
+    <span>Question Lbael HTML</span>
+</div>
 <div class="question-review-required">
     @if( $questionObj->review_required == 1)
     <div class="question-label-tag">Teacher Review Required</div>
