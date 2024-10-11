4870c4e53fbf8e57b811c80ac6b2ff29bb1b3942
bazacton <baz.chimpstudio@gmail.com>
2024-10-11 11:14:26 +0500

    Label add after input - Attachment Element
    Label add after input - Attachment Element

--- resources/views/web/default/question_layouts/attachment_quiz_layout.blade.php
+++ resources/views/web/default/question_layouts/attachment_quiz_layout.blade.php
@@ -1,3 +1,4 @@
 <div id="rureraform-element-{{$element_id}}" class="rureraform-element-{{$element_id}} rureraform-element quiz-group rureraform-element-html ui-sortable-handle" data-type="{{$elementObj->type}}">
 	<input type="file" class="editor-field input-simple" data-id="{{$element_unique_id}}" id="field-{{$element_unique_id}}" name="field-{{$element_unique_id}}">
+	<label for="file" class="choose-file-btn" />choose file</label>
 </div>
