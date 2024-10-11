ab0b85a995dddabd4a7b8a7bdbbbdb3502d03318
bazacton <baz.chimpstudio@gmail.com>
2024-10-11 18:25:58 +0500

    books objects data rendering
    books objects data rendering

--- resources/views/web/default/books/includes/look_for_clues.blade.php
+++ resources/views/web/default/books/includes/look_for_clues.blade.php
@@ -1,13 +1,12 @@
 @php $data_values = json_decode($pageInfoLink->data_values);
-$content = isset($data_values->infobox_value)? base64_decode(trim(stripslashes($data_values->infobox_value))) : '';
-
+$content = isset($data_values->info_content)? $data_values->info_content : '';
 @endphp
 <div class="flipbook-slide-menu">
     <div class="slide-menu-head">
         <div class="menu-controls">
             <a href="#" class="close-btn"><i class="fa fa-chevron-right"></i></a>
         </div>
-        <h4>{{$pageInfoLink->info_title}}</h4>
+        <h4>{{$pageInfoLink->item_title}}</h4>
     </div>
     <div class="slide-menu-body">
         {!! $content !!}
