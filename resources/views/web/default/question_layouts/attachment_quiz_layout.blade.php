06ee6b083e373927a9a7808370ac59046cdf2dde
yasir rafique <yasir.chimp@gmail.com>
2024-10-11 04:51:00 -0700

    for testing

--- resources/views/web/default/question_layouts/attachment_quiz_layout.blade.php
+++ resources/views/web/default/question_layouts/attachment_quiz_layout.blade.php
@@ -1,4 +1,4 @@
 <div id="rureraform-element-{{$element_id}}" class="rureraform-element-{{$element_id}} rureraform-element quiz-group rureraform-element-html ui-sortable-handle" data-type="{{$elementObj->type}}">
-	<input type="file" class="editor-field input-simple" data-id="{{$element_unique_id}}" id="field-{{$element_unique_id}}" name="field-{{$element_unique_id}}">
+	<input type="file" class="editor-field input-simple" data-id="{{$element_unique_id}}" id="field-{{$element_unique_id}}" name="">
 	<label for="field-{{$element_unique_id}}" class="choose-file-btn">choose file</label>
 </div>
