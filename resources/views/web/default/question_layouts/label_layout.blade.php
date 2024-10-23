80f8c6ff433de563f4514eae0a49cdcf3c789e98
bazacton <baz.chimpstudio@gmail.com>
2024-10-23 12:30:00 +0500

    question label type
    question label type

--- resources/views/web/default/question_layouts/label_layout.blade.php
+++ resources/views/web/default/question_layouts/label_layout.blade.php
@@ -1,3 +1,3 @@
 <div id="rureraform-element-{{$element_id}}" class="rureraform-element-{{$element_id}} rureraform-element quiz-group rureraform-element-html ui-sortable-handle" data-type="{{$elementObj->type}}">
-	<div class="question-label"><span>{!! $elementObj->content !!}</span></div>
+	<div class="question-label {{isset( $elementObj->label_type ) ? $elementObj->label_type : ''}}"><span>{!! $elementObj->content !!}</span></div>
 </div>
