87ca6137e9ace6317e30900c04c2db0e5a08cd6f
yasir rafique <yasir.chimp@gmail.com>
2024-11-04 05:22:18 -0800

    Update generatet_form.blade.php

--- resources/views/admin/questions_generator/generatet_form.blade.php
+++ resources/views/admin/questions_generator/generatet_form.blade.php
@@ -144,43 +144,54 @@
 		@csrf
 		
         <!-- Content Text Area -->
-		 <div class="col-md-12 col-lg-12">
-			<label for="content">Enter Content (Max 400 Words):</label>
-			<textarea name="content" id="content" rows="4" maxlength="400" required></textarea>
-		 </div>
-        
-		<div class="col-md-12 col-lg-12">
-		<div class="form-group">
-			<label class="input-label">{{trans('admin/main.category')}}</label>
-			<select name="category_id" data-plugin-selectTwo class="form-control populate ajax-category-courses" data-course_id="">
-				<option value="">{{trans('admin/main.all_categories')}}</option>
-				@foreach($categories as $category)
-				@if(!empty($category->subCategories) and count($category->subCategories))
-				<optgroup label="{{  $category->title }}">
-					@foreach($category->subCategories as $subCategory)
-					<option value="{{ $subCategory->id }}">{{ $subCategory->title }}</option>
+        <label for="content">Enter Content (Max 400 Words):</label>
+        <textarea name="content" id="content" rows="4" maxlength="400" required></textarea>
+
+		
+		
+		<div class="col-md-3">
+			<div class="form-group">
+				<label class="input-label">{{trans('admin/main.category')}}</label>
+				<select name="category_id" data-plugin-selectTwo class="form-control populate ajax-category-courses" data-course_id="">
+					<option value="">{{trans('admin/main.all_categories')}}</option>
+					@foreach($categories as $category)
+					@if(!empty($category->subCategories) and count($category->subCategories))
+					<optgroup label="{{  $category->title }}">
+						@foreach($category->subCategories as $subCategory)
+						<option value="{{ $subCategory->id }}">{{ $subCategory->title }}</option>
+						@endforeach
+					</optgroup>
+					@else
+					<option value="{{ $category->id }}">{{ $category->title }}</option>
+					@endif
 					@endforeach
 				</select>
 			</div>
 		</div>
 	
-	<div class="col-md-12 col-md-12 col-lg-12">
-	<div class="form-group">
-		<label>Subjects</label>
-		<select data-return_type="option"
-				data-default_id="{{request()->get('subject_id')}}" data-chapter_id=""
-				class="ajax-courses-dropdown year_subjects form-control select2 @error('subject_id') is-invalid @enderror"
-				id="subject_id" name="subject_id">
-			<option disabled selected>Subject</option>
-		</select>
-		@error('subject_id')
-		<div class="invalid-feedback">
-			{{ $message }}
+	
+	
+	
+
+	<div class="col-md-3">
+		<div class="form-group">
+			<label>Subjects</label>
+			<select data-return_type="option"
+					data-default_id="{{request()->get('subject_id')}}" data-chapter_id=""
+					class="ajax-courses-dropdown year_subjects form-control select2 @error('subject_id') is-invalid @enderror"
+					id="subject_id" name="subject_id">
+				<option disabled selected>Subject</option>
+			</select>
+			@error('subject_id')
+			<div class="invalid-feedback">
+				{{ $message }}
+			</div>
+			@enderror
 		</div>
 	</div>
 	
 	
-	<div class="col-md-12 col-lg-12">
+	<div class="col-md-2">
 	<div class="form-group">
 		<label class="input-label">Topic</label>
 		<select data-sub_chapter_id="" id="chapter_id"
@@ -198,7 +209,7 @@
 	</div>
 	
 	
-	<div class="col-md-12 col-lg-12">
+	<div class="col-md-2">
 	<div class="form-group">
 		<label class="input-label">Sub Topic</label>
 		<select id="chapter_id"
