d78c59733bc4fcc2ae95a411ae103675e72ae086
yasir rafique <yasir.chimp@gmail.com>
2024-11-04 22:41:10 -0800

    Update generatet_form.blade.php

--- resources/views/admin/questions_generator/generatet_form.blade.php
+++ resources/views/admin/questions_generator/generatet_form.blade.php
@@ -431,25 +431,26 @@
 							</div>
 						</div>
 					</div>
-					
-					<label>Options:</label>
-					<div class="options-container" data-options-container="0" >
-						<div class="option-group" data-option-index="0">
-							<input type="text" class="form-control" name="options[]" value="Option 1">
-							<input type="checkbox" name="correct_answers[]" value="0">
-							<div class="option-buttons">
-								<button type="button" class="move-up-btn" onclick="moveOptionUp(this)">↑</button>
-								<button type="button" class="move-down-btn" onclick="moveOptionDown(this)">↓</button>
-								<button type="button" class="remove-option-btn" onclick="removeOption(this)">✖</button>
+					<div class="form-group">
+						<label>Options:</label>
+						<div class="options-container" data-options-container="0" >
+							<div class="option-group" data-option-index="0">
+								<input type="text" class="form-control" name="options[]" value="Option 1">
+								<input type="checkbox" name="correct_answers[]" value="0">
+								<div class="option-buttons">
+									<button type="button" class="move-up-btn" onclick="moveOptionUp(this)">↑</button>
+									<button type="button" class="move-down-btn" onclick="moveOptionDown(this)">↓</button>
+									<button type="button" class="remove-option-btn" onclick="removeOption(this)">✖</button>
+								</div>
 							</div>
-						</div>
-						<div class="option-group" data-option-index="1">
-							<input type="text" class="form-control" name="options[]" value="Option 2">
-							<input type="checkbox" name="correct_answers[]" value="1">
-							<div class="option-buttons">
-								<button type="button" class="move-up-btn" onclick="moveOptionUp(this)">↑</button>
-								<button type="button" class="move-down-btn" onclick="moveOptionDown(this)">↓</button>
-								<button type="button" class="remove-option-btn" onclick="removeOption(this)">✖</button>
+							<div class="option-group" data-option-index="1">
+								<input type="text" class="form-control" name="options[]" value="Option 2">
+								<input type="checkbox" name="correct_answers[]" value="1">
+								<div class="option-buttons">
+									<button type="button" class="move-up-btn" onclick="moveOptionUp(this)">↑</button>
+									<button type="button" class="move-down-btn" onclick="moveOptionDown(this)">↓</button>
+									<button type="button" class="remove-option-btn" onclick="removeOption(this)">✖</button>
+								</div>
 							</div>
 						</div>
 					</div>
