737bae9fb7017822e9409e53bc18a35821d71574
yasir rafique <yasir.chimp@gmail.com>
2024-11-13 22:21:04 -0800

    Update audio_recording_layout.blade.php

--- resources/views/web/default/question_layouts/audio_recording_layout.blade.php
+++ resources/views/web/default/question_layouts/audio_recording_layout.blade.php
@@ -6,7 +6,7 @@
 		<button id="stopRecord" class="btn-icon" disabled="">
 			<i class="fas fa-stop"></i>
 		</button>
-		<span id="timer" class="time-left">Time remaining: <span id="timeLeft">{{$elementObj->content}}</span> seconds</span>
+		<span id="timer" class="time-left ml-10">Time remaining: <span id="timeLeft">{{$elementObj->content}}</span> seconds</span>
 		<audio id="audioPlayer" data-id="98361" type="audio_record" data-field_type="audio_record" controls="" class="audio-control rurera-hide editor-field"></audio>
 	</div>
 </div>
