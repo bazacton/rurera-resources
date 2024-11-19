fb8e5ca1de9f9a4510012cba4ca046eda7cb4fc3
bazacton <baz.chimpstudio@gmail.com>
2024-11-19 14:12:00 +0500

    ai questions builder
    ai questions builder

--- resources/views/admin/questions_generator/generated_questions_form.blade.php
+++ resources/views/admin/questions_generator/generated_questions_form.blade.php
@@ -102,7 +102,7 @@ $rand_id = rand(999,99999);
 	<h2>{{$QuestionsBulkListObj->quizData->getTitleAttribute()}}</h2>
 @else
 <span>{{$QuestionsBulkListObj->category->getTitleAttribute()}} / {{$QuestionsBulkListObj->subject->getTitleAttribute()}} / {{$QuestionsBulkListObj->chapter->getTitleAttribute()}}</span>
-<div class="title-search-field">
+<div class="title-search-field d-flex align-items-center justify-content-between">
 <h2>{{$QuestionsBulkListObj->subChapter->sub_chapter_title}}</h2> <select name="part_item_id" data-bulk_list_id="{{$QuestionsBulkListObj->id}}" class="part_item_selection form-control populate">
 			@php $topic_counter = 1; @endphp
 			@if($topic_parts_items->count() > 0)
