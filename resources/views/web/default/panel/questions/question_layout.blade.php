a8cb4b2ee51b687d561dd9cbd20fc60630c3f881
yasir rafique <yasir.chimp@gmail.com>
2024-11-18 23:11:46 -0800

    Update question_layout.blade.php

--- resources/views/web/default/panel/questions/question_layout.blade.php
+++ resources/views/web/default/panel/questions/question_layout.blade.php
@@ -62,7 +62,7 @@ $total_questions = count(json_decode($quizAttempt->questions_list));
                          class="{{$classes}} rureraform-form rureraform-elements rureraform-form-input-medium rureraform-form-icon-inside rureraform-form-description-bottom ui-sortable"
                          _data-parent="1"
                          _data-parent-col="0" style="display: block;">
-                        <div class="question-layout row d-flex align-items-center">
+                        <div class="question-layout row d-flex align-items-start">
                             @if( isset( $show_marks ) && $show_marks == true)
                                 @if($layout_type != 'results')
                                 <span class="marks" data-marks="{{$question->question_score}}">{{$question->question_score}} marks</span>
