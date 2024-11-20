0331ce8b300972c3527aa3e92c39c7ceb9714ab7
yasir rafique <yasir.chimp@gmail.com>
2024-11-19 21:02:46 -0800

    Update generated_questions_form.blade.php

--- resources/views/admin/questions_generator/generated_questions_form.blade.php
+++ resources/views/admin/questions_generator/generated_questions_form.blade.php
@@ -102,7 +102,7 @@ $rand_id = rand(999,99999);
 	<h2>{{$QuestionsBulkListObj->quizData->getTitleAttribute()}}</h2>
 @else
 <span>{{$QuestionsBulkListObj->category->getTitleAttribute()}} / {{$QuestionsBulkListObj->subject->getTitleAttribute()}} / {{$QuestionsBulkListObj->chapter->getTitleAttribute()}}</span>
-<div class="title-search-field d-flex align-items-center justify-content-between">
+<div class="title-search-field d-flex align-items-center justify-content-between mt-10">
 <h2>{{$QuestionsBulkListObj->subChapter->sub_chapter_title}}</h2> <select data-default_question_id="{{$default_question_id}}" name="part_item_id" data-bulk_list_id="{{$QuestionsBulkListObj->id}}" class="part_item_selection form-control populate w-auto">
 			@php $topic_counter = 1; @endphp
 			@if($topic_parts_items->count() > 0)
