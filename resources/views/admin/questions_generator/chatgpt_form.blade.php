15bea55a355b0f725ee45d8d5cfef4527e3df673
yasir rafique <yasir.chimp@gmail.com>
2024-11-19 23:22:55 -0800

    Update chatgpt_form.blade.php

--- resources/views/admin/questions_generator/chatgpt_form.blade.php
+++ resources/views/admin/questions_generator/chatgpt_form.blade.php
@@ -84,7 +84,7 @@
 						</div>
 					</div>
 					<div class="multi-choice-template-modal modal fade" id="multi-choice-template-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-						<div class="modal-dialog">
+						<div class="modal-dialog modal-xl">
 							<div class="modal-content">
 							<div class="modal-header">
 								<h5 class="modal-title" id="exampleModalLabel">Questions Template</h5>
