f5968c38feb317f83c551b96331e3fa2439dac7d
yasir rafique <yasir.chimp@gmail.com>
2024-11-19 21:41:36 -0800

    Update chatgpt_form.blade.php

--- resources/views/admin/questions_generator/chatgpt_form.blade.php
+++ resources/views/admin/questions_generator/chatgpt_form.blade.php
@@ -8,6 +8,16 @@
 @section('content')
 
 <section class="section">
+	<div class="col-md-12 col-lg-12">
+		<div class="section-header">
+			<h1 class="copyable-text">Add Response</h1>
+			<div class="section-header-breadcrumb">
+				<div class="breadcrumb-item active"><a href="/admin/">{{trans('admin/main.dashboard')}}</a>
+				</div>
+				<div class="breadcrumb-item">Add Response</div>
+			</div>
+		</div>
+	</div>
 	<div class="row">
 		<div class="col-12 col-md-12">
 			<div class="card">
