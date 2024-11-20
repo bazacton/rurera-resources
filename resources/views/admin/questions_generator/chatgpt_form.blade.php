1f7b5742d47352d712cd00cd83a07550dd4b234a
yasir rafique <yasir.chimp@gmail.com>
2024-11-19 23:15:29 -0800

    Update chatgpt_form.blade.php

--- resources/views/admin/questions_generator/chatgpt_form.blade.php
+++ resources/views/admin/questions_generator/chatgpt_form.blade.php
@@ -96,7 +96,7 @@
 								<div class="row">
 								<div class="col-12">
 									<div class="form-group">
-									<input type="text" placeholder="Search Templates" class="form-controle">
+									<input type="text" placeholder="Search Templates" class="form-control">
 									</div>
 								</div>
 								<div class="col-12 col-lg-3 col-md-6">
