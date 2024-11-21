48d5e6d862945ee0a1fc10a37e00389a85510b27
bazacton <baz.chimpstudio@gmail.com>
2024-11-21 17:43:20 +0500

    ai questions builder
    ai questions builder

--- resources/views/admin/questions_generator/generated_questions_form.blade.php
+++ resources/views/admin/questions_generator/generated_questions_form.blade.php
@@ -246,7 +246,7 @@ function decodeHtml(html) {
 	 var is_deleted = $(this).attr('data-is_deleted');
 	 var similiarity_responses = $('.nav').attr('data-similiarity_responses');
 	 console.log(rureraform_decode64(similiarity_responses));
-	 similiarity_responses = JSON.parse(similiarity_responses);
+	 similiarity_responses = JSON.parse(rureraform_decode64(similiarity_responses));
 	 current_similarities_array = [];
 	 if (similiarity_responses.hasOwnProperty(question_index)) {
 		 var current_similarities_array = similiarity_responses[question_index];
