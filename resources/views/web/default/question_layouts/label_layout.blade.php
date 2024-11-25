0b3b9c67ec3a2e73253098182c7da710a6968f67
bazacton <baz.chimpstudio@gmail.com>
2024-11-25 13:41:10 +0500

    headings addd in label questions
    headings addd in label questions

--- resources/views/web/default/question_layouts/label_layout.blade.php
+++ resources/views/web/default/question_layouts/label_layout.blade.php
@@ -1,6 +1,6 @@
 @php $label_type = isset( $elementObj->label_type ) ? $elementObj->label_type : 'question_label'; @endphp
 <div id="rureraform-element-{{$element_id}}" class="rureraform-element-{{$element_id}} rureraform-element quiz-group rureraform-element-html ui-sortable-handle" data-type="{{$elementObj->type}}">
-	@if($label_type == 'h1' || $label_type == 'h2' || $label_type == 'h3')
+	@if($label_type == 'h1' || $label_type == 'h2' || $label_type == 'h3' || $label_type == 'h4' || $label_type == 'h5' || $label_type == 'h6')
 		<{{$label_type}}>{!! $elementObj->content !!}</{{$label_type}}>
 	@else
 		<div class="question-label {{$label_type}}"><span>{!! $elementObj->content !!}</span></div>
