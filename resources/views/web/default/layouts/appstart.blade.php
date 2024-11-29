bc0d0064c8f3e9c1c1ca9cee4965f31278c7bb3f
yasir rafique <yasir.chimp@gmail.com>
2024-11-29 00:16:52 -0800

    Update appstart.blade.php

--- resources/views/web/default/layouts/appstart.blade.php
+++ resources/views/web/default/layouts/appstart.blade.php
@@ -24,11 +24,11 @@ $rand_no = rand(99,9999);
     <link rel="stylesheet" href="/assets/default/vendors/sweetalert2/dist/sweetalert2.min.css">
     <link rel="stylesheet" href="/assets/default/vendors/toast/jquery.toast.min.css">
     <link rel="stylesheet" href="/assets/default/vendors/simplebar/simplebar.css">
-	<link rel="stylesheet" href="/assets/default/css/common.min.css?ver={{$rand_no}}">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="/assets/default/vendors/swiper/swiper-bundle.min.css">
     <link rel="stylesheet" href="/assets/vendors/jquerygrowl/jquery.growl.css">
     <link rel="stylesheet" href="/assets/default/css/responsive.min.css"> -->
+    <link rel="stylesheet" href="/assets/default/css/common.min.css?ver={{$rand_no}}">
 	<link rel="stylesheet" href="/assets/default/css/quiz-create.css?ver={{$rand_no}}">
 
 
