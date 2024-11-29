f34e650a2c9344735f74d0864401573126717b25
yasir rafique <yasir.chimp@gmail.com>
2024-11-29 02:22:48 -0800

    Update generated_questions_form.blade.php

--- resources/views/admin/questions_generator/generated_questions_form.blade.php
+++ resources/views/admin/questions_generator/generated_questions_form.blade.php
@@ -104,7 +104,7 @@ $rand_id = rand(999,99999);
 <span>{{$QuestionsBulkListObj->category->getTitleAttribute()}} / {{$QuestionsBulkListObj->subject->getTitleAttribute()}} / {{$QuestionsBulkListObj->chapter->getTitleAttribute()}}</span>
 <div class="title-search-field d-flex align-items-center justify-content-between mt-10">
     <h1>{{$QuestionsBulkListObj->subChapter->sub_chapter_title}}</h1> 
-    <div class="search-dropdown">
+    <div class="form-group mb-0">
         <label>Search Dropdown</label>
         <select data-default_question_id="{{$default_question_id}}" name="part_item_id" data-bulk_list_id="{{$QuestionsBulkListObj->id}}" class="part_item_selection form-control populate w-auto">
             @php $topic_counter = 1; @endphp
