58aa701dea63243b40c4c2b1287ebbcc07bf682c
yasir rafique <yasir.chimp@gmail.com>
2024-11-29 03:18:04 -0800

    Update assignment_practice_start.blade.php

--- resources/views/web/default/panel/quizzes/assignment_practice_start.blade.php
+++ resources/views/web/default/panel/quizzes/assignment_practice_start.blade.php
@@ -5,23 +5,6 @@ $rand_id = rand(99,9999);
 
 @endphp
 
-
-<style>
-    .ui-state-highlight {
-        margin: 0px 10px;
-    }
-
-    .field-holder.wrong, .form-field.wrong, .form-field.wrong label {
-        background: #ff4a4a;
-        color: #fff;
-    }
-
-    .rurera-hide {
-        display: none;
-    }
-
-</style>
-
 @php $timer_counter = 0;
 if( $duration_type == 'per_question'){
 $timer_counter = $time_interval;
