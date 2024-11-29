c25142b4eaa390347107b3c5b56d994ca086e8bb
yasir rafique <yasir.chimp@gmail.com>
2024-11-29 04:49:10 -0800

    edit select-topic-filters

--- resources/views/admin/questions_generator/generated_questions_form.blade.php
+++ resources/views/admin/questions_generator/generated_questions_form.blade.php
@@ -102,8 +102,15 @@ $rand_id = rand(999,99999);
 	<h2>{{$QuestionsBulkListObj->quizData->getTitleAttribute()}}</h2>
 @else
 <span>{{$QuestionsBulkListObj->category->getTitleAttribute()}} / {{$QuestionsBulkListObj->subject->getTitleAttribute()}} / {{$QuestionsBulkListObj->chapter->getTitleAttribute()}}</span>
-<div class="title-search-field d-flex align-items-center justify-content-between mt-10">
-<h1>{{$QuestionsBulkListObj->subChapter->sub_chapter_title}}</h1>  <div class="form-group mb-0">
+<div class="title-search-field mt-10">
+    <h1>{{$QuestionsBulkListObj->subChapter->sub_chapter_title}}</h1>      
+</div>
+@endif
+
+
+<!-- Edit-questions Tabs Start -->
+<div class="edit-questions-difficulty-tabs">
+    <div class="select-topic-filters">
         <label>Select Topic Part</label>
         <select data-default_question_id="{{$default_question_id}}" name="part_item_id" data-bulk_list_id="{{$QuestionsBulkListObj->id}}" class="part_item_selection form-control populate w-auto">
             @php $topic_counter = 1; @endphp
@@ -117,13 +124,7 @@ $rand_id = rand(999,99999);
                 @endforeach
             @endif
         </select>
-    </div>
-		</div>
-@endif
-
-
-<!-- Edit-questions Tabs Start -->
-<div class="edit-questions-difficulty-tabs">	
+    </div>	
 </div>
 <div class="edit-questions-tabs">	
 </div>
