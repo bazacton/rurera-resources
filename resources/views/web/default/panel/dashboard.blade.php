300537b3be9a58ee9aba33709d6d260f50b407b9
yasir rafique <yasir.chimp@gmail.com>
2024-12-02 02:28:24 -0800

    Update dashboard.blade.php

--- resources/views/web/default/panel/dashboard.blade.php
+++ resources/views/web/default/panel/dashboard.blade.php
@@ -1,8 +1,8 @@
 @extends(getTemplate() .'.panel.layouts.panel_layout')
 
 @push('styles_top')
-<link rel="stylesheet" href="/assets/default/vendors/chartjs/chart.min.css"/>
-<link rel="stylesheet" href="/assets/default/vendors/apexcharts/apexcharts.css"/>
+    <link rel="stylesheet" href="/assets/default/vendors/chartjs/chart.min.css"/>
+    <link rel="stylesheet" href="/assets/default/vendors/apexcharts/apexcharts.css"/>
 @endpush
 
 @section('content')
