39873d94d2816dc6714f35d09d45cef4aa27bab7
yasir rafique <yasir.chimp@gmail.com>
2024-12-01 21:12:46 -0800

    Update question_layout.blade - Copy (2).php

--- resources/views/web/default/panel/questions/question_layout.blade - Copy (2).php
+++ resources/views/web/default/panel/questions/question_layout.blade - Copy (2).php
@@ -11,7 +11,7 @@
                  class="rureraform-form rureraform-elements rureraform-form-input-medium rureraform-form-icon-inside rureraform-form-description-bottom ui-sortable"
                  _data-parent="1"
                  _data-parent-col="0" style="display: block;">
-                <div class="question-layout">
+                <div class="question-layout row">
 
                 </div>
             </div>
