9646a3d1fa6ab779cd9f78b616a724691db32a98
yasir rafique <yasir.chimp@gmail.com>
2024-12-01 22:43:47 -0800

    Update index.blade.php

--- resources/views/web/default/vocabulary/index.blade.php
+++ resources/views/web/default/vocabulary/index.blade.php
@@ -157,7 +157,7 @@
                                                             </a>
                                                         </li>';
                                 @endphp
-
+                                <!-- Spell Levels Listing Start -->
                                 <div class="spell-levels {{$spell_quiz_completed}}">
                                     <div class="spell-levels-top">
                                         <div class="spell-top-left">
@@ -199,6 +199,7 @@
                                         </div>
                                     </div>
                                 </div>
+                                <!-- Spell Levels Listing End -->
                                 @endforeach
 
 
