d175367d97de2ba2244abe53808a205459fec769
yasir rafique <yasir.chimp@gmail.com>
2024-12-02 23:27:03 -0800

    Update index.blade.php

--- resources/views/web/default/panel/setting/index.blade.php
+++ resources/views/web/default/panel/setting/index.blade.php
@@ -32,7 +32,7 @@
                     </div>
                     <div class="col-12">
                         <div class="setup-quiz-card mb-30 bg-white panel-border rounded-sm p-25">
-                            <div class="setup-quiz-header">
+                            <div class="setup-quiz-header d-flex align-items-center justify-content-between">
                                 <h3>Set up your quiz</h3>
                                 <div class="form-group custom-switches-stacked">
                                     <label class="custom-switch pl-0">
