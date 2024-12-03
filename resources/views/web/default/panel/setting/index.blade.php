7a63fc1c3557b04216164babe2c6fa71e19db52c
yasir rafique <yasir.chimp@gmail.com>
2024-12-03 06:25:27 -0800

    Update index.blade.php

--- resources/views/web/default/panel/setting/index.blade.php
+++ resources/views/web/default/panel/setting/index.blade.php
@@ -22,6 +22,206 @@
 @section('content')
     <div class="row">
         <div class="col-12">
+            <div class="elements-holder mb-30 bg-white panel-border rounded-sm p-15">
+                <div class="dashboard-categories">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <div class="categories-card">
+                                <span class="icon-box"><img src="/assets/default/svgs/accuracy.svg" alt=""></span>
+                                <div class="text-holder">
+                                    <span>Accuracy</span>
+                                    <strong>67%</strong>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="categories-card">
+                                <span class="icon-box"><img src="/assets/default/svgs/accuracy.svg" alt=""></span>
+                                <div class="text-holder">
+                                    <span>Completion Rate</span>
+                                    <strong>100%</strong>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="categories-card">
+                                <span class="icon-box"><img src="/assets/default/svgs/accuracy.svg" alt=""></span>
+                                <div class="text-holder">
+                                    <span>Students Assigned</span>
+                                    <strong>2</strong>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="categories-card">
+                                <span class="icon-box"><img src="/assets/default/svgs/accuracy.svg" alt=""></span>
+                                <div class="text-holder">
+                                    <span>Questions</span>
+                                    <strong>8</strong>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="class-insight">
+                    <span class="insight-lable">Class Insight</span>
+                    <ul>
+                        <li><span class="icon-box">&#10003;</span> <em>2</em> completed</li>
+                        <li><span class="icon-box">&#10003;</span> <em>0</em> incompleted</li>
+                        <li><span class="icon-box">&#10003;</span> <em>2</em> unattempted</li>
+                    </ul>
+                </div>
+                <div class="class-controls">
+                    <div class="left-area">
+                        <button type="button">View quiz</button>
+                        <button type="button">Flashcards</button>
+                        <button type="button">Manage Accomodations</button>
+                    </div>
+                    <div class="right-area">
+                        <div class="class-controls-option">
+                            <button type="button"><img src="/assets/default/svgs/delete.svg" alt=""></button>
+                            <button type="button"><img src="/assets/default/svgs/print.svg" alt=""></button>
+                            <button type="button"><img src="/assets/default/svgs/download.svg" alt=""></button>
+                        </div>
+                        <button type="button"><span class="icon-box"><img src="/assets/default/svgs/envelope.svg" alt=""></span>Email all parents</button>
+                        <button type="button"><span class="icon-box"><img src="/assets/default/svgs/share.svg" alt=""></span>Share report</button>
+                    </div>
+                </div>
+            </div>
+            <div class="elements-holder mb-30 bg-white panel-border rounded-sm p-15">
+                <div class="quiz-tabs">
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link active" id="participants-tab" data-toggle="tab" data-target="#participants" type="button" role="tab" aria-controls="participants" aria-selected="true">Participants</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="questions-tab" data-toggle="tab" data-target="#questions" type="button" role="tab" aria-controls="questions" aria-selected="false">Questions</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="accommodations-tab" data-toggle="tab" data-target="#accommodations" type="button" role="tab" aria-controls="accommodations" aria-selected="false">Accommodations</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="overview-tab" data-toggle="tab" data-target="#overview" type="button" role="tab" aria-controls="overview" aria-selected="false">Overview</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="tags-tab" data-toggle="tab" data-target="#tags" type="button" role="tab" aria-controls="tags" aria-selected="false">Tags <span>Beta</span></button>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="quiz-filters">
+                            <div class="select-box">
+                                <span>Showing</span>
+                                <select>
+                                    <option value="Best">Best</option>
+                                    <option value="Best">Bad</option>
+                                </select>
+                                <span>attempt</span>
+                            </div>
+                            <div class="sorting-by-filters">
+                                <span class="sorting-lable">Sort by:</span>
+                                <select>
+                                    <option value="Accuracy">Accuracy</option>
+                                    <option value="Attempt">Attempt</option>
+                                </select>
+                                <button type="button" class="accuracy-btn"><img src="/assets/default/svgs/download.svg" alt=""></button>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade show active" id="participants" role="tabpanel" aria-labelledby="participants-tab">
+                            <div class="participants-holder">
+                                <div class="quiz-status">
+                                    <ul>
+                                        <li class="correct">Correct</li>
+                                        <li class="pending">Yet to b graded</li>
+                                        <li class="incorrect">Incorrect</li>
+                                        <li class="urgent">Urgent</li>
+                                    </ul>
+                                </div>
+                                <div class="participants-table">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th></th>
+                                                <th>Accuracy</th>
+                                                <th>Points</th>
+                                                <th>Score</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <div class="user-info">
+                                                        <span class="img-box">
+                                                            <img src="/avatar/svgA04056688806304609.png" class="rounded-circle" alt="Rehan Qaiser" width="400" height="400" itemprop="image" loading="eager" title="rounded circle">
+                                                        </span>
+                                                        <span class="user-name">
+                                                            Muhammad Rehan
+                                                            <em>2 attempts</em>
+                                                        </span>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="user-attempt-status">
+                                                        <ul>
+                                                            <li class="correct"><span></span></li>
+                                                            <li class="correct"><span></span></li>
+                                                            <li class="correct"><span></span></li>
+                                                            <li class="pending"><span></span></li>
+                                                            <li class="pending"><span></span></li>
+                                                            <li class="incorrect"><span></span></li>
+                                                            <li class="urgent"><span></span></li>
+                                                            <li class="urgent"><span></span></li>
+                                                        </ul>
+                                                        <div class="attempt-numbers">
+                                                            <span class="correct">3</span>
+                                                            <span class="incorrect">1</span>
+                                                            <span class="urgent">2</span>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="progress-holder">
+                                                        <div class="progress-box">
+                                                            <div class="circle_percent" data-percent="35">
+                                                                <div class="circle_inner">
+                                                                    <div class="round_per"></div>
+                                                                </div>
+                                                                <div class="circle_inbox">
+                                                                    <span class="percent_text">35%</span>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <span class="points">5<em>/6</em></span>
+                                                </td>
+                                                <td>
+                                                    <span class="score">Score</span>
+                                                </td>
+                                                <td>
+                                                    <div class="quiz-controls">
+                                                        <button type="button">Evaluate</button>
+                                                        <button type="button"><img src="/assets/default/svgs/refresh.svg" alt=""></button>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="questions" role="tabpanel" aria-labelledby="questions-tab">...</div>
+                        <div class="tab-pane fade" id="accommodations" role="tabpanel" aria-labelledby="accommodations-tab">...</div>
+                        <div class="tab-pane fade" id="overview" role="tabpanel" aria-labelledby="overview-tab">...</div>
+                        <div class="tab-pane fade" id="tags" role="tabpanel" aria-labelledby="tags-tab">...</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
             <div class="quiz-setup-listings">
                 <div class="row">
                     <div class="col-12 col-lg-3 col-md-6">
