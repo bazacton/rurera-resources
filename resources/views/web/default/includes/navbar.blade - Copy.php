1c01ee52cf110afbfeba40184fa96a2b33b24ff5
yasir rafique <yasir.chimp@gmail.com>
2024-12-02 20:32:17 -0800

    Update navbar.blade - Copy.php

--- resources/views/web/default/includes/navbar.blade - Copy.php
+++ resources/views/web/default/includes/navbar.blade - Copy.php
@@ -154,11 +154,11 @@ $navBtnText = $navbarButton->title;
             </div>
 
             <div class="nav-icons-or-start-live navbar-order">
-                <div class="xs-w-100 d-flex align-items-center justify-content-between ">
+                <div class="xs-w-100 d-flex align-items-center justify-content-between">
                     @if(!empty($authUser))
-                    <div class="d-flex">
-                        <div class="border-left mx-5 mx-lg-15"></div>
-                    </div>
+                        <div class="d-flex">
+                            <div class="border-left mx-5 mx-lg-15"></div>
+                        </div>
                     @endif
 
                     @if(!empty($authUser))
