46b7b9c2eeea4893ffdb5ad3bf7868332ca582a9
bazacton <baz.chimpstudio@gmail.com>
2024-12-06 01:55:37 +0500

    Custom Pages Teacher Panel
    Custom Pages Teacher Panel

---
+++ resources/views/teachers/custom_pages/tailer_hub.blade.php
@@ -0,0 +1,25 @@
+@extends('admin.layouts.app')
+
+@push('libraries_top')
+
+@endpush
+
+@section('content')
+<section class="section">
+   
+
+    <div class="section-body">
+        <div class="row">
+            <div class="col-12 col-md-12">
+				Tailer Hub
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
+
+@push('scripts_bottom')
+
+
+@endpush
