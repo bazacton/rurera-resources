ae944e94c1bd922b68791500645038a654fe61c5
yasir rafique <yasir.chimp@gmail.com>
2024-12-06 02:34:52 -0800

    google-class-modal style

--- resources/views/teachers/classes/lists.blade.php
+++ resources/views/teachers/classes/lists.blade.php
@@ -276,7 +276,7 @@
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                                <button type="button" class="btn btn-primary">Create class</button>
+                                <button type="button" class="btn btn-primary">Next</button>
                             </div>
                         </div>
                     </div>
