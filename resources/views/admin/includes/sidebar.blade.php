0d16d7dc3e6668da5c5eadf86f4b92339d23af55
yasir rafique <yasir.chimp@gmail.com>
2024-12-14 21:13:55 -0800

    Update sidebar.blade.php

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -2039,6 +2039,112 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <div class="col-12 col-lg-8 col-md-8">
+                                        <div class="quiz-layout-edit-options">
+                                            <div class="controls-heading">
+                                                <h6>Question 1 -</h6> <button type="button">Add question title</button> 
+                                            </div>
+                                            <div class="edit-options-right">
+                                                <div class="edit-options-controls">
+                                                    <button type="button" class="arrow-up-btn" data-toggle="tooltip" data-placement="top" title="Move up"><img src="/assets/default/svgs/arrow-up-btn.svg" alt=""></button>
+                                                    <button type="button" class="arrow-down-btn" data-toggle="tooltip" data-placement="top" title="Move down"><img src="/assets/default/svgs/arrow-down-btn.svg" alt=""></button>
+
+                                                    <button type="button" class="show-btn" data-toggle="tooltip" data-placement="top" title="Show/Hide question">
+                                                        <img class="show-img" src="/assets/default/svgs/eye-show.svg" alt="">
+                                                        <img class="hide-img" src="/assets/default/svgs/eye-off.svg" alt="">
+                                                    </button>
+
+                                                    <button type="button" class="copy-btn" data-toggle="tooltip" data-placement="top" title="Duplicate question"><img src="/assets/default/svgs/copy-btn.svg" alt=""></button>
+                                                    <button type="button" class="delete-btn" data-toggle="tooltip" data-placement="top" title="Trash"><img src="/assets/default/svgs/trash-bin.svg" alt=""></button>
+                                                    <button type="button" class="crown-btn" data-toggle="tooltip" data-placement="top" title="Crown"><img src="/assets/default/svgs/crown-btn.svg" alt=""></button>
+                                                    <button type="button" class="stars-btn" data-toggle="tooltip" data-placement="top" title="Stars"><img src="/assets/default/svgs/stars-new.svg" alt=""></button>
+                                                    <button type="button" class="add-btn" data-toggle="tooltip" data-placement="top" title="Add question"><img src="/assets/default/svgs/add-btn.svg" alt=""></button>
+                                                    <div class="dropdown">
+                                                        <button class="btn btn-link text-white dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                            <i class="fas fa-ellipsis-v"></i>
+                                                        </button>
+                                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                                                            <a class="dropdown-item" href="#"><i class="fas fa-user"></i> Shorten</a>
+                                                            <a class="dropdown-item" href="#"><i class="fas fa-edit"></i> Rewrite</a>
+                                                            <a class="dropdown-item" href="#"><i class="fas fa-lock"></i> More fun</a>
+                                                            <a class="dropdown-item" href="#"><i class="fa fa-archive"></i> More formal</a>
+                                                            <a class="dropdown-item text-danger" href="#"><i class="fa fa-trash"></i> Sprinkle fairy dust</a>
+                                                            <a class="dropdown-item" href="#"><i class="fa fa-archive"></i> Change voice</a>
+                                                            <a class="dropdown-item" href="#"><i class="fas fa-edit"></i> Fix spelling</a>
+                                                            <a class="dropdown-item" href="#"><i class="fas fa-edit"></i> Custom prompt...</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="question-layout-holder mb-15 bg-white panel-border rounded-sm p-25">
+                                            <div class="question-layout-block">
+                                                <form class="question-fields" action="javascript:;" data-question_id="10180">
+                                                <div class="left-content has-bg">
+                                                    <div id="rureraform-form-1" class=" rureraform-form rureraform-elements rureraform-form-input-medium rureraform-form-icon-inside rureraform-form-description-bottom ui-sortable" _data-parent="1" _data-parent-col="0" style="display: block;">
+                                                    <div class="question-layout row d-flex align-items-start">
+                                                        <div class="rureraform-col rureraform-col-12">
+                                                            <div id="rureraform-element-0" class="rureraform-element-0 rureraform-element quiz-group rureraform-element-html ui-sortable-handle" data-type="question_label">
+                                                                <div class="question-label question_heading">
+                                                                    <span>Read the text, then answer the question.</span>
+                                                                    <div class="quiz-layout-edit-options">
+                                                                        
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="rureraform-col rureraform-col-12">
+                                                        <div id="rureraform-element-0" class="rureraform-element-0 rureraform-element quiz-group rureraform-element-html ui-sortable-handle" data-type="paragraph_quiz"> Each day, a school has a break from 10:15 am to 10:30 am and lunchtime from 12:40 pm to 1:30 pm. <div class="rureraform-element-cover"></div>
+                                                        </div>
+                                                        </div>
+                                                        <div class="rureraform-col rureraform-col-12">
+                                                        <div id="rureraform-element-0" class="rureraform-element-0 rureraform-element quiz-group rureraform-element-html ui-sortable-handle" data-type="question_label">
+                                                            <div class="question-label question_label">
+                                                            <span>What is the total time the school has for breaks and lunchtime in a 5-day week?</span>
+                                                            </div>
+                                                        </div>
+                                                        </div>
+                                                        <div class="rureraform-col rureraform-col-12">
+                                                        <div id="rureraform-element-24192" class="quiz-group rureraform-element-24192 rureraform-element ui-sortable-handle" data-type="checkbox">
+                                                            <div class="rureraform-column-label">
+                                                            <label class="rureraform-label">Mark two answers</label>
+                                                            </div>
+                                                            <div class="rureraform-column-input">
+                                                            <div class="rureraform-input">
+                                                                <div class="form-box  rurera-in-row alphabet-list-style  ">
+                                                                <div class="form-field rureraform-cr-container-medium">
+                                                                    <input class="editor-field rureraform-checkbox-medium" data-min="2" type="checkbox" name="field-24192" id="field-24192-00-2424" value="3 hours 45 minutes">
+                                                                    <label for="field-24192-00-2424"> 3 hours 45 minutes </label>
+                                                                </div>
+                                                                <div class="form-field rureraform-cr-container-medium">
+                                                                    <input class="editor-field rureraform-checkbox-medium" data-min="2" type="checkbox" name="field-24192" id="field-24192-11-2424" value="4 hours 10 minutes">
+                                                                    <label for="field-24192-11-2424"> 4 hours 10 minutes </label>
+                                                                </div>
+                                                                <div class="form-field rureraform-cr-container-medium">
+                                                                    <input class="editor-field rureraform-checkbox-medium" data-min="2" type="checkbox" name="field-24192" id="field-24192-22-2424" value="3 hours 30 minutes">
+                                                                    <label for="field-24192-22-2424"> 3 hours 30 minutes </label>
+                                                                </div>
+                                                                <div class="form-field rureraform-cr-container-medium">
+                                                                    <input class="editor-field rureraform-checkbox-medium" data-min="2" type="checkbox" name="field-24192" id="field-24192-33-2424" value="4 hours 35 minutes">
+                                                                    <label for="field-24192-33-2424"> 4 hours 35 minutes </label>
+                                                                </div>
+                                                                <div class="form-field rureraform-cr-container-medium">
+                                                                    <input class="editor-field rureraform-checkbox-medium" data-min="2" type="checkbox" name="field-24192" id="field-24192-44-2424" value="4 hours">
+                                                                    <label for="field-24192-44-2424"> 4 hours </label>
+                                                                </div>
+                                                                </div>
+                                                            </div>
+                                                            </div>
+                                                            <div class="rureraform-element-cover"></div>
+                                                        </div>
+                                                        </div>
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
