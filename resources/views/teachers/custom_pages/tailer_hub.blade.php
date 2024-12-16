d04db3b9ac6d83c96686c4992d7e1da908777699
yasir rafique <yasir.chimp@gmail.com>
2024-12-15 22:45:27 -0800

    Update tailer_hub.blade.php

--- resources/views/teachers/custom_pages/tailer_hub.blade.php
+++ resources/views/teachers/custom_pages/tailer_hub.blade.php
@@ -247,12 +247,12 @@
                                         <button type="button" class="delete-btn" data-toggle="tooltip" data-placement="top" title="Trash"><img src="/assets/default/svgs/trash-bin.svg" alt=""></button>
                                         <div class="dropdown" data-toggle="tooltip" data-placement="top" title="Ask Ai">
                                             <button class="btn btn-link dropdown-toggle stars-btn" type="button" title="Stars" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                <img src="/assets/default/svgs/stars-new.svg" alt="">
+                                                <img src="/assets/default/svgs/stars-colord.svg" alt="">
                                             </button>
                                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
                                                 <ul>
                                                     <li>
-                                                        <a class="dropdown-item" href="#"><i class="fas fa-user"></i> Add a similar question</a>
+                                                        <a class="dropdown-item" href="#"><img src="/assets/default/svgs/stars-colord.svg" alt=""> Add a similar question</a>
                                                     </li>
                                                     <li>
                                                         <a class="dropdown-item" href="#"><i class="fas fa-edit"></i> Turn into</a>
