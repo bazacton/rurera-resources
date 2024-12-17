03f1c7b9a245f7fcba17ecc558c587e9fdecf946
yasir rafique <yasir.chimp@gmail.com>
2024-12-16 21:50:06 -0800

    Update bulk_lists.blade.php

--- resources/views/admin/gallery_images/bulk_lists.blade.php
+++ resources/views/admin/gallery_images/bulk_lists.blade.php
@@ -575,7 +575,9 @@ ul.crop_sizes {
 								</div>
 							</div>
 							<div class="col-12 col-md-12 col-lg-12 img-contaner w-100 image-crop-area"></div>
-							<div class="col-12 col-md-12 col-lg-12  preview-cropped-img"></div>
+							<div class="col-12 col-md-12 col-lg-12  preview-cropped-img">
+								<div class="preview-cropped-img-inner"></div>
+							</div>
 						</div>
 					</div>
 				</div>
