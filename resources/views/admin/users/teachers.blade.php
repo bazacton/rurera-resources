86dd8ad3011a44dca7ad72f43c73537c91ccb12a
yasir rafique <yasir.chimp@gmail.com>
2024-12-17 02:31:33 -0800

    Update teachers.blade.php

--- resources/views/admin/users/teachers.blade.php
+++ resources/views/admin/users/teachers.blade.php
@@ -199,7 +199,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="createClassModalLabel">Create a New Class</h5>
+                    <h5 class="modal-title" id="createClassModalLabel">Create a New Teacher</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
