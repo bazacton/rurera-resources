ff37d3e33b21e6f567bb1b38427ac2d1facdae8d
yasir rafique <yasir.chimp@gmail.com>
2024-12-19 03:11:27 -0800

    Update scripts.js

--- public/assets/admin/js/scripts.js
+++ public/assets/admin/js/scripts.js
@@ -86,9 +86,9 @@ $(function () {
     }
 
     var sidebar_dropdown = function () {
-        if ($(".main-sidebar").length) {
-            $(".main-sidebar").niceScroll(sidebar_nicescroll_opts);
-            sidebar_nicescroll = $(".main-sidebar").getNiceScroll();
+        if ($(".main-sidebar, .blank-canvas-sidebar, .canvas-editable-options").length) {
+            $(".main-sidebar, .blank-canvas-sidebar, .canvas-editable-options").niceScroll(sidebar_nicescroll_opts);
+            sidebar_nicescroll = $(".main-sidebar, .blank-canvas-sidebar, .canvas-editable-options").getNiceScroll();
 
             $(".main-sidebar .sidebar-menu li a.has-dropdown").off('click').on('click', function () {
                 var me = $(this);
