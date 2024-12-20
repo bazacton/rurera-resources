c184d58639f4fa273561d90d14cbad4c653e5660
yasir rafique <yasir.chimp@gmail.com>
2024-12-19 23:01:03 -0800

    add explanation-box style

--- resources/views/teachers/custom_pages/tailer_hub.blade.php
+++ resources/views/teachers/custom_pages/tailer_hub.blade.php
@@ -2473,7 +2473,7 @@
                                                             <i class="fas fa-plus"></i>
                                                             <label>Add Explanation..</label>
                                                         </a>
-                                                        <div class="explanation-box" id="explanation">
+                                                        <div class="explanation-box mt-15" id="explanation">
                                                             <textarea name="explanation"></textarea>
                                                         </div>
                                                     </div>
