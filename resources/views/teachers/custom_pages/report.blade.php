cf759d7245b641a4af49a02e8db61ed44f158aaf
yasir rafique <yasir.chimp@gmail.com>
2024-12-23 10:32:45 -0800

    Update report.blade.php

--- resources/views/teachers/custom_pages/report.blade.php
+++ resources/views/teachers/custom_pages/report.blade.php
@@ -1301,36 +1301,30 @@
         /*Circle Progress Function End*/
 
         /*Overview Circle Progress Function Start*/
-        $(".overview_percent").each(function () {
+        $(".Overview_percent").each(function() {
             var $this = $(this),
-                $dataV = $this.data("percent"),
-                $dataDeg = $dataV * 3.6, // Convert percent to degrees
-                $round = $this.find(".round_per");
-
-            // Set initial rotation
-            $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
-
-            // Add inner content for percentage display
+            $dataV = $this.data("percent"),
+            $dataDeg = $dataV * 3.6,
+            $round = $this.find(".round_per");
+            $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)"); 
             $this.append('<div class="circle_inbox"><span class="percent_text"></span></div>');
-
-            // Animate the percentage count
-            $this.prop('Counter', 0).animate(
-                { Counter: $dataV },
-                {
-                    duration: 2000,
-                    easing: 'swing',
-                    step: function (now) {
-                        $this.find(".percent_text").text(Math.ceil(now));
-                    },
+            $this.prop('Counter', 0).animate({Counter: $dataV},
+        {
+            duration: 2000, 
+            easing: 'swing', 
+            step: function (now) {
+                    $this.find(".percent_text").text(Math.ceil(now));
                 }
-            );
-
-            // Handle percentage more than 50
-            if ($dataV > 50) {
-                setTimeout(function () {
-                    $this.addClass("percent_more");
-                }, 1000);
-            }
+            });
+            if($dataV >= 51){
+                $round.css("transform", "rotate(" + 360 + "deg)");
+                setTimeout(function(){
+                $this.addClass("percent_more");
+                },1000);
+                setTimeout(function(){
+                $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+                },1000);
+            } 
         });
         /*Overview Circle Progress Function End*/
     });
