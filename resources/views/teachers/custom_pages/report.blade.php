227ac52c5ccbf29ab2a98a9f71868b431cbf5671
yasir rafique <yasir.chimp@gmail.com>
2024-12-23 10:15:36 -0800

    Update report.blade.php

--- resources/views/teachers/custom_pages/report.blade.php
+++ resources/views/teachers/custom_pages/report.blade.php
@@ -1306,7 +1306,7 @@
             $dataV = $this.data("percent"),
             $dataDeg = $dataV * 3.6,
             $round = $this.find(".round_per");
-            $round.css("transform", "rotate(" + parseInt($dataDeg + 280) + "deg)"); 
+            $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)"); 
             $this.append('<div class="circle_inbox"><span class="percent_text"></span></div>');
             $this.prop('Counter', 0).animate({Counter: $dataV},
         {
@@ -1316,15 +1316,15 @@
                     $this.find(".percent_text").text(Math.ceil(now));
                 }
             });
-        if($dataV >= 51){
-            $round.css("transform", "rotate(" + 360 + "deg)");
-            setTimeout(function(){
-            $this.addClass("percent_more");
-            },1000);
-            setTimeout(function(){
-            $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
-            },1000);
-        } 
+            if($dataV >= 51){
+                $round.css("transform", "rotate(" + 360 + "deg)");
+                setTimeout(function(){
+                $this.addClass("percent_more");
+                },1000);
+                setTimeout(function(){
+                $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+                },1000);
+            } 
         });
         /*Overview Circle Progress Function End*/
     });
