df47f45caaf1fb078071c8ff468ffbb3549c3550
yasir rafique <yasir.chimp@gmail.com>
2024-12-23 10:06:01 -0800

    Update report.blade.php

--- resources/views/teachers/custom_pages/report.blade.php
+++ resources/views/teachers/custom_pages/report.blade.php
@@ -18,7 +18,7 @@
                             <div class="col-12 col-lg-5 col-md-6">
                                 <div class="progress-holder">
                                     <div class="progress-box">
-                                        <div class="circle_percent circle-orange" data-percent="164">
+                                        <div class="overview_percent circle-orange" data-percent="164">
                                             <div class="circle_inner">
                                                 <div class="round_per"></div>
                                             </div>
@@ -1299,6 +1299,34 @@
         } 
         });
         /*Circle Progress Function End*/
+
+        /*Overview Circle Progress Function Start*/
+        $(".Overview_percent").each(function() {
+            var $this = $(this),
+            $dataV = $this.data("percent"),
+            $dataDeg = $dataV * 3.6,
+            $round = $this.find(".round_per");
+            $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)"); 
+            $this.append('<div class="circle_inbox"><span class="percent_text"></span></div>');
+            $this.prop('Counter', 0).animate({Counter: $dataV},
+        {
+            duration: 2000, 
+            easing: 'swing', 
+            step: function (now) {
+                    $this.find(".percent_text").text(Math.ceil(now)+"%");
+                }
+            });
+        if($dataV >= 51){
+            $round.css("transform", "rotate(" + 360 + "deg)");
+            setTimeout(function(){
+            $this.addClass("percent_more");
+            },1000);
+            setTimeout(function(){
+            $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+            },1000);
+        } 
+        });
+        /*Overview Circle Progress Function End*/
     });
     </script>
     <script>
