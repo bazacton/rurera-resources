6dd73f8486f642bff5d1d93ba813e7ea9bb3064b
bazacton <baz.chimpstudio@gmail.com>
2024-12-24 17:47:28 +0500

    questions builder add response report
    questions builder add response report

--- resources/views/admin/questions_generator/chatgpt_form.blade.php
+++ resources/views/admin/questions_generator/chatgpt_form.blade.php
@@ -117,6 +117,7 @@
 													@php 
 													$total_pending_questions = $total_unreviewed_questions = 0;
 													$expected_part_questions = getPartQuestions($subTopicObj->difficulty_level);
+													$expected_part_questions = ($expected_part_questions*$subTopicObj->topicPartItemPrompts->count());
 													$total_part_questions = $subTopicObj->topicPartItemQuestions->count();
 													$pending_part_questions = $expected_part_questions-$total_part_questions;
 													$pending_part_questions = ( $pending_part_questions < 0 )? 0 : $pending_part_questions;
