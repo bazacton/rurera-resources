7e3ad05abb93bb9caa7666e472cbadf95f2ee7fb
yasir rafique <yasir.chimp@gmail.com>
2024-12-27 03:41:56 -0800

    Update index.blade.php

--- resources/views/web/default/course/index.blade.php
+++ resources/views/web/default/course/index.blade.php
@@ -489,6 +489,38 @@ $(document).on('click', '.view-change-btn', function (e) {
 	$(".chapter-views").addClass('rurera-hide');
 	$(".chapter-views."+view_chapter).removeClass('rurera-hide');
 });
+
+/*percent circle function start*/
+$(document).ready(function () {
+    $(".stats_percent").each(function() {
+        var $this = $(this),
+            $dataV = $this.data("percent"),
+            $dataDeg = $dataV * 3.6,
+            $round = $this.find(".round_per");
+
+        $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)"); 
+        $this.after('<div class="circle_inbox"><span class="percent_text"></span></div>');
+
+        $this.prop('Counter', 0).animate({ Counter: $dataV }, {
+            duration: 2000, 
+            easing: 'swing', 
+            step: function (now) {
+                $this.next(".circle_inbox").find(".percent_text").text(Math.ceil(now) + "%");
+            }
+        });
+
+        if ($dataV >= 51) {
+            $round.css("transform", "rotate(" + 360 + "deg)");
+            setTimeout(function() {
+                $this.addClass("percent_more");
+            }, 1000);
+            setTimeout(function() {
+                $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+            }, 1000);
+        } 
+    });
+});
+/*percent circle function end*/
 </script>
 
 @endpush
