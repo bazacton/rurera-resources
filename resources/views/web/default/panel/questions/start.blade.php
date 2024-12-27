6906308df6431668cb7cc030682d4aabd50d7c9f
yasir rafique <yasir.chimp@gmail.com>
2024-12-26 21:47:22 -0800

    Update start.blade.php

--- resources/views/web/default/panel/questions/start.blade.php
+++ resources/views/web/default/panel/questions/start.blade.php
@@ -11,11 +11,12 @@ $rand_id = rand(99,9999);
 @endpush
 
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="/assets/default/css/responsive.css">
 
 @section('content')
 
 
-<div class="learning-page type-practice type-sats test">
+<div class="learning-page type-practice type-sats">
 	<section class="lms-quiz-section">
         <div class="container questions-data-block read-quiz-content" data-total_questions="0">
             <div class="justify-content-center">
