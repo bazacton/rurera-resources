3ee8828cc30ab7b8dfcfb0d771a2465363bfc3a2
yasir rafique <yasir.chimp@gmail.com>
2024-12-30 01:40:37 -0800

    Update my_library.blade.php

--- resources/views/teachers/custom_pages/my_library.blade.php
+++ resources/views/teachers/custom_pages/my_library.blade.php
@@ -19,7 +19,7 @@
                             <tr>
                                 <th rowspan="2">First Name</th>
                                 <th rowspan="2">Last Name</th>
-                                <th colspan="4" class="report-title">1: Global Geography</th>
+                                <th colspan="4" class="report-title bg-danger">1: Global Geography</th>
                                 <th colspan="5" class="report-title">2: UK Geographical Issues</th>
                                 <th colspan="4" class="report-title">3: People & Environment</th>
                                 <th rowspan="2">Maximum Target Grade</th>
