83e65dd29ef7765005613b37c5fa898e92a9ed2e
yasir rafique <yasir.chimp@gmail.com>
2024-12-31 04:02:32 -0800

    Update teachers.blade.php

--- resources/views/admin/users/teachers.blade.php
+++ resources/views/admin/users/teachers.blade.php
@@ -90,7 +90,7 @@
         </div>
         <div class="row">
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                <div class="card card-statistic-1 test">
+                <div class="card card-statistic-1">
                     <div class="card-icon bg-primary">
                         <i class="fas fa-users"></i>
                     </div>
