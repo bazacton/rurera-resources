a6bbb85fbe3abf422c37d2c69a0a8256a24a5052
yasir rafique <yasir.chimp@gmail.com>
2025-01-01 10:13:06 -0800

    Update index.blade.php

--- resources/views/web/default/panel/dashboard/index.blade.php
+++ resources/views/web/default/panel/dashboard/index.blade.php
@@ -886,7 +886,40 @@ $('body').on('click', '.set-work-ajax li', function (e) {
 
 });
 </script>
-    @endpush
+<script>
+$(document).ready(function () {
+    $(".circle_percent").each(function() {
+        var $this = $(this),
+            $dataV = $this.data("percent"),
+            $dataDeg = $dataV * 3.6,
+            $round = $this.find(".round_per");
+
+        $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)"); 
+
+        // Append circle_inbox outside of circle_percent
+        $this.after('<div class="circle_inbox"><span class="percent_text"></span></div>');
+
+        $this.prop('Counter', 0).animate({ Counter: $dataV }, {
+            duration: 2000, 
+            easing: 'swing', 
+            step: function (now) {
+                $this.next(".circle_inbox").find(".percent_text").text(Math.ceil(now) + "%");
+            }
+        });
+
+        if ($dataV >= 51) {
+            $round.css("transform", "rotate(" + 360 + "deg)");
+            setTimeout(function() {
+                $this.addClass("percent_more");
+            }, 1000);
+            setTimeout(function() {
+                $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+            }, 1000);
+        } 
+    });
+});
+</script>
+@endpush
 
     @if(!empty($giftModal))
     @push('scripts_bottom2')
