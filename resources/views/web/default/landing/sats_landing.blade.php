307af5c5905769b90532b3758af0dc5447a3efd6
yasir rafique <yasir.chimp@gmail.com>
2025-01-02 23:32:43 -0800

    Update sats_landing.blade.php

--- resources/views/web/default/landing/sats_landing.blade.php
+++ resources/views/web/default/landing/sats_landing.blade.php
@@ -613,14 +613,14 @@
     </section>
     <section class="home-sections home-sections-swiper container find-instructor-section position-relative instructor-img-first">
         <div class="row align-items-center">
-            <div class="col-12 col-lg-6 mt-20 mt-lg-0">
+            <div class="col-12 col-lg-6 mt-20 mt-lg-0" data-aos-once="true" data-aos="fade-right" data-aos-anchor=".find-instructor-section" data-aos-offset="100" data-aos-duration="3000">
                 <div class="position-relative img-holder">
                     <img src="/store/1/default_images/home_sections_banners/quiz-sats.jpg" class="find-instructor-section-hero" width="400" height="460" alt="Track Student Progress" />
                     <img src="/assets/default/img/home/circle-4.png" width="170" height="170" class="find-instructor-section-circle" alt="circle" />
                     <img src="/assets/default/img/home/dot.png" width="70" height="110" class="find-instructor-section-dots" alt="dots" />
                 </div>
             </div>
-            <div class="col-12 col-lg-6">
+            <div class="col-12 col-lg-6" data-aos="fade-left" data-aos-anchor=".find-instructor-section" data-aos-offset="200" data-aos-duration="3000">
                 <div class="">
                     <div class="section-title section-inner-title">
                         <h2 class="font-40 text-dark">Ace SATs with Quizzes & Assessments!
@@ -640,7 +640,7 @@
     </section>
     <section class="home-sections home-sections-swiper container find-instructor-section position-relative">
         <div class="row align-items-center">
-            <div class="col-12 col-lg-6">
+            <div class="col-12 col-lg-6" data-aos-once="true" data-aos="fade-right" data-aos-anchor=".find-instructor-section" data-aos-offset="100" data-aos-duration="3000">
                 <div class="">
                     <div class="section-title section-inner-title">
                         <h2 class="font-40 text-dark">Track Individual Progress with Ease</h2>
@@ -655,7 +655,7 @@
                     </div> -->
                 </div>
             </div>
-            <div class="col-12 col-lg-6 mt-20 mt-lg-0">
+            <div class="col-12 col-lg-6 mt-20 mt-lg-0" data-aos="fade-left" data-aos-anchor=".find-instructor-section" data-aos-offset="200" data-aos-duration="3000">
                 <div class="position-relative img-holder">
                     <img src="/store/1/default_images/home_sections_banners/individual-performance.jpg" width="400" height="460" class="find-instructor-section-hero" alt="Have a Question? Ask it in forum and get answer" />
                     <img src="/assets/default/img/home/circle-4.png" width="170" height="170" class="find-instructor-section-circle" alt="circle" />
@@ -666,14 +666,14 @@
     </section>
     <section class="home-sections home-sections-swiper container find-instructor-section position-relative instructor-img-first">
         <div class="row align-items-center">
-            <div class="col-12 col-lg-6 mt-20 mt-lg-0">
+            <div class="col-12 col-lg-6 mt-20 mt-lg-0" data-aos-once="true" data-aos="fade-right" data-aos-anchor=".find-instructor-section" data-aos-offset="100" data-aos-duration="3000">
                 <div class="position-relative img-holder">
                     <img src="/store/1/default_images/home_sections_banners/activity-tracking.jpg" width="400" height="460" class="find-instructor-section-hero" alt="Have a Question? Ask it in forum and get answer" />
                     <img src="/assets/default/img/home/circle-4.png" width="170" height="170" class="find-instructor-section-circle" alt="circle" />
                     <img src="/assets/default/img/home/dot.png" width="70" height="110" class="find-instructor-section-dots" alt="dots" />
                 </div>
             </div>
-            <div class="col-12 col-lg-6">
+            <div class="col-12 col-lg-6" data-aos="fade-left" data-aos-anchor=".find-instructor-section" data-aos-offset="200" data-aos-duration="3000">
                 <div>
                     <div class="section-title section-inner-title">
                         <h2 class="font-40 text-dark"> Effortlessly Track Practice</h2>
