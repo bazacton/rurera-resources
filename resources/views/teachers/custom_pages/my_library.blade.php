45dcfbfbc5005ac0fb821ad57ec85e8cea4af215
yasir rafique <yasir.chimp@gmail.com>
2025-01-05 22:26:17 -0800

    Update my_library.blade.php

--- resources/views/teachers/custom_pages/my_library.blade.php
+++ resources/views/teachers/custom_pages/my_library.blade.php
@@ -606,11 +606,11 @@
                                         </div>
                                     </div>
                                 </div>
-                                <ul class="list-options question-list-options mb-15">      
-                                    <li><span class="icon-box"><img src="/assets/default/svgs/question-circle.svg" alt=""></span> 8 questions</li>
-                                    <li><span class="icon-box"><img src="/assets/default/svgs/grades.svg" alt=""></span> 7th-8th  Grade</li>
-                                    <li><span class="icon-box"><img src="/assets/default/svgs/book-saved.svg" alt=""></span> Science</li>
-                                </ul>
+                                <div class="class-controls-option border-0 p-0 mr-0 mb-15">
+                                    <button type="button"><img src="/assets/default/svgs/delete.svg" alt=""></button>
+                                    <button type="button"><img src="/assets/default/svgs/print.svg" alt=""></button>
+                                    <button type="button"><img src="/assets/default/svgs/download.svg" alt=""></button>
+                                </div>
                                 <div class="class-controls">
                                     <div class="right-area">
                                         <div class="class-controls-option">
