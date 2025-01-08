9a8a45ec463489dd9c2f7d48cdfab3a153f9e954
Baz <baz.chimpstudio@gmail.com>
2025-01-09 00:07:54 +0500

    Gallery Images
    Gallery Images

--- resources/views/admin/gallery_images/bulk_lists.blade.php
+++ resources/views/admin/gallery_images/bulk_lists.blade.php
@@ -135,6 +135,23 @@ ul.crop_sizes {
     </div>
 
 
+    <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Images Added</h4>
+                    </div>
+                    <div class="card-body">
+                        {{$total_added_images}}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
     <div class="section-body">
 
         <section class="card">
