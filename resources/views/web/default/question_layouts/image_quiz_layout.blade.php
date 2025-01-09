eb8a2b14528f6c9df9c48a2561ac2ecb2ce22f2a
yasir rafique <yasir.chimp@gmail.com>
2025-01-09 02:41:28 -0800

    Update image_quiz_layout.blade.php

--- resources/views/web/default/question_layouts/image_quiz_layout.blade.php
+++ resources/views/web/default/question_layouts/image_quiz_layout.blade.php
@@ -37,6 +37,7 @@
 
 
 	#nav-contact2 img {
+		max-width: 250px;
 		border-radius: 140px 0;
 		background: linear-gradient(0.06deg, #fafafa 0%, #f6f6f6 29.62%, #eaeaea 58.47%, #d7d7d7 86.92%, #cccccc 99.22%);
 	}
