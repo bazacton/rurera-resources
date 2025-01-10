338b5a7d80c2207e2ca78d9216d4876a1cfd328f
yasir rafique <yasir.chimp@gmail.com>
2025-01-10 05:18:56 -0800

    Update basic_information.blade.php

--- resources/views/web/default/panel/setting/setting_includes/basic_information.blade.php
+++ resources/views/web/default/panel/setting/setting_includes/basic_information.blade.php
@@ -340,7 +340,9 @@ if( !empty( $emojisArray ) ){
 <div class="modal fade" id="profile-image-modal" tabindex="-1" role="dialog" aria-labelledby="profile-image-modal">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
             <div class="modal-body">
                     <div id="svgAvatars"></div>
             </div>
