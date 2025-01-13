4c27f82d3f4f324969310c0a4f110c8ce5086f8c
yasir rafique <yasir.chimp@gmail.com>
2025-01-13 00:24:38 -0800

    Update rewards_landing.blade.php

--- resources/views/web/default/landing/rewards_landing.blade.php
+++ resources/views/web/default/landing/rewards_landing.blade.php
@@ -600,7 +600,7 @@
     $packages_only = isset( $packages )? $packages : array();
     $show_details = isset( $show_details )? $show_details : true;
     @endphp
-    <section class="lms-setup-progress-section lms-membership-section mb-0 pt-70 pb-60" data-currency_sign="{{getCurrencySign()}}" style="background-color: #fff;">
+    <section class="lms-setup-progress-section lms-membership-section mb-0 pt-50 pb-60" data-currency_sign="{{getCurrencySign()}}" style="background-color: #fff;">
         <div class="container">
             <div class="row">
                 <div class="col-12 col-lg-12 text-center">
