d9f135413fe492ebfc7482eee473b11f5b64d2e9
yasir rafique <yasir.chimp@gmail.com>
2025-01-14 21:50:30 -0800

    Update testimonials.blade.php

--- resources/views/web/default/landing/testimonials.blade.php
+++ resources/views/web/default/landing/testimonials.blade.php
@@ -273,13 +273,6 @@
     </div>
   </div>
 </section>
-<div class="scroll-btn" style="display:block"
-  ><div class="round">
-    <div id="cta">
-      <span class="arrow primera next"></span
-      ><span class="arrow segunda next"></span>
-    </div></div
-></div>
 
 @endsection
 
