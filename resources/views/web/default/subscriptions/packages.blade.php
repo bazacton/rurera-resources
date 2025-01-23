a460d171792614899351406ba3e13cc4072d75eb
yasir rafique <yasir.chimp@gmail.com>
2025-01-23 03:42:00 -0800

    Update packages.blade.php

--- resources/views/web/default/subscriptions/packages.blade.php
+++ resources/views/web/default/subscriptions/packages.blade.php
@@ -66,7 +66,7 @@ $user_subscribed_for = isset( $user_subscribed_for)? $user_subscribed_for : 1;
         </div>
     </section>
     <section class="social-info-section pb-60 packages-list-modal {{$packages_list_class}}">
-    <div class="container rurera-hide">
+    <div class="container">
         <div class="row">
             <div class="col-12">
                 <div class="social-info-holder">
