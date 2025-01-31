285d153a7688ec9fa08edf88347d06308484a782
yasir rafique <yasir.chimp@gmail.com>
2025-01-30 16:57:52 -0800

    Update lists.blade.php

--- resources/views/admin/assignments/lists.blade.php
+++ resources/views/admin/assignments/lists.blade.php
@@ -251,7 +251,7 @@
                     </div>
 
                     <div class="col-md-3">
-                        <div class="form-group test">
+                        <div class="form-group">
                             <label class="input-label">{{ trans('admin/main.start_date') }}</label>
                             <div class="input-group">
                                 <input type="date" id="fsdate" class="text-center form-control" name="from"
