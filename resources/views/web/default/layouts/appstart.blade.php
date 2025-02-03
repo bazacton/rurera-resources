100ae285cf2e12d333a2ca6de77bac24c69a7e11
yasir rafique <yasir.chimp@gmail.com>
2025-02-03 01:47:44 -0800

    Update appstart.blade.php

--- resources/views/web/default/layouts/appstart.blade.php
+++ resources/views/web/default/layouts/appstart.blade.php
@@ -53,135 +53,133 @@ $rand_no = rand(99,9999);
 </head>
 
 <body class="@if($isRtl) rtl @endif">
+    <div id="app">
+        @yield('content')
+    </div>
+    <!-- Template JS File -->
+    <script src="/assets/default/vendors/feather-icons/dist/feather.min.js"></script>
+    <script src="/assets/default/vendors/moment.min.js"></script>
+    <script src="/assets/default/vendors/sweetalert2/dist/sweetalert2.min.js"></script>
+    <script src="/assets/default/vendors/toast/jquery.toast.min.js"></script>
 
-<div id="app">
-
-    @yield('content')
-</div>
-<!-- Template JS File -->
-<script src="/assets/default/vendors/feather-icons/dist/feather.min.js"></script>
-<script src="/assets/default/vendors/moment.min.js"></script>
-<script src="/assets/default/vendors/sweetalert2/dist/sweetalert2.min.js"></script>
-<script src="/assets/default/vendors/toast/jquery.toast.min.js"></script>
-
-<script type="text/javascript" src="/assets/default/vendors/simplebar/simplebar.min.js"></script>
+    <script type="text/javascript" src="/assets/default/vendors/simplebar/simplebar.min.js"></script>
 
 
-@if(empty($justMobileApp) and checkShowCookieSecurityDialog())
-    @include('web.default.includes.cookie-security')
-@endif
+    @if(empty($justMobileApp) and checkShowCookieSecurityDialog())
+        @include('web.default.includes.cookie-security')
+    @endif
 
 
-<script>
-    var deleteAlertTitle = '{{ trans('public.are_you_sure') }}';
-    var deleteAlertHint = '{{ trans('public.deleteAlertHint') }}';
-    var deleteAlertConfirm = '{{ trans('public.deleteAlertConfirm') }}';
-    var deleteAlertCancel = '{{ trans('public.cancel') }}';
-    var deleteAlertSuccess = '{{ trans('public.success') }}';
-    var deleteAlertFail = '{{ trans('public.fail') }}';
-    var deleteAlertFailHint = '{{ trans('public.deleteAlertFailHint') }}';
-    var deleteAlertSuccessHint = '{{ trans('public.deleteAlertSuccessHint') }}';
-    var forbiddenRequestToastTitleLang = '{{ trans('public.forbidden_request_toast_lang') }}';
-    var forbiddenRequestToastMsgLang = '{{ trans('public.forbidden_request_toast_msg_lang') }}';
-
-
-
-
-</script>
-
-@if(session()->has('toast'))
     <script>
+        var deleteAlertTitle = '{{ trans('public.are_you_sure') }}';
+        var deleteAlertHint = '{{ trans('public.deleteAlertHint') }}';
+        var deleteAlertConfirm = '{{ trans('public.deleteAlertConfirm') }}';
+        var deleteAlertCancel = '{{ trans('public.cancel') }}';
+        var deleteAlertSuccess = '{{ trans('public.success') }}';
+        var deleteAlertFail = '{{ trans('public.fail') }}';
+        var deleteAlertFailHint = '{{ trans('public.deleteAlertFailHint') }}';
+        var deleteAlertSuccessHint = '{{ trans('public.deleteAlertSuccessHint') }}';
+        var forbiddenRequestToastTitleLang = '{{ trans('public.forbidden_request_toast_lang') }}';
+        var forbiddenRequestToastMsgLang = '{{ trans('public.forbidden_request_toast_msg_lang') }}';
+
+
+
+
+    </script>
+
+    @if(session()->has('toast'))
+        <script>
+            (function () {
+                "use strict";
+
+                $.toast({
+                    heading: '{{ session()->get('toast')['title'] ?? '' }}',
+                    text: '{{ session()->get('toast')['msg'] ?? '' }}',
+                    bgColor: '@if(session()->get('toast')['status'] == 'success') #43d477 @else #f63c3c @endif',
+                    textColor: 'white',
+                    hideAfter: 10000,
+                    position: 'bottom-right',
+                    icon: '{{ session()->get('toast')['status'] }}'
+                });
+            })(jQuery)
+        </script>
+    @endif
+
+    @stack('styles_bottom')
+    @stack('scripts_bottom')
+
+    <script>
+        @if(session()->has('registration_package_limited'))
         (function () {
             "use strict";
 
-            $.toast({
-                heading: '{{ session()->get('toast')['title'] ?? '' }}',
-                text: '{{ session()->get('toast')['msg'] ?? '' }}',
-                bgColor: '@if(session()->get('toast')['status'] == 'success') #43d477 @else #f63c3c @endif',
-                textColor: 'white',
-                hideAfter: 10000,
-                position: 'bottom-right',
-                icon: '{{ session()->get('toast')['status'] }}'
-            });
+            handleLimitedAccountModal('{!! session()->get('registration_package_limited') !!}')
         })(jQuery)
-    </script>
-@endif
 
-@stack('styles_bottom')
-@stack('scripts_bottom')
+        {{ session()->forget('registration_package_limited') }}
+        @endif
 
-<script>
-    @if(session()->has('registration_package_limited'))
-    (function () {
-        "use strict";
+        {!! !empty(getCustomCssAndJs('js')) ? getCustomCssAndJs('js') : '' !!}
 
-        handleLimitedAccountModal('{!! session()->get('registration_package_limited') !!}')
-    })(jQuery)
-
-    {{ session()->forget('registration_package_limited') }}
-    @endif
-
-    {!! !empty(getCustomCssAndJs('js')) ? getCustomCssAndJs('js') : '' !!}
-
-    if(jQuery('.lms-data-table table').length > 0){
-        $('.lms-data-table table').DataTable();
-      }
-    if(jQuery('.quiz-pagination .swiper-container').length > 0){
-                  console.log('slides-count');
-                  console.log($(".quiz-pagination ul li").length);
-                const swiper = new Swiper('.quiz-pagination .swiper-container', {
-                  slidesPerView: ($(".quiz-pagination ul li").length > 10)? 10 : $(".quiz-pagination ul li").length,
-                  spaceBetween: 0,
-                  slidesPerGroup: 5,
-                  observer: true,
-                  observeParents: true,
-                  navigation: {
-                      nextEl: ".swiper-button-next",
-                      prevEl: ".swiper-button-prev",
-                  },
-                  breakpoints: {
-                    320: {
-                      slidesPerView: 3,
-                      spaceBetween: 5
+        if(jQuery('.lms-data-table table').length > 0){
+            $('.lms-data-table table').DataTable();
+        }
+        if(jQuery('.quiz-pagination .swiper-container').length > 0){
+                    console.log('slides-count');
+                    console.log($(".quiz-pagination ul li").length);
+                    const swiper = new Swiper('.quiz-pagination .swiper-container', {
+                    slidesPerView: ($(".quiz-pagination ul li").length > 10)? 10 : $(".quiz-pagination ul li").length,
+                    spaceBetween: 0,
+                    slidesPerGroup: 5,
+                    observer: true,
+                    observeParents: true,
+                    navigation: {
+                        nextEl: ".swiper-button-next",
+                        prevEl: ".swiper-button-prev",
                     },
+                    breakpoints: {
+                        320: {
+                        slidesPerView: 3,
+                        spaceBetween: 5
+                        },
 
-                    480: {
-                      slidesPerView: ($(".quiz-pagination ul li").length > 10)? 10 : $(".quiz-pagination ul li").length,
-                      spaceBetween: 5
-                    },
+                        480: {
+                        slidesPerView: ($(".quiz-pagination ul li").length > 10)? 10 : $(".quiz-pagination ul li").length,
+                        spaceBetween: 5
+                        },
 
-                    640: {
-                      slidesPerView: ($(".quiz-pagination ul li").length > 10)? 10 : $(".quiz-pagination ul li").length,
-                      spaceBetween: 5
+                        640: {
+                        slidesPerView: ($(".quiz-pagination ul li").length > 10)? 10 : $(".quiz-pagination ul li").length,
+                        spaceBetween: 5
+                        }
                     }
-                  }
-                });
-                $(document).on('click', '.quiz-pagination ul li, .questions-nav-controls .prev-btn, .questions-nav-controls .next-btn', function (e) {
-                    var question_id = $(this).attr('data-question_id');
-                    var li_index = $('.quiz-pagination ul li[data-question_id="'+question_id+'"]').index();
-                    swiper.slideTo(li_index);
-                });
+                    });
+                    $(document).on('click', '.quiz-pagination ul li, .questions-nav-controls .prev-btn, .questions-nav-controls .next-btn', function (e) {
+                        var question_id = $(this).attr('data-question_id');
+                        var li_index = $('.quiz-pagination ul li[data-question_id="'+question_id+'"]').index();
+                        swiper.slideTo(li_index);
+                    });
 
 
-                $(document).on('click', '.flag-question.notflaged', function (e) {
-                    var question_id = $(this).attr('data-question_id');
-                    var li_index = $('.quiz-pagination ul li[data-question_id="' + question_id + '"]').index();
-                     swiper.slideTo(li_index);
-                });
+                    $(document).on('click', '.flag-question.notflaged', function (e) {
+                        var question_id = $(this).attr('data-question_id');
+                        var li_index = $('.quiz-pagination ul li[data-question_id="' + question_id + '"]').index();
+                        swiper.slideTo(li_index);
+                    });
 
 
-              }
-    
+                }
+        
 
-</script>
-<script type="text/javascript">
-  window._mfq = window._mfq || [];
-  (function() {
-    var mf = document.createElement("script");
-    mf.type = "text/javascript"; mf.defer = true;
-    mf.src = "//cdn.mouseflow.com/projects/b545a93a-901f-443b-9f07-429206fd8fde.js";
-    document.getElementsByTagName("head")[0].appendChild(mf);
-  })();
-</script>
+    </script>
+    <script type="text/javascript">
+    window._mfq = window._mfq || [];
+    (function() {
+        var mf = document.createElement("script");
+        mf.type = "text/javascript"; mf.defer = true;
+        mf.src = "//cdn.mouseflow.com/projects/b545a93a-901f-443b-9f07-429206fd8fde.js";
+        document.getElementsByTagName("head")[0].appendChild(mf);
+     })();
+    </script>
 </body>
 </html>
