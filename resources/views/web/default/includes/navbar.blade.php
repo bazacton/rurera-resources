c8e69d13c6696417b7bd8ac6c0f91b3921399138
yasir rafique <yasir.chimp@gmail.com>
2025-02-03 02:03:34 -0800

    Update navbar.blade.php

--- resources/views/web/default/includes/navbar.blade.php
+++ resources/views/web/default/includes/navbar.blade.php
@@ -28,6 +28,7 @@
             <div class="d-flex align-items-center justify-content-between w-100">
 
                 <a class="navbar-brand navbar-order d-flex align-items-center justify-content-center mr-0 {{ (empty($navBtnUrl) and empty($navBtnText)) ? 'ml-auto' : '' }}" href="{{url('/')}}/" itemprop="url">
+                    
                     @if(!empty($generalSettings['logo']))
                     <img src="{{ $generalSettings['logo'] }}" class="img-cover" alt="Rurera Logo" title="Rurera Logo"
                         width="185" height="38" itemprop="image" loading="eager">
