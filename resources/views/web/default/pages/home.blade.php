a982b1a2f531457ecd08428bf32bc801f76d03fe
yasir rafique <yasir.chimp@gmail.com>
2025-02-02 23:53:19 -0800

    Update home.blade.php

--- resources/views/web/default/pages/home.blade.php
+++ resources/views/web/default/pages/home.blade.php
@@ -13,23 +13,6 @@
     .yellow-filter {filter: brightness(0) saturate(100%) invert(82%) sepia(51%) saturate(5470%) hue-rotate(9deg) brightness(108%) contrast(99%);}
     .orange-filter {filter: brightness(0) saturate(100%) invert(46%) sepia(70%) saturate(3496%) hue-rotate(359deg) brightness(96%) contrast(98%);}
     .blue-light-filter {filter: brightness(0) saturate(100%) invert(43%) sepia(68%) saturate(2203%) hue-rotate(219deg) brightness(104%) contrast(87%);}
-    #change {
-        position: relative;
-        white-space: nowrap;
-        }
-    #change .new {
-        position: absolute;
-        user-select: none;
-        left: 0px;
-        bottom: 0.9em;
-        opacity: 0;
-        }
-
-        #change .old {
-        display: inline-block;
-        position: relative;
-        top: 0px;
-        }
 </style>
 @endpush
 
