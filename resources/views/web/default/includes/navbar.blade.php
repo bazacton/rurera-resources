e7a7d5f4cd6f806611bee2957c38fee3f2733ede
yasir rafique <yasir.chimp@gmail.com>
2025-02-03 01:33:18 -0800

    Update navbar.blade.php

--- resources/views/web/default/includes/navbar.blade.php
+++ resources/views/web/default/includes/navbar.blade.php
@@ -21,6 +21,7 @@ $navbarPages = isset( $navData['navbarPages'] )? $navData['navbarPages'] : array
 $profile_navs = isset( $navData['profile_navs'] )? $navData['profile_navs'] : array();
 @endphp
 @if( !isset( $authUser ) || !$authUser->isUser())
+
 <div id="navbarVacuum"></div>
 <nav id="navbar" class="navbar1 navbar-expand-lg navbar-light top-navbar">
     <div class="{{ (!empty($isPanel) and $isPanel) ? 'container-fluid' : 'container-fluid'}}">
