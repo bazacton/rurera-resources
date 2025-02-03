0cf1c498d2d4ee5ab37bca3ed87471b71621c70a
yasir rafique <yasir.chimp@gmail.com>
2025-02-03 04:50:12 -0800

    Update books_landing.blade.php

--- resources/views/web/default/landing/books_landing.blade.php
+++ resources/views/web/default/landing/books_landing.blade.php
@@ -88,7 +88,7 @@
             <div class="img-holder">
               <figure>
                 <a href="#">
-                  <img src="https:../assets/default/img/book-masonry6.png" alt="default staff image" title="default staff image" width="192" height="157" itemprop="image" loading="eager">
+                  <img src="/assets/default/img/book-masonry6.png" alt="default staff image" title="default staff image" width="192" height="157" itemprop="image" loading="eager">
                 </a>
               </figure>
             </div>
@@ -97,7 +97,7 @@
             <div class="img-holder">
               <figure>
                 <a href="#">
-                  <img src="../assets/default/img/book05.png" alt="default staff image" title="default staff image" width="192" height="193" itemprop="image" loading="eager">
+                  <img src="/assets/default/img/book05.png" alt="default staff image" title="default staff image" width="192" height="193" itemprop="image" loading="eager">
                 </a>
               </figure>
             </div>
