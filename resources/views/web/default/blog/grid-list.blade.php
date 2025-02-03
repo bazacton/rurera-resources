2e7803dc9ee231950494b2743f898235e068fad5
yasir rafique <yasir.chimp@gmail.com>
2025-02-03 04:58:44 -0800

    Update grid-list.blade.php

--- resources/views/web/default/blog/grid-list.blade.php
+++ resources/views/web/default/blog/grid-list.blade.php
@@ -1,7 +1,7 @@
 <div class="rurera-blog blog-medium"  itemscope itemtype="https://schema.org/NewsArticle">
     <div class="blog-grid-detail">
         <span class="badge created-at d-flex align-items-center">
-            <i data-feather="calendar" width="20" height="20" class="mr-5"></i>
+            <i data-feather="calendar" style="height: 20px; width: 20px;" class="mr-5"></i>
             <span  itemprop="datePublished" content="2023-04-05T08:00:00+08:00">{{ dateTimeFormat($post->created_at, 'j M Y') }}</span>
         </span>
 
