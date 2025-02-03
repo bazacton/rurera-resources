d69f618b753970d6d53fb33b614f755cf17a6f8f
yasir rafique <yasir.chimp@gmail.com>
2025-02-03 04:59:53 -0800

    Update show.blade.php

--- resources/views/web/default/blog/show.blade.php
+++ resources/views/web/default/blog/show.blade.php
@@ -44,7 +44,7 @@
                             <div class="rurera-blog blog-medium" itemscope="" itemtype="https://schema.org/NewsArticle">
                                 <div class="blog-grid-detail">
                                     <span class="badge created-at d-flex align-items-center">
-                                    <i data-feather="calendar" width="20" height="20" class="mr-5"></i>
+                                    <i data-feather="calendar" style="height: 20px; width: 20px;" class="mr-5"></i>
                                     <span itemprop="datePublished" content="2023-04-05T08:00:00+08:00">19 Sep 2023</span>
                                     </span>
                                     <h2 class="blog-grid-title mt-10" itemprop="headline">
@@ -60,7 +60,7 @@
                             <div class="rurera-blog blog-medium" itemscope="" itemtype="https://schema.org/NewsArticle">
                                 <div class="blog-grid-detail">
                                     <span class="badge created-at d-flex align-items-center">
-                                    <i data-feather="calendar" width="20" height="20" class="mr-5"></i>
+                                    <i data-feather="calendar" style="height: 20px; width: 20px;" class="mr-5"></i>
                                     <span itemprop="datePublished" content="2023-04-05T08:00:00+08:00">27 Apr 2023</span>
                                     </span>
                                     <h2 class="blog-grid-title mt-10" itemprop="headline">
@@ -76,7 +76,7 @@
                             <div class="rurera-blog blog-medium" itemscope="" itemtype="https://schema.org/NewsArticle">
                                 <div class="blog-grid-detail">
                                     <span class="badge created-at d-flex align-items-center">
-                                    <i data-feather="calendar" width="20" height="20" class="mr-5"></i>
+                                    <i data-feather="calendar" style="height: 20px; width: 20px;" class="mr-5"></i>
                                     <span itemprop="datePublished" content="2023-04-05T08:00:00+08:00">28 Apr 2023</span>
                                     </span>
                                     <h2 class="blog-grid-title mt-10" itemprop="headline">
