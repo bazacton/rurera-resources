4f5653bcbe39ccc30023cf8607054c513b404d94
yasir rafique <yasir.chimp@gmail.com>
2025-02-03 23:49:50 -0800

    Update cat4-preparation.blade.php

--- resources/views/web/default/landing/cat4-preparation.blade.php
+++ resources/views/web/default/landing/cat4-preparation.blade.php
@@ -919,8 +919,7 @@
             </div>
         </div>
     </section>
-    <div class="modal fade lms-choose-membership" id="subscriptionModal" tabindex="-1"
-         aria-labelledby="subscriptionModalLabel" aria-hidden="true">
+    <div class="modal fade lms-choose-membership" id="subscriptionModal">
         <div class="modal-dialog">
             <div class="modal-content">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
