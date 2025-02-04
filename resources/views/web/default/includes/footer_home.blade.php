a420f1a94dd8caf95e6dc59686f493ab3234a599
yasir rafique <yasir.chimp@gmail.com>
2025-02-04 04:48:33 -0800

    Update footer_home.blade.php

--- resources/views/web/default/includes/footer_home.blade.php
+++ resources/views/web/default/includes/footer_home.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer  position-relative user-select-none footer-home">
+<footer class="footer position-relative user-select-none footer-home">
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-4 col-lg-3">
