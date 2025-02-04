d52c3981b2a4f65ba05feea8605d76f9a38056d7
yasir rafique <yasir.chimp@gmail.com>
2025-02-04 02:19:48 -0800

    Update index.blade.php

--- resources/views/web/default/pricing/index.blade.php
+++ resources/views/web/default/pricing/index.blade.php
@@ -396,7 +396,7 @@
                                             <ul class="mt-20 plan-feature">
                                                 <li class="mt-10">15 days of subscription</li>
                                             </ul>
-                                            <button type="submit" id="contact-tab" data-toggle="tab" data-target="#contact" role="tab" aria-controls="contact" aria-selected="false" class="btn btn-primary btn-block mt-30 rounded-pill bg-none">
+                                            <button type="submit" id="contact-tab2" data-toggle="tab" data-target="#contact" role="tab" aria-controls="contact" aria-selected="false" class="btn btn-primary btn-block mt-30 rounded-pill bg-none">
                                                 Purchase
                                             </button>
                                         </div>
@@ -408,7 +408,7 @@
                                             <ul class="mt-20 plan-feature">
                                                 <li class="mt-10">30 days of subscription</li>
                                             </ul>
-                                            <button type="submit" id="contact-tab" data-toggle="tab" data-target="#contact2" role="tab" aria-controls="contact" aria-selected="false" class="btn btn-primary btn-block mt-30 rounded-pill">
+                                            <button type="submit" id="contact-tab3" data-toggle="tab" data-target="#contact2" role="tab" aria-controls="contact" aria-selected="false" class="btn btn-primary btn-block mt-30 rounded-pill">
                                                 Purchase
                                             </button>
                                         </div>
@@ -417,7 +417,7 @@
                                     <div class="col-12 col-lg-12 col-md-12 col-sm-12 text-center bg-dark-green bg-dark-green">
                                         <strong>96% of subscribing parents in rurera Family report significant improvement in their child's reading skills.</strong>
                                         <div class="subscription mt-20">
-                                            <span>Already have a subscription?<a href="." id="contact-tab" data-toggle="tab" data-target="#contact3" type="button" role="tab" aria-controls="contact" aria-selected="false">log in</a></span>
+                                            <span>Already have a subscription?<a href="." id="contact-tab5" data-toggle="tab" data-target="#contact3" type="button" role="tab" aria-controls="contact" aria-selected="false">log in</a></span>
                                         </div>
                                     </div>
                                 </div>
