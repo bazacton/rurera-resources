29cfcb6d6e6bd36089749c6b3b5942592eb2a762
yasir rafique <yasir.chimp@gmail.com>
2025-02-04 04:55:14 -0800

    Update invoice.blade.php

--- resources/views/web/default/panel/store/invoice.blade.php
+++ resources/views/web/default/panel/store/invoice.blade.php
@@ -24,7 +24,6 @@
         <div class="container mt-5">
             <div class="row">
                 <div class="col-12 col-md-10 offset-md-1 col-lg-10 offset-lg-1">
-
                     <div class="card card-primary">
                         <div class="row m-0">
                             <div class="col-12 col-md-12">
