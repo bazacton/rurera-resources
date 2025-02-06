c60600a3c3ad1c52f49ce14a3f2a064bceb25748
yasir rafique <yasir.chimp@gmail.com>
2025-02-05 20:41:16 -0800

    Update navbar.blade.php

--- resources/views/web/default/includes/navbar.blade.php
+++ resources/views/web/default/includes/navbar.blade.php
@@ -209,14 +209,11 @@
                             <a href="#!" class="navbar-user d-flex align-items-center dropdown-toggle" type="button"
                             id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
                             aria-expanded="false">
-                                <img src="{{ $authUser->getAvatar() }}" class="rounded-circle"
-                                    alt="{{ $authUser->get_full_name() }}" width="100%" height="auto" itemprop="image"
-                                    alt="rounded circle" loading="eager" title="rounded circle">
+                                <img src="{{ $authUser->getAvatar() }}" class="rounded-circle" alt="{{ $authUser->get_full_name() }}" width="100%" height="auto" itemprop="image" alt="rounded circle" loading="eager" title="rounded circle">
                             </a>
                             <div class="dropdown-menu user-profile-dropdown" aria-labelledby="dropdownMenuButton">
                                 <div class="dropdown-item user-nav-detail">
-                                    <img src="{{ $authUser->getAvatar() }}" class="rounded-circle" alt="{{ $authUser->get_full_name() }}" width="100%" height="auto" itemprop="image"
-                                    alt="rounded circle" loading="eager" title="rounded circle">
+                                    <img src="{{ $authUser->getAvatar() }}" class="rounded-circle" alt="{{ $authUser->get_full_name() }}" width="100%" height="auto" itemprop="image" alt="rounded circle" loading="eager" title="rounded circle">
                                     <span class="font-16 text-dark-blue user-name">{{ $authUser->get_full_name() }}</span>
                                     <span class="font-16 text-dark-blue user-email">{{ $authUser->email }}</span>
                                     <a href="/panel" class="font-16 text-dark-blue user-manage-btn">Manage Account</a>
@@ -238,8 +235,7 @@
                                 </div>
                                 @endif
                                 <a class="dropdown-item nav-logout" href="/logout">
-                                    <img src="/assets/default/img/icons/sidebar/logout.svg" height="auto" itemprop="image"
-                                        width="25" alt="nav-icon" title="nav-icon" loading="eager">
+                                    <img src="/assets/default/img/icons/sidebar/logout.svg" height="auto" itemprop="image" width="25" alt="nav-icon" title="nav-icon" loading="eager">
                                     <span class="font-16 text-dark-blue">{{ trans('panel.log_out') }}</span>
                                 </a>
                             </div>
