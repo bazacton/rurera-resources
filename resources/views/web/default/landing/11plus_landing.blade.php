f57afd08483f1601b7e67b0cf2f0e260de04ade9
yasir rafique <yasir.chimp@gmail.com>
2025-02-05 22:37:22 -0800

    Update 11plus_landing.blade.php

--- resources/views/web/default/landing/11plus_landing.blade.php
+++ resources/views/web/default/landing/11plus_landing.blade.php
@@ -1,16 +1,12 @@
 @extends(getTemplate().'.layouts.app')
 
 @push('styles_top')
-<style>
-.gallery-sub-header {
-    min-height: 650px;
-}
-</style>
+
 @endpush
 
 @section('content')
 <section class="content-section">
-    <section class="position-relative job-singup-sub-header gallery-sub-header page-sub-header pb-0 pt-80 mb-70">
+    <section class="position-relative job-singup-sub-header gallery-sub-header page-sub-header pb-0 pt-80 mb-70" style="min-height: 650px;">
         <div class="container h-100">
             <div class="row h-100">
                 <div class="col-12 col-md-6 col-lg-6">
