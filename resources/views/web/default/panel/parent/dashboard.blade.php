6d5288a466625cf29ab807bc755115cf455cdc36
yasir rafique <yasir.chimp@gmail.com>
2025-02-10 02:06:38 -0800

    Update dashboard.blade.php

--- resources/views/web/default/panel/parent/dashboard.blade.php
+++ resources/views/web/default/panel/parent/dashboard.blade.php
@@ -769,7 +769,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="panel-header">
-                <div class="modal-logo"><img src="/assets/default/img/sidebar/logo.svg"></div>
+                <div class="modal-logo"><img src="/assets/default/img/sidebar/logo.svg" alt="Rurera Logo" width="150" height="38"></div>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">Back to Dashboard <span aria-hidden="true">×</span></button>
             </div>
             <div class="container">
@@ -793,7 +793,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="panel-header">
-                <div class="modal-logo"><img src="/assets/default/img/sidebar/logo.svg"></div>
+                <div class="modal-logo"><img src="/assets/default/img/sidebar/logo.svg" alt="Rurera Logo" width="150" height="38"></div>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">Back to Dashboard <span aria-hidden="true">×</span></button>
             </div>
             <div class="modal-body">
