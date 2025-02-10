abc9708937b0c730869a5a0b1133985514d7ec4b
Baz <baz.chimpstudio@gmail.com>
2025-02-10 11:30:50 +0500

    learning journey
    learning journey

--- resources/views/admin/learning_journey/create.blade.php
+++ resources/views/admin/learning_journey/create.blade.php
@@ -1153,254 +1153,263 @@
 
 
     function flowChartInitialize(){
-        var is_already_initiated = $(".book-dropzone.active").attr('data-intiated_already');
-        if(is_already_initiated == 'yes'){
-            return false;
-        }
-        $(".book-dropzone.active").attr('data-intiated_already', 'yes');
 
-        $flowchart = $('#flowchartworkspace');
-        $flowchart = $(".book-dropzone.active");
-        var $container = $flowchart.parent();
+        $(".book-dropzone").each(function () {
 
 
+            var is_already_initiated = $(this).attr('data-intiated_already');
+            if(is_already_initiated == 'yes'){
+                return false;
+            }
+            $(this).attr('data-intiated_already', 'yes');
 
-
-
-        // Apply the plugin on a standard, empty div...
-        $flowchart.flowchart({
-            data: defaultFlowchartData,
-            defaultSelectedLinkColor: '#000055',
-            grid: 10,
-            multipleLinksOnInput: true,
-            multipleLinksOnOutput: true
-        });
+            $flowchart = $('#flowchartworkspace');
+            $flowchart = $(this);
+            var $container = $flowchart.parent();
 
 
 
 
-        function getOperatorData($element) {
-            var nbInputs = parseInt($element.data('nb-inputs'), 10);
-            var nbOutputs = parseInt($element.data('nb-outputs'), 10);
-            var data = {
-                properties: {
-                    title: $element.text(),
-                    inputs: {},
-                    outputs: {}
+
+            // Apply the plugin on a standard, empty div...
+            $flowchart.flowchart({
+                data: defaultFlowchartData,
+                defaultSelectedLinkColor: '#000055',
+                grid: 10,
+                multipleLinksOnInput: true,
+                multipleLinksOnOutput: true
+            });
+
+
+
+
+            function getOperatorData($element) {
+                var nbInputs = parseInt($element.data('nb-inputs'), 10);
+                var nbOutputs = parseInt($element.data('nb-outputs'), 10);
+                var data = {
+                    properties: {
+                        title: $element.text(),
+                        inputs: {},
+                        outputs: {}
+                    }
+                };
+
+                var i = 0;
+                for (i = 0; i < nbInputs; i++) {
+                    data.properties.inputs['input_' + i] = {
+                        label: 'Input ' + (i + 1)
+                    };
                 }
-            };
+                for (i = 0; i < nbOutputs; i++) {
+                    data.properties.outputs['output_' + i] = {
+                        label: 'Output ' + (i + 1)
+                    };
+                }
 
-            var i = 0;
-            for (i = 0; i < nbInputs; i++) {
-                data.properties.inputs['input_' + i] = {
-                    label: 'Input ' + (i + 1)
-                };
-            }
-            for (i = 0; i < nbOutputs; i++) {
-                data.properties.outputs['output_' + i] = {
-                    label: 'Output ' + (i + 1)
-                };
+                return data;
             }
 
-            return data;
-        }
+
+
+            //-----------------------------------------
+            //--- operator and link properties
+            //--- start
+            var $operatorProperties = $('#operator_properties');
+            $operatorProperties.hide();
+            var $linkProperties = $('#link_properties');
+            $linkProperties.hide();
+            var $operatorTitle = $('#operator_title');
+            var $linkColor = $('#link_color');
+
+            $flowchart.flowchart({
+                onOperatorSelect: function(operatorId) {
+                    $operatorProperties.show();
+                    $operatorTitle.val($flowchart.flowchart('getOperatorTitle', operatorId));
+                    return true;
+                },
+                onOperatorUnselect: function() {
+                    $operatorProperties.hide();
+                    return true;
+                },
+                onLinkSelect: function(linkId) {
+                    $linkProperties.show();
+                    $linkColor.val($flowchart.flowchart('getLinkMainColor', linkId));
+                    return true;
+                },
+                onLinkUnselect: function() {
+                    $linkProperties.hide();
+                    return true;
+                }
+            });
+
+
+            //reinitialize_items();
+
+            $operatorTitle.keyup(function() {
+                var selectedOperatorId = $flowchart.flowchart('getSelectedOperatorId');
+                if (selectedOperatorId != null) {
+                    $flowchart.flowchart('setOperatorTitle', selectedOperatorId, $operatorTitle.val());
+                }
+            });
+
+            $linkColor.change(function() {
+                var selectedLinkId = $flowchart.flowchart('getSelectedLinkId');
+                if (selectedLinkId != null) {
+                    $flowchart.flowchart('setLinkMainColor', selectedLinkId, $linkColor.val());
+                }
+            });
+            //--- end
+            //--- operator and link properties
+            //-----------------------------------------
+
+            //-----------------------------------------
+            //--- delete operator / link button
+            //--- start
+            $flowchart.parent().siblings('.delete_selected_button').click(function() {
+                $flowchart.flowchart('deleteSelected');
+            });
+            //--- end
+            //--- delete operator / link button
+            //-----------------------------------------
 
 
 
-        //-----------------------------------------
-        //--- operator and link properties
-        //--- start
-        var $operatorProperties = $('#operator_properties');
-        $operatorProperties.hide();
-        var $linkProperties = $('#link_properties');
-        $linkProperties.hide();
-        var $operatorTitle = $('#operator_title');
-        var $linkColor = $('#link_color');
-
-        $flowchart.flowchart({
-            onOperatorSelect: function(operatorId) {
-                $operatorProperties.show();
-                $operatorTitle.val($flowchart.flowchart('getOperatorTitle', operatorId));
-                return true;
-            },
-            onOperatorUnselect: function() {
-                $operatorProperties.hide();
-                return true;
-            },
-            onLinkSelect: function(linkId) {
-                $linkProperties.show();
-                $linkColor.val($flowchart.flowchart('getLinkMainColor', linkId));
-                return true;
-            },
-            onLinkUnselect: function() {
-                $linkProperties.hide();
-                return true;
-            }
-        });
-
-
-        //reinitialize_items();
-
-        $operatorTitle.keyup(function() {
-            var selectedOperatorId = $flowchart.flowchart('getSelectedOperatorId');
-            if (selectedOperatorId != null) {
-                $flowchart.flowchart('setOperatorTitle', selectedOperatorId, $operatorTitle.val());
-            }
-        });
-
-        $linkColor.change(function() {
-            var selectedLinkId = $flowchart.flowchart('getSelectedLinkId');
-            if (selectedLinkId != null) {
-                $flowchart.flowchart('setLinkMainColor', selectedLinkId, $linkColor.val());
-            }
-        });
-        //--- end
-        //--- operator and link properties
-        //-----------------------------------------
-
-        //-----------------------------------------
-        //--- delete operator / link button
-        //--- start
-        $flowchart.parent().siblings('.delete_selected_button').click(function() {
-            $flowchart.flowchart('deleteSelected');
-        });
-        //--- end
-        //--- delete operator / link button
-        //-----------------------------------------
-
-
-
-        //-----------------------------------------
-        //--- create operator button
-        //--- start
-        var operatorI = 0;
-        $flowchart.parent().siblings('.create_operator').click(function() {
-            var operatorId = 'created_operator_' + operatorI;
-            var operatorData = {
-                top: ($flowchart.height() / 2) - 30,
-                left: ($flowchart.width() / 2) - 100 + (operatorI * 10),
-                properties: {
-                    title: 'Operator ' + (operatorI + 3),
-                    inputs: {
-                        input_1: {
-                            label: 'Input 1',
-                        }
-                    },
-                    outputs: {
-                        output_1: {
-                            label: 'Output 1',
+            //-----------------------------------------
+            //--- create operator button
+            //--- start
+            var operatorI = 0;
+            $flowchart.parent().siblings('.create_operator').click(function() {
+                var operatorId = 'created_operator_' + operatorI;
+                var operatorData = {
+                    top: ($flowchart.height() / 2) - 30,
+                    left: ($flowchart.width() / 2) - 100 + (operatorI * 10),
+                    properties: {
+                        title: 'Operator ' + (operatorI + 3),
+                        inputs: {
+                            input_1: {
+                                label: 'Input 1',
+                            }
+                        },
+                        outputs: {
+                            output_1: {
+                                label: 'Output 1',
+                            }
                         }
                     }
-                }
-            };
+                };
 
-            operatorI++;
+                operatorI++;
 
-            $flowchart.flowchart('createOperator', operatorId, operatorData);
+                $flowchart.flowchart('createOperator', operatorId, operatorData);
 
-        });
-        //--- end
-        //--- create operator button
-        //-----------------------------------------
-
+            });
+            //--- end
+            //--- create operator button
+            //-----------------------------------------
 
 
 
-        //-----------------------------------------
-        //--- draggable operators
-        //--- start
-        //var operatorId = 0;
-        var $draggableOperators = $('.draggable_operator');
-        $draggableOperators.draggable({
-            cursor: "move",
-            opacity: 0.7,
 
-            // helper: 'clone',
-            appendTo: 'body',
-            zIndex: 1000,
+            //-----------------------------------------
+            //--- draggable operators
+            //--- start
+            //var operatorId = 0;
+            var $draggableOperators = $('.draggable_operator');
+            $draggableOperators.draggable({
+                cursor: "move",
+                opacity: 0.7,
 
-            helper: function(e) {
-                var $this = $(this);
-                var data = getOperatorData($this);
-                return $flowchart.flowchart('getOperatorElement', data);
-            },
-            stop: function(e, ui) {
-                var $this = $(this);
-                var elOffset = ui.offset;
-                var containerOffset = $container.offset();
-                if (elOffset.left > containerOffset.left &&
-                    elOffset.top > containerOffset.top &&
-                    elOffset.left < containerOffset.left + $container.width() &&
-                    elOffset.top < containerOffset.top + $container.height()) {
+                // helper: 'clone',
+                appendTo: 'body',
+                zIndex: 1000,
 
-                    var flowchartOffset = $flowchart.offset();
-
-                    var relativeLeft = elOffset.left - flowchartOffset.left;
-                    var relativeTop = elOffset.top - flowchartOffset.top;
-
-                    var positionRatio = $flowchart.flowchart('getPositionRatio');
-                    relativeLeft /= positionRatio;
-                    relativeTop /= positionRatio;
-
+                helper: function(e) {
+                    var $this = $(this);
                     var data = getOperatorData($this);
-                    data.left = relativeLeft;
-                    data.top = relativeTop;
+                    return $flowchart.flowchart('getOperatorElement', data);
+                },
+                stop: function(e, ui) {
+                    var $this = $(this);
+                    var elOffset = ui.offset;
+                    var containerOffset = $container.offset();
+                    if (elOffset.left > containerOffset.left &&
+                        elOffset.top > containerOffset.top &&
+                        elOffset.left < containerOffset.left + $container.width() &&
+                        elOffset.top < containerOffset.top + $container.height()) {
 
-                    $flowchart.flowchart('addOperator', data);
+                        var flowchartOffset = $flowchart.offset();
+
+                        var relativeLeft = elOffset.left - flowchartOffset.left;
+                        var relativeTop = elOffset.top - flowchartOffset.top;
+
+                        var positionRatio = $flowchart.flowchart('getPositionRatio');
+                        relativeLeft /= positionRatio;
+                        relativeTop /= positionRatio;
+
+                        var data = getOperatorData($this);
+                        data.left = relativeLeft;
+                        data.top = relativeTop;
+
+                        $flowchart.flowchart('addOperator', data);
+                    }
+                }
+            });
+            //--- end
+            //--- draggable operators
+            //-----------------------------------------
+
+
+            //-----------------------------------------
+            //--- save and load
+            //--- start
+            function Flow2Text() {
+                var data = $flowchart.flowchart('getData');
+                $('#flowchart_data').val(JSON.stringify(data, null, 2));
+            }
+            $('#get_data').click(Flow2Text);
+
+            function Text2Flow() {
+                var data = JSON.parse($('#flowchart_data').val());
+                $flowchart.flowchart('setData', data);
+            }
+            $('#set_data').click(Text2Flow);
+
+            /*global localStorage*/
+            function SaveToLocalStorage() {
+                if (typeof localStorage !== 'object') {
+                    alert('local storage not available');
+                    return;
+                }
+                Flow2Text();
+                localStorage.setItem("stgLocalFlowChart", $('#flowchart_data').val());
+            }
+            $('#save_local').click(SaveToLocalStorage);
+
+            function LoadFromLocalStorage() {
+                if (typeof localStorage !== 'object') {
+                    alert('local storage not available');
+                    return;
+                }
+                var s = localStorage.getItem("stgLocalFlowChart");
+                if (s != null) {
+                    $('#flowchart_data').val(s);
+                    Text2Flow();
+                }
+                else {
+                    alert('local storage empty');
                 }
             }
+            $('#load_local').click(LoadFromLocalStorage);
+            //--- end
+            //--- save and load
+            //-----------------------------------------
+
+            levels_sorting_render();
+
         });
-        //--- end
-        //--- draggable operators
-        //-----------------------------------------
 
 
-        //-----------------------------------------
-        //--- save and load
-        //--- start
-        function Flow2Text() {
-            var data = $flowchart.flowchart('getData');
-            $('#flowchart_data').val(JSON.stringify(data, null, 2));
-        }
-        $('#get_data').click(Flow2Text);
 
-        function Text2Flow() {
-            var data = JSON.parse($('#flowchart_data').val());
-            $flowchart.flowchart('setData', data);
-        }
-        $('#set_data').click(Text2Flow);
-
-        /*global localStorage*/
-        function SaveToLocalStorage() {
-            if (typeof localStorage !== 'object') {
-                alert('local storage not available');
-                return;
-            }
-            Flow2Text();
-            localStorage.setItem("stgLocalFlowChart", $('#flowchart_data').val());
-        }
-        $('#save_local').click(SaveToLocalStorage);
-
-        function LoadFromLocalStorage() {
-            if (typeof localStorage !== 'object') {
-                alert('local storage not available');
-                return;
-            }
-            var s = localStorage.getItem("stgLocalFlowChart");
-            if (s != null) {
-                $('#flowchart_data').val(s);
-                Text2Flow();
-            }
-            else {
-                alert('local storage empty');
-            }
-        }
-        $('#load_local').click(LoadFromLocalStorage);
-        //--- end
-        //--- save and load
-        //-----------------------------------------
-
-        levels_sorting_render();
     }
     $(document).ready(function() {
 
