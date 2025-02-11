242e3c85d12212bd10b69943251592af3dd5ed06
yasir rafique <yasir.chimp@gmail.com>
2025-02-10 23:08:17 -0800

    Update navbar.blade.php

--- resources/views/web/default/includes/navbar.blade.php
+++ resources/views/web/default/includes/navbar.blade.php
@@ -208,11 +208,11 @@
                         <div class="dropdown">
                             <a href="#!" class="navbar-user d-flex align-items-center dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
                             aria-expanded="false">
-                                <img src="{{ $authUser->getAvatar() }}" class="rounded-circle" alt="{{ $authUser->get_full_name() }}" width="100%" height="auto" itemprop="image" alt="rounded circle" loading="eager" title="rounded circle">
+                                <img src="{{ $authUser->getAvatar() }}" class="rounded-circle" alt="{{ $authUser->get_full_name() }}" width="400" height="400" itemprop="image" alt="rounded circle" loading="eager" title="rounded circle">
                             </a>
                             <div class="dropdown-menu user-profile-dropdown" aria-labelledby="dropdownMenuButton">
                                 <div class="dropdown-item user-nav-detail">
-                                    <img src="{{ $authUser->getAvatar() }}" class="rounded-circle" alt="{{ $authUser->get_full_name() }}" width="100%" height="auto" itemprop="image" alt="rounded circle" loading="eager" title="rounded circle">
+                                    <img src="{{ $authUser->getAvatar() }}" class="rounded-circle" alt="{{ $authUser->get_full_name() }}" width="400" height="400" itemprop="image" alt="rounded circle" loading="eager" title="rounded circle">
                                     <span class="font-16 text-dark-blue user-name">{{ $authUser->get_full_name() }}</span>
                                     <span class="font-16 text-dark-blue user-email">{{ $authUser->email }}</span>
                                     <a href="/panel" class="font-16 text-dark-blue user-manage-btn">Manage Account</a>
@@ -224,7 +224,7 @@
                                 <div class="user-nav-list">
                                 @foreach( $profile_navs as $profile_nav)
                                 <a class="dropdown-item " href="/panel/switch_user/{{$profile_nav['id']}}">
-                                    <img src="{{ $profile_nav->getAvatar() }}" class="rounded-circle" alt="{{ $profile_nav['full_name'] }}" width="100%" height="auto" itemprop="image"
+                                    <img src="{{ $profile_nav->getAvatar() }}" class="rounded-circle" alt="{{ $profile_nav['full_name'] }}" width="400" height="400" itemprop="image"
                                     alt="rounded circle" loading="eager" title="rounded circle">
                                     @php $full_name = (isset( $navData['is_parent'] ) && $navData['is_parent'] == true)? 'Parent' :  $profile_nav['full_name']; @endphp
                                     <span class="font-16 text-dark-blue user-list-name">{{ $full_name }}</span>
@@ -234,7 +234,7 @@
                                 </div>
                                 @endif
                                 <a class="dropdown-item nav-logout" href="/logout">
-                                    <img src="/assets/default/img/icons/sidebar/logout.svg" height="auto" itemprop="image" width="25" alt="nav-icon" title="nav-icon" loading="eager">
+                                    <img src="/assets/default/img/icons/sidebar/logout.svg" height="24" itemprop="image" width="24" alt="nav-icon" title="nav-icon" loading="eager">
                                     <span class="font-16 text-dark-blue">{{ trans('panel.log_out') }}</span>
                                 </a>
                             </div>
