548f20c326f7b20565fcb3624f65d8f09f71716a
yasir rafique <yasir.chimp@gmail.com>
2025-02-11 02:30:12 -0800

    Update books.blade.php

--- resources/views/web/default/pages/books.blade.php
+++ resources/views/web/default/pages/books.blade.php
@@ -73,7 +73,7 @@
                                                 <div class="btn-holder">
                                                     <a href="/books/{{$bookData->book_slug}}" class="read-btn {{ subscriptionCheckLink('bookshelf') }}" itemprop="url">
                                                         <span class="btn-icon">
-                                                            <img src="/assets/default/svgs/open-book.svg" alt="open-book">
+                                                            <img src="/assets/default/svgs/book-open.svg" alt="book open">
                                                         </span>
                                                         Read the eBook
                                                     </a>
