c0aef353ab663e57df328e607de8ed2410893ac2
yasir rafique <yasir.chimp@gmail.com>
2025-02-12 04:51:57 -0800

    Update quizpage.blade.php

--- resources/views/web/default/pages/quizpage.blade.php
+++ resources/views/web/default/pages/quizpage.blade.php
@@ -161,15 +161,7 @@
                             </svg>
                         </a>
                         <a href="#" class="prev-btn">
-                            <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="512.000000pt"
-                                 height="512.000000pt" viewBox="0 0 512.000000 512.000000"
-                                 preserveAspectRatio="xMidYMid meet">
-                                <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="#000000"
-                                   stroke="none">
-                                    <path
-                                        d="M3620 5103 c-39 -13 -198 -168 -1238 -1207 -1095 -1093 -1194 -1195 -1212 -1244 -25 -67 -25 -117 0 -184 18 -49 117 -151 1212 -1244 1141 -1140 1195 -1193 1247 -1209 214 -69 408 147 315 352 -11 25 -377 398 -1093 1115 l-1076 1078 1076 1077 c701 703 1082 1091 1093 1115 61 135 -4 297 -140 348 -64 23 -121 24 -184 3z"></path>
-                                </g>
-                            </svg>
+                            <img src="/assets/default/svgs/next-btn.svg" width="683" height="683" alt="next-btn">
                         </a>
                         <a href="#" class="next-btn">
                             Next
