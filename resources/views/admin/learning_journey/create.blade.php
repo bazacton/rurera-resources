ea582d73eae1243536574fd651963f44809e6999
Baz <baz.chimpstudio@gmail.com>
2025-02-13 01:56:59 +0500

    learning journey
    learning journey

--- resources/views/admin/learning_journey/create.blade.php
+++ resources/views/admin/learning_journey/create.blade.php
@@ -1094,6 +1094,59 @@
         $(".journey-settings-modal").modal('hide');
     });
 
+
+
+        $(document).on('click', '.add-level-stage-treasure-btn', function () {
+            var level_type = 'treasure_mission';
+            var treasure_mission_points = $('.treasure_selection').val();
+
+            if(treasure_mission_points  == 'custom'){
+                treasure_mission_points = $('.custom_treasure_field_data').val();
+            }
+            var unique_id = Math.floor((Math.random() * 99999) + 1);
+            var field_random_number = 'rand_' + unique_id;
+            var layer_html = '';
+            $el = $('<div></div>');
+
+            $el.append($('<div data-no_of_coins="'+treasure_mission_points+'" id="' + field_random_number + '" style="width:20%;left:0%; top:%;" data-item_title="Treasure" data-unique_id="' + unique_id + '" data-is_new="yes" class="path-initializer flowchart-operator flowchart-default-operator drop-item form-group draggablecl field_settings draggable_field_' + field_random_number + '" data-id="' + field_random_number + '" data-item_path="default/treasure_1.svg" data-field_type="treasure" data-trigger_class="infobox-treasure_1-fields" data-item_type="treasure" data-paragraph_value="Test text here..."><div class="field-data"><svg width="100%" height="100%" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--twemoji" preserveAspectRatio="xMidYMid meet"><path fill="#FFAC33" d="M27.287 34.627c-.404 0-.806-.124-1.152-.371L18 28.422l-8.135 5.834a1.97 1.97 0 0 1-2.312-.008a1.971 1.971 0 0 1-.721-2.194l3.034-9.792l-8.062-5.681a1.98 1.98 0 0 1-.708-2.203a1.978 1.978 0 0 1 1.866-1.363L12.947 13l3.179-9.549a1.976 1.976 0 0 1 3.749 0L23 13l10.036.015a1.975 1.975 0 0 1 1.159 3.566l-8.062 5.681l3.034 9.792a1.97 1.97 0 0 1-.72 2.194a1.957 1.957 0 0 1-1.16.379z"></path></svg><div class="flowchart-operator-inputs-outputs"><div class="flowchart-operator-inputs"></div><div class="flowchart-operator-outputs"></div></div>'));
+            $el.append('</div>');
+            layer_html += `<li data-id="${field_random_number}" data-field_postition="2"><label contenteditable="true">Treasure</label>
+                <div class="actions-menu">
+                    <i class="fa fa-trash"></i><i class="lock-layer fa fa-unlock"></i><i class="fa fa-sort ui-sortable-handle"></i>
+                </div>
+            </li>`;
+
+
+            $(".book-dropzone.active").closest('.editor-zone').find(".editor-objects-list-all").append(layer_html);
+            //$(".book-dropzone.active").closest('.editor-zone').find(".levels-objects-list").append(layer_html);
+            $(".book-dropzone.active").closest('.editor-zone').find(".levels-objects-list").find(".stage_end").last().before(layer_html).length || $(".levels-objects-list").append(layer_html);
+
+
+            $(".book-dropzone.active").append($el);
+            $(".level_add_modal").modal('hide');
+
+
+            $(".editor-objects-list-all").sortable({
+                update: function(event, ui) {
+                    var $list = $(this);
+                    var $stageEnd = $list.find(".stage_end").detach(); // Remove and store the .stage_end element
+                    $list.append($stageEnd);
+
+                    var $stageStart = $list.find(".stage_start").detach(); // Remove and store the .stage_end element
+                    $list.prepend($stageStart);
+                    sorting_render(); // Call your function here
+                }
+            });
+
+            $('.draggable_field_' + field_random_number)
+                .draggable({
+                    preventCollision: true,
+                    containment: $('.book-dropzone.active')
+                })
+                .off('wheel');
+            levels_sorting_render();
+        });
+
         $(document).on('click', '.add-level-stage-topic-btn', function () {
             var level_type = 'topic';
             var unique_id = Math.floor((Math.random() * 99999) + 1);
@@ -1142,6 +1195,57 @@
                 })
                 .off('wheel');
             levels_sorting_render();
+    });
+
+    $(document).on('click', '.add-level-stage-custom-btn', function () {
+        var level_type = 'custom_topic';
+        var unique_id = Math.floor((Math.random() * 99999) + 1);
+        var field_random_number = 'rand_' + unique_id;
+        var layer_html = '';
+        $el = $('<div></div>');
+        var topic_part_item_id = $(this).attr('data-id');
+        var topic_title = $(this).attr('data-title');
+
+        var unique_id = Math.floor((Math.random() * 99999) + 1);
+        var field_random_number = 'rand_' + unique_id;
+        // Perform an action with each topic_part_item_id
+        $el.append($('<div id="' + field_random_number + '" data-topic_part_item_id="'+topic_part_item_id+'" style="width:20%;left:0%; top:0%;" data-item_title="'+topic_title+'" data-unique_id="' + unique_id + '" data-is_new="yes" class="path-initializer flowchart-operator flowchart-default-operator drop-item form-group draggablecl field_settings draggable_field_' + field_random_number + '" data-id="' + field_random_number + '" data-item_path="default/topic_numbers.svg" data-field_type="topic" data-trigger_class="infobox-topic_numbers-fields" data-item_type="topic_numbers" data-paragraph_value="Test text here..."><div class="field-data"><svg width="100%" height="100%" viewBox="0 0 258 264" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="257.641" width="263.774" height="257.64" rx="49.0743" transform="rotate(90 257.641 0)" fill="#8F5C57" fill-opacity="0.79"></rect></svg><div class="flowchart-operator-inputs-outputs"><div class="flowchart-operator-inputs"></div><div class="flowchart-operator-outputs"></div></div>'));
+        $el.append('</div>');
+        layer_html += `<li data-id="${field_random_number}" data-field_postition="2">${topic_title}
+               <div class="actions-menu">
+                    <i class="fa fa-trash"></i><i class="lock-layer fa fa-unlock"></i><i class="fa fa-sort ui-sortable-handle"></i>
+                </div>
+            </li>`;
+
+
+        $(".book-dropzone.active").closest('.editor-zone').find(".editor-objects-list-all").append(layer_html);
+        //$(".book-dropzone.active").closest('.editor-zone').find(".levels-objects-list").append(layer_html);
+        $(".book-dropzone.active").closest('.editor-zone').find(".levels-objects-list").find(".stage_end").last().before(layer_html).length || $(".levels-objects-list").append(layer_html);
+
+
+        $(".book-dropzone.active").append($el);
+        $(".level_add_modal").modal('hide');
+
+
+        $(".editor-objects-list-all").sortable({
+            update: function(event, ui) {
+                var $list = $(this);
+                var $stageEnd = $list.find(".stage_end").detach(); // Remove and store the .stage_end element
+                $list.append($stageEnd);
+
+                var $stageStart = $list.find(".stage_start").detach(); // Remove and store the .stage_end element
+                $list.prepend($stageStart);
+                sorting_render(); // Call your function here
+            }
+        });
+
+        $('.draggable_field_' + field_random_number)
+            .draggable({
+                preventCollision: true,
+                containment: $('.book-dropzone.active')
+            })
+            .off('wheel');
+        levels_sorting_render();
     });
 
 
--- resources/views/admin/learning_journey/includes/topic_treasure_selection.blade.php
+++ resources/views/admin/learning_journey/includes/topic_treasure_selection.blade.php
@@ -2,16 +2,16 @@
         <div class="mb-30 bg-white panel-border rounded-sm p-15">
             <div class="rureraform-search-field mb-15">
                 <div class="input-field">
-                    <input type="text" placeholder="Search question..">
-                    <button type="button"><i class="fas fa-search"></i> <span>Search questions</span></button>
+                    <input type="text" class="conditional-block topics_block" placeholder="Search question..">
+                    <button type="button" class="conditional-block topics_block"><i class="fas fa-search"></i> <span>Search questions</span></button>
                 </div>
-                <div class="featured-controls">
-                    <button type="button" class="active">Featured List</button>
-                    <button type="button">Community</button>
-                    <button type="button">My Collection</button>
+                <div class="add-object-type-list featured-controls">
+                    <button type="button" data-type="topics" class="active">Topics</button>
+                    <button type="button" data-type="custom_topics">Custom Topics</button>
+                    <button type="button" data-type="treasures">Treasures</button>
                 </div>
             </div>
-            <div class="search-filters mb-0">
+            <div class="search-filters mb-0 conditional-block topics_block">
                 <div class="select-field">
                     <span>By:</span>
                     <select>
@@ -38,7 +38,7 @@
                 </div>
             </div>
         </div>
-        <div class="row">
+        <div class="row conditional-block topics_block">
             <div class="col-12 col-lg-4 col-md-4">
                 <div class="featured-list-sidebar">
                     <div class="featured-filters">
@@ -58,7 +58,7 @@
                             </select>
                         </div>
                     </div>
-                    <div class="featured-list-sidebar-inner">
+                    <div class="featured-list-sidebar-inner sub-chapters-list-data">
                         @if($sub_chapters->count() > 0)
                             @foreach($sub_chapters as $subChapterObj)
                                 @if($subChapterObj->TopicPartsItems->count() > 0)
@@ -97,10 +97,155 @@
             </div>
             <div class="col-12 col-lg-8 col-md-8 topic-part-item-data"></div>
         </div>
+        <div class="row conditional-block rurera-hide custom_topics_block">
+            <div class="col-12 col-lg-4 col-md-4">
+                <div class="featured-list-sidebar">
+                    <div class="featured-filters">
+                        <div class="text-box">
+                            <h6>Custom Topics</h6>
+                        </div>
+                        <div class="select-field">
+                            <select class="custom-sub-chapters-list">
+                                @if($sub_chapters->count() > 0)
+                                    @php $counter_i = 1; @endphp
+                                    @foreach($sub_chapters as $subChapterObj)
+                                        @php $selected = ($counter_i == 1)? 'selected' : ''; @endphp
+                                        <option value="{{$subChapterObj->id}}" {{$selected}}>{{isset($subChapterObj->sub_chapter_title )? $subChapterObj->sub_chapter_title : ''}}</option>
+                                        @php $counter_i++; @endphp
+                                    @endforeach
+                                @endif
+                            </select>
+                        </div>
+                    </div>
+                    <div class="featured-list-sidebar-inner custom-sub-chapters-list-data">
+                        @if($custom_quizzes->count() > 0)
+                            @foreach($custom_quizzes as $quizObj)
+                                <div class="listing-card mb-15 bg-white panel-border rounded-sm custom-topic-part-item-list" data-id="{{$quizObj->id}}">
+                                    <a href="javascript:;" class="listing-card-link">
+                                        <div class="img-holder">
+                                            <figure>
+                                                <img src="/assets/default/img/games/go-jetters-hero-academy-cc-v2.jpg" height="270" width="480" alt="">
+                                            </figure>
+                                        </div>
+                                        <div class="text-holder">
+                                            <h3>{{$quizObj->getTitleAttribute()}}</h3>
+                                            <div class="author-info">
+                                                <span class="info-text">
+                                                    <span>{{$quizObj->creator->get_full_name()}}</span>
+                                                    <span>2 hours ago</span>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <ul class="list-options">
+
+                                            <li><span class="icon-box"><img src="/assets/default/svgs/question-circle.svg" alt=""></span> 0 questions</li>
+                                            <li><span class="icon-box"><img src="/assets/default/svgs/grades.svg" alt=""></span> {{$quizObj->quizYear->getTitleAttribute()}}</li>
+                                            <li><span class="icon-box"><img src="/assets/default/svgs/book-saved.svg" alt=""></span> {{$quizObj->subject_id}}</li>
+                                        </ul>
+                                    </a>
+                                </div>
+                            @endforeach
+                        @endif
+                        <button class="load-more-btn"><i class="fas fa-plus"></i> Load More</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-8 col-md-8 custom-topic-part-item-data"></div>
+        </div>
+        <div class="row conditional-block rurera-hide treasures_block">
+            <div class="col-12 col-lg-12 col-md-12 years-group populated-data">
+
+                <div class="form-group">
+                    <div class="input-group">
+                        <div class="radio-buttons">
+                            <label class="card-radio">
+                                <input type="radio" name="treasure_count" class="treasure_selection" value="500" checked>
+                                <span class="radio-btn"><i class="las la-check"></i>
+                                        <div class="card-icon">
+                                            <img src="/assets/default/img/treasure.png">
+                                            <h3>500</h3>
+                                       </div>
+                                  </span>
+                            </label>
+                            <label class="card-radio">
+                                <input type="radio" name="treasure_count" class="treasure_selection" value="400">
+                                <span class="radio-btn"><i class="las la-check"></i>
+                                        <div class="card-icon">
+                                            <img src="/assets/default/img/treasure.png">
+                                            <h3>400</h3>
+                                       </div>
+                                  </span>
+                            </label>
+                            <label class="card-radio">
+                                <input type="radio" name="treasure_count" class="treasure_selection" value="300">
+                                <span class="radio-btn"><i class="las la-check"></i>
+                                        <div class="card-icon">
+                                            <img src="/assets/default/img/treasure.png">
+                                            <h3>300</h3>
+                                       </div>
+                                  </span>
+                            </label>
+                            <label class="card-radio">
+                                <input type="radio" name="treasure_count" class="treasure_selection" value="200">
+                                <span class="radio-btn"><i class="las la-check"></i>
+                                        <div class="card-icon">
+                                            <img src="/assets/default/img/treasure.png">
+                                            <h3>200</h3>
+                                       </div>
+                                  </span>
+                            </label>
+                            <label class="card-radio">
+                                <input type="radio" name="treasure_count" class="treasure_selection" value="100">
+                                <span class="radio-btn"><i class="las la-check"></i>
+                                        <div class="card-icon">
+                                            <img src="/assets/default/img/treasure.png">
+                                            <h3>100</h3>
+                                       </div>
+                                  </span>
+                            </label>
+
+                            <label class="card-radio">
+                                <input type="radio" name="treasure_count" class="treasure_selection" value="custom">
+                                <span class="radio-btn"><i class="las la-check"></i>
+                                        <div class="card-icon">
+                                            <img src="/assets/default/img/treasure.png">
+                                            <h3>Custom</h3>
+                                       </div>
+                                  </span>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="input-group custom_treasure_field rurera-hide">
+                        <input type="text" name="custom_treasure" class="form-control custom_treasure_field_data" value="0">
+                    </div>
+                </div>
+                <button type="button" class="add-level-stage-treasure-btn">Add Treasure</button>
+            </div>
+            <div class="col-12 col-lg-8 col-md-8 topic-part-item-data"></div>
+        </div>
     </div>
 
     <script>
+
+
+
+        $(document).on('change', '.treasure_selection', function () {
+            var current_value = $(this).val();
+            $(".custom_treasure_field").addClass('rurera-hide');
+            if(current_value == 'custom'){
+                $(".custom_treasure_field").removeClass('rurera-hide');
+            }
+        })
+        $(document).on('click', '.add-object-type-list button', function () {
+
+        $(".add-object-type-list button").removeClass('active');
+            $(this).addClass('active');
+            var selected_type = $(this).attr('data-type');
+            $(".conditional-block").addClass('rurera-hide');
+            $("."+selected_type+'_block').removeClass('rurera-hide');
+        });
         var ItemloaderDivMain = $(".topic-part-item-data");
+        var CustomItemloaderDivMain = $(".custom-topic-part-item-data");
 
 
         var topicPartItemDataRequest = null;
@@ -128,8 +273,32 @@
             });
         });
 
+        $(document).on('click', '.custom-topic-part-item-list', function () {
+            $(".topic-part-item-list").removeClass('active');
+            $(this).addClass('active');
+            rurera_loader(CustomItemloaderDivMain, 'div');
+            var quiz_id = $(this).attr('data-id');
+            topicPartItemDataRequest = $.ajax({
+                type: "GET",
+                url: '/admin/learning_journey/get_custom_topic_part_item_layout',
+                beforeSend: function () {
+                    if (topicPartItemDataRequest != null) {
+                        topicPartItemDataRequest.abort();
+                    }
+                },
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                data: {'quiz_id': quiz_id},
+                success: function (response) {
+                    rurera_remove_loader(CustomItemloaderDivMain, 'button');
+                    $(".custom-topic-part-item-data").html(response);
+                }
+            });
+        });
+
         var subChapterDataRequest = null;
-        var subChapterDivMain = $(".featured-list-sidebar-inner");
+        var subChapterDivMain = $(".sub-chapters-list-data");
         $(document).on('change', '.sub-chapters-list', function () {
             rurera_loader(subChapterDivMain, 'div');
             var sub_chapter_id = $(this).val();
@@ -147,7 +316,30 @@
                 data: {'sub_chapter_id': sub_chapter_id},
                 success: function (response) {
                     rurera_remove_loader(subChapterDivMain, 'button');
-                    $(".featured-list-sidebar-inner").html(response);
+                    $(".sub-chapters-list-data").html(response);
+                }
+            });
+        });
+
+        var subChapterDivMain = $(".custom-sub-chapters-list-data");
+        $(document).on('change', '.custom-sub-chapters-list', function () {
+            rurera_loader(subChapterDivMain, 'div');
+            var sub_chapter_id = $(this).val();
+            subChapterDataRequest = $.ajax({
+                type: "GET",
+                url: '/admin/learning_journey/get_topic_part_items_list',
+                beforeSend: function () {
+                    if (subChapterDataRequest != null) {
+                        subChapterDataRequest.abort();
+                    }
+                },
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                data: {'sub_chapter_id': sub_chapter_id},
+                success: function (response) {
+                    rurera_remove_loader(subChapterDivMain, 'button');
+                    $(".custom-sub-chapters-list-data").html(response);
                 }
             });
         });
