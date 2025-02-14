140f2f616398eb3f4ad56a6e1f638fac28b3efbc
yasir rafique <yasir.chimp@gmail.com>
2025-02-13 21:50:42 -0800

    Update custom_topic_list_item_layout.blade.php

--- resources/views/admin/learning_journey/includes/custom_topic_list_item_layout.blade.php
+++ resources/views/admin/learning_journey/includes/custom_topic_list_item_layout.blade.php
@@ -39,7 +39,7 @@
             </div>
         </div>
     </div>
-    <div class="question-layout-holder mb-0 bg-white panel-border p-25 border-bottom-0 test">
+    <div class="question-layout-holder mb-0 bg-white panel-border p-25">
 
         @if($quizObj->quizQuestionsList->count() > 0)
             @foreach($quizObj->quizQuestionsList as $quizQuestionObj)
