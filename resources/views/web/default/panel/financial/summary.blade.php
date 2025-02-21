ff619b62055cbe2bd0a229902f8446eb22628df2
yasir rafique <yasir.chimp@gmail.com>
2025-02-20 23:28:15 -0800

    Update summary.blade.php

--- resources/views/web/default/panel/financial/summary.blade.php
+++ resources/views/web/default/panel/financial/summary.blade.php
@@ -1,59 +1,59 @@
-        <section>
-            <h2 class="section-title mb-30">{{ trans('financial.financial_documents') }}</h2>
+<section>
+    <h2 class="section-title mb-30">{{ trans('financial.financial_documents') }}</h2>
 
-            <div class="panel-section-card bg-white panel-border rounded-sm p-25">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="table-responsive transactions-table table-sm">
-                            <table class="table text-center custom-table">
-                                <thead>
-                                <tr>
-                                    <th class="font-16 font-weight-500 text-left">{{ trans('public.title') }}</th>
-                                    <th class="font-16 font-weight-500 text-left">Student</th>
-                                    <th class="text-center font-16 font-weight-500">{{ trans('panel.amount') }} ({{ $currency }})</th>
-                                    <th class="text-center font-16 font-weight-500">{{ trans('public.date') }}</th>
-                                </tr>
-                                </thead>
-                                <tbody>
+    <div class="panel-section-card bg-white panel-border rounded-sm p-25">
+        <div class="row">
+            <div class="col-12">
+                <div class="table-responsive transactions-table table-sm">
+                    <table class="table text-center custom-table">
+                        <thead>
+                        <tr>
+                            <th class="font-16 font-weight-500 text-left">{{ trans('public.title') }}</th>
+                            <th class="font-16 font-weight-500 text-left">Student</th>
+                            <th class="text-center font-16 font-weight-500">{{ trans('panel.amount') }} ({{ $currency }})</th>
+                            <th class="text-center font-16 font-weight-500">{{ trans('public.date') }}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
 
-                                @if($accountings->count() > 0)
-                                @foreach($accountings as $accounting)
-                                    <tr>
-                                        <td class="text-left" data-th="{{ trans('public.title') }}">
-                                            <div class="d-flex flex-column">
-                                            <span class="font-16 font-weight-normal">
-												{{isset( $accounting->orderItem->order->package->title )? $accounting->orderItem->order->package->title : ''}}
-                                            </span>
-                                            </div>
-                                        </td>
-                                        <td class="text-left align-middle" data-th="Student">
-                                            <span class="font-16 font-weight-normal">{{isset( $accounting->orderItem->order->student->id )? $accounting->orderItem->order->student->get_full_name() : ''}}</span>
-                                        </td>
-                                        <td class="text-center align-middle" data-th="{{ trans('panel.amount') }} ({{ $currency }})">
-                                            @switch($accounting->type)
-                                                @case(\App\Models\Accounting::$addiction)
-                                                <span class="font-16 font-weight-normal">+{{ $accounting->amount }}</span>
-                                                @break;
-                                                @case(\App\Models\Accounting::$deduction)
-                                                <span class="font-16 font-weight-normal">-{{ $accounting->amount }}</span>
-                                                @break;
-                                            @endswitch
-                                        </td>
-                                        <td class="text-center align-middle font-16 font-weight-normal text-gray" data-th="{{ trans('public.date') }}">
-                                            <span>{{ dateTimeFormat($accounting->created_at, 'j M Y') }}</span>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                @else
-                                <tr class="no-record-found">
-                                    <td class="text-left font-16 font-weight-normal" colspan="4">No Records Found</td>
-                                </tr>
-                                @endif
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                        @if($accountings->count() > 0)
+                        @foreach($accountings as $accounting)
+                            <tr>
+                                <td class="text-left" data-th="{{ trans('public.title') }}">
+                                    <div class="d-flex flex-column">
+                                    <span class="font-16 font-weight-normal">
+                                        {{isset( $accounting->orderItem->order->package->title )? $accounting->orderItem->order->package->title : ''}}
+                                    </span>
+                                    </div>
+                                </td>
+                                <td class="text-left align-middle" data-th="Student">
+                                    <span class="font-16 font-weight-normal">{{isset( $accounting->orderItem->order->student->id )? $accounting->orderItem->order->student->get_full_name() : ''}}</span>
+                                </td>
+                                <td class="text-center align-middle" data-th="{{ trans('panel.amount') }} ({{ $currency }})">
+                                    @switch($accounting->type)
+                                        @case(\App\Models\Accounting::$addiction)
+                                        <span class="font-16 font-weight-normal">+{{ $accounting->amount }}</span>
+                                        @break;
+                                        @case(\App\Models\Accounting::$deduction)
+                                        <span class="font-16 font-weight-normal">-{{ $accounting->amount }}</span>
+                                        @break;
+                                    @endswitch
+                                </td>
+                                <td class="text-center align-middle font-16 font-weight-normal text-gray" data-th="{{ trans('public.date') }}">
+                                    <span>{{ dateTimeFormat($accounting->created_at, 'j M Y') }}</span>
+                                </td>
+                            </tr>
+                        @endforeach
+                        @else
+                        <tr class="no-record-found">
+                            <td class="text-left font-16 font-weight-normal" colspan="4">No Records Found</td>
+                        </tr>
+                        @endif
+                        </tbody>
+                    </table>
                 </div>
             </div>
+        </div>
+    </div>
 
-        </section>
+</section>
