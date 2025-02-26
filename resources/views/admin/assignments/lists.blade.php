3de1ea8e92843fba9610c0855dc2560a367f955a
yasir rafique <yasir.chimp@gmail.com>
2025-02-25 23:59:56 -0800

    Update lists.blade.php

--- resources/views/admin/assignments/lists.blade.php
+++ resources/views/admin/assignments/lists.blade.php
@@ -89,10 +89,10 @@
     <div class="row">
         <div class="col-lg-3 col-md-6 col-sm-6 col-12">
             <div class="card card-statistic-1">
-                <div class="card-icon bg-primary">
+                <div class="card-icon bg-primary skelton-hide">
                     <i class="fas fa-file-alt"></i>
                 </div>
-                <div class="card-wrap">
+                <div class="card-wrap skelton-hide">
                     <div class="card-header">
                         <h4>Total Assignments</h4>
                     </div>
