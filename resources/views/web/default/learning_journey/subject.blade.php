be5a599d0bdb1b882673d6b1b2355e299dc35c5c
Baz <baz.chimpstudio@gmail.com>
2025-04-24 12:36:27 +0500

    Learning Journey
    Learning Journey

--- resources/views/web/default/learning_journey/subject.blade.php
+++ resources/views/web/default/learning_journey/subject.blade.php
@@ -46,6 +46,115 @@
 }
 
 </style>
+<style>
+    .flowchart-operator-inputs-outputs {
+        position: absolute;
+        top:50%;
+        z-index:-1;
+    }
+
+    .field-data.ui-rotatable {
+        position: relative;
+    }
+
+
+    .flowchart-operator-outputs {
+        position: absolute;
+        left: 0%;
+    }
+
+    .flowchart-operator-inputs {
+        position: absolute;
+        left: 50%;
+    }
+
+
+
+    .flowchart-operator-outputs {
+        left: 0%;
+    }
+
+    .flowchart-operator-inputs {
+        left: 50%;
+    }
+
+
+
+
+
+    .flowchart-operator{
+        display: inline-table;
+    }
+    .flowchart-operator-connector-arrow1, .flowchart-operator-connector-small-arrow1 {
+        visibility: hidden;
+    }
+
+
+
+    .flowchart-temporary-link-layer{display:none !important;}
+    .field-options {
+        display: none !important;
+    }
+    .right-in2 .flowchart-operator-outputs {
+        position: absolute;
+        right: 0;
+    }
+    .right-in2 .flowchart-operator-inputs {
+        position: absolute;
+        left: 0;
+    }
+
+
+    .field-data1 {
+        position: relative;
+    }.flowchart-operator-inputs1 {
+         position: absolute;
+         top: 2;
+     }
+    .roadmap-road .roadmap-default {
+        stroke: #000000;
+        stroke-width:15px;
+    }
+    .roadmap-default .roadmap-default, .roadmap-road .roadmap-road, .roadmap-steps .roadmap-steps{
+        display:block;
+    }
+    .roadmap-path{
+        display:none;
+    }
+
+    .spacer-block{width:0px !important;}
+    .spacer-block svg {
+        width: 10px !important;
+        height: 10px !important;
+    }
+    .spacer-svg-controls .flowchart-operator-outputs .flowchart-operator-connector-small-arrow {
+        right: 10px !important;!i;!;
+    }
+
+    .spacer-svg-controls .flowchart-operator-outputs .flowchart-operator-connector-arrow{
+        right:10px !important;
+    }
+
+    ul.editor-objects.sets-selection.active {
+        background: #b2b2b2;
+    }
+
+    .right-in .flowchart-operator-connector-arrow {
+        border-right: 10px solid rgb(204, 204, 204);
+        border-left: none;
+    }
+    .right-in1 .flowchart-operator-inputs, .right-in1 .flowchart-operator-outputs {
+        display: contents !important;
+    }
+    .flowchart-operator-inputs-outputs.right-in .flowchart-operator-inputs .flowchart-operator-connector-arrow {
+        right: -10px !important;
+        left: auto !important;
+    }
+    .flowchart-operator-inputs-outputs.right-in .flowchart-operator-outputs .flowchart-operator-connector-arrow {
+        left: -10px !important;
+        right: auto !important;
+    }
+</style>
 <section class="p-0 mt-30 treasure-mission-layout">
 
 	@if( !empty( $items_data ) )
