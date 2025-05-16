0814a2aa6fbfecd109f37438c78f1febf4d41c8a
yasir rafique <yasir.chimp@gmail.com>
2025-05-15 22:44:49 -0700

    Update lists.blade.php

--- resources/views/admin/custom_quiz/lists.blade.php
+++ resources/views/admin/custom_quiz/lists.blade.php
@@ -22,8 +22,8 @@
                     <div class="card-icon bg-primary skelton-hide">
                         <i class="fas fa-file-alt"></i>
                     </div>
-                    <div class="card-wrap skelton-hide skelton-height-lg skelton-mb-0">
-                        <div class="card-header">
+                    <div class="card-wrap">
+                        <div class="card-header skelton-hide skelton-height-lg skelton-mb-0">
                             <h4>Total Quizzes</h4>
                         </div>
                         <div class="card-body skelton-hide skelton-height-lg skelton-mb-0">
