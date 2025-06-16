4c3bb917637daed70ba1a30efae13b14f22479ea
yasir rafique <yasir.chimp@gmail.com>
2025-06-16 01:28:44 -0700

    Update lists.blade.php

--- resources/views/teachers/classes/lists.blade.php
+++ resources/views/teachers/classes/lists.blade.php
@@ -477,6 +477,12 @@
 <div id="class-edit-modal" class="class-edit-modal modal fade" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="createClassModalLabel">Edit Class</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
             <div class="modal-body class-edit-content">
 
             </div>
