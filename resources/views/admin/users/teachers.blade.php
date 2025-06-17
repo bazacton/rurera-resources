3d4bfa9761d1e52e2834d29f65a3cd9839286d99
yasir rafique <yasir.chimp@gmail.com>
2025-06-16 23:51:53 -0700

    Update teachers.blade.php

--- resources/views/admin/users/teachers.blade.php
+++ resources/views/admin/users/teachers.blade.php
@@ -106,7 +106,7 @@
                 </div>
                 @endforeach
             @endif
-            <div class="row">
+            <div class="row w-100">
                 <div class="col-12 col-md-12">
                     <ul class="col-10 col-md-10 col-lg-10 admin-rurera-tabs nav nav-pills" id="assignment_tabs" role="tablist">
                         <li class="nav-item skelton-hide skelton-height-lg">
