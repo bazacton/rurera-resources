997b30bceb674a44103d8eaba4d0a2bfa7400f89
yasir rafique <yasir.chimp@gmail.com>
2025-06-17 07:08:30 -0700

    Update teachers.blade.php

--- resources/views/admin/users/teachers.blade.php
+++ resources/views/admin/users/teachers.blade.php
@@ -222,9 +222,7 @@
                                                 Bulk Actions <img src="/assets/default/svgs/arrow-down-btn.svg" alt="arrow-down-btn.svg">
                                             </a>
                                             <div class="dropdown-menu">
-                                                <a class="dropdown-item" href="#"><img src="/assets/default/svgs/print.svg" alt="print"> Print</a>
                                                 <a class="dropdown-item" href="#"><img src="/assets/default/svgs/trash-bin.svg" alt="trash-bin"> Delete</a>
-                                                <a class="dropdown-item" href="#"><img src="/assets/default/svgs/envelope.svg" alt="envelope"> Email To Prent</a>
                                             </div>
                                         </div>
                                     </div>
