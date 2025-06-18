6ee7b8c2f1130bb18cdf7e2d4b8944e9099482f8
yasir rafique <yasir.chimp@gmail.com>
2025-06-18 08:24:49 -0700

    Update billing.blade.php

--- resources/views/teachers/billing.blade.php
+++ resources/views/teachers/billing.blade.php
@@ -376,11 +376,11 @@
     <div class="modal fade class-join-modal" id="class-join-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-            <div class="modal-header">
+            <div class="teacher-header">
                 <h2 class="modal-title">Class Join Link</h2>
                 <p class="subheading">Apple - 5th Grade</p>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
+                    <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
