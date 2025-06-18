fd14d75cff1ac8453b6ebf7a0cecf46f5675962d
yasir rafique <yasir.chimp@gmail.com>
2025-06-17 23:54:34 -0700

    Update billing.blade.php

--- resources/views/teachers/billing.blade.php
+++ resources/views/teachers/billing.blade.php
@@ -109,9 +109,9 @@
                             <div class="col-md-3 faq-sidebar mb-0">
                                 
                                 <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                                    <a href="#" class="nav-link active" id="nav-home-tab" data-toggle="tab" data-target="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Subscription details</a>
-                                    <a href="#" class="nav-link" id="nav-profile-tab" data-toggle="tab" data-target="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Billing and payments</a>
-                                    <a href="#" class="nav-link" id="nav-contact-tab" data-toggle="tab" data-target="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Other</a>
+                                    <a href="#" class="active" id="nav-home-tab" data-toggle="tab" data-target="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Subscription details</a>
+                                    <a href="#" id="nav-profile-tab" data-toggle="tab" data-target="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Billing and payments</a>
+                                    <a href="#" id="nav-contact-tab" data-toggle="tab" data-target="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Other</a>
                                 </div>
                             </div>
 
