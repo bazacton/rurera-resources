db32d3410f9cc93fabf6806781a1b86eaf678ddf
yasir rafique <yasir.chimp@gmail.com>
2025-06-23 00:27:35 -0700

    Update navbar.blade.php

--- resources/views/admin/includes/navbar.blade.php
+++ resources/views/admin/includes/navbar.blade.php
@@ -69,7 +69,7 @@
                     <img src="/assets/default/svgs/collections.svg" alt="collections"> Collections
                 </a>
                 <a href="{{ getAdminPanelUrl() }}/custom_page/track" class="dropdown-item has-icon">
-                    <i class="fas fa-cog"></i> Track
+                    <img src="/assets/default/svgs/progress-report.svg" alt="progress-report"> Track
                 </a>
                 <a href="{{ getAdminPanelUrl() }}/custom_page/tailer_hub" class="dropdown-item has-icon">
                     <i class="fas fa-cog"></i> Tailor Hub
