d1a106deb63fad54df9a3f87a9d4be4ccc5b9786
yasir rafique <yasir.chimp@gmail.com>
2025-06-24 21:55:18 -0700

    Update joining_requests.blade.php

--- resources/views/teachers/sections/joining_requests.blade.php
+++ resources/views/teachers/sections/joining_requests.blade.php
@@ -39,20 +39,20 @@
                         <div class="table-responsive">
                             <table class="table table-striped font-14">
                                 <tr>
-                                    <th class="text-left skelton-hide skelton-height-lg skelton-mb-0">Student</th>
-                                    <th class="text-left skelton-hide skelton-height-lg skelton-mb-0">Class</th>
-                                    <th class="text-left skelton-hide skelton-height-lg skelton-mb-0">Section</th>
-                                    <th class="skelton-hide skelton-height-lg skelton-mb-0">Action</th>
+                                    <th class="text-left">Student</th>
+                                    <th class="text-left">Class</th>
+                                    <th class="text-left">Section</th>
+                                    <th>Action</th>
                                 </tr>
 
                                 @foreach($joining_requests as $requestObj)
                                 <tr>
-                                    <td class="skelton-hide skelton-height-lg skelton-mb-0">
+                                    <td>
                                         <span>{{ $requestObj->student->get_full_name() }}</span>
                                     </td>
-                                    <td class="text-left skelton-hide skelton-height-lg skelton-mb-0">{{ isset( $requestObj->section->sectionClass->title)? $requestObj->section->sectionClass->title : '' }}</td>
-                                    <td class="text-left skelton-hide skelton-height-lg skelton-mb-0">{{ $requestObj->section->title }}</td>
-                                    <td class="skelton-hide skelton-height-lg skelton-mb-0">
+                                    <td class="text-left">{{ isset( $requestObj->section->sectionClass->title)? $requestObj->section->sectionClass->title : '' }}</td>
+                                    <td class="text-left">{{ $requestObj->section->title }}</td>
+                                    <td>
                                         <a href="javascript:;" class="btn-transparent btn-sm text-primary request-action" data-action_type="approved" data-request_id="{{$requestObj->id}}">
                                             <i class="fa fa-check"></i>
                                         </a>
