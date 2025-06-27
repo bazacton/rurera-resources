24832ae91bd296ff35fa3728b0b44307743cceaa
yasir rafique <yasir.chimp@gmail.com>
2025-06-27 06:51:49 -0700

    Update students.blade.php

--- resources/views/admin/users/students.blade.php
+++ resources/views/admin/users/students.blade.php
@@ -313,7 +313,7 @@
     </div>
     <!-- Add Student Listing Modal -->
     <div class="modal fade add-student-modal" id="add-student-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-body student-modal-box">
                     <div id="modalNav" class="modal-section student-modal-main active">
