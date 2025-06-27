1ca052ea8d96c9383a778673991ebf0acf6ce080
yasir rafique <yasir.chimp@gmail.com>
2025-06-27 06:23:52 -0700

    Update sidebar.blade.php

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -627,7 +627,7 @@
             @can('admin_teachers_list')
                 <li class="nav-item {{ (request()->is('admin/teachers')) ? 'active' : '' }}">
                     <a href="{{ getAdminPanelUrl('/teachers') }}" class="nav-link">
-                        <img src="/assets/default/img/sidebar/teacher.png" alt="teacher">
+                        <img src="/assets/default/img/sidebar/learn.svg" alt="learn">
                         <span>Teachers</span>
                     </a>
                 </li>
