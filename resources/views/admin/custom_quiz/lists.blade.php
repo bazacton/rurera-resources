a308cded3972ba854b995b085a8105e01329ea5e
yasir rafique <yasir.chimp@gmail.com>
2025-06-30 23:26:32 -0700

    Update lists.blade.php

--- resources/views/admin/custom_quiz/lists.blade.php
+++ resources/views/admin/custom_quiz/lists.blade.php
@@ -211,7 +211,7 @@
 
                                         <td>
                                             <div class="skelton-hide skelton-height-lg skelton-mb-0">
-                                                <div class="quiz-controls">
+                                                <div class="quiz-table-controls">
                                                     @can('admin_quizzes_results')
                                                     <a href="{{ getAdminPanelUrl() }}/quizzes/{{ $quiz->id }}/results"
                                                     class="btn-transparent btn-sm text-primary" data-toggle="tooltip"
@@ -227,7 +227,7 @@
                                                         <i class="fa fa-edit"></i>
                                                     </a>
                                                     @endcan
-                                                    
+
                                                     @can('admin_assignments_delete')
                                                     @include('admin.includes.delete_button',['url' =>
                                                     getAdminPanelUrl().'/quizzes/'.$quiz->id.'/delete' , 'btnClass' => 'btn-sm'])
