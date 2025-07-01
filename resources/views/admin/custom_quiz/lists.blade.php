a0c48401819d96240658039278f8e102ab66a335
yasir rafique <yasir.chimp@gmail.com>
2025-07-01 01:28:21 -0700

    Update lists.blade.php

--- resources/views/admin/custom_quiz/lists.blade.php
+++ resources/views/admin/custom_quiz/lists.blade.php
@@ -21,24 +21,24 @@
             <div class="row">
                 <div class="col-12 col-md-12">
                     <section class="card">
-                        <div class="row">
-                            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                                <div class="card card-statistic-1">
-                                    <div class="card-icon bg-primary skelton-hide">
-                                        <i class="fas fa-file-alt"></i>
-                                    </div>
-                                    <div class="card-wrap">
-                                        <div class="card-header skelton-hide skelton-height-lg skelton-mb-0">
-                                            <h4>Total Quizzes</h4>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                                    <div class="card card-statistic-1">
+                                        <div class="card-icon bg-primary skelton-hide">
+                                            <i class="fas fa-file-alt"></i>
                                         </div>
-                                        <div class="card-body skelton-hide skelton-height-lg skelton-mb-0">
-                                            {{ $totalAssignments }}
+                                        <div class="card-wrap">
+                                            <div class="card-header skelton-hide skelton-height-lg skelton-mb-0">
+                                                <h4>Total Quizzes</h4>
+                                            </div>
+                                            <div class="card-body skelton-hide skelton-height-lg skelton-mb-0">
+                                                {{ $totalAssignments }}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="card-body">
                             <form action="{{ getAdminPanelUrl() }}/quizzes" method="get" class="row mb-0">
                                 <div class="col-md-3">
                                     <div class="form-group">
