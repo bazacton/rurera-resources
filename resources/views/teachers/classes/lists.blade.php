6400c728248fb4b3d9871ab1e9d04d8434e41cd4
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-07-03 21:53:49 -0700

    Update lists.blade.php

--- resources/views/teachers/classes/lists.blade.php
+++ resources/views/teachers/classes/lists.blade.php
@@ -85,9 +85,9 @@
                                         <div class="admin-rurera-tabs-details details-tab">
                                             <div class="form-group">
                                                 <label for="SchoolName">Select School</label>
-                                                <div class="select-holder input-group">
+                                                <div class="select-holder input-group mb-15">
                                                     <div class="select-box">
-                                                        <select class="student-school-change" name="school_id">
+                                                        <select class="student-school-change form-control" name="school_id">
                                                             @if($schools_list->count() > 0)
                                                                 @php $row_no = 0; @endphp
                                                                 @foreach($schools_list as $schoolObj)
