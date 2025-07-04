bd9098bb47e4771ff782bb51507f1a7f7f3c3953
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-07-04 04:01:28 -0700

    Update question-layout.php

--- resources/views/admin/custom_quiz/question-layout.php
+++ resources/views/admin/custom_quiz/question-layout.php
@@ -155,7 +155,7 @@
                     <div class="card">
                         <div class="card-header">
                             @can('admin_assignments_create')
-                            <div class="text-right skelton-hide skelton-height-lg skelton-mb-0">
+                            <div class="text-right ml-auto">
                                 <a href="{{ getAdminPanelUrl() }}/custom_quiz/create" class="simple-btn">Create
                                     Custom Quiz</a>
                             </div>
