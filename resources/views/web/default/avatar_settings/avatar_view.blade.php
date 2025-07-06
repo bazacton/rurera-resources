e35bd0dd4fc6c055e1efe1e765340efc8ebb8511
Baz <baz.chimpstudio@gmail.com>
2025-07-07 01:40:25 +0500

    avtar settings mobile view
    avtar settings mobile view

--- resources/views/web/default/avatar_settings/avatar_view.blade.php
+++ resources/views/web/default/avatar_settings/avatar_view.blade.php
@@ -22,7 +22,7 @@
         <script src="/assets/default/vendors/svgavatars/js/svgavatars.core.min.js"></script>
 
 
-<div id="svgAvatars" data-token_id="{{$token_id}}" data-user_pref="{{$user->user_preference}}">ssssss</div>
+<div id="svgAvatars" class="mobile-avar-view" data-token_id="{{$token_id}}" data-user_pref="{{$user->user_preference}}">ssssss</div>
 
     @push('scripts_bottom')
 
