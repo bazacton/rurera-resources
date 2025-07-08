8681f220778570dfbc2897297f687c50402ab3a5
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-07-08 00:23:21 -0700

    Update create.blade.php

--- resources/views/teachers/classes/create.blade.php
+++ resources/views/teachers/classes/create.blade.php
@@ -910,21 +910,29 @@
                         </div>
                         <div class="form-group">
                             <label>Full Name</label>
-                            <input type="text" name="full_name" class="form-control  " value="" placeholder="Enter the teacher Full Name" required autocomplete="off">
+                            <div>
+                                <span class="icon-box">
+                                    <img src="/assets/default/img/user.png" alt="user">
+                                </span>
+                                <input type="text" name="full_name" class="form-control  " value="" placeholder="Full name of the new member" required autocomplete="off">
+                            </div>
                         </div>
                         <div class="form-group">
                             <label for="email">Email:</label>
-                            <input name="email" type="text" class="form-control " id="email" value="" required placeholder="Teacher email for updates" autocomplete="off">
+                            <div class="input-group">
+                                <span class="icon-box">
+                                    <img src="/assets/default/img/envelope.jpg" alt="envelope">
+                                </span>
+                                <input name="email" type="text" class="form-control " id="email" value="" required placeholder="Email for login & updates" autocomplete="off">
+                            </div>
                         </div>
                         <div class="form-group">
                             <label class="input-label">Password</label>
                             <div class="input-group">
-                                <div class="input-group-prepend">
-                                <span type="button" class="input-group-text">
-                                <i class="fa fa-lock"></i>
+                                <span class="icon-box">
+                                    <img src="/assets/default/img/key-lock.jpg" alt="key-lock">
                                 </span>
-                                </div>
-                                <input type="password" name="password" class="form-control" placeholder="Create a strong password" autocomplete="new-password">
+                                <input type="password" name="password" class="form-control" placeholder="Create a strong password" autocomplete="new-password">
                             </div>
                         </div>
                         <div class="text-right mt-4">
