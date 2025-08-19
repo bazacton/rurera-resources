5cd9ad480e27a64c4ba5b8c6da05beb00bfd1733
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-08-19 03:49:34 -0700

    Update create-bk.blade.php

--- resources/views/admin/assignments/create-bk.blade.php
+++ resources/views/admin/assignments/create-bk.blade.php
@@ -188,7 +188,7 @@
             </div>
         </div>
     </div>
-    <div class="section-body">
+    <div class="section-body test">
         <div class="container">
             <div class="row">
                 <div class="col-12 col-md-12 col-lg-12">
