17490a7a055b276f9d28e979ab7a5d0a9d00a442
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-08-21 06:13:25 -0700

    Update lists.blade.php

--- resources/views/admin/assignments/lists.blade.php
+++ resources/views/admin/assignments/lists.blade.php
@@ -15,62 +15,60 @@
             <div class="breadcrumb-item">Timestables Assignments</div>
         </div>
     </div>
-
-    <div class="row">
-
-        <div class="col-12 col-md-12">
-            <div class="admin-rurera-tabs-holder">
-                <ul class="col-10 col-md-10 col-lg-10 admin-rurera-tabs nav nav-pills" id="assignment_tabs" role="tablist">
-                    <li class="nav-item">
-                        <div class="skelton-hide skelton-height-lg skelton-mb-0">
-                            <a class="nav-link {{($assignment_tab == 'all')? 'active' : ''}}" id="topics-tab" href="/admin/assignments">
-                                <i class="fas fa-file mx-0"></i>
-                                <span class="tab-title">All Assignments</span>
-                            </a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="skelton-hide skelton-height-lg skelton-mb-0">
-                            <a class="nav-link {{($assignment_tab == 'scheduled')? 'active' : ''}}" id="topics-tab" href="/admin/assignments/scheduled" >
-                                <i class="fas fa-calendar-week mx-0"></i>
-                                <span class="tab-title">Scheduled</span>
-                            </a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="skelton-hide skelton-height-lg skelton-mb-0">
-                            <a class="nav-link {{($assignment_tab == 'running')? 'active' : ''}}" id="topics-tab" href="/admin/assignments/running" >
-                                <i class="fas fa-car mx-0"></i>
-                                <span class="tab-title">Running</span>
-                            </a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="skelton-hide skelton-height-lg skelton-mb-0">
-                            <a class="nav-link {{($assignment_tab == 'completed')? 'active' : ''}}" id="topics-tab" href="/admin/assignments/completed" >
-                                <i class="fas fa-thumbs-up mx-0"></i>
-                                <span class="tab-title">Completed</span>
-                            </a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <div class="skelton-hide skelton-height-lg skelton-mb-0">
-                            <a class="nav-link {{($assignment_tab == 'paused')? 'active' : ''}}" id="topics-tab" href="/admin/assignments/paused" >
-                                <i class="fas fa-pause mx-0"></i>
-                                <span class="tab-title">Paused</span>
-                            </a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            @can('admin_assignments_create')
-                <div class="text-right ml-auto">
-                    <a href="{{ getAdminPanelUrl() }}/assignments/create" class="simple-btn">Create Assignment</a>
+    <div class="card">
+        <div class="row">
+            <div class="col-12 col-md-12">
+                <div class="admin-rurera-tabs-holder">
+                    <ul class="col-10 col-md-10 col-lg-10 admin-rurera-tabs nav nav-pills" id="assignment_tabs" role="tablist">
+                        <li class="nav-item">
+                            <div class="skelton-hide skelton-height-lg skelton-mb-0">
+                                <a class="nav-link {{($assignment_tab == 'all')? 'active' : ''}}" id="topics-tab" href="/admin/assignments">
+                                    <i class="fas fa-file mx-0"></i>
+                                    <span class="tab-title">All Assignments</span>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <div class="skelton-hide skelton-height-lg skelton-mb-0">
+                                <a class="nav-link {{($assignment_tab == 'scheduled')? 'active' : ''}}" id="topics-tab" href="/admin/assignments/scheduled" >
+                                    <i class="fas fa-calendar-week mx-0"></i>
+                                    <span class="tab-title">Scheduled</span>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <div class="skelton-hide skelton-height-lg skelton-mb-0">
+                                <a class="nav-link {{($assignment_tab == 'running')? 'active' : ''}}" id="topics-tab" href="/admin/assignments/running" >
+                                    <i class="fas fa-car mx-0"></i>
+                                    <span class="tab-title">Running</span>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <div class="skelton-hide skelton-height-lg skelton-mb-0">
+                                <a class="nav-link {{($assignment_tab == 'completed')? 'active' : ''}}" id="topics-tab" href="/admin/assignments/completed" >
+                                    <i class="fas fa-thumbs-up mx-0"></i>
+                                    <span class="tab-title">Completed</span>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <div class="skelton-hide skelton-height-lg skelton-mb-0">
+                                <a class="nav-link {{($assignment_tab == 'paused')? 'active' : ''}}" id="topics-tab" href="/admin/assignments/paused" >
+                                    <i class="fas fa-pause mx-0"></i>
+                                    <span class="tab-title">Paused</span>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-            @endcan
-        </div>
-        <div class="col-12 col-md-12">
-            <div class="card">
+                @can('admin_assignments_create')
+                    <div class="text-right ml-auto">
+                        <a href="{{ getAdminPanelUrl() }}/assignments/create" class="simple-btn">Create Assignment</a>
+                    </div>
+                @endcan
+            </div>
+            <div class="col-12 col-md-12">
                 <div class="card-body pl-0 pr-0">
                     <form action="/admin/assignments" id="topic_parts_search_form" method="get" class="row mb-0">
                         <div class="col-md-2">
@@ -123,82 +121,83 @@
                     </form>
                 </div>
             </div>
-        </div>
-        <div class="col-12">
-            <div class="assignments-table">
-                <div class="card">
+            <div class="col-12">
+                <div class="assignments-table">
+                    <div class="card">
 
-                    <div class="card-body p-0 table-sm">
-                        <table class="table mb-0">
-                            <thead class="thead-light">
-                                <tr>
-                                    <th>Type</th>
-                                    <th>Questions</th>
-                                    <th>Participations</th>
-                                    <th>Start & End Date</th>
-                                    <th>Accuracy</th>
-                                    <th>Status</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            @if($assignments->count() > 0)
-                            @foreach($assignments as $assignmentObj)
-                                <tr>
-                                    <td data-th="Type">
-                                        <div class="skelton-hide skelton-height-lg skelton-mb-0">{{ getQuizTypeTitle($assignmentObj->assignment_type) }}</div>
-                                    </td>
-                                    <td data-th="Questions">
-                                        <div class="skelton-hide skelton-height-lg skelton-mb-0">{{$assignmentObj->no_of_questions}}</div>
-                                    </td>
-                                    <td data-th="Participations">
-                                        <div class="skelton-hide skelton-height-lg skelton-mb-0">{{$assignmentObj->students->where('status', '!=', 'inactive')->count()}}</div>
-                                    </td>
-                                    <td data-th="Start & End Date">
-                                        <div class="skelton-hide skelton-height-lg skelton-mb-0">{{dateTimeFormat($assignmentObj->assignment_start_date, 'j M Y')}} / {{dateTimeFormat($assignmentObj->assignment_end_date, 'j M Y')}}</div>
-                                    </td>
-                                    <td data-th="Accuracy">
-                                        <div class="skelton-hide skelton-height-lg skelton-mb-0">
-                                            <div class="circle_percent circle-green" data-percent="50">
-                                                <div class="circle_inner">
-                                                    <div class="round_per" style="transform: rotate(360deg);"></div>
-                                                </div>
-                                                <div class="circle_inbox">
-                                                    <span class="percent_text">50%</span>
+                        <div class="card-body p-0 table-sm">
+                            <table class="table mb-0">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th>Type</th>
+                                        <th>Questions</th>
+                                        <th>Participations</th>
+                                        <th>Start & End Date</th>
+                                        <th>Accuracy</th>
+                                        <th>Status</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @if($assignments->count() > 0)
+                                @foreach($assignments as $assignmentObj)
+                                    <tr>
+                                        <td data-th="Type">
+                                            <div class="skelton-hide skelton-height-lg skelton-mb-0">{{ getQuizTypeTitle($assignmentObj->assignment_type) }}</div>
+                                        </td>
+                                        <td data-th="Questions">
+                                            <div class="skelton-hide skelton-height-lg skelton-mb-0">{{$assignmentObj->no_of_questions}}</div>
+                                        </td>
+                                        <td data-th="Participations">
+                                            <div class="skelton-hide skelton-height-lg skelton-mb-0">{{$assignmentObj->students->where('status', '!=', 'inactive')->count()}}</div>
+                                        </td>
+                                        <td data-th="Start & End Date">
+                                            <div class="skelton-hide skelton-height-lg skelton-mb-0">{{dateTimeFormat($assignmentObj->assignment_start_date, 'j M Y')}} / {{dateTimeFormat($assignmentObj->assignment_end_date, 'j M Y')}}</div>
+                                        </td>
+                                        <td data-th="Accuracy">
+                                            <div class="skelton-hide skelton-height-lg skelton-mb-0">
+                                                <div class="circle_percent circle-green" data-percent="50">
+                                                    <div class="circle_inner">
+                                                        <div class="round_per" style="transform: rotate(360deg);"></div>
+                                                    </div>
+                                                    <div class="circle_inbox">
+                                                        <span class="percent_text">50%</span>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                    <td data-th="Status" class="text-success font-weight-bold">
-                                        <div class="skelton-hide skelton-height-lg skelton-mb-0">{{$assignmentObj->status}}</div>
-                                    </td>
-                                    <td data-th="Actions">
-                                        <div class="pending-invites-controls">
-                                            <a class="dropdown-item" href="/admin/assignments/{{$assignmentObj->id}}/progress" data-toggle="tooltip" data-placement="top" data-trigger="hover" data-original-title="Progress"><img src="/assets/default/svgs/action-progress.svg" alt="envelope" class="mr-0"></a>
-                                            <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" data-trigger="hover" data-original-title="Print"><img src="/assets/default/svgs/print-action.svg" alt="print" class="mr-0"></a>
-                                            <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" data-trigger="hover" data-original-title="Delete"><img src="/assets/default/svgs/delete-menu.svg" alt="trash-bin" class="mr-0"></a>
-                                            <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" data-trigger="hover" data-original-title="Email To Prent"><img src="/assets/default/svgs/envelope.svg" alt="envelope" class="mr-0"></a>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            @else
-                                <tr>
-                                    <td data-th="no-records" class="no-records-found" colspan="7">
-                                        No Records Found!
-                                    </td>
-                                </tr>
-                            @endif
+                                        </td>
+                                        <td data-th="Status" class="text-success font-weight-bold">
+                                            <div class="skelton-hide skelton-height-lg skelton-mb-0">{{$assignmentObj->status}}</div>
+                                        </td>
+                                        <td data-th="Actions">
+                                            <div class="pending-invites-controls">
+                                                <a class="dropdown-item" href="/admin/assignments/{{$assignmentObj->id}}/progress" data-toggle="tooltip" data-placement="top" data-trigger="hover" data-original-title="Progress"><img src="/assets/default/svgs/action-progress.svg" alt="envelope" class="mr-0"></a>
+                                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" data-trigger="hover" data-original-title="Print"><img src="/assets/default/svgs/print-action.svg" alt="print" class="mr-0"></a>
+                                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" data-trigger="hover" data-original-title="Delete"><img src="/assets/default/svgs/delete-menu.svg" alt="trash-bin" class="mr-0"></a>
+                                                <a class="dropdown-item" href="#" data-toggle="tooltip" data-placement="top" data-trigger="hover" data-original-title="Email To Prent"><img src="/assets/default/svgs/envelope.svg" alt="envelope" class="mr-0"></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                @else
+                                    <tr>
+                                        <td data-th="no-records" class="no-records-found" colspan="7">
+                                            No Records Found!
+                                        </td>
+                                    </tr>
+                                @endif
 
 
-                            </tbody>
-                        </table>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
+                    <div class="rurera-pagination">{{$assignments->links()}}</div>
                 </div>
-                <div class="rurera-pagination">{{$assignments->links()}}</div>
             </div>
         </div>
     </div>
+    
 </section>
 @endsection
 
