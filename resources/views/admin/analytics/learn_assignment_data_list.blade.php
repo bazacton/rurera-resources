1bf28ac37a0540603c5227b7c51f331e71060a2f
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-09-02 22:39:59 -0700

    Update learn_assignment_data_list.blade.php

--- resources/views/admin/analytics/learn_assignment_data_list.blade.php
+++ resources/views/admin/analytics/learn_assignment_data_list.blade.php
@@ -54,7 +54,7 @@
 <div class="table-responsive">
     <table class="table heatmap-table">
         <thead>
-        <tr class="topic-heading-top">
+        <tr class="topic-heading-top 33333">
             <th class="font-14"> &nbsp;</th>
             <th class="font-14" colspan="4"> {{$assignmentObj->title}}</th>
         </tr>
