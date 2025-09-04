054f535bd7737605036c44e97afdcf2b192fb109
Baz <baz.chimpstudio@gmail.com>
2025-09-04 16:43:41 +0500

    sidebar
    sidebar

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -35,6 +35,7 @@
         <ul class="sidebar-menu">
             @if(!auth()->user()->isAuthor() && !auth()->user()->isReviewer())
                 @can('admin_general_dashboard_show')
+                    {{request()->is(getAdminPanelUrl('/'))}}
                     <li class="{{ (request()->is(getAdminPanelUrl('/'))) ? 'active' : '' }} nav-item">
                         <a href="/admin" class="nav-link dropdown-toggle11" id="sub-menu" data-toggle="dropdown" aria-haspopup="true">
                             <img src="/assets/default/img/sidebar/home.svg" alt="home">
