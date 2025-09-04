5511784b25bf5923660456d3c27e8d546ba3c7f3
Baz <baz.chimpstudio@gmail.com>
2025-09-04 18:31:48 +0500

    sidebar
    sidebar

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -669,7 +669,7 @@
                         <a href="/admin/analytics" class="nav-link dropdown-toggle" id="analytics-menu" data-toggle="dropdown" aria-haspopup="true">
                             <img src="/assets/default/img/sidebar/analytics.svg.svg" alt="analytics">
                             <span>Analytics</span>
-                                <div class="sub-menu-sidebar dropdown-menu show" aria-labelledby="analytics-menu">
+                                <div class="sub-menu-sidebar dropdown-menu" aria-labelledby="analytics-menu">
                                     <div class="dropdown-content">
                                         <div class="dropdown-nav-area">
                                             <h5>Analytics Reports</h5>
