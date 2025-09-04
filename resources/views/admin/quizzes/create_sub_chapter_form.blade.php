932927dbec3c7c487ac488387af5e7dfb04e9349
Baz <baz.chimpstudio@gmail.com>
2025-09-04 12:24:16 +0500

    sub chapter options
    sub chapter options

---
+++ resources/views/admin/quizzes/create_sub_chapter_form.blade.php
@@ -0,0 +1,138 @@
+<div data-action="{{ !empty($subChapter) ? '/admin/webinars/'. $subChapter->id .'/update_sub_chapter' : '/admin/webinars/store_sub_chapter' }}" class="js-content-form quiz-form webinar-form">
+    {{ csrf_field() }}
+    <section>
+        <div class="row">
+            <div class="col-12 col-md-12">
+
+                <input type="hidden" name="ajax[practice_type]" class="webinar_practice_type_field" value="">
+
+                <div class="d-flex align-items-center justify-content-between">
+                    <div class="">
+                        <h2 class="section-title">{{ !empty($subChapter) ? (trans('public.edit').' ('. $subChapter->sub_chapter_title .')') : trans('quiz.sub_section') }}</h2>
+                        <p>{{ trans('admin/main.instructor') }}: {{ $creator->get_full_name() }}</p>
+                    </div>
+                </div>
+
+                @if(!empty(getGeneralSettings('content_translate')))
+                    <div class="form-group">
+                        <label class="input-label">{{ trans('auth.language') }}</label>
+                        <select name="ajax[locale]" class="form-control {{ !empty($subChapter) ? 'js-edit-content-locale' : '' }}">
+                            @foreach($userLanguages as $lang => $language)
+                                <option value="{{ $lang }}" @if(mb_strtolower(request()->get('locale', app()->getLocale())) == mb_strtolower($lang)) selected @endif>{{ $language }}</option>
+                            @endforeach
+                        </select>
+                        @error('locale')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                @else
+                    <input type="hidden" name="locale" value="{{ getDefaultLocale() }}">
+                @endif
+
+                @if(empty($selectedWebinar))
+                    <div class="form-group mt-3">
+                        <label class="input-label">{{ trans('panel.webinar') }}</label>
+                        <select name="ajax[webinar_id]" class="custom-select">
+                            <option {{ !empty($subChapter) ? 'disabled' : 'selected disabled' }} value="">{{ trans('panel.choose_webinar') }}</option>
+                            @foreach($webinars as $webinar)
+                                <option value="{{ $webinar->id }}" {{  (!empty($subChapter) and $subChapter->webinar_id == $webinar->id) ? 'selected' : '' }}>{{ $webinar->title }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                @else
+                    <input type="hidden" name="ajax[webinar_id]" value="{{ $selectedWebinar->id }}">
+                @endif
+
+                <input type="hidden" name="ajax[chapter_id]" value="{{ !empty($chapter) ? $chapter->id :'' }}" class="chapter-input">
+                <input type="hidden" name="ajax[quiz_type]" value="auto_builder">
+
+                <div class="form-group">
+                    <label class="input-label">Sub Chapter Title</label>
+                    <input type="text" value="{{ !empty($subChapter) ? $subChapter->sub_chapter_title : old('title') }}" name="ajax[title]" class="form-control @error('title')  is-invalid @enderror" placeholder=""/>
+                    @error('title')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+
+                @php $random_id = rand(99,9999); @endphp
+                <div class="form-group">
+                    <label class="input-label">Sub Chapter Image</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <button type="button" class="input-group-text admin-file-manager" data-input="sub_chapter_image_{{$random_id}}_record"
+                                    data-preview="holder">
+                                <i class="fa fa-upload"></i>
+                            </button>
+                        </div>
+                        <input type="text" value="{{ !empty($subChapter) ? $subChapter->sub_chapter_image : old('sub_chapter_image') }}" name="ajax[sub_chapter_image]"
+                               id="sub_chapter_image_{{$random_id}}_record"
+                               class="form-control js-ajax-sub_chapter_image_{{$random_id}}"/>
+                        <div class="input-group-append">
+                            <button type="button" class="input-group-text admin-file-view" data-input="sub_chapter_image_{{$random_id}}_record">
+                                <i class="fa fa-eye"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="input-label">Sub Chapter Slug</label>
+                    <input type="text" value="{{ !empty($subChapter) ? $subChapter->sub_chapter_slug : old('sub_chapter_slug') }}" name="ajax[sub_chapter_slug]" class="form-control" placeholder=""/>
+                </div>
+
+                @php $chapter_type = isset($subChapter->chapter->id)?$subChapter->chapter->chapter_type : 'Course'; @endphp
+                @if($chapter_type == 'Mock Exams' || $chapter_type == 'Both')
+                    <div class="row">
+                        <div class="col-4 col-md-4 webinar_type_fields test_total_time_field">
+                            <div class="form-group">
+                                <label class="input-label">Emerging</label>
+                                <input type="text" value="{{ !empty($subChapter) ? $subChapter->Emerging : old('sub_chapter_slug') }}" name="ajax[Emerging]" class="form-control" placeholder=""/>
+                            </div>
+                        </div>
+                        <div class="col-4 col-md-4">
+                            <div class="form-group">
+                                <label class="input-label">Expected</label>
+                                <input type="text" value="{{ !empty($subChapter) ? $subChapter->Expected : old('sub_chapter_slug') }}" name="ajax[Expected]" class="form-control" placeholder=""/>
+                            </div>
+                        </div>
+                        <div class="col-4 col-md-4">
+                            <div class="form-group">
+                                <label class="input-label">Exceeding</label>
+                                <input type="text" value="{{ !empty($subChapter) ? $subChapter->Exceeding : old('sub_chapter_slug') }}" name="ajax[Exceeding]" class="form-control" placeholder=""/>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+				@php
+				$chapter_settings = array();
+				if( isset( $subChapter->chapter_settings ) ){
+					$chapter_settings  = $subChapter->chapter_settings;
+					$chapter_settings    = json_decode($chapter_settings);
+					$chapter_settings	= (array)$chapter_settings;
+				}
+				@endphp
+
+            </div>
+        </div>
+    </section>
+
+
+    <input type="hidden" name="ajax[is_webinar_page]" value="@if(!empty($inWebinarPage) and $inWebinarPage) 1 @else 0 @endif">
+
+    <div class="mt-20 mb-20">
+        <button type="button" class="js-submit-quiz-form btn btn-sm btn-primary">{{ !empty($subChapter) ? trans('public.save_change') : trans('public.create') }}</button>
+
+        @if(empty($subChapter) and !empty($inWebinarPage))
+            <button type="button" class="btn btn-sm btn-danger ml-10 cancel-accordion">{{ trans('public.close') }}</button>
+        @endif
+    </div>
+</div>
+
+@if(!empty($subChapter))
+    @include('admin.quizzes.modals.multiple_question')
+    @include('admin.quizzes.modals.descriptive_question')
+@endif
