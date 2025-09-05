df1aaaa3567931c851af06af32473d35a07b4d90
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-09-04 23:24:03 -0700

    Update scripts.js

--- public/assets/admin/js/scripts.js
+++ public/assets/admin/js/scripts.js
@@ -914,24 +914,4 @@ $(document).ready(function() {
     });
 
 });
-$(document).ready(function () {
-  var $body = $('body');
-  var $btn = $('.main-navbar .nav-link');
 
-  var menu_state = localStorage.getItem('sidebar_state');
-  if (menu_state === 'close') {
-    $body.addClass('sidebar-mini');
-  }
-
-  $btn.on('click', function (e) {
-    e.preventDefault();
-
-    $body.toggleClass('sidebar-mini');
-
-    if ($body.hasClass('sidebar-mini')) {
-      localStorage.setItem('sidebar_state', 'close');
-    } else {
-      localStorage.setItem('sidebar_state', 'open');
-    }
-  });
-});
