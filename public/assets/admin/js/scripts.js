1353a8f34a010c52155f15ec320f6e094348ebac
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-09-04 23:14:46 -0700

    Update scripts.js

--- public/assets/admin/js/scripts.js
+++ public/assets/admin/js/scripts.js
@@ -184,24 +184,6 @@ $(function () {
                 }
             });
         }
-        var $btn = $('.main-navbar .nav-link');
-
-        var menu_state = localStorage.getItem('sidebar_state');
-        if (menu_state === 'close') {
-            $body.addClass('sidebar-mini');
-        }
-
-        $btn.on('click', function (e) {
-            e.preventDefault();
-
-            $body.toggleClass('sidebar-mini');
-
-            if ($body.hasClass('sidebar-mini')) {
-            localStorage.setItem('sidebar_state', 'close');
-            } else {
-            localStorage.setItem('sidebar_state', 'open');
-            }
-        });
     }
 
     // $("[data-toggle='sidebar']").on('click', function () {
@@ -932,7 +914,24 @@ $(document).ready(function() {
     });
 
 });
-// $(document).ready(function () {
-//   var $body = $('body');
-  
-// });
+$(document).ready(function () {
+  var $body = $('body');
+  var $btn = $('.main-navbar .nav-link');
+
+  var menu_state = localStorage.getItem('sidebar_state');
+  if (menu_state === 'close') {
+    $body.addClass('sidebar-mini');
+  }
+
+  $btn.on('click', function (e) {
+    e.preventDefault();
+
+    $body.toggleClass('sidebar-mini');
+
+    if ($body.hasClass('sidebar-mini')) {
+      localStorage.setItem('sidebar_state', 'close');
+    } else {
+      localStorage.setItem('sidebar_state', 'open');
+    }
+  });
+});
