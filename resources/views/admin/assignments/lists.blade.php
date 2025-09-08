2328ddf936a81456fd66fce941cc956d1d44aa2e
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-09-08 03:36:35 -0700

    Update lists.blade.php

--- resources/views/admin/assignments/lists.blade.php
+++ resources/views/admin/assignments/lists.blade.php
@@ -841,33 +841,154 @@
         return false;
     });
     </script>
-    <script>
-        var comboTree1, comboTree2, comboTree3;
+    <script type="text/javascript">
+      const SampleJSONData = [
+        {
+          id: 0,
+          title: "Horse",
+        },
+        {
+          id: 1,
+          title: "Birds",
+          isSelectable: false,
+          subs: [
+            {
+              id: 10,
+              title: "Pigeon",
+              isSelectable: false,
+            },
+            {
+              id: 11,
+              title: "Parrot",
+            },
+            {
+              id: 12,
+              title: "Owl",
+            },
+            {
+              id: 13,
+              title: "Falcon",
+            },
+          ],
+        },
+        {
+          id: 2,
+          title: "Rabbit",
+        },
+        {
+          id: 3,
+          title: "Fox",
+        },
+        {
+          id: 5,
+          title: "Cats",
+          subs: [
+            {
+              id: 50,
+              title: "Kitty",
+            },
+            {
+              id: 51,
+              title: "Bigs",
+              subs: [
+                {
+                  id: 510,
+                  title: "Cheetah",
+                },
+                {
+                  id: 511,
+                  title: "Jaguar",
+                },
+                {
+                  id: 512,
+                  title: "Leopard",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          id: 6,
+          title: "Fish",
+        },
+      ];
+      const SampleJSONData2 = [
+        {
+          id: 1,
+          title: "Four Wheels",
+          subs: [
+            {
+              id: 10,
+              title: "Car",
+            },
+            {
+              id: 11,
+              title: "Truck",
+            },
+            {
+              id: 12,
+              title: "Transporter",
+            },
+            {
+              id: 13,
+              title: "Dozer",
+            },
+          ],
+        },
+        {
+          id: 2,
+          title: "Two Wheels",
+          subs: [
+            {
+              id: 20,
+              title: "Cycle",
+            },
+            {
+              id: 21,
+              title: "Motorbike",
+            },
+            {
+              id: 22,
+              title: "Scooter",
+            },
+          ],
+        },
+        {
+          id: 2,
+          title: "Van",
+        },
+        {
+          id: 3,
+          title: "Bus",
+        },
+      ];
 
-        jQuery(document).ready(function ($) {
-            comboTree1 = $("#justAnInputBox").comboTree({
-            source: SampleJSONData,
-            isMultiple: true,
-            cascadeSelect: false,
-            collapse: false,
-            selectAll: true,
-            });
+      var comboTree1, comboTree2, comboTree3;
 
-            comboTree3 = $("#justAnInputBox1").comboTree({
-            source: SampleJSONData,
-            isMultiple: true,
-            cascadeSelect: true,
-            collapse: false,
-            });
-
-            // comboTree3.setSource(SampleJSONData2);
-
-            comboTree2 = $("#justAnotherInputBox").comboTree({
-            source: SampleJSONData,
-            isMultiple: false,
-            });
-
-            comboTree1.toggleDropDown();
+      jQuery(document).ready(function ($) {
+        comboTree1 = $("#justAnInputBox").comboTree({
+          source: SampleJSONData,
+          isMultiple: true,
+          cascadeSelect: false,
+          collapse: false,
+          selectAll: true,
         });
+
+        comboTree3 = $("#justAnInputBox1").comboTree({
+          source: SampleJSONData,
+          isMultiple: true,
+          cascadeSelect: true,
+          collapse: false,
+        });
+
+        // comboTree3.setSource(SampleJSONData2);
+
+        comboTree2 = $("#justAnotherInputBox").comboTree({
+          source: SampleJSONData,
+          isMultiple: false,
+        });
+
+        comboTree1.toggleDropDown();
+      });
     </script>
 @endpush
