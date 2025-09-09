4154c4b38beef6d2e5064da5a9fd8a6a5015d4a2
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-09-09 03:32:44 -0700

    Update lists.blade.php

--- resources/views/admin/questions_generator/lists.blade.php
+++ resources/views/admin/questions_generator/lists.blade.php
@@ -36,12 +36,14 @@
 @section('content')
 <section class="section">
     <div class="section-header">
-        <h1>AI prompt</h1>
-        <div class="section-header-breadcrumb">
-            <div class="breadcrumb-item active"><a href="/admin/">{{trans('admin/main.dashboard')}}</a>
-            </div>
-            <div class="breadcrumb-item">AI prompt</div>
-        </div>
+        <div class="heading-holder">
+			<h1>AI prompt</h1>
+			<div class="section-header-breadcrumb">
+				<div class="breadcrumb-item active"><a href="/admin/">{{trans('admin/main.dashboard')}}</a>
+				</div>
+				<div class="breadcrumb-item">AI prompt</div>
+			</div>
+		</div>
     </div>
 
 
