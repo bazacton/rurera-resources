e46e1b08569f4760cec2e3d29342ddf6501399bb
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-09-09 22:23:12 -0700

    Update create.blade.php

--- resources/views/admin/webinars/create.blade.php
+++ resources/views/admin/webinars/create.blade.php
@@ -436,7 +436,7 @@
 
                                             <div class="row">
                                                 <div class="col-12">
-                                                    <h2 class="section-title after-line">SEO Fields</h2>
+                                                    <h2 class="section-title after-line">SEO Settings</h2>
                                                 </div>
                                                 <div class="col-12 col-md-4 col-lg-4">
                                                     <div class="form-group">
