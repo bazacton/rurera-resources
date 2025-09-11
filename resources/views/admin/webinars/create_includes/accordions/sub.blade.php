bf26a00e1ba01b650a6bf4da57cf1f5270f9c834
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-09-11 06:29:13 -0700

    Update sub.blade.php

--- resources/views/admin/webinars/create_includes/accordions/sub.blade.php
+++ resources/views/admin/webinars/create_includes/accordions/sub.blade.php
@@ -80,7 +80,7 @@ $chapter  = isset( $chapter )? $chapter : '';
     
     
     
-    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal sub-topic-modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
             
