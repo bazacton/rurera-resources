ce640b1e20fc16ad5c7a756c67193c532a4c19b5
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-09-16 00:05:35 -0700

    Update teachers.blade.php

--- resources/views/admin/users/teachers.blade.php
+++ resources/views/admin/users/teachers.blade.php
@@ -734,7 +734,6 @@
                                     </div>
                                 </div>
                             </div>
-                            
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-3 col-12">
