983f1f721623b402677d661ad896ab5071ad1b18
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-09-18 01:04:32 -0700

    Update school_zone_mode.blade.php

--- resources/views/web/default/timestables/school_zone_mode.blade.php
+++ resources/views/web/default/timestables/school_zone_mode.blade.php
@@ -101,7 +101,7 @@
                 <div class="col-12">
                     <div class="referral-withdraw mb-30">
                       <div class="withdraw-card p-15 d-flex align-items-center flex-wrap">
-                          <div class="icon-box mr-10">
+                          <div class="icon-box">
                               <img src="/assets/default/svgs/shuttlecock.svg" alt="shuttlecock" height="30" width="30">
                           </div>
                               <div class="withdraw-text" itemscope itemtype="https://schema.org/Article">
