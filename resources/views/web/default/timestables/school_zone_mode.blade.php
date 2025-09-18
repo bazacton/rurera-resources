4400df12b63883860abc608e7652abf891e54ad1
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-09-18 01:00:32 -0700

    Update school_zone_mode.blade.php

--- resources/views/web/default/timestables/school_zone_mode.blade.php
+++ resources/views/web/default/timestables/school_zone_mode.blade.php
@@ -15,7 +15,7 @@
     <li data-type="leaderboard">Leaderboard</li>
 </ul>
 <section class="mb-30 school-zone-data my-class-data">
-    <div class="container">
+    <div class="container px-0">
         <div class="row">
             <div class="col-12">
                 <div class="school-zone-students row">
