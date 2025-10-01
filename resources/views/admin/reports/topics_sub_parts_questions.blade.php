a7c257d7f579f97bf6eead63acfa89521489bbdc
Baz <baz.chimpstudio@gmail.com>
2025-10-01 15:41:46 +0500

    report
    report

--- resources/views/admin/reports/topics_sub_parts_questions.blade.php
+++ resources/views/admin/reports/topics_sub_parts_questions.blade.php
@@ -358,12 +358,14 @@
 														$difficulty_level_class = ($difficulty_level == 'Exceeding')? 'table-col-yellow' : $difficulty_level_class;
 														@endphp
 														@php $total_questions = $subTopicObj->topicPartItemQuestions()->whereJsonContains('category_id', $category_id)->where('question_difficulty_level', $difficulty_level)->count();
-														$pending_questions = $expected_part_questions-$total_questions;
-														$pending_questions = ($pending_questions < 0)? 0 : $pending_questions;
-														$total_pending_questions += $pending_questions;
+
                                                         if(in_array($TopicPartObj->subject->id, $subject_ids)){
                                                             $expected_part_questions = ($difficulty_level == 'Emerging')? 0 : $expected_part_questions_prev;
                                                         }
+                                                        $pending_questions = $expected_part_questions-$total_questions;
+														$pending_questions = ($pending_questions < 0)? 0 : $pending_questions;
+														$total_pending_questions += $pending_questions;
+
 														@endphp
 														<td class="{{$difficulty_level_class}} value-for-parent" data-parent_key="expected-questions-{{$difficulty_level}}">{{$expected_part_questions}}</td>
 														<td class="{{$difficulty_level_class}} value-for-parent" data-parent_key="total-questions-{{$difficulty_level}}">{{$total_questions}}</td>
