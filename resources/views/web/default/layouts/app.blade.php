669e3735fdb61e38179b7936a714b1ba46cb612e
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-10-07 23:40:04 -0700

    Update app.blade.php

--- resources/views/web/default/layouts/app.blade.php
+++ resources/views/web/default/layouts/app.blade.php
@@ -13,11 +13,11 @@
     <title>{{ $page_title ?? '' }}{{ !empty($generalSettings['site_name']) ? (' | '.$generalSettings['site_name']) : '' }}</title>
 
     <!-- General CSS File -->
-    <link rel="stylesheet" href="/assets/admin/vendor/bootstrap/bootstrap.min.css"/>
-    <link rel="stylesheet" href="/assets/default/css/common.min.css?ver={{$rand_no}}">
-    <link rel="stylesheet" href="/assets/default/css/app.min.css?ver={{$rand_no}}">
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/assets/default/css/responsive.min.css?ver={{$rand_no}}">
+    <link rel="stylesheet" href="/assets/admin/vendor/bootstrap/bootstrap.min.css" media="print" onload="this.onload=null;this.media='all';"/>
+    <link rel="stylesheet" href="/assets/default/css/common.min.css?ver={{$rand_no}}" media="print" onload="this.onload=null;this.media='all';">
+    <link rel="stylesheet" href="/assets/default/css/app.min.css?ver={{$rand_no}}" media="print" onload="this.onload=null;this.media='all';">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" media="print" onload="this.onload=null;this.media='all';">
+    <link rel="stylesheet" href="/assets/default/css/responsive.min.css?ver={{$rand_no}}" media="print" onload="this.onload=null;this.media='all';">
 
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
