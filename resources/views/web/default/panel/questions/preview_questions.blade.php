60d810fcc16fc049a1895248a65457e99b13030b
Baz <baz.chimpstudio@gmail.com>
2025-10-15 15:06:21 +0500

    preview
    preview

--- resources/views/web/default/panel/questions/preview_questions.blade.php
+++ resources/views/web/default/panel/questions/preview_questions.blade.php
@@ -124,7 +124,9 @@ $rand_id = rand(99,9999);
                                                                                                                                    height="20"
                                                                                                                                    class=""></i></div>
                                                                                                     <div class="lms-card-info">
-                                                                                                        <h5>{{$logObj->user->get_full_name()}} @ <b>{{ dateTimeFormat
+                                                                                                        <h5>
+                                                                                                            <span class="user-avatar"><img src="{{url('/').$logObj->user->getAvatar(40)}}" width="40"></span>
+                                                                                                            {{$logObj->user->get_full_name()}} @ <b>{{ dateTimeFormat
                                                         ($logObj->action_at, 'j M y | H:i')
                                                         }} <span><i data-feather="arrow-right" width="20" height="20"
                                                                     class=""></i></span>
@@ -220,7 +222,7 @@ $rand_id = rand(99,9999);
                                                                                                                                    class=""></i></div>
                                                                                                     <div class="lms-card-info">
                                                                                                         <h5>
-                                                                                                            <span class="user-avatar"><img src="{{url('/').$logObj->user->getAvatar(40)}}"></span>
+                                                                                                            <span class="user-avatar"><img src="{{url('/').$logObj->user->getAvatar(40)}}" width="40"></span>
                                                                                                             {{$logObj->user->get_full_name()}} @ <b>{{ dateTimeFormat
                                                         ($logObj->action_at, 'j M y | H:i')
                                                         }} <span><i data-feather="arrow-right" width="20" height="20"
