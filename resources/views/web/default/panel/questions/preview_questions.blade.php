fd6c7d4ea18ae5d2808eb5c48418ac6ce6d43ebf
Baz <baz.chimpstudio@gmail.com>
2025-10-15 14:46:12 +0500

    preview question
    preview question

--- resources/views/web/default/panel/questions/preview_questions.blade.php
+++ resources/views/web/default/panel/questions/preview_questions.blade.php
@@ -92,6 +92,63 @@ $rand_id = rand(99,9999);
                                                                                     </div>
                                                                                 </div>
                                                                             </div>
+                                                                            <div class="row">
+                                                                                @php $questionLogs = QuestionLogs::where('question_id', $id)->orderBy('id', 'desc')->with('user')
+                                                                                ->get();
+                                                                                @endphp
+                                                                                @if($questionLogs->count() > 0)
+                                                                                    <ul class="lms-card-timeline">
+
+                                                                                        @if( !empty( $questionLogs ))
+                                                                                            @foreach($questionLogs as $logObj)
+
+
+                                                                                                <li class="lms-card-list active">
+                                                                                                    <div class="lms-card-icons"><i data-feather="arrow-right-circle" width="20"
+                                                                                                                                   height="20"
+                                                                                                                                   class=""></i></div>
+                                                                                                    <div class="lms-card-info">
+                                                                                                        <h5>{{$logObj->user->get_full_name()}} @ <b>{{ dateTimeFormat
+                                                        ($logObj->action_at, 'j M y | H:i')
+                                                        }} <span><i data-feather="arrow-right" width="20" height="20"
+                                                                    class=""></i></span>
+                                                                                                        </h5>
+                                                                                                        <p>{{$logObj->action_type}}</p>
+                                                                                                        <p>{!! $logObj->log_data !!}</p>
+                                                                                                        @if($logObj->action_type == 'Status Updated - Published' &&
+                                                                                                        $logObj->action_role ==
+                                                                                                        'reviewer')
+                                                                                                            @php
+                                                                                                                $log_storred_data = json_decode($logObj->log_storred_data, true);
+                                                                                                                $log_storred_data['Difficulty Level']  = isset( $log_storred_data['Difficulty Level'] )? $log_storred_data['Difficulty Level'] : 0;
+
+                                                                                                                if(!empty($log_storred_data)){
+
+                                                                                                                $log_storred_data['Solution'] = $log_storred_data['Solution'].'('.$log_storred_data['Solution Label'].')';
+                                                                                                                $log_storred_data['Difficulty Level'] = $log_storred_data['Difficulty Level'].'('.$log_storred_data['Difficulty Level Label'].')';
+                                                                                                                unset($log_storred_data['Solution Label']);
+                                                                                                                unset($log_storred_data['Difficulty Level Label']);
+                                                                                                                unset($log_storred_data['status_details']);
+                                                                                                                $log_storred_data['Accepted'] = 20;
+                                                                                                                }
+
+                                                                                                            @endphp
+                                                                                                            @if( !empty( $log_storred_data ))
+                                                                                                                @foreach( $log_storred_data as $storred_dataObj_key =>
+                                                                                                                $storred_dataObj_value)
+                                                                                                                    <span>{{$storred_dataObj_key}}: {{$storred_dataObj_value}}</span><br>
+                                                                                                                @endforeach
+                                                                                                            @endif
+
+                                                                                                        @endif
+                                                                                                    </div>
+                                                                                                </li>
+
+                                                                                            @endforeach
+                                                                                        @endif
+                                                                                    </ul>
+                                                                                @endif
+                                                                            </div>
                                                                             <button type="submit" class="btn btn-primary">Submit</button>
                                                                         </div>
                                                                     </form>
