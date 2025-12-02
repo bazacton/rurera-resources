ab3642ba2db9370301cd611cf0c1dd29a4c4d47b
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-02 01:58:37 -0800

    Update question-layout.php

--- resources/views/admin/custom_quiz/question-layout.php
+++ resources/views/admin/custom_quiz/question-layout.php
@@ -43,7 +43,7 @@
         </div>
         <div class="section-body">
             <section class="card">
-                <div class="card-body">
+                <div class="card-body p-0">
                     <form action="{{ getAdminPanelUrl() }}/quizzes" method="get" class="row mb-0 skelton-hide">
                         <div class="col-md-2">
                             <div class="form-group skelton-hide skelton-height-lg">
@@ -161,7 +161,7 @@
             <div class="row">
                 <div class="col-12 col-md-12">
                     <div class="card">
-                        <div class="card-body">
+                        <div class="card-body p-0">
                             <div class="table-responsive">
                                 <table class="table table-striped font-14">
                                     <tr>
