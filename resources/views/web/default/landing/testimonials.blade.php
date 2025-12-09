8782e2a0974771a1c98dcf8f59246f492dfc661c
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-09 02:34:42 -0800

    Update testimonials.blade.php

--- resources/views/web/default/landing/testimonials.blade.php
+++ resources/views/web/default/landing/testimonials.blade.php
@@ -254,6 +254,14 @@
   });
 </script>
 <script>
+  // Initialize Masonry
+  var grid = document.querySelector('.grid');
+  var msnry = new Masonry(grid, {
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-item',
+    percentPosition: true
+  });
+
   // Filter buttons callback
   var filtersElem = document.getElementById('filters');
   filtersElem.addEventListener('click', function(event) {
