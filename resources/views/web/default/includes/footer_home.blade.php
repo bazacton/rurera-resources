3ea8afa7a57dc7a472ec75dc742249e0846a975e
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-08 23:44:56 -0800

    Update footer_home.blade.php

--- resources/views/web/default/includes/footer_home.blade.php
+++ resources/views/web/default/includes/footer_home.blade.php
@@ -28,6 +28,7 @@
                                 <li itemprop="blog"><a itemprop="url" href="/blog">Blog</a></li>
                                 <li itemprop="careers"><a itemprop="url" href="/careers">Careers <span class="hiring-label">Hiring!</span></a></li>
                                 <li itemprop="testimonials"><a itemprop="url" href="/testimonials">Testimonials</a></li>
+                                <li itemprop="contact-us"><a itemprop="url" href="/contact-us">Contact Us</a></li>
                             </ul>
                         </div>
                     </div>
