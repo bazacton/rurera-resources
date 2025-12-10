e7912b5a8dff98d5fb2c26a63cd82a8794b8ac30
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-10 00:03:58 -0800

    Update avatar_view.blade.php

--- resources/views/web/default/avatar_settings/avatar_view.blade.php
+++ resources/views/web/default/avatar_settings/avatar_view.blade.php
@@ -109,9 +109,9 @@
                 function setMobilePadding() {
                 if ($(window).width() <= 767) {
                     var adjacentHeight = $('.mobile-avar-view .svga-col-right').outerHeight();
-                    $('.svga-col-left').css('padding-top', adjacentHeight + 'px');
+                    $('.first-row').css('padding-top', adjacentHeight + 'px');
                 } else {
-                    $('.svga-col-left').css('padding-top', '');
+                    $('.first-row').css('padding-top', '');
                 }
             }
             setMobilePadding();
