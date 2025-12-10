81d8b1e59debcc57d2ed709830212acee42a9cb6
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-10 03:54:33 -0800

    Update student.blade.php

--- resources/views/web/default/panel/parent/student.blade.php
+++ resources/views/web/default/panel/parent/student.blade.php
@@ -398,7 +398,7 @@ $subscribe = isset( $user->userSubscriptions->subscribe)? $user->userSubscriptio
                         <form class="child-edit-form" method="post" action="javascript:;">
                             {{ csrf_field() }}
                             <div class="row">
-                                <div class="col-6 col-lg-6 col-md-6">
+                                <div class="col-12">
                                     <label>Test Prep School Choice</label>
                                     <div class="select-field">
                                         <select class="form-control rurera-req-field" name="test_prep_school">
@@ -409,20 +409,26 @@ $subscribe = isset( $user->userSubscriptions->subscribe)? $user->userSubscriptio
                                         </select>
                                     </div>
                                 </div>
-                                <div class="col-6 col-lg-6 col-md-6">
+                                <div class="col-12">
                                     <label>Preference 1</label>
                                     <div class="preference-field-group">
-                                        <div class="select-field">
-                                            <select class="form-control preference_field rurera-req-field" name="school_preference_1">
-                                                <option value="">Select Preference</option>
-                                                @foreach( $schools as $schoolObj)
-                                                    <option value="{{$schoolObj->id}}" {{($schoolObj->id == $user->school_preference_1)? 'selected' : ''}}>{{$schoolObj->title}}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                        <div class="input-field">
-                                            <span class="icon-box"><img src="/assets/default/svgs/edit-menu-user.svg" alt=""></span>
-                                            <input type="text" class="preference-date rureradatepicker" min="{{date('Y-m-d')}}" placeholder="Preference 1 Date" name="school_preference_1_date" value="{{($user->school_preference_1_date != '')? dateTimeFormatNumeric($user->school_preference_1_date, 'Y-m-d', 'numeric') : ''}}">
+                                        <div class="row">
+                                            <div class="col-6 col-lg-6 col-md-6">
+                                                <div class="select-field">
+                                                    <select class="form-control preference_field rurera-req-field" name="school_preference_1">
+                                                        <option value="">Select Preference</option>
+                                                        @foreach( $schools as $schoolObj)
+                                                            <option value="{{$schoolObj->id}}" {{($schoolObj->id == $user->school_preference_1)? 'selected' : ''}}>{{$schoolObj->title}}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-6 col-lg-6 col-md-6">
+                                                <div class="input-field">
+                                                    <span class="icon-box"><img src="/assets/default/svgs/edit-menu-user.svg" alt=""></span>
+                                                    <input type="text" class="preference-date rureradatepicker" min="{{date('Y-m-d')}}" placeholder="Preference 1 Date" name="school_preference_1_date" value="{{($user->school_preference_1_date != '')? dateTimeFormatNumeric($user->school_preference_1_date, 'Y-m-d', 'numeric') : ''}}">
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                     
@@ -439,17 +445,23 @@ $subscribe = isset( $user->userSubscriptions->subscribe)? $user->userSubscriptio
                                 <div class="col-6 col-lg-6 col-md-6">
                                     <label>Preference 2</label>
                                     <div class="preference-field-group">
-                                        <div class="select-field">
-                                            <select class="form-control preference_field" name="school_preference_2">
-                                                <option value="">Select Preference</option>
-                                                @foreach( $schools as $schoolObj)
-                                                    <option value="{{$schoolObj->id}}" {{($schoolObj->id == $user->school_preference_2)? 'selected' : ''}}>{{$schoolObj->title}}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                        <div class="input-field">
-                                            <span class="icon-box"><img src="/assets/default/svgs/edit-menu-user.svg" alt="edit-menu-user"></span>
-                                            <input type="text" class="preference-date rureradatepicker rurera-req-field" min="{{date('Y-m-d')}}" placeholder="Preference 2 Date" name="school_preference_2_date" value="{{($user->school_preference_2_date != '')? dateTimeFormatNumeric($user->school_preference_2_date, 'Y-m-d', 'numeric') : ''}}">
+                                        <div class="row">
+                                            <div class="col-6 col-lg-6 col-md-6">
+                                                <div class="select-field">
+                                                    <select class="form-control preference_field" name="school_preference_2">
+                                                        <option value="">Select Preference</option>
+                                                        @foreach( $schools as $schoolObj)
+                                                            <option value="{{$schoolObj->id}}" {{($schoolObj->id == $user->school_preference_2)? 'selected' : ''}}>{{$schoolObj->title}}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-6 col-lg-6 col-md-6">
+                                                <div class="input-field">
+                                                    <span class="icon-box"><img src="/assets/default/svgs/edit-menu-user.svg" alt="edit-menu-user"></span>
+                                                    <input type="text" class="preference-date rureradatepicker rurera-req-field" min="{{date('Y-m-d')}}" placeholder="Preference 2 Date" name="school_preference_2_date" value="{{($user->school_preference_2_date != '')? dateTimeFormatNumeric($user->school_preference_2_date, 'Y-m-d', 'numeric') : ''}}">
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                     
@@ -466,17 +478,23 @@ $subscribe = isset( $user->userSubscriptions->subscribe)? $user->userSubscriptio
                                 <div class="col-6 col-lg-6 col-md-6">
                                     <label>Preference 3</label>
                                     <div class="preference-field-group">
-                                        <div class="select-field">
-                                            <select class="form-control preference_field rurera-req-field" name="school_preference_3">
-                                                <option value="">Select Preference</option>
-                                                @foreach( $schools as $schoolObj)
-                                                    <option value="{{$schoolObj->id}}" {{($schoolObj->id == $user->school_preference_3)? 'selected' : ''}}>{{$schoolObj->title}}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                        <div class="input-field">
-                                            <span class="icon-box"><img src="/assets/default/svgs/edit-menu-user.svg" alt="edit-menu-user"></span>
-                                            <input type="text" class="preference-date rureradatepicker rurera-req-field" min="{{date('Y-m-d')}}" placeholder="Preference 3 Date" name="school_preference_3_date" value="{{($user->school_preference_3_date != '')? dateTimeFormatNumeric($user->school_preference_3_date, 'Y-m-d', 'numeric') : ''}}">
+                                        <div class="row">
+                                            <div class="col-6 col-lg-6 col-md-6">
+                                                <div class="select-field">
+                                                    <select class="form-control preference_field rurera-req-field" name="school_preference_3">
+                                                        <option value="">Select Preference</option>
+                                                        @foreach( $schools as $schoolObj)
+                                                            <option value="{{$schoolObj->id}}" {{($schoolObj->id == $user->school_preference_3)? 'selected' : ''}}>{{$schoolObj->title}}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-6 col-lg-6 col-md-6">
+                                                <div class="input-field">
+                                                    <span class="icon-box"><img src="/assets/default/svgs/edit-menu-user.svg" alt="edit-menu-user"></span>
+                                                    <input type="text" class="preference-date rureradatepicker rurera-req-field" min="{{date('Y-m-d')}}" placeholder="Preference 3 Date" name="school_preference_3_date" value="{{($user->school_preference_3_date != '')? dateTimeFormatNumeric($user->school_preference_3_date, 'Y-m-d', 'numeric') : ''}}">
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                     
