a6465448f56d92a95c718ee91eee8f584fa7110a
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-10 03:35:33 -0800

    finding file

--- resources/views/web/default/panel/parent/student.blade.php
+++ resources/views/web/default/panel/parent/student.blade.php
@@ -395,7 +395,7 @@ $subscribe = isset( $user->userSubscriptions->subscribe)? $user->userSubscriptio
                     </ul>
                     
                     <div class="edit-profile edit-profile-block mt-20 rurera-hide bg-white p-25">
-                        <form class="child-edit-form" method="post" action="javascript:;">
+                        <form class="child-edit-form test@@" method="post" action="javascript:;">
                             {{ csrf_field() }}
                             <div class="row">
                                 <div class="col-6 col-lg-6 col-md-6">
