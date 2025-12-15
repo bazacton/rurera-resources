bfa9aeced610db87be620dc2e621736975a464d2
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-15 03:25:38 -0800

    Update student.blade.php

--- resources/views/web/default/panel/parent/student.blade.php
+++ resources/views/web/default/panel/parent/student.blade.php
@@ -880,7 +880,7 @@ $subscribe = isset( $user->userSubscriptions->subscribe)? $user->userSubscriptio
                         </div>
                     @endif
                     <div class="subscription-controls d-flex align-items-center ml-auto">
-                        <a href="javascript:;" class="package-update-btn btn w-100 subscription-modal" data-type="update_package" data-id="{{$user->id}}">Update Subscription</a>
+                        <a href="javascript:;" class="package-update-btn btn subscription-modal" data-type="update_package" data-id="{{$user->id}}">Update Subscription</a>
                         @if(isset( $user->userSubscriptions->subscribe ) && $user->userSubscriptions->is_cancelled == 0 )
                         <a href="javascript:;" class="reset-btn package-payment-btn cancel-subscription-modal" data-type="child_payment" data-id="{{$user->id}}">
                             <span class="icon-box"><img src="/assets/default/svgs/retry.svg" alt="retry"></span>
