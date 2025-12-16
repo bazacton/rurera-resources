ee315ab39e2ffc19910a891b9f123bfbf86ffac5
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-16 05:26:53 -0800

    Update parent_profile.blade.php

--- resources/views/web/default/panel/setting/setting_includes/parent_profile.blade.php
+++ resources/views/web/default/panel/setting/setting_includes/parent_profile.blade.php
@@ -8,8 +8,8 @@ $avatar_color_settings = json_encode($avatar_color_settings);
 @endphp
 
 <section>
-    <h2 class="section-title font-24">Account Settings</h2>
-    <div class="row mt-15">
+    <h2 class="section-title font-22">Account Settings</h2>
+    <div class="row mt-10">
         <div class="col-12 user-profile-block">
             <div class="user-detail mb-30 bg-white panel-border rounded-sm">
                 <div class="detail-header mb-15">
