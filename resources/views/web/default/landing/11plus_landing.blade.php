8701220a6cd7598b326ef403e8e3fd8def98ba20
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-17 07:16:57 -0800

    Update 11plus_landing.blade.php

--- resources/views/web/default/landing/11plus_landing.blade.php
+++ resources/views/web/default/landing/11plus_landing.blade.php
@@ -553,18 +553,16 @@
                     </div>
                 </div>
                 <div class="col-12 col-sm-12 col-md-12 col-lg-12">
-                    <div itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question" class="accordion lms-accordion-modern"
-                        id="accordion-modern">
-                        <div class="card active" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+                    <div itemscope itemtype="https://schema.org/FAQPage" class="accordion lms-accordion-modern" id="accordion-modern">
+                        <div class="card active" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
                             <div class="card-header" id="heading-1">
-                                <button itemprop="button" class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse-1" aria-expanded="false" aria-controls="collapse-1">
+                                <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse-1" aria-expanded="false" aria-controls="collapse-1">
                                     <span itemprop="size">#1</span>Adaptive Assessments Testing
                                 </button>
                             </div>
-                            <div id="collapse-1" class="collapse show" aria-labelledby="heading-1"
-                                data-parent="#accordion-modern">
-                                <div class="card-body">
-                                    <p itemprop="description" class="font-16 font-weight-normal">
+                            <div id="collapse-1" class="collapse show" aria-labelledby="heading-1" data-parent="#accordion-modern">
+                                <div class="card-body" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                                    <p itemprop="text" class="font-16 font-weight-normal">
                                         It helps schools to offer adaptive assessments that adjust the difficulty of
                                         questions as below, emerging, expected, exceeding and challenge which is based
                                         on a student's previous responses, history,
@@ -578,18 +576,17 @@
                                         itemprop="image" loading="eager" /></figure>
                             </div>
                         </div>
-                        <div class="card" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+                        <div class="card" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
                             <div class="card-header" id="heading-2">
-                                <button itemprop="button" class="btn btn-link btn-block text-left collapsed"
+                                <button class="btn btn-link btn-block text-left collapsed"
                                     type="button" data-toggle="collapse" data-target="#collapse-2" aria-expanded="false"
                                     aria-controls="collapse-2">
                                     <span itemprop="size">#2</span>Quick Results via automated marking
                                 </button>
                             </div>
-                            <div id="collapse-2" class="collapse" aria-labelledby="heading-2"
-                                data-parent="#accordion-modern">
-                                <div class="card-body">
-                                    <p itemprop="description" class="font-16 font-weight-normal">
+                            <div id="collapse-2" class="collapse" aria-labelledby="heading-2" data-parent="#accordion-modern">
+                                <div class="card-body" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                                    <p itemprop="text" class="font-16 font-weight-normal">
                                         Real-time marking data allows for quick identification of students who may
                                         require additional support or challenges. Students receive instant personalized
                                         feedback for every question and learning nugget.
@@ -602,18 +599,17 @@
                                         itemprop="image" loading="eager" /></figure>
                             </div>
                         </div>
-                        <div class="card" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+                        <div class="card" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
                             <div class="card-header" id="heading-3">
-                                <button itemprop="button" class="btn btn-link btn-block text-left collapsed"
+                                <button class="btn btn-link btn-block text-left collapsed"
                                     type="button" data-toggle="collapse" data-target="#collapse-3" aria-expanded="false"
                                     aria-controls="collapse-3">
                                     <span itemprop="size">#3</span>Diverse Question Formats
                                 </button>
                             </div>
-                            <div id="collapse-3" class="collapse" aria-labelledby="heading-3"
-                                data-parent="#accordion-modern">
-                                <div class="card-body">
-                                    <p itemprop="description" class="font-16 font-weight-normal">
+                            <div id="collapse-3" class="collapse" aria-labelledby="heading-3" data-parent="#accordion-modern">
+                                <div class="card-body" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                                    <p itemprop="text" class="font-16 font-weight-normal">
                                         Online assessments can include a variety of question types, from multiple choice
                                         to audio types offering a more comprehensive evaluation of student knowledge.
                                     </p>
@@ -625,18 +621,17 @@
                                         itemprop="image" loading="eager" /></figure>
                             </div>
                         </div>
-                        <div class="card" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+                        <div class="card" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
                             <div class="card-header" id="heading-4">
-                                <button itemprop="button" class="btn btn-link btn-block text-left collapsed"
+                                <button class="btn btn-link btn-block text-left collapsed"
                                     type="button" data-toggle="collapse" data-target="#collapse-4" aria-expanded="false"
                                     aria-controls="collapse-4">
                                     <span itemprop="size">#4</span>Timely Reporting
                                 </button>
                             </div>
-                            <div id="collapse-4" class="collapse" aria-labelledby="heading-4"
-                                data-parent="#accordion-modern">
-                                <div class="card-body">
-                                    <p itemprop="description" class="font-16 font-weight-normal">Online platforms often
+                            <div id="collapse-4" class="collapse" aria-labelledby="heading-4" data-parent="#accordion-modern">
+                                <div class="card-body" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                                    <p itemprop="text" class="font-16 font-weight-normal">Online platforms often
                                         provide real-time reporting to both students and teachers, allowing for prompt
                                         interventions and support.</p>
                                 </div>
@@ -647,18 +642,17 @@
                                         itemprop="image" loading="eager" /></figure>
                             </div>
                         </div>
-                        <div class="card" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+                        <div class="card" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
                             <div class="card-header" id="heading-5">
-                                <button itemprop="button" class="btn btn-link btn-block text-left collapsed"
+                                <button class="btn btn-link btn-block text-left collapsed"
                                     type="button" data-toggle="collapse" data-target="#collapse-5" aria-expanded="false"
                                     aria-controls="collapse-5">
                                     <span itemprop="size">#5</span>Breakthrough insights at every level
                                 </button>
                             </div>
-                            <div id="collapse-5" class="collapse" aria-labelledby="heading-5"
-                                data-parent="#accordion-modern">
-                                <div class="card-body">
-                                    <p itemprop="description" class="font-16 font-weight-normal">
+                            <div id="collapse-5" class="collapse" aria-labelledby="heading-5" data-parent="#accordion-modern">
+                                <div class="card-body" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                                    <p itemprop="text" class="font-16 font-weight-normal">
                                         Students and their parents can access a dashboard that displays their live
                                         progress in various subjects. It allows them to identify their strengths and
                                         areas needing improvement. Parents can monitor
@@ -672,18 +666,17 @@
                                         itemprop="image" loading="eager" /></figure>
                             </div>
                         </div>
-                        <div class="card" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+                        <div class="card" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
                             <div class="card-header" id="heading-6">
-                                <button itemprop="button" class="btn btn-link btn-block text-left collapsed"
+                                <button class="btn btn-link btn-block text-left collapsed"
                                     type="button" data-toggle="collapse" data-target="#collapse-6" aria-expanded="true"
                                     aria-controls="collapse-6">
                                     <span itemprop="size">#6</span>Students Engagement
                                 </button>
                             </div>
-                            <div id="collapse-6" class="collapse" aria-labelledby="heading-6"
-                                data-parent="#accordion-modern">
-                                <div class="card-body">
-                                    <p itemprop="description" class="font-16 font-weight-normal">
+                            <div id="collapse-6" class="collapse" aria-labelledby="heading-6" data-parent="#accordion-modern">
+                                <div class="card-body" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                                    <p itemprop="text" class="font-16 font-weight-normal">
                                         Ignite student’s passion more to join your school. A fantastic learning
                                         experience is possible using interactive challenges, Online Test practices,
                                         Entrance Exams preparations and rewarding them with
@@ -697,18 +690,17 @@
                                         itemprop="image" loading="eager" /></figure>
                             </div>
                         </div>
-                        <div class="card" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+                        <div class="card" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
                             <div class="card-header" id="heading-7">
-                                <button itemprop="button" class="btn btn-link btn-block text-left collapsed"
+                                <button class="btn btn-link btn-block text-left collapsed"
                                     type="button" data-toggle="collapse" data-target="#collapse-7" aria-expanded="true"
                                     aria-controls="collapse-7">
                                     <span itemprop="size">#7</span>Reward points
                                 </button>
                             </div>
-                            <div id="collapse-7" class="collapse" aria-labelledby="heading-7"
-                                data-parent="#accordion-modern">
-                                <div class="card-body">
-                                    <p itemprop="description" class="font-16 font-weight-normal">
+                            <div id="collapse-7" class="collapse" aria-labelledby="heading-7" data-parent="#accordion-modern">
+                                <div class="card-body" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                                    <p itemprop="text" class="font-16 font-weight-normal">
                                         Unlock Knowledge and Reward Yourself with Exciting Toys. It implies through
                                         continuous learning and improvement, students can increase their chances of
                                         winning playful toys. We believe in recognizing and
