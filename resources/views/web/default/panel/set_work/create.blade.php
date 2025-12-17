cd6851f4d3cb1cdf17e00ef740dbafc185828c5b
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-16 23:08:15 -0800

    Update create.blade.php

--- resources/views/web/default/panel/set_work/create.blade.php
+++ resources/views/web/default/panel/set_work/create.blade.php
@@ -1170,10 +1170,11 @@
 <div class="modal fade lms-choose-membership" id="subscriptionModal" tabindex="-1" aria-labelledby="subscriptionModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="panel-header">
+            <!-- <div class="panel-header">
                 <div class="modal-logo"><img src="/assets/default/img/sidebar/logo.svg" alt="Rurera Logo" width="150" height="38"></div>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">Back to Dashboard <span aria-hidden="true">×</span></button>
-            </div>
+            </div> -->
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
             <div class="modal-body">
                 <div class="container container-nosidebar">
                     <div class="tab-content subscription-content" id="nav-tabContent"></div>
