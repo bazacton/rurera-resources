17c3ad3cfb941b1fb08d5c3c40b10fc440dc7767
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-22 23:03:38 -0800

    Update list_item.blade.php

--- resources/views/web/default/panel/set_work/list_item.blade.php
+++ resources/views/web/default/panel/set_work/list_item.blade.php
@@ -1,14 +1,14 @@
 <div class="list-group-item">
     <div class="table-sm">
         <table class="students-table">
-            <thead>
+            <thead class="rurera-hide">
                 <tr>
                     <th><h6 class="font-16 font-weight-500">Title</h6></th>
-                    <th><h6 class="listing-title font-16 font-weight-500">Student</h6></th>
-                    <th><h6 class="listing-title font-16 font-weight-500">Type</h6></th>
-                    <th><h6 class="listing-title font-16 font-weight-500">Due Date</h6></th>
-                    <th><h6 class="listing-title font-16 font-weight-500">Completion Date</h6></th>
-                    <th><h6 class="listing-title font-16 font-weight-500">Action</h6></th>
+                    <th><h6 class="font-16 font-weight-500">Student</h6></th>
+                    <th><h6 class="font-16 font-weight-500">Type</h6></th>
+                    <th><h6 class="font-16 font-weight-500">Due Date</h6></th>
+                    <th><h6 class="font-16 font-weight-500">Completion Date</h6></th>
+                    <th><h6 class="font-16 font-weight-500">Action</h6></th>
                 </tr>
             </thead>
             <tbody>
