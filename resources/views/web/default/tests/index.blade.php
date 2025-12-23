181c5236eeb3fa4cddd0a5b98ff391677c3c0046
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-22 22:51:25 -0800

    Update index.blade.php

--- resources/views/web/default/tests/index.blade.php
+++ resources/views/web/default/tests/index.blade.php
@@ -15,7 +15,7 @@
                 <div class="col-12">
                     @if( !empty( $childs ) )
                         <div class="p-15 mt-20 p-lg-20 db-form-tabs panel-border font-weight-500 text-dark-blue rounded-sm panel-shadow mb-20 switch-user-block">
-                            <div class="list-group-item ttt">
+                            <div class="list-group-item">
                                 <div class="row align-items-center">
                                     <div class="col-auto">
                                         <a href="javascript:;" class="avatar"><img src="{{$switchUserObj->getAvatar()}}" alt="{{isset( $switchUserObj->id )? $switchUserObj->get_full_name() : ''}}" class="avatar rounded-circle"></a>
@@ -111,7 +111,7 @@
                     <div class="p-15 db-form-tabs mt-20 p-lg-20 font-weight-500 text-dark-blue rounded-sm panel-shadow mb-20 switch-user-block">
 
                         @foreach( $childs as $childObj)
-                        <div class="list-group-item">
+                        <div class="list-group-item tttt">
                             <div class="row align-items-center">
                                 <div class="col-auto">
                                     <a href="javascript:;" class="avatar"><img src="{{$childObj->getAvatar()}}" alt="{{isset( $childObj->id )? $childObj->get_full_name() : ''}}" class="avatar rounded-circle"></a>
