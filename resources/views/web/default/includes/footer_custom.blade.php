5a15addf88438f423a65d5fe312e0f4ac270adb1
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2025-12-23 23:55:37 -0800

    Update footer_custom.blade.php

--- resources/views/web/default/includes/footer_custom.blade.php
+++ resources/views/web/default/includes/footer_custom.blade.php
@@ -56,7 +56,7 @@
 					</div>
 					<div class="footer-logo-box">
 						<img src="/store/1/logo.png" width="185" height="38" alt="footer logo image">
-						<a href="/contact-us"><span>Schedule Consultation</span></a>
+						<a href="/contact-us" class="mt-20"><span>Schedule Consultation</span></a>
 					</div>
 				</div>
 			</div>
