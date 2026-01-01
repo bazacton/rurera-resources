12b50069218bdbac459e491913756961efb1788a
Baz <baz.chimpstudio@gmail.com>
2026-01-01 18:32:27 +0500

    cloud layout
    cloud layout

--- resources/views/web/default/question_layouts/label_layout.blade.php
+++ resources/views/web/default/question_layouts/label_layout.blade.php
@@ -3,8 +3,14 @@
 	@if($label_type == 'h1' || $label_type == 'h2' || $label_type == 'h3' || $label_type == 'h4' || $label_type == 'h5' || $label_type == 'h6')
 		<{{$label_type}}>{!! $elementObj->content !!}</{{$label_type}}>
 	@elseif($label_type == 'cloud_text')
-        @php pre($elementObj, false); @endphp
-        <div class="question-label {{$label_type}} medium left_top"><span class="cloud_text_element"><span class="cloud_avatar"><img src="/assets/default/svgs/cloud-kids-avatars/boy-svg-2.svg"></span><span class="cloud_nner_text" style="background:white"><svgdata class="rurera-svg-data0_6684">{!! $elementObj->content !!}</svgdata></span></span></div>
+        @php
+            $cloud_direction = isset($elementObj->cloud_direction)? $elementObj->cloud_direction : 'top_right';
+            $cloud_color = isset($elementObj->cloud_color)? $elementObj->cloud_color : '#000000';
+            $cloud_size = isset($elementObj->cloud_size)? $elementObj->cloud_size : 'small';
+            $cloud_avatar = isset($elementObj->cloud_avatar)? $elementObj->cloud_avatar : 'boy-svg-1.svg';
+            $cloud_avatar = '/assets/default/svgs/cloud-kids-avatars/'.$cloud_avatar;
+            @endphp
+        <div class="question-label {{$label_type}} {{$cloud_size}} {{$cloud_direction}}"><span class="cloud_text_element"><span class="cloud_avatar"><img src="/assets/default/svgs/cloud-kids-avatars/boy-svg-2.svg"></span><span class="cloud_nner_text" style="background:white"><svgdata class="rurera-svg-data0_6684">{!! $elementObj->content !!}</svgdata></span></span></div>
     @else
 		<div class="question-label {{$label_type}}"><span>{!! $elementObj->content !!}</span></div>
 	@endif
