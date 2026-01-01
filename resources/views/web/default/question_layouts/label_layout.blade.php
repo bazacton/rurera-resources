cf92965b10584aa4e19e2f9f7521cc1b7eb87253
Baz <baz.chimpstudio@gmail.com>
2026-01-01 18:27:44 +0500

    cloud text layout
    cloud text layout

--- resources/views/web/default/question_layouts/label_layout.blade.php
+++ resources/views/web/default/question_layouts/label_layout.blade.php
@@ -2,7 +2,9 @@
 <div id="rureraform-element-{{$element_id}}" class="rureraform-element-{{$element_id}} rureraform-element quiz-group rureraform-element-html ui-sortable-handle" data-type="{{$elementObj->type}}">
 	@if($label_type == 'h1' || $label_type == 'h2' || $label_type == 'h3' || $label_type == 'h4' || $label_type == 'h5' || $label_type == 'h6')
 		<{{$label_type}}>{!! $elementObj->content !!}</{{$label_type}}>
-	@else
+	@elseif($label_type == 'cloud_text')
+        <div class="question-label {{$label_type}} medium left_top"><span class="cloud_text_element"><span class="cloud_avatar"><img src="/assets/default/svgs/cloud-kids-avatars/boy-svg-2.svg"></span><span class="cloud_nner_text" style="background:white"><svgdata class="rurera-svg-data0_6684">{!! $elementObj->content !!}</svgdata></span></span></div>
+    @else
 		<div class="question-label {{$label_type}}"><span>{!! $elementObj->content !!}</span></div>
 	@endif
 </div>
