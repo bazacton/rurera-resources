aaadb01c281cdef627a0f8718d75fdb56d066a0b
Baz <baz.chimpstudio@gmail.com>
2026-01-01 18:28:43 +0500

    cloud layout
    cloud layout

--- resources/views/web/default/question_layouts/label_layout.blade.php
+++ resources/views/web/default/question_layouts/label_layout.blade.php
@@ -3,6 +3,7 @@
 	@if($label_type == 'h1' || $label_type == 'h2' || $label_type == 'h3' || $label_type == 'h4' || $label_type == 'h5' || $label_type == 'h6')
 		<{{$label_type}}>{!! $elementObj->content !!}</{{$label_type}}>
 	@elseif($label_type == 'cloud_text')
+        @php pre($elementObj, false); @endphp
         <div class="question-label {{$label_type}} medium left_top"><span class="cloud_text_element"><span class="cloud_avatar"><img src="/assets/default/svgs/cloud-kids-avatars/boy-svg-2.svg"></span><span class="cloud_nner_text" style="background:white"><svgdata class="rurera-svg-data0_6684">{!! $elementObj->content !!}</svgdata></span></span></div>
     @else
 		<div class="question-label {{$label_type}}"><span>{!! $elementObj->content !!}</span></div>
