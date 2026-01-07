21a25729586799dac81c54b5a79afa9ae030516a
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-06 21:43:25 -0800

    finding file

--- resources/views/web/default/panel/quizzes/enterance_exams_start.blade.php
+++ resources/views/web/default/panel/quizzes/enterance_exams_start.blade.php
@@ -91,8 +91,22 @@ $incorrect_answer_explaination = 1;//isset($incorrect_answer_explaination)? $inc
                         </div>
                     </div>
                     <div data-total_questions="{{count($questions_layout)}}" class="question-area-block" data-active_question_id="{{$active_question_id}}" data-questions_layout="{{json_encode($questions_layout)}}">
+                        <div class="quiz-time-bar">
+                            <div class="timer-wrap">
+                                <span class="time-label"><img src="/assets/default/svgs/time-past.svg" alt="time-past"> Time left:</span>
+
+                                <div class="time-box" id="hh">00</div>
+                                <span class="colon">:</span>
+                                <div class="time-box" id="mm">05</div>
+                                <span class="colon">:</span>
+                                <div class="time-box" id="ss">15</div>
+                            </div>
+                            <span class="coin-numbers">
+                                <img src="/assets/default/img/quests-coin.png" alt="quests-coin">
+                                <span class="total-earned-coins">1</span>
+                            </span>
+                        </div>
                         <div class="show-hide-statusbar mb-30">
-                            <button class="status-bar-on-off collapsed" type="button" data-toggle="collapse" data-target="#statusbar-data" aria-expanded="false" aria-controls="statusbar-data"><span class="rurera-hide">Hide</span> <span class="show-label">Show</span> Status</button>
                             <div class="collapse" id="statusbar-data">
                                 <div class="card card-body p-0 border-0">
                                     <div class="quiz-status-bar">
@@ -112,23 +126,6 @@ $incorrect_answer_explaination = 1;//isset($incorrect_answer_explaination)? $inc
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        
-                        
-                        <div class="quiz-time-bar">
-                            <div class="timer-wrap">
-                            <span class="time-label"><img src="/assets/default/svgs/time-past.svg" alt="time-past"> Time left:</span>
-
-                            <div class="time-box" id="hh">00</div>
-                            <span class="colon">:</span>
-                            <div class="time-box" id="mm">05</div>
-                            <span class="colon">:</span>
-                            <div class="time-box" id="ss">15</div>
-                            </div>
-                            <span class="coin-numbers">
-                                <img src="/assets/default/img/quests-coin.png" alt="quests-coin">
-                                <span class="total-earned-coins">1</span>
-                            </span>
                         </div>
                         <div class="question-counts">
                             <span class="icon-box"><img src="/assets/default/svgs/question-simple.svg" alt="question-simple"></span>
