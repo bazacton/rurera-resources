b63e501f710d7f419061b2f10aa5ce0dd175e52d
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-07 23:43:18 -0800

    Update appstart.blade.php

--- resources/views/web/default/layouts/appstart.blade.php
+++ resources/views/web/default/layouts/appstart.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ app()->getLocale() }}" itemscope itemtype="https://schema.org/FAQPage">
 
 @php
     $rtlLanguages = !empty($generalSettings['rtl_languages']) ? $generalSettings['rtl_languages'] : [];
