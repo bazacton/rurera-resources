f984e1d9ab2edbf1f23be2787141c14412e4772a
Baz <baz.chimpstudio@gmail.com>
2026-01-08 18:34:37 +0500

    draggable
    draggable

--- resources/views/web/default/question_layouts/draggable_quiz_layout.blade.php
+++ resources/views/web/default/question_layouts/draggable_quiz_layout.blade.php
@@ -4,7 +4,11 @@ $has_image_class = ($hasImage == 1)? 'lms-radio-img' : '';
 @php $randomID = rand(0,9999); @endphp
 
 
-
+<style>
+    .active-option {
+        background: #efefef !important;
+    }
+</style>
 <div id="rureraform-element-{{$element_id}}" class="quiz-group draggable3 rureraform-element-{{$element_id}} rureraform-element rureraform-element-label-undefined rureraform-element-description-undefined ui-sortable-handle" data-type="draggable_quiz">
 
 
@@ -37,5 +41,21 @@ $has_image_class = ($hasImage == 1)? 'lms-radio-img' : '';
     });
     $(document).on("dblclick", ".droppable-field-{{$element_id}}", function () {
         $(this).val('');
+    });
+
+    $(document).on("click", ".draggable-option-{{$element_id}}", function () {
+        selectedOptionText = $.trim($(this).text());
+
+        $('.draggable-option-{{$element_id}}').removeClass("active-option");
+        // Optional visual feedback
+        $(".draggable-option").removeClass("active-option");
+        $(this).addClass("active-option");
+    });
+
+    $(document).on("click", ".droppable-field-{{$element_id}}", function () {
+        if (!selectedOptionText) return;
+
+        $('.draggable-option-{{$element_id}}').removeClass("active-option");
+        $(this).val(selectedOptionText);
     });
 </script>
