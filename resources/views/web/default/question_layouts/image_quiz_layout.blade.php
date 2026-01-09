7b383224ae3dc60706b2a47b101a296dea8e0410
Baz <baz.chimpstudio@gmail.com>
2026-01-09 12:51:52 +0500

    checkbox and image remove extra divs
    checkbox and image remove extra divs

--- resources/views/web/default/question_layouts/image_quiz_layout.blade.php
+++ resources/views/web/default/question_layouts/image_quiz_layout.blade.php
@@ -1,3 +1,1 @@
-<div id="rureraform-element-{{$element_id}}" class="{{ isset( $elementObj->image_position ) ? $elementObj->image_position : ''}} {{ isset( $elementObj->image_size ) ? $elementObj->image_size : ''}} rureraform-element-{{$element_id}} rureraform-element quiz-group rureraform-element-html ui-sortable-handle" data-type="{{$elementObj->type}}">
-	<span class="block-holder image-field"><img data-field_type="image" data-id="{{$element_unique_id}}" id="field-{{$element_unique_id}}" class="image-editor-field" src="{{ isset( $elementObj->content ) ? $elementObj->content : ''}}"></span>
-</div>
+<span class="block-holder image-field"><img data-field_type="image" data-id="{{$element_unique_id}}" id="field-{{$element_unique_id}}" class="image-editor-field" src="{{ isset( $elementObj->content ) ? $elementObj->content : ''}}"></span>
