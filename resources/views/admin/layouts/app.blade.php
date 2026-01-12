fcd13d7486c3bd757ef2a940d2a92482cfc201f9
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-11 23:02:03 -0800

    Update app.blade.php

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -70,7 +70,7 @@
             @endif
 
             <div class="main-content">
-                <div class="container">
+                <div class="container mx-0">
                     <div class="row">
                         <div class="col-12">
                             @yield('content')
