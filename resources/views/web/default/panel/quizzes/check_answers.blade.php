c1d3981ac69822f731a8a61a194a984ae30d675d
Baz <baz.chimpstudio@gmail.com>
2026-01-13 15:23:02 +0500

    lotee test
    lotee test

--- resources/views/web/default/panel/quizzes/check_answers.blade.php
+++ resources/views/web/default/panel/quizzes/check_answers.blade.php
@@ -19,7 +19,7 @@ $rand_id = rand(99,9999);
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="/assets/vendors/flipbook/js/flipbook.min.js"></script>
 <link rel="stylesheet" href="/assets/default/vendors/swiper/swiper-bundle.min.css" media="print" onload="this.onload=null;this.media='all';">
-<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+
 <style>
 .field-holder.correct, .form-field.correct, .form-field.correct label {
     background: #d7fbdf !important;
@@ -73,7 +73,7 @@ $total_questions = is_array( $questions_list )? count($questions_list): 0;
                 <div class="chart-summary-fields result-layout-summary">
                     
                     <lottie-player
-                    src="assets/default/lottie/pigeon.json"
+                    src="/assets/default/lottie/pigeon.json"
                     style="width:200px;height:200px"
                     loop
                     autoplay>
