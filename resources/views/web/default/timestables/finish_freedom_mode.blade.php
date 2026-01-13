b0b70788a4b9e75706ea649f3fade972be314b14
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-13 05:06:16 -0800

    Update finish_freedom_mode.blade.php

--- resources/views/web/default/timestables/finish_freedom_mode.blade.php
+++ resources/views/web/default/timestables/finish_freedom_mode.blade.php
@@ -122,6 +122,66 @@
                     </div>
                 </div>
             </li>
+            <li>
+                <div class="quests-item">
+                    <div class="icon-box">
+                        <img src="/assets/default/img/types/timestables.svg" alt="learning image" width="50" height="50">
+                    </div>
+                    <div class="item-text">
+                        <h5 class="font-18 font-weight-bold">Play times table Power-Up Mode for 1 minute.</h5>
+                        <div class="levels-progress horizontal">
+                            <span class="progress-box">
+                                <span class="progress-count" style="width: 0%;"></span>
+                            </span>
+                            <span class="progress-numbers">0%</span>
+                        </div>
+                        <span class="progress-icon font-16">
+                            <img src="/assets/default/img/quests-coin.png" alt="quests-coin" width="35" height="35">
+                            +100
+                        </span>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="quests-item">
+                    <div class="icon-box">
+                        <img src="/assets/default/img/sidebar/refer_friend.svg" alt="learning image" width="50" height="50">
+                    </div>
+                    <div class="item-text">
+                        <h5 class="font-18 font-weight-bold">Invite 1 friend to get X600&nbsp;Coins</h5>
+                        <div class="levels-progress horizontal">
+                            <span class="progress-box">
+                                <span class="progress-count" style="width: 0%;"></span>
+                            </span>
+                            <span class="progress-numbers">0%</span>
+                        </div>
+                        <span class="progress-icon font-16">
+                            <img src="/assets/default/img/quests-coin.png" alt="quests-coin" width="35" height="35">
+                            +100
+                        </span>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="quests-item">
+                    <div class="icon-box">
+                        <img src="/assets/default/img/types/special_quest.svg" alt="learning image" width="50" height="50">
+                    </div>
+                    <div class="item-text">
+                        <h5 class="font-18 font-weight-bold">Rate Rurera on play store to get X500&nbsp;Coins</h5>
+                        <div class="levels-progress horizontal">
+                            <span class="progress-box">
+                                <span class="progress-count" style="width: 0%;"></span>
+                            </span>
+                            <span class="progress-numbers">0%</span>
+                        </div>
+                        <span class="progress-icon font-16">
+                            <img src="/assets/default/img/quests-coin.png" alt="quests-coin" width="35" height="35">
+                            +100
+                        </span>
+                    </div>
+                </div>
+            </li>
         </ul>
     </div>
     <div class="prev-next-controls text-center mb-50 questions-nav-controls">
