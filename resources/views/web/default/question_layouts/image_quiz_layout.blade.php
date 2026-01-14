7ac724ce380823bf822f91433151860b46e67028
Baz <baz.chimpstudio@gmail.com>
2026-01-15 02:34:43 +0500

    images element classes
    images element classes

--- resources/views/web/default/question_layouts/image_quiz_layout.blade.php
+++ resources/views/web/default/question_layouts/image_quiz_layout.blade.php
@@ -1,3 +1,7 @@
+@php $image_size = isset( $elementObj->image_size ) ? $elementObj->image_size : 'image-small';
+ $image_position = isset( $elementObj->image_position ) ? $elementObj->image_position : 'image-left';
+ @endphp
+
 <div class="image-element">
-<span class="block-holder image-field"><img data-field_type="image" data-id="{{$element_unique_id}}" id="field-{{$element_unique_id}}" class="image-editor-field" src="{{ isset( $elementObj->content ) ? $elementObj->content : ''}}"></span>
+<span class="block-holder image-field {{$image_size}} {{$image_position}}"><img data-field_type="image" data-id="{{$element_unique_id}}" id="field-{{$element_unique_id}}" class="image-editor-field" src="{{ isset( $elementObj->content ) ? $elementObj->content : ''}}"></span>
 </div>
