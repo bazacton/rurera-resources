9a4ff5a9b828eb3ac472e0fd375b8dfd19f93f33
Baz <baz.chimpstudio@gmail.com>
2026-01-14 13:50:00 +0500

    quiz start page files
    quiz start page files

--- resources/views/web/default/layouts/appstart.blade.php
+++ resources/views/web/default/layouts/appstart.blade.php
@@ -13,6 +13,8 @@ $rand_no = rand(99,9999);
     <title>{{ $page_title ?? '' }}{{ !empty($generalSettings['site_name']) ? (' | '.$generalSettings['site_name']) : '' }}</title>
 
     <!-- General CSS File -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" type="text/javascript"></script>
 
     <link rel="stylesheet" href="/assets/admin/vendor/bootstrap/bootstrap.min.css"/>
     <link rel="stylesheet" href="/assets/default/css/common.min.css?ver={{$rand_no}}">
