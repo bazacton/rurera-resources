ab8f42d159defc37178e3c559ea6cc35a0d3daf6
Baz <baz.chimpstudio@gmail.com>
2026-01-14 13:49:10 +0500

    start page files
    start page files

--- resources/views/web/default/layouts/appstart.blade.php
+++ resources/views/web/default/layouts/appstart.blade.php
@@ -13,8 +13,6 @@ $rand_no = rand(99,9999);
     <title>{{ $page_title ?? '' }}{{ !empty($generalSettings['site_name']) ? (' | '.$generalSettings['site_name']) : '' }}</title>
 
     <!-- General CSS File -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" type="text/javascript"></script>
 
     <link rel="stylesheet" href="/assets/admin/vendor/bootstrap/bootstrap.min.css"/>
     <link rel="stylesheet" href="/assets/default/css/common.min.css?ver={{$rand_no}}">
@@ -29,14 +27,6 @@ $rand_no = rand(99,9999);
     @stack('styles_top')
     @stack('scripts_top')
 
-    <style>
-        {!! !empty(getCustomCssAndJs('css')) ? getCustomCssAndJs('css') : '' !!}
-
-        {!! getThemeFontsSettings() !!}
-
-        {!! getThemeColorsSettings() !!}
-    </style>
-	
 	<style>
 	label.frontend-field-error-label {
 		border-color: #f95555;
