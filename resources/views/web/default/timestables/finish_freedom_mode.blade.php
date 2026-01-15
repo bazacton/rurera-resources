08471bda9c074269458351d4b2070f62b7b44de1
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-15 12:14:14 -0800

    Update finish_freedom_mode.blade.php

--- resources/views/web/default/timestables/finish_freedom_mode.blade.php
+++ resources/views/web/default/timestables/finish_freedom_mode.blade.php
@@ -118,7 +118,7 @@
                         <img src="/assets/default/img/types/practice.svg" alt="learning image" width="50" height="50">
                     </div>
                     <div class="item-text">
-                        <h5 class="font-18 font-weight-bold">90% Completion in 2 lessons</h5>
+                        <h5 class="font-16 font-weight-bold text-dark-charcoal">90% Completion in 2 lessons</h5>
                         <div class="levels-progress horizontal">
                             <span class="progress-box">
                                 <span class="progress-count" data-progress="50"><span class="progress-numbers">0%</span></span>
@@ -139,7 +139,7 @@
                         <img src="/assets/default/img/types/timestables.svg" alt="learning image" width="50" height="50">
                     </div>
                     <div class="item-text">
-                        <h5 class="font-18 font-weight-bold">Play times table Power-Up Mode for 1 minute.</h5>
+                        <h5 class="font-16 font-weight-bold text-dark-charcoal">Play times table Power-Up Mode for 1 minute.</h5>
                         <div class="levels-progress horizontal">
                             <span class="progress-box">
                                 <span class="progress-count" data-progress="70"><span class="progress-numbers">0%</span></span>
@@ -160,7 +160,7 @@
                         <img src="/assets/default/img/sidebar/refer_friend.svg" alt="learning image" width="50" height="50">
                     </div>
                     <div class="item-text">
-                        <h5 class="font-18 font-weight-bold">Invite 1 friend to get X600&nbsp;Coins</h5>
+                        <h5 class="font-16 font-weight-bold text-dark-charcoal">Invite 1 friend to get X600&nbsp;Coins</h5>
                         <div class="levels-progress horizontal">
                             <span class="progress-box">
                                 <span class="progress-count" data-progress="80"><span class="progress-numbers">0%</span></span>
@@ -181,7 +181,7 @@
                         <img src="/assets/default/img/types/special_quest.svg" alt="learning image" width="50" height="50">
                     </div>
                     <div class="item-text">
-                        <h5 class="font-18 font-weight-bold">Rate Rurera on play store to get X500&nbsp;Coins</h5>
+                        <h5 class="font-16 font-weight-bold text-dark-charcoal">Rate Rurera on play store to get X500&nbsp;Coins</h5>
                         <div class="levels-progress horizontal">
                             <span class="progress-box">
                                 <span class="progress-count" data-progress="90"><span class="progress-numbers">0%</span></span>
