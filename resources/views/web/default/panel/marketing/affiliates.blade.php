8bcc3c072b1a0934432e03c9ebd2471fce5a83aa
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-15 12:29:23 -0800

    Update affiliates.blade.php

--- resources/views/web/default/panel/marketing/affiliates.blade.php
+++ resources/views/web/default/panel/marketing/affiliates.blade.php
@@ -86,8 +86,8 @@
     </section>
 
     <section class="mt-25">
-        <h2 class="section-title">{{ trans('panel.earnings') }}</h2>
-        <div class="panel-section-card rounded-sm mt-20">
+        <h2 class="section-title font-22">{{ trans('panel.earnings') }}</h2>
+        <div class="panel-section-card rounded-sm mt-15">
             <div class="row">
                 <div class="col-12 ">
                     <div class="table-responsive transactions-table">
@@ -104,7 +104,7 @@
                             @if($affiliates->count() > 0)
                             @foreach($affiliates as $affiliate)
                                 <tr>
-                                    <td class="text-left">
+                                    <td class="text-left font-14">
                                         <div class="user-inline-avatar d-flex align-items-center">
                                             <div class="avatar bg-gray200">
                                                 <img src="{{ $affiliate->referredUser->getAvatar() }}" class="img-cover" alt="{{ $affiliate->referredUser->get_full_name() }}">
@@ -114,13 +114,13 @@
                                             </div>
                                         </div>
                                     </td>
-                                    <td>{{ addCurrencyToPrice($affiliate->getAffiliateRegistrationAmountsOfEachReferral()) }}</td>
-                                    <td>{{ addCurrencyToPrice($affiliate->getTotalAffiliateCommissionOfEachReferral()) }}</td>
-                                    <td>{{ dateTimeFormat($affiliate->created_at, 'Y M j | H:i') }}</td>
+                                    <td class="font-14">{{ addCurrencyToPrice($affiliate->getAffiliateRegistrationAmountsOfEachReferral()) }}</td>
+                                    <td class="font-14">{{ addCurrencyToPrice($affiliate->getTotalAffiliateCommissionOfEachReferral()) }}</td>
+                                    <td class="font-14">{{ dateTimeFormat($affiliate->created_at, 'Y M j | H:i') }}</td>
                                 </tr>
                             @endforeach
                             @else
-                            <tr class="no-record-found">
+                            <tr class="no-record-found font-14">
                                 <td class="text-left no-records-found" colspan="4">No Records Found</td>
                             </tr>
                             @endif
