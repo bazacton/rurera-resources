564fecea648dece6e0c6848e0dc9bda366c5a7bd
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-15 05:23:19 -0800

    Update index.blade.php

--- resources/views/web/default/course/index.blade.php
+++ resources/views/web/default/course/index.blade.php
@@ -125,7 +125,7 @@
 
                                                             @if($chapter->ChapterYearTopicParts->count() > 0)
                                                                 <div class="lms-chapter-ul-outer">
-                                                                    <ul>
+                                                                    <ul class="font-14 font-weight-500">
                                                                         @foreach($chapter->ChapterYearTopicParts as $topicPartObj)
 
                                                                         @php
@@ -162,7 +162,7 @@
                                                                         //$total_completion = 40;
                                                                             @endphp
 
-                                                                            <li class="font-14 font-weight-500">
+                                                                            <li>
                                                                                 <a href="#" class="{{ subscriptionCheckLink('courses') }} collapsed" data-toggle="collapse" data-target="#collapse{{$topicPartObj->id}}" aria-expanded="true">{{ $topicPartObj->title }}
                                                                                     <span class="topic-accuracy {{$completion_class}}" data-title="{{$completion_title}}"><img src="/assets/default/svgs/{{$completion_icon}}.svg"></span>                                                                                </a>
                                                                                 <div id="collapse{{$topicPartObj->id}}" class="collapse" data-parent="#accordion">
