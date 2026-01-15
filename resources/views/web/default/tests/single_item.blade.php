88180a982f6e7fcdc98a9d17657da71572b3d62c
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-15 12:04:27 -0800

    finding file

--- resources/views/web/default/tests/single_item.blade.php
+++ resources/views/web/default/tests/single_item.blade.php
@@ -15,7 +15,7 @@ $quiz_image = ($rowObj->quiz_image != '')? $rowObj->quiz_image : '/assets/defaul
 <tr>
     <td>
         <img src="{{$quiz_image}}" alt="quiz_image" width="59" height="59">
-        <h4 class="font-19 font-weight-bold"><a href="/{{isset($url_slug)? $url_slug : 'sats'}}/{{$rowObj->quiz_slug}}" class="{{ subscriptionCheckLink('bookshelf') }}">{{$rowObj->getTitleAttribute()}}</a>
+        <h4 class="font-19 font-weight-bold ccc"><a href="/{{isset($url_slug)? $url_slug : 'sats'}}/{{$rowObj->quiz_slug}}" class="{{ subscriptionCheckLink('bookshelf') }}">{{$rowObj->getTitleAttribute()}}</a>
             <br> <span class="sub_label">{{$rowObj->no_of_questions}} Question(s),</span> <span class="sub_label">Time:{{$rowObj->time}}m,</span> <span class="sub_label">{{getQuizTypeTitle($rowObj->quiz_type)}}</span>
 			@if( $rowObj->time == 10)
 				<img src="/assets/default/img/stop-watch.png" alt="stop-watch" width="360" height="360">
