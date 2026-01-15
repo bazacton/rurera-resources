bc7a90af17d205a8041906a569024ac4b703b12f
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-15 10:51:15 -0800

    Update quest_completed.blade.php

--- resources/views/web/default/quests/quest_completed.blade.php
+++ resources/views/web/default/quests/quest_completed.blade.php
@@ -10,7 +10,94 @@
 			<p> You have successfully Completed the quest {{$questObj->title}}</p>
 			<img src="/assets/default/img/panel-sidebar/coins.svg" alt="coins" width="30"> {{$RewardAccounting->score}}
 		</div>
-		
+		<div class="quests-list panel-border bg-white rounded-sm p-30">
+			<ul>
+				<li>
+					<div class="quests-item">
+						<div class="icon-box">
+							<img src="/assets/default/img/types/practice.svg" alt="learning image" width="50" height="50">
+						</div>
+						<div class="item-text">
+							<h5 class="font-18 font-weight-bold">90% Completion in 2 lessons</h5>
+							<div class="levels-progress horizontal">
+								<span class="progress-box">
+									<span class="progress-count" data-progress="50"><span class="progress-numbers">0%</span></span>
+									
+								</span>
+								
+							</div>
+							<span class="progress-icon font-16">
+								<img src="/assets/default/img/quests-coin.png" alt="quests-coin" width="35" height="35">
+								+100
+							</span>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="quests-item">
+						<div class="icon-box">
+							<img src="/assets/default/img/types/timestables.svg" alt="learning image" width="50" height="50">
+						</div>
+						<div class="item-text">
+							<h5 class="font-18 font-weight-bold">Play times table Power-Up Mode for 1 minute.</h5>
+							<div class="levels-progress horizontal">
+								<span class="progress-box">
+									<span class="progress-count" data-progress="70"><span class="progress-numbers">0%</span></span>
+									
+								</span>
+								
+							</div>
+							<span class="progress-icon font-16">
+								<img src="/assets/default/img/quests-coin.png" alt="quests-coin" width="35" height="35">
+								+100
+							</span>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="quests-item">
+						<div class="icon-box">
+							<img src="/assets/default/img/sidebar/refer_friend.svg" alt="learning image" width="50" height="50">
+						</div>
+						<div class="item-text">
+							<h5 class="font-18 font-weight-bold">Invite 1 friend to get X600&nbsp;Coins</h5>
+							<div class="levels-progress horizontal">
+								<span class="progress-box">
+									<span class="progress-count" data-progress="80"><span class="progress-numbers">0%</span></span>
+									
+								</span>
+								
+							</div>
+							<span class="progress-icon font-16">
+								<img src="/assets/default/img/quests-coin.png" alt="quests-coin" width="35" height="35">
+								+100
+							</span>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="quests-item">
+						<div class="icon-box">
+							<img src="/assets/default/img/types/special_quest.svg" alt="learning image" width="50" height="50">
+						</div>
+						<div class="item-text">
+							<h5 class="font-18 font-weight-bold">Rate Rurera on play store to get X500&nbsp;Coins</h5>
+							<div class="levels-progress horizontal">
+								<span class="progress-box">
+									<span class="progress-count" data-progress="90"><span class="progress-numbers">0%</span></span>
+									
+								</span>
+								
+							</div>
+							<span class="progress-icon font-16">
+								<img src="/assets/default/img/quests-coin.png" alt="quests-coin" width="35" height="35">
+								+100
+							</span>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
 		<div class="prev-next-controls text-center mb-50 questions-nav-controls">
 			<a href="javascript:;" class="review-btn finish-next-step">
 				NEXT
