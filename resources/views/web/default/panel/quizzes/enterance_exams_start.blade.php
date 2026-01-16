0c17372c9182d6f51cea82402f4986f4724014d4
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-16 03:25:15 -0800

    Update enterance_exams_start.blade.php

--- resources/views/web/default/panel/quizzes/enterance_exams_start.blade.php
+++ resources/views/web/default/panel/quizzes/enterance_exams_start.blade.php
@@ -74,21 +74,7 @@ $incorrect_answer_explaination = 1;//isset($incorrect_answer_explaination)? $inc
 
             <div class="justify-content-center row">
                 <div class="col-lg-9 col-md-12 col-sm-12 mx-auto preview-question-content px-0">
-                    <div class="quiz-time-bar mb-80 pt-15 mt-0">
-                        <div class="timer-wrap">
-                            <span class="time-label"><img src="/assets/default/svgs/time-past.svg" alt="time-past"> Time left:</span>
-
-                            <div class="time-box" id="hh">00</div>
-                            <span class="colon">:</span>
-                            <div class="time-box" id="mm">05</div>
-                            <span class="colon">:</span>
-                            <div class="time-box" id="ss">15</div>
-                        </div>
-                        <span class="coin-numbers">
-                            <img src="/assets/default/img/quests-coin.png" alt="quests-coin">
-                            <span class="total-earned-coins">1</span>
-                        </span>
-                    </div>
+                    
                     <div class="question-step quiz-complete" style="display:none">
                         <div class="question-layout-block">
                             <div class="left-content has-bg">
@@ -134,6 +120,21 @@ $incorrect_answer_explaination = 1;//isset($incorrect_answer_explaination)? $inc
                             <div class="question-inner-step-area">
                                 <div class="question-layout-block">
                                     <div class="left-content has-bg">
+                                        <div class="quiz-time-bar mb-80 pt-15 mt-0">
+                                            <div class="timer-wrap">
+                                                <span class="time-label"><img src="/assets/default/svgs/time-past.svg" alt="time-past"> Time left:</span>
+
+                                                <div class="time-box" id="hh">00</div>
+                                                <span class="colon">:</span>
+                                                <div class="time-box" id="mm">05</div>
+                                                <span class="colon">:</span>
+                                                <div class="time-box" id="ss">15</div>
+                                            </div>
+                                            <span class="coin-numbers">
+                                                <img src="/assets/default/img/quests-coin.png" alt="quests-coin">
+                                                <span class="total-earned-coins">1</span>
+                                            </span>
+                                        </div>
                                         <div class="question-counts">
                                             <span class="icon-box"><img src="/assets/default/svgs/question-simple.svg" alt="question-simple"></span>
                                             <span>Question 8 of 20</span>
