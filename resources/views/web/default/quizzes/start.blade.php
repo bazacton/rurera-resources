50e1195296a265a8a3b5ca2289ce4309cb7eba9e
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-16 01:58:29 -0800

    Update start.blade.php

--- resources/views/web/default/quizzes/start.blade.php
+++ resources/views/web/default/quizzes/start.blade.php
@@ -112,6 +112,9 @@ $is_new = isset( $is_new )? $is_new : 'no';
                                     <div class="stat-value stat-value-accent font-14 font-weight-bold">27 XP</div>
                                 </div>
                                 <div class="card-box">
+                                    <div class="icon-box">
+                                        <img src="/assets/default/svgs/target.svg" height="800" width="800" alt="target">
+                                    </div>
                                     <div class="stat-label font-16 font-weight-500">Attempts</div>
                                     <div class="stat-value font-14 font-weight-bold">2</div>
                                 </div>
