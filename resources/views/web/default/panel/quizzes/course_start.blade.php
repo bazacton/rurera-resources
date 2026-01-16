e840678de96ddc24ad2803afc5f2a3ed4ec8edb9
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-15 22:35:47 -0800

    Update course_start.blade.php

--- resources/views/web/default/panel/quizzes/course_start.blade.php
+++ resources/views/web/default/panel/quizzes/course_start.blade.php
@@ -516,7 +516,7 @@ var container = null;
 function getActiveContainer() {
     var activeSection = document.querySelector('.rurera-question-block.active');
     return activeSection
-        ? activeSection.querySelector('.question-inner-step-area .left-content')
+        ? activeSection.querySelector('.question-area-block .left-content')
         : null;
 }
 
