96a788716d7ae1f6c2f0343eade219d81985c427
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-16 02:06:38 -0800

    Update start.blade.php

--- resources/views/web/default/quizzes/start.blade.php
+++ resources/views/web/default/quizzes/start.blade.php
@@ -104,6 +104,9 @@ $is_new = isset( $is_new )? $is_new : 'no';
                                     <div class="stat-value font-14 font-weight-bold">4</div>
                                 </div>
                                 <div class="card-box">
+                                    <div class="icon-box">
+                                        <img src="/assets/default/svgs/clock-colord.svg" height="64" width="64" alt="clock-colord">
+                                    </div>
                                     <div class="stat-label font-16 font-weight-500">Time for quiz</div>
                                     <div class="stat-value font-14 font-weight-bold">20 min</div>
                                 </div>
