0830165acdc2f087fa817c217b5093311b222eb4
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-19 05:43:51 -0800

    Update start.blade.php

--- resources/views/web/default/quizzes/start.blade.php
+++ resources/views/web/default/quizzes/start.blade.php
@@ -125,7 +125,7 @@ $started_already = isset($started_already)? $started_already : false;
                                     <div class="setting-header">
                                         <div class="setting-title">
                                             <h3 class="font-16 font-weight-bold text-dark-charcoal text-left">Settings</h3>
-                                            <span class="font-14 d-block setting-sub-title text-left">Chose number of questions</span>
+                                            <span class="font-14 d-block setting-sub-title text-left">Choose number of questions</span>
                                         </div>
                                         
                                         <div class="group-buttons">
