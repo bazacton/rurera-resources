d14b9eec71a9cc3d21e555e06662bd0960e6c9a7
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-20 21:08:17 -0800

    Update course_start.blade.php

--- resources/views/web/default/panel/quizzes/course_start.blade.php
+++ resources/views/web/default/panel/quizzes/course_start.blade.php
@@ -191,7 +191,7 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
                                                 Finish
                                                 <img src="/assets/default/svgs/review-btn-flag.svg" width="683" height="683" alt="review-btn-flag">
                                             </a>
-                                            <button type="button" class="report-btn mr-auto"
+                                            <button type="button" class="report-btn mr-md-auto"
                                             data-toggle="tooltip"
                                             title="Report this question"
                                             data-target="#reportModal"
