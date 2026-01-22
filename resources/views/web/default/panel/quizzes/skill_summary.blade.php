c6a28b90cb794ee16ba5ce0059d3a3e2c6579a9e
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-22 01:58:14 -0800

    Update skill_summary.blade.php

--- resources/views/web/default/panel/quizzes/skill_summary.blade.php
+++ resources/views/web/default/panel/quizzes/skill_summary.blade.php
@@ -9,18 +9,6 @@ $rand_id = rand(99,9999);
 @push('styles_top')
 <link rel="stylesheet" href="/assets/default/css/quiz-common.css">
 
-<style>
-.field-holder.correct, .form-field.correct, .form-field.correct label {
-    background: #d7fbdf !important;
-    color: #333;
-    border-color: #5ace75;
-}
-.field-holder.wrong, .form-field.wrong, .form-field.wrong label {
-    background: #fae3e5 !important;
-    color: #333;
-    border-color: #f97d89;
-}
-</style>
 @endpush
 @section('content')
 <div class="content-section">
