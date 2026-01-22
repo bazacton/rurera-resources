381f9aea2a2a1e7dc824cf605405d4ed97e1b3de
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-22 03:32:40 -0800

    Update panel_layout.blade.php

--- resources/views/web/default/panel/layouts/panel_layout.blade.php
+++ resources/views/web/default/panel/layouts/panel_layout.blade.php
@@ -11,7 +11,7 @@
 <head>
     <title>{{ $pageTitle ?? '' }}{{ !empty($generalSettings['site_name']) ? (' | '.$generalSettings['site_name']) : '' }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-    <link rel="apple-touch-icon" href="/store/1/default_images/banners/fav-icon.png">
+    <link rel="apple-touch-icon" href="/assets/default/img/fav-icon.png">
     <!-- General CSS File -->
     <link rel="stylesheet" href="/assets/default/css/panel-pages/dashboard.css?ver={{$rand_no}}">
 	<link rel="stylesheet" href="/assets/default/css/common.css?ver={{$rand_no}}">
