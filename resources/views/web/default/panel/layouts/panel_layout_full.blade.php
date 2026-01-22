0ab22aaf14e2e567957bc7c27300a9eae6ae3362
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-22 04:54:21 -0800

    Update panel_layout_full.blade.php

--- resources/views/web/default/panel/layouts/panel_layout_full.blade.php
+++ resources/views/web/default/panel/layouts/panel_layout_full.blade.php
@@ -10,6 +10,12 @@
 <head>
     <title>{{ $pageTitle ?? '' }}{{ !empty($generalSettings['site_name']) ? (' | '.$generalSettings['site_name']) : '' }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <!-- Browser favicon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/default/img/fav-icon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/default/img/fav-icon.png">
+
+    <!-- Apple devices -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/default/img/fav-icon.png">
     <!-- General CSS File -->
     <link rel="stylesheet" href="/assets/default/vendors/sweetalert2/dist/sweetalert2.min.css">
     <link rel="stylesheet" href="/assets/default/vendors/toast/jquery.toast.min.css">
