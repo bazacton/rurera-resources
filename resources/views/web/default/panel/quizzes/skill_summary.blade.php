f05a63d155a6267b28e46f7e5181441419f4c6ea
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-21 22:50:50 -0800

    Update skill_summary.blade.php

--- resources/views/web/default/panel/quizzes/skill_summary.blade.php
+++ resources/views/web/default/panel/quizzes/skill_summary.blade.php
@@ -255,6 +255,35 @@ $rand_id = rand(99,9999);
                             </div>
                         </div>
                     </div>
+                    
+                    <div class="question-result-layout question-status-correct">
+                        <div class="status-badge font-14">
+                            <i>&#10003;</i>
+                            <span class="time-text">2.2s</span>
+                        </div>
+                        <div class="rureraform-col rureraform-col-12">
+                            <div class="question-label question_label">
+                                <span>2. Which examples show sensitivity in living organisms?</span>
+                            </div>
+	                    </div>
+                        <div class="question-area">
+                            <div class="question-step">
+                                <div class="lms-radio-lists">
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Correct answer:</span>
+                                        <ul class="lms-radio-btn-group lms-user-answer-block font-14">
+                                            <li><label class="lms-question-label" for="radio2"><span>A cat running when scared</span></label></li>
+                                            <li><label class="lms-question-label" for="radio2"><span>A sunflower turning to light</span></label></li>
+                                        </ul>
+                                    </div>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    
                     <div class="question-result-layout question-status-incorrect">
                         <div class="status-badge font-14">
                             <i>&#x00D7;</i>
@@ -262,7 +291,7 @@ $rand_id = rand(99,9999);
                         </div>
                         <div class="rureraform-col rureraform-col-12">
                             <div class="question-label question_label">
-                                <span>2. How do living organisms obtain and use energy?</span>
+                                <span>3. How do living organisms obtain and use energy?</span>
                             </div>
 	                    </div>
                         <div class="question-area">
@@ -285,33 +314,6 @@ $rand_id = rand(99,9999);
                             </div>
                         </div>
                     </div>
-                    <div class="question-result-layout question-status-correct">
-                        <div class="status-badge font-14">
-                            <i>&#10003;</i>
-                            <span class="time-text">2.2s</span>
-                        </div>
-                        <div class="rureraform-col rureraform-col-12">
-                            <div class="question-label question_label">
-                                <span>3. Which examples show sensitivity in living organisms?</span>
-                            </div>
-	                    </div>
-                        <div class="question-area">
-                            <div class="question-step">
-                                <div class="lms-radio-lists">
-                                    <div class="lms-user-answer-block">
-                                        <span class="list-title">Correct answer:</span>
-                                        <ul class="lms-radio-btn-group lms-user-answer-block font-14">
-                                            <li><label class="lms-question-label" for="radio2"><span>A cat running when scared</span></label></li>
-                                            <li><label class="lms-question-label" for="radio2"><span>A sunflower turning to light</span></label></li>
-                                        </ul>
-                                    </div>
-                                   
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-
                     <div class="question-result-layout question-status-incorrect">
                         <div class="status-badge font-14">
                             <i>&#x00D7;</i>
@@ -328,8 +330,8 @@ $rand_id = rand(99,9999);
                                     <div class="lms-user-answer-block">
                                         <span class="list-title">Correct answer:</span>
                                         <ul class="lms-radio-btn-group font-14">
-                                            <li><label class="lms-question-label" for="radio2"><span>Plants using sunlight,</span></label></li>
-                                            <li><label class="lms-question-label" for="radio2"><span>Humans eating food</span></label></li>
+                                            <li><label class="lms-question-label" for="radio2"><span>True,</span></label></li>
+                                            <li><label class="lms-question-label" for="radio2"><span>False</span></label></li>
                                         </ul>
                                     </div>
                                     
@@ -344,6 +346,8 @@ $rand_id = rand(99,9999);
                             </div>
                         </div>
                     </div>
+
+                    
                     <div class="question-result-layout question-status-correct">
                         <div class="status-badge font-14">
                             <i>&#10003;</i>
