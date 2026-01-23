0b2c049319bfaeaca1dcf5e53faded147f3c1ffe
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-23 05:28:02 -0800

    Create my-component.js

---
+++ public/assets/default/js/my-component.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function MyComponent() {
+  // You can use JSX here thanks to Babel
+  return (
+    <div>
+      <h2>Hello from React!</h2>
+      <p>This is a simple component.</p>
+    </div>
+  );
+}
+
+const domContainer = document.querySelector('#react-container');
+const root = ReactDOM.createRoot(domContainer); // Use createRoot for React 18+
+root.render(<MyComponent />);
