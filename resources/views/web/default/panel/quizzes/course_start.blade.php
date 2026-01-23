bd1943f39b25bd6b7d7bc09cd2c29486f4af7a42
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-23 02:44:51 -0800

    Update course_start.blade.php

--- resources/views/web/default/panel/quizzes/course_start.blade.php
+++ resources/views/web/default/panel/quizzes/course_start.blade.php
@@ -77,7 +77,7 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
         @endif
 
         <div class="container questions-data-block read-quiz-content" data-total_questions="{{$quizQuestions->count()}}">
-            <div class="quiz-status-bar mb-50 mt-15">
+            <div class="quiz-status-bar mb-md-50 mt-15">
                 <div class="questions-bar-box">
                     <div class="quiz-questions-bar-holder">
                         <div class="quiz-questions-bar">
