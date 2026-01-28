42170c2dbe82520c68a27e0cc772d43392327e2a
Baz <baz.chimpstudio@gmail.com>
2026-01-29 00:52:13 +0500

    set work
    set work

--- resources/views/web/default/panel/set_work/set_work_listing.blade.php
+++ resources/views/web/default/panel/set_work/set_work_listing.blade.php
@@ -52,7 +52,6 @@
                                                     </td>
                                                     <td data-th="Due Date">
                                                         <div class="last-activity">
-                                                            <span class="font-16 d-block due-date-label">Due Date: </span>
                                                             <span class="due-date font-16">20 Feb 2026</span>
                                                         </div>
                                                     </td>
