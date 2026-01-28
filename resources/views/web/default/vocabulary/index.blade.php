a335d68c23654cf504411c33236ab6a11c917a0b
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-28 04:15:21 -0800

    Update index.blade.php

--- resources/views/web/default/vocabulary/index.blade.php
+++ resources/views/web/default/vocabulary/index.blade.php
@@ -11,7 +11,7 @@
 <section class="content-section spells-page">
     <div class="section-title text-left mb-30">
         <h2 class="mt-0 mb-10 font-22">Spelling word lists</h2>
-        <p class="font-16"> Work through a variety of practice questions to improve your skills and become familiar with the types of questions you'll encounter on the SATs. </p>
+        <p class="font-14"> Work through a variety of practice questions to improve your skills and become familiar with the types of questions you'll encounter on the SATs. </p>
     </div>
     <!-- Listing Search Start -->
     <div class="listing-search lms-jobs-form mb-20">
