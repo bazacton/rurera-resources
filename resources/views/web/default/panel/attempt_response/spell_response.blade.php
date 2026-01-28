28adffcf48825c1bf165cba7f03969f0c277ceed
Baz <baz.chimpstudio@gmail.com>
2026-01-28 17:27:04 +0500

    set work
    set work

--- resources/views/web/default/panel/attempt_response/spell_response.blade.php
+++ resources/views/web/default/panel/attempt_response/spell_response.blade.php
@@ -1,8 +1,8 @@
 @if( $incorrect_flag == true)
 	<span class="question-status-wrong">Thats incorrect, but well done for trying
 	</span>
-	<span class="inc" style="text-decoration: line-through;">{{$question_user_input}}</span>
-	<span class="cor">{{$question_correct_answere}}</span>
+	<span class="inc" style="text-decoration: line-through;">{{is_array($question_user_input)? explode(',', $question_user_input) : $question_user_input}}</span>
+	<span class="cor">{{is_array($question_correct_answere)? explode(',', $question_correct_answere) : $question_correct_answere}}</span>
 
 @else
 	<span class="question-status-correct">Well done! Thats exactly right.</span>
