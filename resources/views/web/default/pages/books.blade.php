1920cdbdd84cb314af375f317ec7197f1ca5dbf4
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-28 00:11:08 -0800

    Update books.blade.php

--- resources/views/web/default/pages/books.blade.php
+++ resources/views/web/default/pages/books.blade.php
@@ -42,53 +42,59 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="panel-border bg-white rounded-sm">
-                                    @if( !empty( $category_books ))
-                                        @foreach( $category_books as $bookData)
-                                        <div class="col-lg-12">
-                                            <div class="listing-card">
-                                                <div class="row">
-                                                    <div class="col-12 col-lg-2 col-md-3 col-sm-3">
-                                                        <div class="img-holder">
-                                                            <figure>
-                                                                <a href="/books/{{$bookData->book_slug}}" class="{{ subscriptionCheckLink('bookshelf') }}">
-                                                                    <img src="{{$bookData->cover_image }}" alt="#" height="182" width="137"/>
-                                                                </a>
-                                                            </figure>
-                                                        </div>
-                                                    </div>
+                                <div class="col-12">
+                                    <div class="panel-border bg-white rounded-sm">
+                                        <div class="row">
+                                            @if( !empty( $category_books ))
+                                                @foreach( $category_books as $bookData)
+                                                <div class="col-lg-12">
+                                                    <div class="listing-card">
+                                                        <div class="row">
+                                                            <div class="col-12 col-lg-2 col-md-3 col-sm-3">
+                                                                <div class="img-holder">
+                                                                    <figure>
+                                                                        <a href="/books/{{$bookData->book_slug}}" class="{{ subscriptionCheckLink('bookshelf') }}">
+                                                                            <img src="{{$bookData->cover_image }}" alt="#" height="182" width="137"/>
+                                                                        </a>
+                                                                    </figure>
+                                                                </div>
+                                                            </div>
 
-                                                    <div class="col-12 col-lg-6 col-md-5 col-sm-5">
-                                                        <div class="text-holder">
-                                                            <h3 class="font-16 font-weight-bold text-dark-charcoal mb-5"><a href="/books/{{$bookData->book_slug}}" class="{{ subscriptionCheckLink('bookshelf') }}">{{$bookData->book_title}}</a>
-                                                            </h3>
-                                                            <ul class="font-14">
-                                                                <li><span>Reading Level :</span>{{$bookData->reading_level }}</li>
-                                                                <li><span>Interest Area :</span>{{$bookData->interest_area }}</li>
-                                                                <li><span>Pages :</span>{{$bookData->no_of_pages }}</li>
-                                                                <li><span>Points :</span>{{$bookData->reading_points }} <img src="../assets/default/svgs/coin-earn.svg"
-                                                                                                                                                width="20" height="24" alt="#"/></li>
-                                                            </ul>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-12 col-lg-4 col-md-4 col-sm-4">
-                                                        <div class="btn-holder">
-                                                            <a href="/books/{{$bookData->book_slug}}" class="read-btn font-14 {{ subscriptionCheckLink('bookshelf') }}">
-                                                                <span class="btn-icon">
-                                                                    <img src="/assets/default/svgs/book-open.svg" alt="book open" width="182" height="150">
-                                                                </span>
-                                                                Read the eBook
-                                                            </a>
+                                                            <div class="col-12 col-lg-6 col-md-5 col-sm-5">
+                                                                <div class="text-holder">
+                                                                    <h3 class="font-16 font-weight-bold text-dark-charcoal mb-5"><a href="/books/{{$bookData->book_slug}}" class="{{ subscriptionCheckLink('bookshelf') }}">{{$bookData->book_title}}</a>
+                                                                    </h3>
+                                                                    <ul class="font-14">
+                                                                        <li><span>Reading Level :</span>{{$bookData->reading_level }}</li>
+                                                                        <li><span>Interest Area :</span>{{$bookData->interest_area }}</li>
+                                                                        <li><span>Pages :</span>{{$bookData->no_of_pages }}</li>
+                                                                        <li><span>Points :</span>{{$bookData->reading_points }} <img src="../assets/default/svgs/coin-earn.svg"
+                                                                                                                                                        width="20" height="24" alt="#"/></li>
+                                                                    </ul>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-12 col-lg-4 col-md-4 col-sm-4">
+                                                                <div class="btn-holder">
+                                                                    <a href="/books/{{$bookData->book_slug}}" class="read-btn font-14 {{ subscriptionCheckLink('bookshelf') }}">
+                                                                        <span class="btn-icon">
+                                                                            <img src="/assets/default/svgs/book-open.svg" alt="book open" width="182" height="150">
+                                                                        </span>
+                                                                        Read the eBook
+                                                                    </a>
+                                                                </div>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
+                                                @endforeach
+                                                @endif
+                                                @endforeach
+                                            @endif
                                         </div>
-                                        @endforeach
-                                        @endif
-                                        @endforeach
-                                        @endif
+                                        
+                                    </div>
                                 </div>
+                                
                                 
                             </div>
                         </div>
