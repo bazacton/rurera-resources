e5f51d2ade810ab54e0f72f9a5ab22f10e5e30e9
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-29 02:03:03 -0800

    Update index.blade.php

--- resources/views/web/default/vocabulary/index.blade.php
+++ resources/views/web/default/vocabulary/index.blade.php
@@ -77,9 +77,7 @@
              <div class="spell-levels">
                 <div class="spell-levels-top">
                     <div class="spell-top-left">
-                        <h3 class="font-16 font-weight-bold">Words Start with A <span class="progress-star ">
-                        </h3>
-                        
+                        <h3 class="font-16 font-weight-bold">Words Start with A</h3>
                     </div>
                     <div class="spell-top-right font-14">
                         <div class="spell-info-list">
@@ -96,7 +94,7 @@
                 
                 <div class="spell-levels-top">
                     <div class="spell-top-left">
-                        <h3 class="font-16 font-weight-bold">{{$dataObj->getTitleAttribute()}} <span class="progress-star {{$percentage_class}}"></span></h3>
+                        <h3 class="font-16 font-weight-bold">{{$dataObj->getTitleAttribute()}}</h3>
                         <div class="spell-links font-14">
                         <a href="javascript:;" class="spell-popup-btn text-gray" data-heading="{{$dataObj->getTitleAttribute()}}" data-play_link="/spells/{{isset( $dataObj->quizYear->slug )? $dataObj->quizYear->slug : ''}}/{{$dataObj->quiz_slug}}/" data-spell_type="word-hunts" data-spell_id="{{$dataObj->id}}">
                             Practice Words
