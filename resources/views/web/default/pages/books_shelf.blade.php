285ea1310bbb647f479a7da45a64c3fb64309551
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-29 05:18:39 -0800

    Update books_shelf.blade.php

--- resources/views/web/default/pages/books_shelf.blade.php
+++ resources/views/web/default/pages/books_shelf.blade.php
@@ -180,7 +180,7 @@
                     @if( !empty( $category_books ))
                     @foreach( $category_books as $bookData)
                     <div class="col-lg-12">
-                        <div class="listing-card test">
+                        <div class="listing-card">
                             <div class="row">
                                 <div class="col-12 col-lg-2 col-md-3">
                                     <div class="img-holder">
