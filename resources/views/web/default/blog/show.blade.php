413056a4015b23662ef075db7bbfe934baaeba09
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-29 22:31:35 -0800

    Update show.blade.php

--- resources/views/web/default/blog/show.blade.php
+++ resources/views/web/default/blog/show.blade.php
@@ -24,7 +24,7 @@
             <div class="row">
                 <div class="col-12 col-lg-9 col-md-9">
                     
-                    <div class="post-show pb-0 pr-50">
+                    <div class="post-show pb-0 pr-50 mb-30">
                         <div class="post-image">
                             <img src="{{ $post->image }}" class="img-cover img-lg" alt="{{ $post->title }}" title="{{ $post->title }}" width="20" height="21" itemprop="image" loading="eager">
                         </div>
