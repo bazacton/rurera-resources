6ea202ba076d132f8cf961fbe927c061ffd88f50
Baz <baz.chimpstudio@gmail.com>
2026-01-30 20:39:34 +0500

    post add/edit
    post add/edit

--- resources/views/admin/blog/create.blade.php
+++ resources/views/admin/blog/create.blade.php
@@ -5,7 +5,7 @@
 @endpush
 
 @section('content')
-    <section class="section">
+    <section class="section block-single-post">
         <div class="section-header">
             <h1>{{ trans('admin/main.'.(!empty($post) ? 'edit_blog' : 'create_blog')) }}</h1>
             <div class="section-header-breadcrumb">
