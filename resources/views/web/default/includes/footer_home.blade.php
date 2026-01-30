feaab5e2f4c84a62438b5198bd82784671b305d5
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-01-29 22:56:10 -0800

    Update footer_home.blade.php

--- resources/views/web/default/includes/footer_home.blade.php
+++ resources/views/web/default/includes/footer_home.blade.php
@@ -5,13 +5,13 @@
                 <div class="widget widget-aboutus">
                     <ul>
                         <li itemprop="school">
-                            <a itemprop="url" href="/features">i'm a school <span>&#8250;</span></a>
+                            <a href="/features">i'm a school <span>&#8250;</span></a>
                         </li>
                         <li itemprop="teacher">
-                            <a itemprop="url"  href="/features">i'm a teacher<span>&#8250;</span></a>
+                            <a href="/features">i'm a teacher<span>&#8250;</span></a>
                         </li>
                         <li itemprop="parent">
-                            <a itemprop="url" href="/features">i'm a parent<span>&#8250;</span></a>
+                            <a href="/features">i'm a parent<span>&#8250;</span></a>
                         </li>
                     </ul>
                 </div>
@@ -24,11 +24,11 @@
                                 <h2>Company</h2>
                             </div>
                             <ul>
-                                <li><a itemprop="url" href="/about-us">About Us</a></li>
-                                <li><a itemprop="url" href="/blog">Blog</a></li>
-                                <li><a itemprop="url" href="/careers">Careers <span class="hiring-label">Hiring!</span></a></li>
-                                <li><a itemprop="url" href="/testimonials">Testimonials</a></li>
-                                <li><a itemprop="url" href="/contact-us">Contact Us</a></li>
+                                <li><a href="/about-us">About Us</a></li>
+                                <li><a href="/blog">Blog</a></li>
+                                <li><a href="/careers">Careers <span class="hiring-label">Hiring!</span></a></li>
+                                <li><a href="/testimonials">Testimonials</a></li>
+                                <li><a href="/contact-us">Contact Us</a></li>
                             </ul>
                         </div>
                     </div>
@@ -38,12 +38,10 @@
                                 <h2>What We Offer</h2>
                             </div>
                            <ul>
-								<li><a itemprop="url" href="/keystage1">KS1 Courses</a></li>
-								<li><a itemprop="url" href="/keystage2">KS2 Courses</a></li>
-								<li><a itemprop="url" href="/11-plus">11+ Exams</a></li>
-								<li><a itemprop="url" href="/timestables">TimeTables</a></li>
-								<li><a itemprop="url" href="/books-shelf">Books Reading</a></li>
-								<li><a itemprop="url" href="/spelling">Spells/Vocabulary</a></li>
+								<li><a href="/11-plus">11+ Exams</a></li>
+								<li><a href="/timestables">TimeTables</a></li>
+								<li><a href="/books-shelf">Books Reading</a></li>
+								<li><a href="/spelling">Spells/Vocabulary</a></li>
 							</ul>
                         </div>
                     </div>
@@ -53,17 +51,17 @@
                                 <h2>Portal</h2>
                             </div>
                             <ul>
-                                <li><a itemprop="url" href="/register">Register / Signup</a></li>
-                                <li><a itemprop="url" href="/register">Sign in / Login</a></li>
-                                <li><a itemprop="url" href="/membership">Membership</a></li>
+                                <li><a href="/register">Register / Signup</a></li>
+                                <li><a href="/register">Sign in / Login</a></li>
+                                <li><a href="/membership">Membership</a></li>
                             </ul>
                             <div class="widget-title mt-15">
                                 <h2>Contact</h2>
                             </div>
                             <ul>
-                                <li><a itemprop="url" href="/contact-us">Contact us</a></li>
-                                <li><a itemprop="url" href="/register">24/7 Support Desk</a></li>
-                                <li><a itemprop="url" href="/register">Login</a></li>
+                                <li><a href="/contact-us">Contact us</a></li>
+                                <li><a href="/register">24/7 Support Desk</a></li>
+                                <li><a href="/register">Login</a></li>
                             </ul>
                         </div>
                     </div>
@@ -73,11 +71,11 @@
                                 <h2>Legal Help</h2>
                             </div>
                             <ul>
-                                <li><a itemprop="url" href="/privacy-policy">Privacy Policy</a></li>
-                                <li><a itemprop="url" href="{{url('/')}}/privacy">Children's Privacy</a></li>
-                                <li><a itemprop="url" href="/terms-and-conditions">Legal</a></li>
-                                <li><a itemprop="url" href="/terms-and-conditions">Terms &amp; conditions</a></li>
-                                <li><a itemprop="url" href="/cookies">Cookie Policy</a></li>
+                                <li><a href="/privacy-policy">Privacy Policy</a></li>
+                                <li><a href="{{url('/')}}/privacy">Children's Privacy</a></li>
+                                <li><a href="/terms-and-conditions">Legal</a></li>
+                                <li><a href="/terms-and-conditions">Terms &amp; conditions</a></li>
+                                <li><a href="/cookies">Cookie Policy</a></li>
                             </ul>
                         </div>
                     </div>
@@ -93,7 +91,7 @@
                 </div>
                 <div class="col-6 col-md-3">
                     <div class="footer-logo">
-                        <a itemprop="url"  href="{{url('/')}}/">
+                        <a  href="{{url('/')}}/">
                             @if(!empty($generalSettings['logo']))
                             <img src="{{ $generalSettings['logo'] }}" class="img-cover" alt="site logo" title="site logo"
                                  width="100%" height="auto" loading="eager">
@@ -104,16 +102,16 @@
                 <div class="col-6 col-md-5">
                     <div class="d-flex justify-content-end align-items-center">
                         <ul class="footer-privacy-links d-flex">
-                        <li><a itemprop="url" href="/terms-and-conditions"class="text-dark-charcoal">Terms & Conditions</a></li>
-                        <li><a itemprop="url" href="/privacy" class="text-dark-charcoal">Privacy Policy</a></li>
+                        <li><a href="/terms-and-conditions"class="text-dark-charcoal">Terms & Conditions</a></li>
+                        <li><a href="/privacy" class="text-dark-charcoal">Privacy Policy</a></li>
                     </ul>
                     <div class="footer-social d-flex">
-                        <a itemprop="url"  href="https://www.instagram.com/rureralearning23/" title="instagram">
+                        <a  href="https://www.instagram.com/rureralearning23/" title="instagram">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-instagram"
                                 viewBox="0 0 16 16" id="IconChangeColor">
                                 <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"id="mainIconPathAttribute"></path></svg>
                         </a>
-                        <a itemprop="url"  href="https://twitter.com/RureraL46947" title="twitter">
+                        <a  href="https://twitter.com/RureraL46947" title="twitter">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-twitter"
                             viewBox="0 0 16 16" id="IconChangeColor">
                             <path
@@ -121,7 +119,7 @@
                                 id="mainIconPathAttribute" fill="#000000"></path>
                         </svg>
                         </a>
-                        <a itemprop="url"  href="https://www.facebook.com/" title="facebook">
+                        <a  href="https://www.facebook.com/" title="facebook">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="20" height="20"
                             viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
                             stroke-linejoin="round" id="IconChangeColor">
