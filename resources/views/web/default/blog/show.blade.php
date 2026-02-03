89a13f6afcc73c0f42da75f8af873ffdf3bd9bec
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-02 21:41:07 -0800

    Update show.blade.php

--- resources/views/web/default/blog/show.blade.php
+++ resources/views/web/default/blog/show.blade.php
@@ -171,22 +171,6 @@
                             </div>
                         </div>
                     </div>
-                    <div class="blog-newsletter text-center py-40">
-                        <span class="icon-box">
-                            <img src="../assets/default/svgs/envelope-mail-svgrepo-com.svg" alt="envelope image" height="800" width="800">
-                        </span>
-                        <div class="section-title mb-30">
-                            <h2 class="font-40 mb-0">Want more Stuff like this?</h2>
-                            <span class="d-block pt-5 font-19">Get the best viral stories straight into your box!</span>
-                        </div>
-                        <form action="">
-                            <div class="form-element">
-                                <input type="email" placeholder="Your email address">
-                                <button type="submit">Subscribe</button>
-                            </div>
-                            <span class="d-block text-right font-14 ">Don't worry, we don't spam</span>
-                        </form>
-                    </div>
                     </div>
                 </div>
                 @if( !empty( $headings_array ) )
