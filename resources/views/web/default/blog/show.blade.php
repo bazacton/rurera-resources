9cb10811da60848f990390bfe5b4f25cee5c56cc
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-02 23:54:47 -0800

    Update show.blade.php

--- resources/views/web/default/blog/show.blade.php
+++ resources/views/web/default/blog/show.blade.php
@@ -109,61 +109,7 @@
                     <div class="dates-diary mb-30">
                         <span class="badge">Works</span>
                     </div>
-                    <div class="row mb-15">
-                        <div class="col-12">
-                            <div class="section-title mb-15">
-                                <h2 class="mb-0 font-22">Related posts</h2>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-4 col-lg-4 mt-20 mt-lg-0 mb-30">
-                            <div class="rurera-blog blog-medium" itemscope="" itemtype="https://schema.org/NewsArticle">
-                                <div class="blog-grid-detail">
-                                    <span class="badge created-at d-flex align-items-center">
-                                    <i data-feather="calendar" style="height: 20px; width: 20px;" class="mr-5"></i>
-                                    <span itemprop="datePublished" content="2023-04-05T08:00:00+08:00">19 Sep 2023</span>
-                                    </span>
-                                    <h2 class="blog-grid-title mt-10 font-16 font-weight-bold" itemprop="headline">
-                                        <a href="/blog/personalized-learning-with-educational-technology-a-game-changer-in-education" itemprop="url" class="text-dark">2 Ways to Boost Learning and SATs Success with Rurera</a>
-                                    </h2>
-                                </div>
-                                <div class="blog-grid-image">
-                                    <img src="/store/1/default_images/blogs/blog-13.webp" class="img-cover" alt="2 Ways to Boost Learning and SATs Success with Rurera" title="2 Ways to Boost Learning and SATs Success with Rurera" width="1380" height="920" itemprop="image" loading="eager">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-4 col-lg-4 mt-20 mt-lg-0 mb-30">
-                            <div class="rurera-blog blog-medium" itemscope="" itemtype="https://schema.org/NewsArticle">
-                                <div class="blog-grid-detail">
-                                    <span class="badge created-at d-flex align-items-center">
-                                    <i data-feather="calendar" style="height: 20px; width: 20px;" class="mr-5"></i>
-                                    <span itemprop="datePublished" content="2023-04-05T08:00:00+08:00">27 Apr 2023</span>
-                                    </span>
-                                    <h2 class="blog-grid-title mt-10 font-16 font-weight-bold" itemprop="headline">
-                                        <a href="/blog/navigating-the-rurera-reward-redemption-process-for-11-exams-and-more" itemprop="url" class="text-dark">Experience realistic 11+ mock exams designed to simulate act</a>
-                                    </h2>
-                                </div>
-                                <div class="blog-grid-image">
-                                    <img src="/store/1/default_images/blogs/blog-29.jpg" class="img-cover" alt="Experience realistic 11+ mock exams designed to simulate act" title="Experience realistic 11+ mock exams designed to simulate act" width="370" height="370" itemprop="image" loading="eager">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-4 col-lg-4 mt-20 mt-lg-0 mb-30">
-                            <div class="rurera-blog blog-medium" itemscope="" itemtype="https://schema.org/NewsArticle">
-                                <div class="blog-grid-detail">
-                                    <span class="badge created-at d-flex align-items-center">
-                                        <i data-feather="calendar" style="height: 20px; width: 20px;" class="mr-5"></i>
-                                        <span itemprop="datePublished" content="2023-04-05T08:00:00+08:00">28 Apr 2023</span>
-                                    </span>
-                                    <h2 class="blog-grid-title mt-10 font-16 font-weight-bold" itemprop="headline">
-                                        <a href="/blog/elevating-education-exploring-the-future-of-learning-for-year-6-students" itemprop="url" class="text-dark">How to Explore the Future of Learning for Year 6 Students.</a>
-                                    </h2>
-                                </div>
-                                <div class="blog-grid-image">
-                                    <img src="/store/1/default_images/blogs/blog-27.jpg" class="img-cover" alt="How to Explore the Future of Learning for Year 6 Students." title="How to Explore the Future of Learning for Year 6 Students." width="370" height="370" itemprop="image" loading="eager">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
                     </div>
                 </div>
                 @if( !empty( $headings_array ) )
@@ -184,6 +130,61 @@
 						</div>
 					</div>
 				@endif
+                <div class="mb-15">
+                    <div class="col-12">
+                        <div class="section-title mb-15">
+                            <h2 class="mb-0 font-22">Related posts</h2>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 col-lg-4 mt-20 mt-lg-0 mb-30">
+                        <div class="rurera-blog blog-medium" itemscope="" itemtype="https://schema.org/NewsArticle">
+                            <div class="blog-grid-detail">
+                                <span class="badge created-at d-flex align-items-center">
+                                <i data-feather="calendar" style="height: 20px; width: 20px;" class="mr-5"></i>
+                                <span itemprop="datePublished" content="2023-04-05T08:00:00+08:00">19 Sep 2023</span>
+                                </span>
+                                <h2 class="blog-grid-title mt-10 font-16 font-weight-bold" itemprop="headline">
+                                    <a href="/blog/personalized-learning-with-educational-technology-a-game-changer-in-education" itemprop="url" class="text-dark">2 Ways to Boost Learning and SATs Success with Rurera</a>
+                                </h2>
+                            </div>
+                            <div class="blog-grid-image">
+                                <img src="/store/1/default_images/blogs/blog-13.webp" class="img-cover" alt="2 Ways to Boost Learning and SATs Success with Rurera" title="2 Ways to Boost Learning and SATs Success with Rurera" width="1380" height="920" itemprop="image" loading="eager">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 col-lg-4 mt-20 mt-lg-0 mb-30">
+                        <div class="rurera-blog blog-medium" itemscope="" itemtype="https://schema.org/NewsArticle">
+                            <div class="blog-grid-detail">
+                                <span class="badge created-at d-flex align-items-center">
+                                <i data-feather="calendar" style="height: 20px; width: 20px;" class="mr-5"></i>
+                                <span itemprop="datePublished" content="2023-04-05T08:00:00+08:00">27 Apr 2023</span>
+                                </span>
+                                <h2 class="blog-grid-title mt-10 font-16 font-weight-bold" itemprop="headline">
+                                    <a href="/blog/navigating-the-rurera-reward-redemption-process-for-11-exams-and-more" itemprop="url" class="text-dark">Experience realistic 11+ mock exams designed to simulate act</a>
+                                </h2>
+                            </div>
+                            <div class="blog-grid-image">
+                                <img src="/store/1/default_images/blogs/blog-29.jpg" class="img-cover" alt="Experience realistic 11+ mock exams designed to simulate act" title="Experience realistic 11+ mock exams designed to simulate act" width="370" height="370" itemprop="image" loading="eager">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 col-lg-4 mt-20 mt-lg-0 mb-30">
+                        <div class="rurera-blog blog-medium" itemscope="" itemtype="https://schema.org/NewsArticle">
+                            <div class="blog-grid-detail">
+                                <span class="badge created-at d-flex align-items-center">
+                                    <i data-feather="calendar" style="height: 20px; width: 20px;" class="mr-5"></i>
+                                    <span itemprop="datePublished" content="2023-04-05T08:00:00+08:00">28 Apr 2023</span>
+                                </span>
+                                <h2 class="blog-grid-title mt-10 font-16 font-weight-bold" itemprop="headline">
+                                    <a href="/blog/elevating-education-exploring-the-future-of-learning-for-year-6-students" itemprop="url" class="text-dark">How to Explore the Future of Learning for Year 6 Students.</a>
+                                </h2>
+                            </div>
+                            <div class="blog-grid-image">
+                                <img src="/store/1/default_images/blogs/blog-27.jpg" class="img-cover" alt="How to Explore the Future of Learning for Year 6 Students." title="How to Explore the Future of Learning for Year 6 Students." width="370" height="370" itemprop="image" loading="eager">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
