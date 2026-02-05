ca11ba20b4acb5c0b7ab90f5962980d48279bdb4
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-05 00:25:21 -0800

    Update show.blade.php

--- resources/views/web/default/blog/show.blade.php
+++ resources/views/web/default/blog/show.blade.php
@@ -6,69 +6,161 @@
 @endpush
 
 @section('content')
-    <section class="position-relative single-post-subheader py-70">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-7 col-lg-7">
-                    <a href="/blog" itemprop="url" class="post-back-btn font-18 font-weight-normal">Back to blog</a>
-                    <h1 class="font-weight-bold my-20">{{ $post->title }}</h1>
-                    <div class="post-excerpt">
-                        {!! getBodyContent($post_description) !!}
-                    </div>
-                    <div class="post-date rurera-hide">
-                        <span class="mt-15 d-block font-16">{{ dateTimeFormat($post->created_at, 'j M Y') }}</span>
-                    </div>
-                    <!-- Share bar -->
-                    <div class="sharebar mb-0 mt-15" role="group">
-                        <div class="sharebar__stats font-12" aria-label="Engagement stats">
-                            <div class="stat">
-                                <div class="stat__value stat__value--blue font-22">1.2k</div>
-                                <div class="stat__label">SHARES</div>
+<main id="main-content">
+    <article class="single-post" itemscope itemtype="https://schema.org/BlogPosting">
+        <header class="position-relative single-post-subheader py-70">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-7 col-lg-7">
+                        <a href="/blog" itemprop="url" class="post-back-btn font-18 font-weight-normal">Back to blog</a>
+                        <h1 class="font-weight-bold my-20">{{ $post->title }}</h1>
+                        <div class="post-excerpt">
+                            {!! getBodyContent($post_description) !!}
+                        </div>
+                        <div class="post-date rurera-hide">
+                            <span class="mt-15 d-block font-16">{{ dateTimeFormat($post->created_at, 'j M Y') }}</span>
+                        </div>
+                        <!-- Share bar -->
+                        <div class="sharebar mb-0 mt-15" role="group">
+                            <div class="sharebar__stats font-12" aria-label="Engagement stats">
+                                <div class="stat">
+                                    <div class="stat__value stat__value--blue font-22">1.2k</div>
+                                    <div class="stat__label">SHARES</div>
+                                </div>
+
+                                <div class="stat">
+                                    <div class="stat__value font-22">6.4k</div>
+                                    <div class="stat__label">VIEWS</div>
+                                </div>
                             </div>
 
-                            <div class="stat">
-                                <div class="stat__value font-22">6.4k</div>
-                                <div class="stat__label">VIEWS</div>
+                            <div class="sharebar__divider" aria-hidden="true"></div>
+
+                            <button type="button" class="btn btn--fb" aria-label="Share on Facebook">
+                                <span class="btn__icon" aria-hidden="true" data-network="facebook">
+                                    <img src="/assets/default/svgs/facebook2.svg" alt="" aria-hidden="true">
+                                </span>
+                                <span class="btn__text">Share on Facebook</span>
+                            </button>
+
+                            <button type="button" class="btn btn--x" aria-label="Share on Twitter">
+                                <span class="btn__icon" aria-hidden="true">
+                                    <img src="/assets/default/svgs/twitter-x.svg" alt="" aria-hidden="true">
+                                </span>
+                                <span class="btn__text">Share on Twitter</span>
+                            </button>
+                            <div class="share-secondary">
+                                <button type="button" class="btn-pinterest" data-network="pinterest" aria-label="Pinterest"><img src="/assets/default/svgs/pinterest.svg" alt="pinterest"></button>
+                                <button type="button" class="btn-whatsapp" data-network="whatsapp" aria-label="Whatsapp"><img src="/assets/default/svgs/whatsapp.svg" alt="whatsapp"></button>
+                                <button type="button" class="btn-email" data-network="email" aria-label="Email"><img src="/assets/default/svgs/envelope2.svg" alt="envelope2"></button>
                             </div>
+                            <button class="btn btn--share" type="button" aria-label="More share options" aria-expanded="false" aria-controls="share-menu">
+                                <span class="btn__icon" aria-hidden="true">
+                                    <img src="/assets/default/svgs/share2.svg" alt="" aria-hidden="true">
+                                </span>
+                            </button>
                         </div>
-
-                        <div class="sharebar__divider" aria-hidden="true"></div>
-
-                        <button type="button" class="btn btn--fb" aria-label="Share on Facebook">
-                            <span class="btn__icon" aria-hidden="true" data-network="facebook">
-                                <img src="/assets/default/svgs/facebook2.svg" alt="" aria-hidden="true">
-                            </span>
-                            <span class="btn__text">Share on Facebook</span>
-                        </button>
-
-                        <button type="button" class="btn btn--x" aria-label="Share on Twitter">
-                            <span class="btn__icon" aria-hidden="true">
-                                <img src="/assets/default/svgs/twitter-x.svg" alt="" aria-hidden="true">
-                            </span>
-                            <span class="btn__text">Share on Twitter</span>
-                        </button>
-                        <div class="share-secondary">
-                            <button type="button" class="btn-pinterest" data-network="pinterest" aria-label="Pinterest"><img src="/assets/default/svgs/pinterest.svg" alt="pinterest"></button>
-                            <button type="button" class="btn-whatsapp" data-network="whatsapp" aria-label="Whatsapp"><img src="/assets/default/svgs/whatsapp.svg" alt="whatsapp"></button>
-                            <button type="button" class="btn-email" data-network="email" aria-label="Email"><img src="/assets/default/svgs/envelope2.svg" alt="envelope2"></button>
-                        </div>
-                        <button class="btn btn--share" type="button" aria-label="More share options" aria-expanded="false" aria-controls="share-menu">
-                            <span class="btn__icon" aria-hidden="true">
-                                <img src="/assets/default/svgs/share2.svg" alt="" aria-hidden="true">
-                            </span>
-                        </button>
                     </div>
-                </div>
-                <div class="col-12 col-lg-5 col-md-5 ml-auto">
-                    <div class="img-box">
-                        <img src="{{ $post->image }}" class="img-cover img-lg" alt="{{ $post->title }}" title="{{ $post->title }}" width="20" height="21" itemprop="image" loading="eager">
+                    <div class="col-12 col-lg-5 col-md-5 ml-auto">
+                        <div class="img-box">
+                            <img src="{{ $post->image }}" class="img-cover img-lg" alt="{{ $post->title }}" title="{{ $post->title }}" width="20" height="21" itemprop="image" loading="eager">
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </header>
+        <section class="mt-15 mb-60 lms-blog blog-single-post mt-80">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-lg-9 col-md-9">
+                        <div class="post-show pb-0 pr-30">
+                            {!! getBodyContent($post_content) !!}
 
-    <section class="mt-15 mb-60 lms-blog blog-single-post mt-80">
+                        {{-- post Comments --}}
+                        @if($post->enable_comment)
+                            @include('web.default.includes.comments',[
+                                'comments' => $post->comments,
+                                'inputName' => 'blog_id',
+                                'inputValue' => $post->id
+                            ])
+                        @endif
+                        {{-- ./ post Comments --}}
+                        
+                        </div>
+                    </div>
+                    @if( !empty( $headings_array ) )
+                        <div class="col-12 col-lg-3 col-md-3">
+                            <div class="blog-sidebar">
+                            <h2 class="mb-10 font-22">Table of content</h2>
+                                <div class="single-post-nav mb-0">
+                                    <nav>
+                                        <ul>
+                                            @php $counter = 1; @endphp
+                                            @foreach( $headings_array as $heading_id => $heading_text)
+                                                <li><a href="#{{$heading_id}}" class="{{($counter == 1)? 'current' : ''}}">{{$thisController->getPostContent($post, $heading_text)}}</a></li>
+                                                @php $counter++; @endphp
+                                            @endforeach
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    <div class="row mb-15 mx-0" aria-label="Related posts">
+                        <div class="col-12">
+                            <div class="section-title mb-15">
+                            <h2 class="mb-0 font-22">Related posts</h2>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-3 col-lg-3 mt-20 mt-lg-0 mb-30">
+                            <article class="rurera-blog blog-medium"
+                                    itemscope
+                                    itemtype="https://schema.org/BlogPosting">
+
+                                <link itemprop="mainEntityOfPage"
+                                    href="https://example.com/blog/personalized-learning-with-educational-technology-a-game-changer-in-education">
+
+                                <meta itemprop="datePublished" content="2023-09-19T08:00:00+08:00">
+                                <meta itemprop="dateModified" content="2023-09-19T08:00:00+08:00">
+
+                                <div class="blog-grid-detail">
+                                <span class="badge created-at d-flex align-items-center">
+                                    <i data-feather="calendar" style="height:20px;width:20px;" class="mr-5"></i>
+                                    <span>19 Sep 2023</span>
+                                </span>
+
+                                <h3 class="blog-grid-title mt-10 font-16 font-weight-bold" itemprop="headline">
+                                    <a href="/blog/personalized-learning-with-educational-technology-a-game-changer-in-education"
+                                    class="text-dark"
+                                    itemprop="url">
+                                    2 Ways to Boost Learning and SATs Success with Rurera
+                                    </a>
+                                </h3>
+                                </div>
+
+                                <div class="blog-grid-image">
+                                <img
+                                    src="/store/1/default_images/blogs/blog-13.webp"
+                                    class="img-cover"
+                                    alt="2 Ways to Boost Learning and SATs Success with Rurera"
+                                    width="1380"
+                                    height="920"
+                                    loading="lazy"
+                                    decoding="async"
+                                    itemprop="image"
+                                >
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </article>
+</main>
+    
+
+    <!-- <section class="mt-15 mb-60 lms-blog blog-single-post mt-80">
         <div class="container">
             <div class="row">
                 <div class="col-12 col-lg-9 col-md-9">
@@ -154,7 +246,7 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
 @endsection
 
 @push('scripts_bottom')
