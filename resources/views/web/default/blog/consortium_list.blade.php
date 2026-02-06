74bf9637ac7c901ead1758fda2106a1a3ad8cfeb
Baz <baz.chimpstudio@gmail.com>
2026-02-06 19:32:38 +0500

    consortium
    consortium

--- resources/views/web/default/blog/consortium_list.blade.php
+++ resources/views/web/default/blog/consortium_list.blade.php
@@ -1,6 +1,6 @@
 <section class="rurera-schools-section" aria-labelledby="consortium-schools-title">
     <h2 id="consortium-schools-title">
-        Grammar schools in the same consortium as {{$grammerSchoolsObj->school_name}}
+        Grammar schools in same consortium
     </h2>
 
     <article class="rurera-school-item">
