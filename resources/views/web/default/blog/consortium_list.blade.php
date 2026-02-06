02e40e449fd15d5a4f0dd939bbd775be80ccb3a2
Baz <baz.chimpstudio@gmail.com>
2026-02-06 20:26:23 +0500

    consortium
    consortium

--- resources/views/web/default/blog/consortium_list.blade.php
+++ resources/views/web/default/blog/consortium_list.blade.php
@@ -1,36 +1,21 @@
-<section class="rurera-schools-section" aria-labelledby="nearby-schools-title">
-    <h2 id="nearby-schools-title">
-        Grammar Schools Near {{$grammerSchoolsObj->school_name}}
+<section class="rurera-schools-section" aria-labelledby="consortium-schools-title">
+    <h2 id="consortium-schools-title">
+        Grammar Schools in Same Consortium
     </h2>
-    @if($nearbySchools->count() > 0)
-        @foreach($nearbySchools as $grammerSchoolObj)
-            <article class="rurera-school-item">
-                <figure class="rurera-school-icon">
-                    <img
-                        src="https://rurera.com/assets/default/svgs/blog-schools.svg"
-                        height="64"
-                        width="64"
-                        alt=""
-                        loading="lazy"
-                    >
-                </figure>
 
-                <div class="rurera-school-content">
-                    <header class="rurera-school-header">
-                        <h3 class="mt-0">
-                            <a href="/blog/{{isset($grammerSchoolObj->schoolBlog->slug)? $grammerSchoolObj->schoolBlog->slug : ''}}" class="rurera-school-name">{{$grammerSchoolObj->school_name}}</a>
-                        </h3>
-                    </header>
-
-                    <p class="rurera-school-age">
-                        {{round($grammerSchoolObj->distance)}} miles away
-                    </p>
-                </div>
-            </article>
+    @if($consortium_schools->count() > 0)
+        @foreach($consortium_schools as $consortium_schoolObj)
+        <article class="rurera-school-item">
+            <div class="rurera-school-content">
+                <header class="rurera-school-header">
+                    <h3 class="mt-0">
+                        <a href="/blog/{{isset($consortium_schoolObj->schoolBlog->slug)? $consortium_schoolObj->schoolBlog->slug : ''}}">
+                            {{$consortium_schoolObj->school_name}}
+                        </a>
+                    </h3>
+                </header>
+            </div>
+        </article>
         @endforeach
-        <p class="rurera-school-note">
-            These distances are calculated in a straight line.
-            The actual route and distance may vary.
-        </p>
     @endif
 </section>
