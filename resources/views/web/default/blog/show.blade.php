d1a527fe0f637090eb2fec58c813c71466b66be5
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-07 09:43:50 -0800

    Update show.blade.php

--- resources/views/web/default/blog/show.blade.php
+++ resources/views/web/default/blog/show.blade.php
@@ -12,7 +12,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12 col-md-7 col-lg-7">
-                        <a href="/blog" class="post-back-btn font-18 font-weight-normal">Back to blog</a>
+                        <a href="/blog" class="post-back-btn font-18 font-weight-normal">&larr; Back to blog</a>
                         <h1 class="font-weight-bold my-20">{{ $post->title }}</h1>
                         <div class="post-excerpt">
                             {!! getBodyContent($post_description) !!}
