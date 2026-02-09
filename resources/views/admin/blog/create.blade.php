99c27674265d023b76f33a7ff83e2396e5258815
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-09 03:44:58 -0800

    Update create.blade.php

--- resources/views/admin/blog/create.blade.php
+++ resources/views/admin/blog/create.blade.php
@@ -1025,17 +1025,6 @@
             margin: 0 0 5px;
         }
         /* Ru-Newsletter Style End */
-        .grammer-school-card .short-code-btn {
-            border: 0;
-            height: auto;
-            padding: 0;
-            width: auto;
-            background-color: inherit;
-            color: var(--primary);
-            text-decoration: underline;
-            outline: none;
-            margin-bottom: 15px;
-        }
         .card.grammer-school-card {
             display: block;
         }
