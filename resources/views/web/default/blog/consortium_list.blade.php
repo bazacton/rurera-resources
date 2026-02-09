258a21737c472f41679b85481cdca76c7a688ef4
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-08 23:07:28 -0800

    Update consortium_list.blade.php

--- resources/views/web/default/blog/consortium_list.blade.php
+++ resources/views/web/default/blog/consortium_list.blade.php
@@ -8,11 +8,9 @@
         <article class="rurera-school-item">
             <div class="rurera-school-content">
                 <header class="rurera-school-header">
-                    <h3 class="mt-0">
-                        <a href="/blog/{{isset($consortium_schoolObj->schoolBlog->slug)? $consortium_schoolObj->schoolBlog->slug : ''}}">
-                            {{$consortium_schoolObj->school_name}}
-                        </a>
-                    </h3>
+                    <a href="/blog/{{isset($consortium_schoolObj->schoolBlog->slug)? $consortium_schoolObj->schoolBlog->slug : ''}}">
+                        {{$consortium_schoolObj->school_name}}
+                    </a>
                 </header>
             </div>
         </article>
