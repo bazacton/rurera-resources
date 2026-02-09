1748a44d78e58d35935ac4ab2971a5159c7a81ef
Baz <baz.chimpstudio@gmail.com>
2026-02-09 13:18:16 +0500

    posts
    posts

--- resources/views/web/default/blog/show.blade.php
+++ resources/views/web/default/blog/show.blade.php
@@ -62,7 +62,9 @@
                         </div>
 
 
-                        {{$thisController->render_shortcode($post,'[GS related_posts]')}}
+                        Posts
+                        {!! $thisController->render_shortcode($post,'[GS related_posts]') !!}
+
                     </div>
                     <div class="col-12 col-lg-5 col-md-5 ml-auto">
                         <div class="img-box">
