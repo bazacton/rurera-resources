d8db7d755d47d0bf4a3e480b8a2118f94b93dc63
Baz <baz.chimpstudio@gmail.com>
2026-02-09 12:08:31 +0500

    consortium
    consortium

--- resources/views/web/default/blog/consortium_list.blade.php
+++ resources/views/web/default/blog/consortium_list.blade.php
@@ -13,7 +13,7 @@
                             {{$consortium_schoolObj->school_name}}
                         </a>
                     </h3>
-                    <div class="address">{{$grammerSchoolObj->complete_address}}</div>
+                    <div class="address">{{$consortium_schoolObj->complete_address}}</div>
                 </header>
             </div>
         </article>
