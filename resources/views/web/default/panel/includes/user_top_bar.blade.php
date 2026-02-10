333ecb7102f241760c873e99d5b5838262752d86
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-09 21:49:42 -0800

    Update user_top_bar.blade.php

--- resources/views/web/default/panel/includes/user_top_bar.blade.php
+++ resources/views/web/default/panel/includes/user_top_bar.blade.php
@@ -209,7 +209,7 @@
                 @endif
                 @else
                 <div class="d-flex align-items-center ml-md-50">
-                    <a href="/login" class="py-5 px-15 mr-10 text-dark-blue font-16 login-btn">Log in</a>
+                    <a href="/login" class="py-5 px-15 mr-10 text-dark-blue font-16 login-btn">Sign in</a>
                     <a href="/register" class="py-5 px-15 text-dark-blue font-16 register-btn">Try for free</a>
                 </div>
                 @endif
