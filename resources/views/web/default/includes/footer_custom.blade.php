f5a8ac63a9e14f782ec17f4d5a96122b500c38e8
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-10 02:08:13 -0800

    Update footer_custom.blade.php

--- resources/views/web/default/includes/footer_custom.blade.php
+++ resources/views/web/default/includes/footer_custom.blade.php
@@ -1,89 +1,87 @@
 
-<section class="custom-footer pt-80 pb-50 wow fadeInUp" style="background-color:#000">
+<section class="custom-footer pt-80 pb-50" style="background-color:#000">
 	<div class="container">
-		<div class="row align-items-center">
-			<div class="col-12 col-lg-8">
-				<div class="row">
-					<div class="col-12 col-md-6 col-lg-4">
-						<div class="widget widget-links">
-							<div class="widget-title"><h2>Company</h2></div>
-							<div class="widget-body">
-								<ul>
-									<li><a href="/about-us">About Us</a></li>
-									<li><a href="/blog">Blog</a></li>
-									<li><a href="/careers">Careers</a></li>
-									<li><a href="/testimonials">Testimonials</a></li>
-									<li><a href="/contact-us">Contact Us</a></li>
-								</ul>
+		<div class="row">
+			<div class="col-12">
+				<div class="footer-widgets-area">
+					<div class="row align-items-center">
+						<div class="col-12 col-lg-8">
+							<div class="row">
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="widget widget-links">
+										<div class="widget-title"><h2>Company</h2></div>
+										<div class="widget-body">
+											<ul>
+												<li><a href="/about-us">About Us</a></li>
+												<li><a href="/blog">Blog</a></li>
+												<li><a href="/careers">Careers</a></li>
+												<li><a href="/testimonials">Testimonials</a></li>
+												<li><a href="/contact-us">Contact Us</a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="widget widget-location">
+										<div class="widget-title"><h2>What We Offer</h2></div>
+										<div class="widget-body">
+											<ul>
+												<li><a href="/11-plus">11+ Exams</a></li>
+												<li><a href="/timestables">TimeTables</a></li>
+												<li><a href="/books-shelf">Books Reading</a></li>
+												<li><a href="/spelling">Spells/Vocabulary</a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="col-12 col-md-6 col-lg-4">
+									<div class="footer-newsletter">
+										<div class="widget-title"><h2>Newsletter</h2></div>
+										<div class="widget-body">
+											<div class="form-field">
+												<input type="text" placeholder="Don’t miss out updates">
+											</div>
+											<div class="form-check-field">
+												<input type="checkbox" id="privacy-policy">
+												<label for="privacy-policy">I agree to the Privacy Policy and give my permission to process my personal data for the purposes specified in the Privacy Policy.</label>
+											</div>
+											<div class="form-btn-field"><button type="submit">Send</button></div>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="col-12 col-md-6 col-lg-4">
-						<div class="widget widget-location">
-							<div class="widget-title"><h2>What We Offer</h2></div>
-							<div class="widget-body">
-								<ul>
-									<li><a href="/11-plus">11+ Exams</a></li>
-									<li><a href="/timestables">TimeTables</a></li>
-									<li><a href="/books-shelf">Books Reading</a></li>
-									<li><a href="/spelling">Spells/Vocabulary</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-6 col-lg-4">
-						<div class="footer-newsletter">
-							<div class="widget-title"><h2>Newsletter</h2></div>
-							<div class="widget-body">
-								<div class="form-field">
-									<input type="text" placeholder="Don’t miss out updates">
+						<div class="col-12 col-lg-4"><div class="footer-logo-wrapper">
+								<div class="footer-dots-img">
+									<figure><img src="/assets/default/svgs/footer-dots.svg" width="502" height="502" alt="footer dots image"></figure>
 								</div>
-								<div class="form-check-field">
-									<input type="checkbox" id="privacy-policy">
-									<label for="privacy-policy">I agree to the Privacy Policy and give my permission to process my personal data for the purposes specified in the Privacy Policy.</label>
+								<div class="footer-logo-box">
+									<img src="/store/1/logo.png" width="185" height="38" alt="footer logo image">
+									<a href="/contact-us" class="mt-20"><span>Schedule Consultation</span></a>
 								</div>
-								<div class="form-btn-field"><button type="submit">Send</button></div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="col-12 col-lg-4"><div class="footer-logo-wrapper">
-					<div class="footer-dots-img">
-						<figure><img src="/assets/default/svgs/footer-dots.svg" width="502" height="502" alt="footer dots image"></figure>
-					</div>
-					<div class="footer-logo-box">
-						<img src="/store/1/logo.png" width="185" height="38" alt="footer logo image">
-						<a href="/contact-us" class="mt-20"><span>Schedule Consultation</span></a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="footer-bottom pt-50">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
+			<div class="col-12">
+				<div class="footer-bottom pt-50">
 					<div class="footer-nav-holder pt-50">
-						<div class="row">
-							<div class="col-12 col-md-12 col-lg-12">
-								<div class="footer-nav-box">
-									<div class="footer-navs">
-										<ul>
-											<li><a href="/privacy" class="active">Privacy</a></li>
-											<li><a href="/cookies">Cookie Policy</a></li>
-											<li><a href="/terms-and-conditions">Terms & Condition</a></li>
-											<li><a href="/acceptable-use">Acceptable Use Policy</a></li>
-										</ul>
-									</div>
-									<div class="footer-social-links">
-										<ul> 
-											<li><a href="https://www.facebook.com/profile.php?id=61564277975339"><img src="/assets/default/svgs/facebook-svgrepo-com.svg" alt="facebook icon" width="50" height="50"></a></li>
-											<li><a href="https://www.instagram.com/rurera.official/gi"><img src="/assets/default/svgs/instagram-svgrepo-com.svg" alt="facebook icon" width="50" height="50"></a></li>
-										</ul>
-									</div>
-									</div>
-								</div>
+						<div class="footer-nav-box">
+							<div class="footer-navs">
+								<ul>
+									<li><a href="/privacy" class="active">Privacy</a></li>
+									<li><a href="/cookies">Cookie Policy</a></li>
+									<li><a href="/terms-and-conditions">Terms & Condition</a></li>
+									<li><a href="/acceptable-use">Acceptable Use Policy</a></li>
+								</ul>
+							</div>
+							<div class="footer-social-links">
+								<ul> 
+									<li><a href="https://www.facebook.com/profile.php?id=61564277975339"><img src="/assets/default/svgs/facebook-svgrepo-com.svg" alt="facebook icon" width="50" height="50"></a></li>
+									<li><a href="https://www.instagram.com/rurera.official/gi"><img src="/assets/default/svgs/instagram-svgrepo-com.svg" alt="facebook icon" width="50" height="50"></a></li>
+								</ul>
+							</div>
 							</div>
 						</div>
 					</div>
@@ -91,6 +89,7 @@
 			</div>
 		</div>
 	</div>
+	
 	<div class="scroll-btn">
 		<div class="round">
 			<div id="cta">
