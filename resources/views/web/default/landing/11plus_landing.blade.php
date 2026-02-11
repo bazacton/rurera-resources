86e40a105490e4c2abcd76a111b29b7e5360af9c
musman029 <malikusmanawan029@gmail.com>
2026-02-11 19:03:42 +0500

    Update 11plus_landing.blade.php
    parent-section-update

--- resources/views/web/default/landing/11plus_landing.blade.php
+++ resources/views/web/default/landing/11plus_landing.blade.php
@@ -714,87 +714,8 @@
     </section>
     @php $faq_items = isset( $faq_items )? $faq_items : array();@endphp
 {!! parseShortcode('[SC_11-plus-faqs-f]') !!}
+{!! parseShortcode('[SC_iseb-parent-account-section-f-end]') !!}
 
-<section class="parent-account-section mt-0" style="background-color: #0065ff;">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-12 col-lg-7 col-md-7">
-                    <div class="section-title mb-50">
-                        <h2 class="font-40 text-white">Parent account</h2>
-                        <p class="font-16 font-weight-500 text-white">Take control of your child's 11 plus journey with our dedicated parent hub.</p>
-                    </div>
-                    <ul>
-                        <li><span class="font-18">Ability to assign specific 11 plus tests and activities</span></li>
-                        <li><span class="font-18">Real-Time diagnostics and performance alerts</span></li>
-                        <li><span class="font-18">Track Child’s Learning progress across all subjects</span></li>
-                        <li><span class="font-18">Assign and monitor personalised Goals for your Child</span></li>
-                        <li><span class="font-18">Personalised learning controls and content filters</span></li>
-                        <li><span class="font-18">Easy to Manage Sibling Accounts from one dashboard</span></li>
-                    </ul>
-                </div>
-                <div class="col-12 col-lg-5 col-md-5">
-                    <div class="imb-box">
-                        <img src="/assets/default/img/banner-home.webp" width="548" height="570" alt="banner-home image">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    @php
-    $packages_only = isset( $packages )? $packages : array();
-    $show_details = isset( $show_details )? $show_details : true;
-    @endphp
-    <section class="rurera-setup-progress-section rurera-membership-section mb-0 pt-70"
-        data-currency_sign="{{getCurrencySign()}}" style="background-color: #fff;">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-lg-12 text-center">
-                    <div class="section-title text-center mb-40">
-                        <h2 class="font-40 text-dark-charcoal mb-0">Choose the right plan for you</h2>
-                        <p class="font-19 pt-10">Save more with annual pricing</p>
-                    </div>
-                </div>
-                <div class="col-12 col-lg-12 text-center">
-                    <div class="plan-switch-holder">
-                        <div class="plan-switch-option">
-                            <span class="switch-label">Pay Monthly</span>
-                            <div class="plan-switch">
-                                <div class="custom-control custom-switch"><input type="checkbox" name="disabled"
-                                        class="custom-control-input subscribed_for-field" value="12"
-                                        id="iNotAvailable" /><label class="custom-control-label"
-                                        for="iNotAvailable"></label></div>
-                            </div>
-                            <span class="switch-label">Pay Yearly</span>
-                        </div>
-                        <div class="save-plan"><span class="font-18 font-weight-500">Save 25%</span></div>
-                    </div>
-                </div>
-                <div class="col-lg-12 col-md-12 col-12 mx-auto">
-                    <div class="row">
-
-                        @include('web.default.pricing.packages_list',['subscribes' => array(), 'packages_only' =>
-                        $packages_only, 'show_details' => false])
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <div class="modal fade rurera-choose-membership" id="subscriptionModal">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                        aria-hidden="true">×</span></button>
-                <div class="modal-body">
-                    <div class="tab-content subscription-content" id="nav-tabContent">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</section>
 @endsection
 
 @push('scripts_bottom')
