5fa32be00fdb4191f15a9c620bf69db95d29e9cd
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-10 22:34:19 -0800

    Update cat4-preparation.blade.php

--- resources/views/web/default/landing/cat4-preparation.blade.php
+++ resources/views/web/default/landing/cat4-preparation.blade.php
@@ -923,8 +923,3 @@
 </section>
 
 @endsection
-
-@push('scripts_bottom')
-<script src="/assets/default/vendors/masonry/masonry.pkgd.min.js"></script>
-<script src="/assets/default/vendors/scroll-animation/wow.js"></script>
-@endpush
