5e55a82d0fc604ffc49af7ce358f2efefc1b2c14
musman029 <malikusmanawan029@gmail.com>
2026-02-11 19:35:42 +0500

    Update 11plus_landing.blade.php
    parent-section-update

--- resources/views/web/default/landing/11plus_landing.blade.php
+++ resources/views/web/default/landing/11plus_landing.blade.php
@@ -537,7 +537,7 @@
     {!! parseShortcode('[SC_accordion-slider-section-f]') !!}
     @php $faq_items = isset( $faq_items )? $faq_items : array();@endphp
 
-{!! parseShortcode('[SC_iseb-parent-account-section-f-end]') !!}
+{!! parseShortcode('[SC_iseb-parent-account-section-f]') !!}
 
     @php
     $packages_only = isset( $packages )? $packages : array();
