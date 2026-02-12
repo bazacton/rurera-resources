74664c4985ce4751765a4e5f5bc847b1a8bba269
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-11 22:47:11 -0800

    Update packages.blade.php

--- resources/views/web/default/elements/packages.blade.php
+++ resources/views/web/default/elements/packages.blade.php
@@ -7,8 +7,8 @@ $show_details = isset( $show_details )? $show_details : true;
 		<div class="row">
 			<div class="col-12 col-lg-12 text-center">
 				<div class="section-title text-center mb-40">
-					<h2 class="font-40 text-dark-charcoal mb-0">Choose the right plan for you</h2>
-					<p class="font-19 pt-10">Save more with annual pricing</p>
+					<h2 class="mb-0">Choose the right plan for you</h2>
+					<p class="pt-10">Save more with annual pricing</p>
 				</div>
 			</div>
 			<div class="col-12 col-lg-12 text-center">
