24cbf524f58521053f5af9fe52c8bd64c3f8d23b
Baz <baz.chimpstudio@gmail.com>
2026-02-13 12:55:13 +0500

    auto load
    auto load

--- public/assets/default/js/admin/question-create.js
+++ public/assets/default/js/admin/question-create.js
@@ -2361,6 +2361,9 @@ function _rureraform_properties_prepare(_object) {
     if(jQuery('select[name="rureraform-have_images"]').length > 0){
         have_images_function();
     }
+    if(jQuery('select[name="rureraform-label_type"]').length > 0){
+        jQuery('select[name="rureraform-label_type"]').change();
+    }
 
     if(jQuery('select[name="rureraform-no_of_options"]').val() > 0){
         jQuery('select[name="rureraform-no_of_options"]').each(function () {
