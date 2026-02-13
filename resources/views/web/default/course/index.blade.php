317becf643b7b52b4c992c8bf1cea394ca1ad95a
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-13 05:56:42 -0800

    Update index.blade.php

--- resources/views/web/default/course/index.blade.php
+++ resources/views/web/default/course/index.blade.php
@@ -196,13 +196,13 @@
 
                                                     @php $total_completion = rand(0,100); @endphp
 
-                                                    <!-- <div class="percent-holder">
+                                                    <div class="percent-holder rurera-hide">
                                                         <div class="chapter_percent circle-blue {{$completion_class}}" data-percent="{{$total_completion}}">
                                                             <div class="circle_inner">
                                                                 <div class="round_per"></div>
                                                             </div>
                                                         </div>
-                                                    </div> -->
+                                                    </div>
                                                     <div class="seg-progress" id="segBar" aria-label="Progress">
                                                         <div class="seg"><div class="seg-fill bg-primary" style="width: 100%;"></div></div>
                                                         <div class="seg"><div class="seg-fill bg-primary" style="width: 100%;"></div></div>
