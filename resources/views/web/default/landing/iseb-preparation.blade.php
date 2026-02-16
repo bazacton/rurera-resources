60da08ae04d66f6467bc278ebbf6b67ec1d4b23a
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-16 02:32:20 -0800

    Update iseb-preparation.blade.php

--- resources/views/web/default/landing/iseb-preparation.blade.php
+++ resources/views/web/default/landing/iseb-preparation.blade.php
@@ -950,7 +950,7 @@ const swiper1 = new Swiper('#featureSwiper', {
 });
 
 function updateCounter(swiper) {
-    const totalSlides = swiper.slides.length - swiper.loopedSlides * 3;
+    const totalSlides = swiper.slides.length - swiper.loopedSlides * 2;
     const currentSlide = swiper.realIndex + 1;
 
     $('#totalSlides').text(totalSlides);
