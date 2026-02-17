7750d64cf43e9ec9ba2587c111d0094c73e5303b
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-17 03:40:47 -0800

    Update footer_custom.blade.php

--- resources/views/web/default/includes/footer_custom.blade.php
+++ resources/views/web/default/includes/footer_custom.blade.php
@@ -80,7 +80,6 @@
 							<figure><img src="/assets/default/svgs/footer-dots.svg" width="502" height="502" alt="footer dots image"></figure>
 						</div>
 					</div>
-					<div class="rurera-name">Rurera</div>
 				</div>
 			</div>
 			<div class="col-12">
@@ -106,6 +105,7 @@
 					</div>
 				</div>
 			</div>
+			<div class="rurera-name">Rurera</div>
 		</div>
 	</div>
 	
