3b90ddbd9fcb2138386d8c8f866855d9d40a74b3
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-18 04:03:23 -0800

    Update index.blade.php

--- resources/views/web/default/course/index.blade.php
+++ resources/views/web/default/course/index.blade.php
@@ -1374,7 +1374,7 @@
                         var $resultElement = $('<a/>', { href: result.target });
                         var $resultTitle = $('<div/>', { class: 'result-title', text: result.name });
                         var $resultDescription = $('<div/>', { class: 'result-description', text: result.description });
-                        var $expandIcon = $('<span/>', { class: 'expand-icon', text: '▼' });
+                        var $expandIcon = $('<span/>', { class: 'expand-icon', text: '&#10005;' });
 
                         $resultElement.append($resultTitle, $resultDescription, $expandIcon);
                         $resultElement.on('click', function(e) {
