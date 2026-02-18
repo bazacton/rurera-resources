236b6fe39da6f65ba236166ed46993b56a6cca86
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-18 03:10:34 -0800

    Update index.blade.php

--- resources/views/web/default/course/index.blade.php
+++ resources/views/web/default/course/index.blade.php
@@ -771,6 +771,7 @@
 @endsection
 
 @push('scripts_bottom')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
     <script src="/assets/default/vendors/swiper-slider/swiper-bundle.min.js"></script>
 
     <script>
