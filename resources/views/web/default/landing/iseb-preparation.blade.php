b8685a7712c03025d62ad349e322d1e2d696fee5
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-22 21:21:41 -0800

    Update iseb-preparation.blade.php

--- resources/views/web/default/landing/iseb-preparation.blade.php
+++ resources/views/web/default/landing/iseb-preparation.blade.php
@@ -505,6 +505,9 @@
       <div class="fade-left"></div>
       <div class="fade-right"></div>
 
+      <!-- Pause Button -->
+      <button class="slider-toggle" id="sliderToggle">❚❚</button>
+
       <!-- Slider -->
       <div class="logo-slider" id="logoSlider">
         <div class="logo-track">
@@ -572,8 +575,6 @@
         </div>
       </div>
     </div>
-    <!-- Pause Button -->
-    <button class="slider-toggle" id="sliderToggle">❚❚</button>
   </div>
 </section>
     <section class="rurera-setup-progress-section rurera-membership-section mb-60" data-currency_sign="{{getCurrencySign()}}" style="background-color: #fff;">
