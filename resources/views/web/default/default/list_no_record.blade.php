5cf6dfa07db1d7f62556a81a2f0e1f47d005717f
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-02-25 02:25:49 -0800

    Update list_no_record.blade.php

--- resources/views/web/default/default/list_no_record.blade.php
+++ resources/views/web/default/default/list_no_record.blade.php
@@ -1,4 +1,4 @@
-<div class="no-record-found">
+<div class="no-record-found text-center">
     {!! isset( $no_records_data )? $no_records_data : '' !!}
     <span class="rurera-no-records">No Records Found</span>
 </div>
