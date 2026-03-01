1b56b905b049391e72279bb4c527c6d895ea8418
Baz <baz.chimpstudio@gmail.com>
2026-03-02 04:05:41 +0500

    section preview
    section preview

---
+++ resources/views/web/default/panel/quizzes/section_preview.blade.php
@@ -0,0 +1,9 @@
+<h1>
+    {{isset($section_data->name)? $section_data->name : ''}}
+</h1>
+
+<div class="section-no-of-questions">Questions: {{isset($section_data->no_of_questions)? $section_data->no_of_questions : 0}}</div>
+<div class="section-time">Time: {{isset($section_data->time)? $section_data->time : 0}}</div>
+<div class="quiz_section_instructions">{!! isset($section_data->instr)? $section_data->instr : '' !!}</div>
+
+<button type="button" class="section-start-quiz">Continue</button>
