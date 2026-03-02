9ad90c8096c8c71ea9dd16a5e5ff8dd131642c42
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-02 00:30:14 -0800

    Update enterance_exams_practice_start.blade.php

--- resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
+++ resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
@@ -112,7 +112,10 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
                                             <div class="questions-bar-box">
                                                 <div class="quiz-questions-bar-holder">
                                                     <div class="quiz-questions-bar">
-                                                        <span class="value-lable progress-bar-counter" data-title="No of Questions" style="left:0%"><span class="no-questions-value">0</span></span>
+                                                        <span class="value-lable progress-bar-counter" data-title="" style="left:0%">
+                                                            <span class="no-questions-lable">No of Questions</span>
+                                                            <span class="no-questions-value">0</span>
+                                                        </span>
                                                         <span class="bar-fill progress-bar-fill" data-title="" style="width: 0%;"></span>
                                                     </div>
                                                     <span class="coin-numbers">
