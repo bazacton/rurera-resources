f61388a110897e252628228fbd3e859950257600
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-01 22:03:04 -0800

    Update section_preview.blade.php

--- resources/views/web/default/panel/quizzes/section_preview.blade.php
+++ resources/views/web/default/panel/quizzes/section_preview.blade.php
@@ -1,5 +1,5 @@
 
-<div class="panel-border bg-white rounded-sm">
+<div class="panel-border bg-white rounded-sm mb-25">
     <h1>
         {{isset($section_data->name)? $section_data->name : ''}}
     </h1>
@@ -7,6 +7,10 @@
     <div class="section-no-of-questions">Questions: {{isset($section_data->no_of_questions)? $section_data->no_of_questions : 0}}</div>
     <div class="section-time">Time: {{isset($section_data->time)? $section_data->time : 0}}</div>
     <div class="quiz_section_instructions">{!! isset($section_data->instr)? $section_data->instr : '' !!}</div>
+</div>
+<div class="test-description">
+    <h2>Section Instructions</h2>
+    <p>Answer as many of these Reading Comprehension questions as you can in the 5 minutes provided. You can move on to the next section when the 5 minutes have passed, or if you have finished all of the questions in this section. Try your best, and good luck!</p>
 </div>
 
 
