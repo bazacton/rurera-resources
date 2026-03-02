2071c783144686f0fa9fe349a228ca51dfe84a90
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-01 22:29:20 -0800

    Update section_preview.blade.php

--- resources/views/web/default/panel/quizzes/section_preview.blade.php
+++ resources/views/web/default/panel/quizzes/section_preview.blade.php
@@ -4,24 +4,25 @@
         {{isset($section_data->name)? $section_data->name : ''}}
     </h1>
     <p class="section-lable font-18">Reading Comprehension <span class="font-14 d-block">Section 2 of 5</span></p>
-    <div class="section-no-of-questions">
-        <i class="icon-box">
-            <img src="/assets/default/svgs/ask.svg" alt="">
-        </i>
-        <div class="text-box">
-            {{isset($section_data->no_of_questions)? $section_data->no_of_questions : 0}} Questions
+    <div class="section-bottom">
+        <div class="section-no-of-questions">
+            <i class="icon-box">
+                <img src="/assets/default/svgs/ask.svg" alt="">
+            </i>
+            <div class="text-box">
+                {{isset($section_data->no_of_questions)? $section_data->no_of_questions : 0}} Questions
+            </div>
+            
         </div>
-        
-    </div>
-    <div class="section-time">
-        <i class="icon-box">
-            <img src="/assets/default/svgs/timer.svg" alt="">
-        </i>
-        <div class="text-box">
-            {{isset($section_data->time)? $section_data->time : 0}} mins <span>Time Limit</span>
+        <div class="section-time">
+            <i class="icon-box">
+                <img src="/assets/default/svgs/timer.svg" alt="">
+            </i>
+            <div class="text-box">
+                {{isset($section_data->time)? $section_data->time : 0}} mins <span>Time Limit</span>
+            </div>
         </div>
     </div>
-    
 </div>
 <div class="quiz_section_instructions">
     <h2>{!! isset($section_data->instr)? $section_data->instr : '' !!}</h2>
