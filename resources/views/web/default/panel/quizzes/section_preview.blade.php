1e217c09614a246ecb02ada18e67d9c50a30eb86
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-01 21:59:38 -0800

    Update section_preview.blade.php

--- resources/views/web/default/panel/quizzes/section_preview.blade.php
+++ resources/views/web/default/panel/quizzes/section_preview.blade.php
@@ -1,9 +1,13 @@
-<h1>
-    {{isset($section_data->name)? $section_data->name : ''}}
-</h1>
 
-<div class="section-no-of-questions">Questions: {{isset($section_data->no_of_questions)? $section_data->no_of_questions : 0}}</div>
-<div class="section-time">Time: {{isset($section_data->time)? $section_data->time : 0}}</div>
-<div class="quiz_section_instructions">{!! isset($section_data->instr)? $section_data->instr : '' !!}</div>
+<div class="panel-border bg-white rounded-sm">
+    <h1>
+        {{isset($section_data->name)? $section_data->name : ''}}
+    </h1>
+
+    <div class="section-no-of-questions">Questions: {{isset($section_data->no_of_questions)? $section_data->no_of_questions : 0}}</div>
+    <div class="section-time">Time: {{isset($section_data->time)? $section_data->time : 0}}</div>
+    <div class="quiz_section_instructions">{!! isset($section_data->instr)? $section_data->instr : '' !!}</div>
+</div>
+
 
 <button type="button" class="section-start-quiz">Continue</button>
