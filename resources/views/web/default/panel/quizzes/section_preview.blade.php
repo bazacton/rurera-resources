14530faee7dbc64ea367defea0f949c87d8c0d93
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-01 22:08:02 -0800

    Update section_preview.blade.php

--- resources/views/web/default/panel/quizzes/section_preview.blade.php
+++ resources/views/web/default/panel/quizzes/section_preview.blade.php
@@ -4,7 +4,7 @@
         {{isset($section_data->name)? $section_data->name : ''}}
     </h1>
 
-    <div class="section-no-of-questions">Questions: {{isset($section_data->no_of_questions)? $section_data->no_of_questions : 0}}</div>
+    <div class="section-no-of-questions">{{isset($section_data->no_of_questions)? $section_data->no_of_questions : 0}} Questions</div>
     <div class="section-time">Time: {{isset($section_data->time)? $section_data->time : 0}}</div>
     <div class="quiz_section_instructions">{!! isset($section_data->instr)? $section_data->instr : '' !!}</div>
 </div>
