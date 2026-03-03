d0b800de3f8b1ee6e0df471efd2119dc5e729f50
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-03 10:48:10 -0800

    Update user_top_bar.blade.php

--- resources/views/web/default/panel/includes/user_top_bar.blade.php
+++ resources/views/web/default/panel/includes/user_top_bar.blade.php
@@ -1,5 +1,224 @@
 
+<div class="user-profile-icons">
+    <ul>
+        <li class="sidebar-logo-mobile">
+            <a class="sidebar-logo" href="{{url('/')}}/" itemscope itemprop="url">
+                <img src="/assets/default/img/sidebar/logo.svg" alt="Rurera Logo" width="150" height="38"><span class="sidebar-logo-text"></span>
+            </a>
+        </li>
+        <li class="streak-dropdown">
+            <button type="button" class="streak-icon" aria-expanded="false" aria-controls="streakMenu">
+                <img src="/assets/default/svgs/streak-fire.svg" height="64" width="64" alt="streak-fire"> 1
+            </button>
+            <div id="streakMenu" class="streak-dropdown-content font-16">
+                <div class="streak-days">
+                    <div class="streak-day-head">
+                        <div class="text-box">
+                            <h4 class="font-16 font-weight-bold mb-5"><span>1</span> day streak</h4>
+                            <p class="sub">You've earned your longest streak ever!</p>
+                        </div>
+                        <div class="icon-box">
+                            <img src="/assets/default/svgs/streak-fire.svg" height="64" width="64" alt="streak-fire">
+                        </div>
+                    </div>
+                    <div class="days-list">
+                        <ul>
+                            <li class="frozen-streak">
+                                <span class="day">M</span>
+                                <span class="day-circle">
+                                    <img src="/assets/default/svgs/snowflake-cold.svg" height="64" width="64" alt="" aria-hidden="true">
+                                </span>
+                            </li>
+                            <li class="active-streak">
+                                <span class="day">T</span>
+                                <span class="day-circle">
+                                    <img src="/assets/default/svgs/fire2.svg" height="64" width="64" alt="" aria-hidden="true">
+                                </span>
+                            </li>
+                            <li class="active-streak">
+                                <span class="day">W</span>
+                                <span class="day-circle">
+                                    <img src="/assets/default/svgs/fire2.svg" height="64" width="64" alt="" aria-hidden="true">
+                                </span>
+                            </li>
+                            <li class="broken-streak">
+                                <span class="day">T</span>
+                                <span class="day-circle">
+                                    <img src="/assets/default/svgs/broken-heart.svg" height="64" width="64" alt="broken-heart">
+                                </span>
+                            </li>
+                            <li class="active-streak">
+                                <span class="day">F</span>
+                                <span class="day-circle">
+                                    <img src="/assets/default/svgs/fire2.svg" height="64" width="64" alt="" aria-hidden="true">
+                                </span>
+                            </li>
+                            <li class="not-started">
+                                <span class="day">S</span>
+                                <span class="day-circle">
+                                    <img src="/assets/default/svgs/red-circle.svg" height="64" width="64" alt="" aria-hidden="true">
+                                </span>
+                            </li>
+                            <li class="not-started">
+                                <span class="day">S</span>
+                                <span class="day-circle">
+                                    <img src="/assets/default/svgs/red-circle.svg" height="64" width="64" alt="" aria-hidden="true">
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="streak-society">
+                    <div class="society-content">
+                        <div class="icon-box">
+                            <span>🔒</span>
+                        </div>
+                        <div class="text-box">
+                            <h5>Streak Society</h5>
+                            <p>Reach a 7 day streak to unlock exclusive rewards.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="btn-holder">
+                    <button type="button" class="view-btn">View more</button>
+                </div>
 
+            </div>
+        </li>
+        @if(auth()->check() && (auth()->user()->isUser()))
+        <li class="dropdown dropdown-list-toggle">
+            <button type="button" class="dropdown-toggle-btn" aria-expanded="false" aria-controls="notificationsMenu">
+                <img src="/assets/default/img/panel-sidebar/1.png" alt="Notifications icon">
+                @if(!empty($unReadNotifications) and count($unReadNotifications))
+                {{ count($unReadNotifications) }}
+                @else
+                0
+                @endif
+            </button>
+
+            <!-- <div id="notificationsMenu" class="notifications-menu user-menu font-14">
+                <div class="dropdown-header font-16">{{ trans('admin/main.notifications') }}
+                    <div class="float-right">
+                        @can('admin_notifications_markAllRead')
+                        <a href="{{ getAdminPanelUrl() }}/notifications/mark_all_read">{{ trans('admin/main.mark_all_read') }}</a>
+                        @endcan
+                    </div>
+                </div>
+
+                <div class="dropdown-list-content dropdown-list-icons">
+                    @foreach($unReadNotifications as $unreadNotification)
+                    <a href="{{ getAdminPanelUrl() }}/notifications" class="dropdown-item">
+                        <div class="dropdown-item-icon bg-info text-white d-flex align-items-center justify-content-center">
+                            <i class="far fa-user"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <span class="notifications-lable">{{ $unreadNotification->title }}</span>
+                            <div class="time text-primary">{{ dateTimeFormat($unreadNotification->created_at,'Y M j | H:i') }}</div>
+                        </div>
+                    </a>
+                    @endforeach
+                </div>
+                <div class="dropdown-footer text-right">
+                    <a href="{{ getAdminPanelUrl() }}/notifications">{{ trans('admin/main.view_all') }} <i class="fas fa-chevron-right"></i></a>
+                </div>
+            </div> -->
+        </li>
+        {{--<li>
+            <div class="assignments">
+                <strong>
+                    <img src="/assets/default/img/sidebar/games.svg" alt="games">
+					@if( $authUser->game_time > 0)
+						{{getTimeWithText($authUser->game_time, false, false, false)}}
+					@else
+						0 m
+					@endif
+                </strong>
+            </div>
+        </li>--}}
+
+        <li class="dropdown dropdown-list-toggle">
+            <strong>
+                <img src="/assets/default/img/panel-sidebar/coins.svg" alt="Coins balance">
+                {{$authUser->getRewardPoints()}}
+            </strong>
+            <!-- <div class="user-coins-menu user-menu">
+                <div class="dropdown-header">
+                    <img src="/assets/default/img/panel-sidebar/coins.svg" alt="Coins balance">
+                    <div class="coins-text">
+                        Coins
+                        <span>You have {{$authUser->getRewardPoints()}} Coins <a href="/shop">Go to Shop</a></span>
+                    </div>
+                </div>
+            </div> -->
+        </li>
+        @endif
+
+        <li class="sidebar-no-mobile">
+            <div class="xs-w-100 d-flex align-items-center justify-content-between">
+                @if(!empty($authUser))
+                
+                @endif
+
+                @if(!empty($authUser))
+
+                @if(auth()->check() && (auth()->user()->isUser()))
+                <div class="dropdown">
+                    <a href="#!" class="navbar-user d-flex align-items-center dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-controls="userProfileDropdown" aria-expanded="false">
+                        <img src="{{ $authUser->getAvatar() }}" class="rounded-circle"
+                             alt="{{ $authUser->get_full_name() }}" width="40" height="40" itemprop="image"
+                             alt="rounded circle" loading="eager" title="rounded circle">
+                    </a>
+
+                    <!-- <div id="userProfileDropdown" class="user-profile-dropdown user-menu" aria-labelledby="dropdownMenuButton">
+                        <div class="dropdown-item user-nav-detail">
+                            <img src="{{ $authUser->getAvatar() }}" class="rounded-circle" alt="{{ $authUser->get_full_name() }}" width="400" height="400" itemprop="image"
+                                 alt="rounded circle" loading="eager" title="rounded circle">
+                            <span class="font-16 text-dark-blue user-name">{{ $authUser->get_full_name() }}</span>
+                            <a href="setting" class="font-16 text-dark-blue user-manage-btn">Manage Account</a>
+                        </div>
+                        <div class="d-md-none border-bottom mb-20 pb-10 text-right">
+                            <i class="close-dropdown" data-feather="x" width="32" height="32" class="mr-10"></i>
+                        </div>
+                        @php $parent_id = 0; @endphp
+                        @if( !empty( $profile_navs ) )
+                        <div class="user-nav-list">
+                            @foreach( $profile_navs as $profile_nav)
+
+                            @php $parent_id = isset($profile_nav['id'])? $profile_nav['id'] : $parent_id; @endphp
+
+                            <a class="dropdown-item " data-toggle="modal" data-target="#switch_user_modal" href="switch_user/{{$profile_nav['id']}}">
+                                <img src="{{ $profile_nav->getAvatar() }}" class="rounded-circle" alt="{{ $profile_nav['full_name'] }}" width="400" height="400" itemprop="image"
+                                     alt="rounded circle" loading="eager" title="rounded circle">
+                                @php $full_name = (isset( $navData['is_parent'] ) && $navData['is_parent'] == true)? 'Parent Dashboard' : $profile_nav['full_name']; @endphp
+                                <span class="font-16 text-dark-blue user-list-name">{{ $full_name }}</span>
+                                <span class="font-16 text-dark-blue user-list-email">{{ $profile_nav['email'] }}</span>
+                            </a>
+                            @endforeach
+                        </div>
+                        @endif
+                        <a class="dropdown-item nav-logout" href="/logout">
+                            <img src="/assets/default/img/icons/sidebar/logout.svg" height="24" itemprop="image"
+                                 width="24" alt="nav-icon" title="nav-icon" loading="eager">
+                            <span class="font-16 text-dark-blue">{{ trans('panel.log_out') }}</span>
+                        </a>
+                    </div> -->
+                </div>
+                @endif
+                @else
+                <div class="d-flex align-items-center ml-md-50">
+                    <a href="/login" class="py-5 px-15 mr-10 text-dark-blue font-16 login-btn">Log in</a>
+                    <a href="/register" class="py-5 px-15 text-dark-blue font-16 register-btn">Try for free</a>
+                </div>
+                @endif
+            </div>
+        </li>
+        <li class="menu-button-mobile">
+            <button class="navbar-toggler navbar-order" type="button" id="navbarToggle">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </li>
+    </ul>
+</div>
 
 <div class="modal fade switch_user_modal" 
     id="switch_user_modal" 
