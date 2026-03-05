d41c819602cff64fb5de85156cbb55943d872a18
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-05 00:57:53 -0800

    Update lists.blade.php

--- resources/views/admin/questions_generator/lists.blade.php
+++ resources/views/admin/questions_generator/lists.blade.php
@@ -90,7 +90,7 @@
             position: absolute;
             width: 100%;
             height: 100%;
-            background-color: rgba(0, 0, 0, .050);
+            background-color: rgba(0, 0, 0, .4);
             pointer-events: none;
             opacity: 0;
         }
