77366680cc68430c4b36583b04e139e3d27845ba
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-10 00:24:10 -0700

    Update lists.blade.php

--- resources/views/admin/questions_generator/lists.blade.php
+++ resources/views/admin/questions_generator/lists.blade.php
@@ -87,7 +87,7 @@
         /* Full-width, full-height modal (scoped via .mock-exam-modal class) */
         .mock-exam-modal:before {
             content: "";
-            position: absolute;
+            position: fixed;
             width: 100%;
             height: 100%;
             background-color: rgba(0, 0, 0, .4);
