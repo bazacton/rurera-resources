09ca1d1112c592613f78653d8404f7c613306763
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-09 23:08:52 -0700

    Update create.blade.php

--- resources/views/web/default/panel/set_work/create.blade.php
+++ resources/views/web/default/panel/set_work/create.blade.php
@@ -549,13 +549,9 @@
 
 
 
-                <div class="row">
+                <div class="row test@@">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-12">
-
-                        <div class="form-group rurera_common_hide_field timestables_heatmap">
-
-
-                        </div>
+                        <div class="form-group rurera_common_hide_field timestables_heatmap"></div>
 
                         <div class="form-group rurera_common_hide_field timestables_modes_selection">
                             <label class="input-label">Timestables Mode</label>
