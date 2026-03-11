93936d2a77f95a4bac611c093339cb48e1a3b949
Baz <baz.chimpstudio@gmail.com>
2026-03-11 11:14:12 +0500

    finish section btn
    finish section btn

--- resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
+++ resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
@@ -190,7 +190,7 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
                                                                 Finish
                                                                 <img src="/assets/default/svgs/review-btn-flag.svg" width="683" height="683" alt="review-btn-flag">
                                                             </a>
-                                                            <a href="javascript:;" class="finish-section rurera-hide1 mr-md-0">
+                                                            <a href="javascript:;" class="review-btn finish-section rurera-hide1 mr-md-0">
                                                                 Finish Section
                                                                 <img src="/assets/default/svgs/review-btn-flag.svg" width="683" height="683" alt="review-btn-flag">
                                                             </a>
