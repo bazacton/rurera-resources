16439f2bde435bfad4ceb8f252a20fe4f13f08e9
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-12 00:10:18 -0700

    Update index.blade.php

--- resources/views/web/default/tests/index.blade.php
+++ resources/views/web/default/tests/index.blade.php
@@ -289,7 +289,7 @@
 
 
 <div class="modal fade" id="rureraMockTestModal" tabindex="-1" role="dialog" aria-labelledby="rureraMockTestModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="rureraMockTestModalLabel">Choose how to complete this test</h5>
