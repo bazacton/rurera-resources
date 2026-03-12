9725ea97d090c0cf704d469fa4911e738492927d
Baz <baz.chimpstudio@gmail.com>
2026-03-12 15:25:05 +0500

    last question
    last question

--- public/assets/default/js/question-layout.js
+++ public/assets/default/js/question-layout.js
@@ -69,7 +69,10 @@ $("body").off("click", ".question-submit-btn").on("click", ".question-submit-btn
 	var thisBlock = $(".rurera-question-block.active");
     var thisForm = thisBlock.find('form');
     if (typeof beforeQuestionSubmit === "function") {
-        return beforeQuestionSubmit();
+        returnType =  beforeQuestionSubmit();
+        if (returnType == false) {
+            return false;
+        }
     }
 
 
