1009f4ab06a93b89eed64f04f22cf990d561b11f
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-12 01:47:05 -0700

    Update index.blade.php

--- resources/views/web/default/tests/index.blade.php
+++ resources/views/web/default/tests/index.blade.php
@@ -192,6 +192,10 @@
                                     1 x English &amp; Verbal Reasoning paper
                                 </li>
                                 <li>
+                                    <span class="icon-box">
+                                        <img src="/media/topic_parts/1050/11.svg" width="50" height="50" alt="" area-hidden="true">
+                                        <img src="/media/topic_parts/1050/12 (1).svg" width="50" height="50" alt="" area-hidden="true">
+                                    </span>
                                     1 x Non-Verbal Reasoning &amp; Maths paper
                                 </li>
                             </ul>
@@ -232,7 +236,13 @@
                                     </span>
                                     1 x English &amp; Verbal Reasoning paper
                                 </li>
-                                <li>1 x Non-Verbal Reasoning &amp; Maths paper</li>
+                                <li>
+                                    <span class="icon-box">
+                                        <img src="/media/topic_parts/1050/11.svg" width="50" height="50" alt="" area-hidden="true">
+                                        <img src="/media/topic_parts/1050/12 (1).svg" width="50" height="50" alt="" area-hidden="true">
+                                    </span>
+                                    1 x Non-Verbal Reasoning &amp; Maths paper
+                                </li>
                             </ul>
                         </div>
                     
@@ -273,7 +283,13 @@
                                     </span>
                                     1 x English &amp; Verbal Reasoning paper
                                 </li>
-                                <li>1 x Non-Verbal Reasoning &amp; Maths paper</li>
+                                <li>
+                                    <span class="icon-box">
+                                        <img src="/media/topic_parts/1050/11.svg" width="50" height="50" alt="" area-hidden="true">
+                                        <img src="/media/topic_parts/1050/12 (1).svg" width="50" height="50" alt="" area-hidden="true">
+                                    </span>
+                                    1 x Non-Verbal Reasoning &amp; Maths paper
+                                </li>
                             </ul>
                         </div>
                     
