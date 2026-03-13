d000b3d04cd231c1ce32e17947044d1fd2b5aa34
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-12 21:49:24 -0700

    Update index.blade.php

--- resources/views/web/default/vocabulary/index.blade.php
+++ resources/views/web/default/vocabulary/index.blade.php
@@ -135,11 +135,11 @@
                             </span>
                         </div>
                         @endif
-                    </div>
-                    <div class="spell-top-right font-12 font-weight-500">
-                        <a href="/spells/{{isset( $dataObj->quizYear->slug )? $dataObj->quizYear->slug : ''}}/{{$dataObj->quiz_slug}}/spelling-list" class="words-count" aria-label="View spelling list: {{$total_questions}} words"><span class="font-16" aria-hidden="true">{{$total_questions}}</span>word(s)</a>
-                        <a href="/spells/{{isset( $dataObj->quizYear->slug )? $dataObj->quizYear->slug : ''}}/{{$dataObj->quiz_slug}}/spelling-list" class="words-count" aria-label="View spelling list: {{$total_questions}} Practiced"><span class="font-16" aria-hidden="true">12</span>Practiced</a>
-                        <a href="/spells/{{isset( $dataObj->quizYear->slug )? $dataObj->quizYear->slug : ''}}/{{$dataObj->quiz_slug}}/spelling-list" class="words-count" aria-label="View spelling list: {{$total_questions}} Mastered"><span class="font-16" aria-hidden="true">22</span>Mastered</a>
+                        <div class="spell-top-right font-12 font-weight-500">
+                            <a href="/spells/{{isset( $dataObj->quizYear->slug )? $dataObj->quizYear->slug : ''}}/{{$dataObj->quiz_slug}}/spelling-list" class="words-count" aria-label="View spelling list: {{$total_questions}} words"><span class="font-16" aria-hidden="true">{{$total_questions}}</span>word(s)</a>
+                            <a href="/spells/{{isset( $dataObj->quizYear->slug )? $dataObj->quizYear->slug : ''}}/{{$dataObj->quiz_slug}}/spelling-list" class="words-count" aria-label="View spelling list: {{$total_questions}} Practiced"><span class="font-16" aria-hidden="true">12</span>Practiced</a>
+                            <a href="/spells/{{isset( $dataObj->quizYear->slug )? $dataObj->quizYear->slug : ''}}/{{$dataObj->quiz_slug}}/spelling-list" class="words-count" aria-label="View spelling list: {{$total_questions}} Mastered"><span class="font-16" aria-hidden="true">22</span>Mastered</a>
+                        </div>
                     </div>
                 </div>
             </div>
