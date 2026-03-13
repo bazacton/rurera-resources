6a86e39c4f1a7b17a48de405e97cda44ca4bcb69
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-12 23:23:20 -0700

    Update 11plus_landing.blade.php

--- resources/views/web/default/landing/11plus_landing.blade.php
+++ resources/views/web/default/landing/11plus_landing.blade.php
@@ -10,7 +10,7 @@
         <div class="container h-100">
             <div class="row h-100">
                 <div class="col-12 col-md-6 col-lg-6">
-                    <h1 class="font-72 font-weight-bold"><span class="text-scribble mr-10">11 Plus</span>and Entrance Exams</h1>
+                    <h1><span class="text-scribble mr-10">11 Plus</span>and Entrance Exams</h1>
                     <h2 class="mb-15 font-30">Unlock Opportunities: Ace the 11 Plus Exam!
                     </h2>
                     <p class="font-19">The 11 Plus is a selective entrance exam for grammar and independent schools in England, 
