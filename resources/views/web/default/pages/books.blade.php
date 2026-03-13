dc376ddb744b485e419cd2c93a5e8c52931df072
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-12 23:20:15 -0700

    Update books.blade.php

--- resources/views/web/default/pages/books.blade.php
+++ resources/views/web/default/pages/books.blade.php
@@ -23,7 +23,7 @@
                     <div class="top-search-form panel-border bg-white rounded-sm p-25">
                         <div class="search-input bg-white">
                             <form action="/books" method="get">
-                                <div class="form-group d-flex align-items-center m-0 mb-25">
+                                <div class="form-group d-flex align-items-center m-0 mb-lg-25">
                                     <div class="input-field d-flex align-items-center rounded-sm">
                                         <img src="/assets/default/svgs/search2.svg" alt="" aria-hidden="true">
                                         <input type="text" name="search" id="search-input" class="form-control border-0 font-14" value="{{ request()->get('search','') }}" placeholder="Search by Author, Title, or Keyword"/>
