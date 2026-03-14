666cb6d1d74fccb9efd6fab5d79480e2ee0a8359
Baz <baz.chimpstudio@gmail.com>
2026-03-15 00:25:31 +0500

    tests
    tests

--- resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
+++ resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
@@ -571,6 +571,7 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
                 if (duration_type == 'total_practice') {
                     if (parseInt(quiz_timer_counter) == 0) {
                         alert('timer is 0');
+                        onSectionMoveConfirm();
                         clearInterval(Quizintervals);
                         //$(".review-btn").click();
                         if ($('.question-review-btn').length > 0) {
