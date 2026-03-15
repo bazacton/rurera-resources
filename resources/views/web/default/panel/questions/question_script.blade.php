6543124075f2db39bbf44da7e15874a3e8058796
Baz <baz.chimpstudio@gmail.com>
2026-03-16 01:38:59 +0500

    tests
    tests

--- resources/views/web/default/panel/questions/question_script.blade.php
+++ resources/views/web/default/panel/questions/question_script.blade.php
@@ -2,15 +2,19 @@
     initQuestionScript(document.currentScript);
     function initQuestionScript(scriptTag){
 
+        if(!scriptTag) return;
+
         var questionBlock = scriptTag.closest('.rurera-question-block');
         if(!questionBlock) return;
 
         var field_type = "{{ $field_type }}";
         var field_id = "{{ $field_key }}";
         var user_selected_value = "{{ is_object($user_selected_value)? '' : $user_selected_value }}";
+        var user_selected_key = "{{ $user_selected_key }}";
         var correct_value = "{{ $correct_value }}";
         var is_result_question = "{{$is_result_question}}";
 
+        /* TEXT / NUMBER */
         if (field_type === 'text' || field_type === 'number') {
 
             var textField = questionBlock.querySelector('#field-' + field_id);
@@ -24,7 +28,12 @@
                 );
             }
 
-        } else if (field_type === 'radio') {
+        }
+
+        /* RADIO */
+        else if (field_type === 'radio') {
+
+            var correctClass = (user_selected_value !== correct_value) ? 'wrong' : 'correct';
 
             var userInput = questionBlock.querySelector('[name="field-'+field_id+'"][value="'+user_selected_value+'"]');
             var correctInput = questionBlock.querySelector('[name="field-'+field_id+'"][value="'+correct_value+'"]');
@@ -32,8 +41,104 @@
             if(userInput) userInput.checked = true;
 
             if(is_result_question == true){
-                if(userInput) userInput.closest('.field-holder')?.classList.add(user_selected_value !== correct_value ? 'wrong':'correct');
-                if(correctInput) correctInput.closest('.field-holder')?.classList.add('correct');
+                if(userInput){
+                    var holder = userInput.closest('.field-holder');
+                    if(holder) holder.classList.add(correctClass);
+                }
+
+                if(correctInput){
+                    var holder2 = correctInput.closest('.field-holder');
+                    if(holder2) holder2.classList.add('correct');
+                }
+            }
+
+        }
+
+        /* CHECKBOX */
+        else if (field_type === 'checkbox') {
+
+            var correctClass = (user_selected_value !== correct_value) ? 'wrong' : 'correct';
+
+            var userInput = questionBlock.querySelector('[name="field-'+field_id+'"][value="'+user_selected_value+'"]');
+            var correctInput = questionBlock.querySelector('[name="field-'+field_id+'"][value="'+correct_value+'"]');
+
+            if(userInput) userInput.checked = true;
+
+            if(is_result_question == true){
+                if(userInput){
+                    var field = userInput.closest('.form-field');
+                    if(field) field.classList.add(correctClass);
+                }
+
+                if(correctInput){
+                    var field2 = correctInput.closest('.form-field');
+                    if(field2) field2.classList.add('correct');
+                }
+            }
+
+        }
+
+        /* TRUE FALSE QUIZ */
+        else if (field_type === 'truefalse_quiz') {
+
+            var correctClass = (user_selected_value !== correct_value) ? 'wrong' : 'correct';
+
+            var userInput = questionBlock.querySelector('[name="field-'+field_id+'"][value="'+user_selected_value+'"]');
+            var correctInput = questionBlock.querySelector('[name="field-'+field_id+'"][value="'+correct_value+'"]');
+
+            if(userInput) userInput.checked = true;
+
+            if(is_result_question == true){
+                if(userInput){
+                    var field = userInput.closest('.form-field');
+                    if(field) field.classList.add(correctClass);
+                }
+
+                if(correctInput){
+                    var field2 = correctInput.closest('.form-field');
+                    if(field2) field2.classList.add('correct');
+                }
+            }
+
+        }
+
+        /* TEXTAREA */
+        else if (field_type === 'textarea') {
+
+            var textField = questionBlock.querySelector('#field-' + field_id);
+            if(!textField) return;
+
+            textField.value = user_selected_value;
+
+            if(is_result_question == true){
+                textField.classList.add(
+                    user_selected_value !== correct_value ? 'wrong' : 'correct'
+                );
+            }
+
+        }
+
+        /* DEFAULT (OTHER FIELD TYPES) */
+        else {
+
+            var fieldInputs = questionBlock.querySelectorAll('[name="field-'+field_id+'"]');
+            var correctInput = questionBlock.querySelector('[name="field-'+field_id+'"][value="'+correct_value+'"]');
+
+            if(user_selected_value !== correct_value){
+                fieldInputs.forEach(function(input){
+
+                    var holder = input.closest('.field-holder');
+                    if(holder) holder.classList.add('wrong');
+
+                    var formField = input.closest('.form-field');
+                    if(formField) formField.classList.add('wrong');
+
+                });
+            }
+
+            if(correctInput){
+                correctInput.classList.add('correct-mark');
+                correctInput.checked = true;
             }
 
         }
