216b3586661d5e47547ee53d5ae2ccad186e5da3
Baz <baz.chimpstudio@gmail.com>
2026-03-16 01:31:33 +0500

    question script
    question script

--- resources/views/web/default/panel/questions/question_script.blade.php
+++ resources/views/web/default/panel/questions/question_script.blade.php
@@ -1,71 +1,107 @@
 <script>
-    var field_type = "{{ $field_type }}";
-    var field_id = "{{ $field_key }}";
-    var user_selected_value = "{{ is_object($user_selected_value)? '' : $user_selected_value }}";
-    var user_selected_key = "{{ $user_selected_key }}";
-    var correct_value = "{{ $correct_value }}";
-	var is_result_question = "{{$is_result_question}}";
+    (function(){
 
-    if (field_type === 'text' || field_type === 'number') {
-        var textField = document.getElementById('field-' + field_id);
-        textField.value = user_selected_value;
-		if( is_result_question == true){
-			if (user_selected_value !== correct_value) {
-				textField.classList.add('wrong');
-			} else {
-				textField.classList.add('correct');
-			}
-		}
-    } else if (field_type === 'radio') {
-        var textField = document.getElementById('field-' + field_id);
-        var correctClass = (user_selected_value !== correct_value) ? 'wrong' : 'correct';
-		if( is_result_question == true){
-			document.querySelector('[name="field-' + field_id + '"][value="' + user_selected_value + '"]').closest('.field-holder').classList.add(correctClass);
-			document.querySelector('[name="field-' + field_id + '"][value="' + correct_value + '"]').closest('.field-holder').classList.add('correct');
-		}
-		document.querySelector('[name="field-' + field_id + '"][value="' + user_selected_value + '"]').checked = true;
-    } else if (field_type === 'checkbox') {
-        var textField = document.getElementById('field-' + field_id);
-        var correctClass = (user_selected_value !== correct_value) ? 'wrong' : 'correct';
-		if( is_result_question == true){
-			document.querySelector('[name="field-' + field_id + '"][value="' + user_selected_value + '"]').closest('.form-field').classList.add(correctClass);
-			document.querySelector('[name="field-' + field_id + '"][value="' + correct_value + '"]').closest('.form-field').classList.add('correct');
-		}
-		document.querySelector('[name="field-' + field_id + '"][value="' + user_selected_value + '"]').checked = true;
-    } else if (field_type === 'truefalse_quiz') {
-        var textField = document.getElementById('field-' + field_id);
-        var correctClass = (user_selected_value !== correct_value) ? 'wrong' : 'correct';
+        var field_type = "{{ $field_type }}";
+        var field_id = "{{ $field_key }}";
+        var user_selected_value = "{{ is_object($user_selected_value)? '' : $user_selected_value }}";
+        var user_selected_key = "{{ $user_selected_key }}";
+        var correct_value = "{{ $correct_value }}";
+        var is_result_question = "{{$is_result_question}}";
 
-		if( is_result_question == true){
-			document.querySelector('[name="field-' + field_id + '"][value="' + user_selected_value + '"]').closest('.form-field').classList.add(correctClass);
-			document.querySelector('[name="field-' + field_id + '"][value="' + correct_value + '"]').closest('.form-field').classList.add('correct');
-		}
-		document.querySelector('[name="field-' + field_id + '"][value="' + user_selected_value + '"]').checked = true;
-    }else if (field_type === 'textarea') {
-		var textField = document.getElementById('field-' + field_id);
-		textField.value = user_selected_value;
-		if( is_result_question == true){
-			if (user_selected_value !== correct_value) {
-				textField.classList.add('wrong');
-			} else {
-				textField.classList.add('correct');
-			}
-		}
-	}  else if (field_type === 'truefalse_quiz') {
+        // Get current question block (script's closest parent)
+        var questionBlock = document.currentScript.closest('.rurera-question-block');
 
-	} else {
-        var fieldInputs = document.querySelectorAll('[name="field-' + field_id + '"]');
-        var correctInput = document.querySelector('[name="field-' + field_id + '"][value="' + correct_value + '"]');
+        if (!questionBlock) return;
 
-        if (user_selected_value !== correct_value) {
-            fieldInputs.forEach(function(input) {
-                input.closest('.field-holder').classList.add('wrong');
-                input.closest('.form-field').classList.add('wrong');
-            });
+        if (field_type === 'text' || field_type === 'number') {
+
+            var textField = questionBlock.querySelector('#field-' + field_id);
+            if (!textField) return;
+
+            textField.value = user_selected_value;
+
+            if (is_result_question == true) {
+                if (user_selected_value !== correct_value) {
+                    textField.classList.add('wrong');
+                } else {
+                    textField.classList.add('correct');
+                }
+            }
+
+        } else if (field_type === 'radio') {
+
+            var correctClass = (user_selected_value !== correct_value) ? 'wrong' : 'correct';
+
+            var userInput = questionBlock.querySelector('[name="field-' + field_id + '"][value="' + user_selected_value + '"]');
+            var correctInput = questionBlock.querySelector('[name="field-' + field_id + '"][value="' + correct_value + '"]');
+
+            if (userInput) userInput.checked = true;
+
+            if (is_result_question == true) {
+                if (userInput) userInput.closest('.field-holder')?.classList.add(correctClass);
+                if (correctInput) correctInput.closest('.field-holder')?.classList.add('correct');
+            }
+
+        } else if (field_type === 'checkbox') {
+
+            var correctClass = (user_selected_value !== correct_value) ? 'wrong' : 'correct';
+
+            var userInput = questionBlock.querySelector('[name="field-' + field_id + '"][value="' + user_selected_value + '"]');
+            var correctInput = questionBlock.querySelector('[name="field-' + field_id + '"][value="' + correct_value + '"]');
+
+            if (userInput) userInput.checked = true;
+
+            if (is_result_question == true) {
+                if (userInput) userInput.closest('.form-field')?.classList.add(correctClass);
+                if (correctInput) correctInput.closest('.form-field')?.classList.add('correct');
+            }
+
+        } else if (field_type === 'truefalse_quiz') {
+
+            var correctClass = (user_selected_value !== correct_value) ? 'wrong' : 'correct';
+
+            var userInput = questionBlock.querySelector('[name="field-' + field_id + '"][value="' + user_selected_value + '"]');
+            var correctInput = questionBlock.querySelector('[name="field-' + field_id + '"][value="' + correct_value + '"]');
+
+            if (userInput) userInput.checked = true;
+
+            if (is_result_question == true) {
+                if (userInput) userInput.closest('.form-field')?.classList.add(correctClass);
+                if (correctInput) correctInput.closest('.form-field')?.classList.add('correct');
+            }
+
+        } else if (field_type === 'textarea') {
+
+            var textField = questionBlock.querySelector('#field-' + field_id);
+            if (!textField) return;
+
+            textField.value = user_selected_value;
+
+            if (is_result_question == true) {
+                if (user_selected_value !== correct_value) {
+                    textField.classList.add('wrong');
+                } else {
+                    textField.classList.add('correct');
+                }
+            }
+
+        } else {
+
+            var fieldInputs = questionBlock.querySelectorAll('[name="field-' + field_id + '"]');
+            var correctInput = questionBlock.querySelector('[name="field-' + field_id + '"][value="' + correct_value + '"]');
+
+            if (user_selected_value !== correct_value) {
+                fieldInputs.forEach(function(input) {
+                    input.closest('.field-holder')?.classList.add('wrong');
+                    input.closest('.form-field')?.classList.add('wrong');
+                });
+            }
+
+            if (correctInput) {
+                correctInput.classList.add('correct-mark');
+                correctInput.checked = true;
+            }
         }
 
-        correctInput.classList.add('correct-mark');
-        correctInput.checked = true;
-    }
-
+    })();
 </script>
