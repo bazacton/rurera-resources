fbc54d376d792178f6f3e13701e15e79165389aa
Baz <baz.chimpstudio@gmail.com>
2026-03-17 01:05:02 +0500

    tests
    tests

--- resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
+++ resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
@@ -42,7 +42,7 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
                         <div class="step-block">Test Completed!!!</div>
 
                     </div>
-                    <div data-result_attempt_id="{{isset($quizAttempt->id)? $quizAttempt->id : 0}}" data-quiz_result_id="{{isset($newQuizStart->id)? $newQuizStart->id : 0}}" class="question-area-block" data-active_question_id="{{$active_question_id}}" data-questions_layout="{{json_encode($questions_layout)}}">
+                    <div data-device_id="{{isset($device_id)? $device_id : 0}}" data-result_attempt_id="{{isset($quizAttempt->id)? $quizAttempt->id : 0}}" data-quiz_result_id="{{isset($newQuizStart->id)? $newQuizStart->id : 0}}" class="question-area-block" data-active_question_id="{{$active_question_id}}" data-questions_layout="{{json_encode($questions_layout)}}">
                         <a href="javascript:;" class="load-more-questions rurera-hide">Load More Questions</a>
                         <div class="question-area dis-arrows1" data-total_questions="{{$total_questions}}">
                             <div class="correct-appriciate" style="display:none"></div>
