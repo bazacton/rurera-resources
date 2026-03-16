a5a8f396b1f45d6532ba0bcfdc58b08e04a12f99
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-16 00:31:38 -0700

    Update enterance_exams_practice_start.blade.php

--- resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
+++ resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
@@ -829,7 +829,7 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
 
         var section_move_html = `
 
-            <h2 class="fw-bold mb-4">Well done!</h2>
+            <h2 class="fw-bold mb-4 font-16 font-weight-bold">Well done!</h2>
 
             <div class="d-flex justify-content-center align-items-center gap-3 mb-4">
                 <div class="border rounded px-3 py-2 bg-light">
