74e2beeada12b42d5d27782f2c37d562f7d05163
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-16 00:47:03 -0700

    Update enterance_exams_practice_start.blade.php

--- resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
+++ resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
@@ -831,7 +831,7 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
 
             <h2 class="fw-bold mb-4 font-16 font-weight-bold text-left">Well done!</h2>
 
-            <div class="d-flex justify-content-center align-items-center gap-3 mb-4">
+            <div class="d-flex justify-content-center align-items-center gap-3 mb-4 time-left">
                 <div class="border rounded px-3 py-2 bg-light">
                     <span class="me-2">⏱</span>
                     <span class="fw-bold text-warning">${getTimeStr(quiz_timer_remaining)}</span>
