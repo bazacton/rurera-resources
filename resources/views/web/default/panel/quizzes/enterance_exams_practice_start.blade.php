703a2fd8ca6340ddb57279bacaa5ce07f3e7d8d8
hafiz-yasir <85584581+hafiz-yasir@users.noreply.github.com>
2026-03-16 02:28:00 -0700

    Update enterance_exams_practice_start.blade.php

--- resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
+++ resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
@@ -897,9 +897,9 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
 
         var section_move_html = `
 
-            <h2 class="fw-bold mb-20 font-16">Well done!</h2>
+            <h2 class="font-weight-bold mb-20 font-16 text-left">Well done!</h2>
 
-            <div class="d-flex justify-content-start align-items-center gap-3 mb-20 font-14">
+            <div class="d-flex justify-content-start align-items-center gap-3 mb-20 font-14 time-left px-20">
                 <div class="border rounded px-3 py-2 bg-light">
                     <span class="me-2">⏱</span>
                     <span class="fw-bold text-warning">${getTimeStr(quiz_timer_remaining)}</span>
@@ -911,7 +911,7 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
                 Now is a great time to check your answers and to try to answer the
                 following questions that you've skipped:
             </p>
-            <div class="d-flex justify-content-center gap-3 mb-5 flex-wrap">
+            <div class="d-flex justify-content-start px-10 gap-3 mb-5 flex-wrap">
                 ${buttonsHTML}
             </div>
 
