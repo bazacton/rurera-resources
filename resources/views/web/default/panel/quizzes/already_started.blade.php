df4687c6d5fea4bfd5c03f9810200953bfb469c6
Baz <baz.chimpstudio@gmail.com>
2026-03-17 00:23:22 +0500

    already started
    already started

--- resources/views/web/default/panel/quizzes/already_started.blade.php
+++ resources/views/web/default/panel/quizzes/already_started.blade.php
@@ -19,39 +19,12 @@
                         <div class="learning-text">
                             <h5 class="font-30 mb-10">Already Started</h5>
                             <p class="font-18">Get PLUS today for accsecc to live student activity and progress from the freedom of your desk.</p>
-                            <button class="get-btn">
-                                <span class="btn-animate-left">
-                                    <span class="shadow-sm">
-                                        <span class="shadow-animate"><img src="/assets/default/img/twinkle-w-shadow.png" alt=""></span>
-                                        <span class="shadow-animate" style="animation-delay: .55s;"><img src="/assets/default/img/twinkle-w-shadow.png" alt=""></span>
-                                    </span>
-                                    <span class="shadow-lg" style="animation-delay: .7s;">
-                                        <span class="shadow-animate"><img src="/assets/default/img/twinkle-w-shadow.png" alt=""></span>
-                                    </span>
-                                </span>
-                                <span class="btn-text"> <img src="/assets/default/svgs/croun-btn.svg" width="20" height="20" alt=""> Get <strong>PLUS</strong> Today</span>
-                                <span class="btn-animate-right">
-                                    <span class="shadow-sm">
-                                        <span class="shadow-animate"><img src="/assets/default/img/twinkle-w-shadow.png" alt=""></span>
-                                        <span class="shadow-animate" style="animation-delay: .25s;"><img src="/assets/default/img/twinkle-w-shadow.png" alt=""></span>
-                                    </span>
-                                    <span class="shadow-lg">
-                                        <span class="shadow-animate" style="animation-delay: .8s;"><img src="/assets/default/img/twinkle-w-shadow.png" alt=""></span>
-                                    </span>
-                                </span>
-                            </button>
-                            <div class="learn-option">
-                                <span>Want to learn more about PLUS?</span>
-                                <a href="#" class="learn-book-btn">Book a demo</a>
-                            </div>
+                            <button type="button" class="">Continue</button>
                         </div>
 
                     </div>
                 </div>
-
             </div>
-
-
         </div>
     </div>
 </div>
