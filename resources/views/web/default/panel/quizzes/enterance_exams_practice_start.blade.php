a66405f6e7f0f475f5ce71c5c67f4b2d0c12d118
Baz <baz.chimpstudio@gmail.com>
2026-03-20 03:06:26 +0500

    tests
    tests

--- resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
+++ resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
@@ -328,12 +328,14 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
                                                                     </span>
 
 
+                                                                        @if($question_counter == 1)
                                                                         <div class="question-comprehension-block rurera-hide">
                                                                             <h3>What is Lorem Ipsum?</h3>
                                                                             <p>
                                                                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                                                                             </p>
                                                                         </div>
+                                                                        @endif
                                                                         {!! $questionLayout !!}
 
                                                                     </div>
@@ -1095,10 +1097,11 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
     }
 
     function afterQuestionActive(){
-        alert('afterQuestionActive');
         const $active = $(".quiz-section-data.active").find(".rurera-question-block.active");
-        var comprehension_html = $active.find('.question-comprehension-block').html();
-        alert(comprehension_html);
+        var comprehension_html = '';
+        if($active.find('.question-comprehension-block').length > 0) {
+            comprehension_html = $active.find('.question-comprehension-block').html();
+        }
         $(".comprehension-block").html(comprehension_html);
     }
 
