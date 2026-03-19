a255dc1a652b552f070f957432def603d61e17ee
Baz <baz.chimpstudio@gmail.com>
2026-03-20 02:49:29 +0500

    tests
    tests

--- resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
+++ resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
@@ -127,6 +127,12 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
                                             {!! $section_layout !!}
 
                                         </div>
+                                        <div class="comprehension-block">
+                                            <h3>What is Lorem Ipsum?</h3>
+                                            <p>
+                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                            </p>
+                                        </div>
                                         <div class="quiz-section-questions rurera-hide">
 
 
@@ -196,14 +202,7 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
                                                                         Question: {{$question_counter}} of {{count($questions_layout)}}
                                                                     </span>
 
-                                                                        @if($question_counter == 1)
-                                                                            <div class="comprehension-block">
-                                                                                <h3>What is Lorem Ipsum?</h3>
-                                                                                <p>
-                                                                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                                                                                </p>
-                                                                            </div>
-                                                                        @endif
+
                                                                         {!! $questionLayout !!}
 
                                                                     </div>
