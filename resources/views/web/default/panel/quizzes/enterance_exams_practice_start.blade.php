57835fdcf71048401f7b9c2301a20106c2a0242d
Baz <baz.chimpstudio@gmail.com>
2026-03-24 11:01:12 +0500

    tests
    tests

--- resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
+++ resources/views/web/default/panel/quizzes/enterance_exams_practice_start.blade.php
@@ -1539,12 +1539,12 @@ $incorrect_answer_explaination = true;//isset($incorrect_answer_explaination)? $
 
     function afterSectionFinishConfirm(){
         //alert('afterSectionFinishConfirm');
+        $(".app-wrap").addClass('rurera-hide');
         var current_question_id =  $(".quiz-section-data.active").find(".quiz-pagination li.active").attr('data-question_id');
         $(".quiz-section-data.active").attr('data-section_finish_confirm', 'yes');
         $(".quiz-section-data.active").attr('data-finish-exclude_id', current_question_id);
         $('.question-submit-btn').attr('data-bypass_validation', 'yes');
         $(".question-submit-btn").click();
-        $(".app-wrap").addClass('rurera-hide');
 
 
         initQuestionStatusPopover();
