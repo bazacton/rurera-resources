60f7dee9fbd0bcf1a834f9994895fe796842d243
Baz <baz.chimpstudio@gmail.com>
2026-03-31 12:44:58 +0500

    setwork play link
    setwork play link

--- resources/views/web/default/panel/set_work/set_work_listing.blade.php
+++ resources/views/web/default/panel/set_work/set_work_listing.blade.php
@@ -23,7 +23,7 @@
                     </div>
                     <div class="col-md-5 rurera-tasks-row-right">
                         <div class="rurera-tasks-actions font-14">
-                            <button class="rurera-tasks-btn rurera-tasks-btn-outline">Continue</button>
+                            <a href="{{$assignmentLink}}" class="rurera-tasks-btn rurera-tasks-btn-outline">Continue</a>
                             <div class="rurera-tasks-actions-dropdown">
                                 <button type="button" class="dropdown-toggle" id="rurera-tasks-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <img src="/assets/default/svgs/dots-three.svg" alt="" aria-hidden="true">
