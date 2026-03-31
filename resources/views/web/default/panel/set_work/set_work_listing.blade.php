884c55e3b03d01d8c7b73acdf9742f60d845ddc9
Baz <baz.chimpstudio@gmail.com>
2026-03-31 12:39:00 +0500

    set work listing
    set work listing

--- resources/views/web/default/panel/set_work/set_work_listing.blade.php
+++ resources/views/web/default/panel/set_work/set_work_listing.blade.php
@@ -1,40 +1,52 @@
 <div class="rurera-tasks-card bg-white panel-border rounded-sm px-20 py-15" id="rureraTasksCard">
-    <div class="rurera-tasks-item">
-        <div class="row align-items-center">
-            <div class="col-md-7">
-                <div class="d-flex align-items-center">
-                    <div class="rurera-tasks-content">
-                        <div class="rurera-tasks-title-row">
-                            <h3 class="rurera-tasks-name font-16 font-weight-bold">Cambridge Select Insight (CEM Select)</h3>
-                            <span class="rurera-tasks-tag rurera-tasks-tag-due-soon">Due Soon</span>
+    @if( $assignmentsArray->count() > 0 )
+        @foreach( $assignmentsArray as $assignmentObj)
+            @php
+                if(!isset($assignmentObj->StudentAssignmentData->id)){
+                    continue;
+                }
+                $assignmentTitle = $assignmentObj->StudentAssignmentData->title;
+                $assignmentLink = '/assignment/'.$assignmentObj->id;
+            @endphp
+            <div class="rurera-tasks-item">
+                <div class="row align-items-center">
+                    <div class="col-md-7">
+                        <div class="d-flex align-items-center">
+                            <div class="rurera-tasks-content">
+                                <div class="rurera-tasks-title-row">
+                                    <h3 class="rurera-tasks-name font-16 font-weight-bold">{{$assignmentTitle}}</h3>
+                                    <span class="rurera-tasks-tag rurera-tasks-tag-due-soon">Due Soon</span>
+                                </div>
+                                <p class="subject-info font-14 text-gray">Mock test • Deadline 01/07/24 • Assigned by you</p>
+                            </div>
                         </div>
-                        <p class="subject-info font-14 text-gray">Mock test • Deadline 01/07/24 • Assigned by you</p>
+                    </div>
+                    <div class="col-md-5 rurera-tasks-row-right">
+                        <div class="rurera-tasks-actions font-14">
+                            <button class="rurera-tasks-btn rurera-tasks-btn-outline">Download</button>
+                            <div class="rurera-tasks-actions-dropdown dropdown">
+                                <button type="button"
+                                        class="dropdown-toggle"
+                                        id="rurera-tasks-dropdown"
+                                        data-toggle="dropdown"
+                                        aria-haspopup="true"
+                                        aria-expanded="false">
+                                    <img src="/assets/default/svgs/dots-three.svg" alt="" aria-hidden="true">
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="rurera-tasks-dropdown">
+                                    <button type="button" class="rurera-tasks-close-btn">
+                                        <img src="/assets/default/svgs/delete.svg" alt="" aria-hidden="true">
+                                        <span>Delete</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-5 rurera-tasks-row-right">
-                <div class="rurera-tasks-actions font-14">
-                    <button class="rurera-tasks-btn rurera-tasks-btn-outline">Download</button>
-                    <div class="rurera-tasks-actions-dropdown dropdown">
-                        <button type="button"
-                                class="dropdown-toggle"
-                                id="rurera-tasks-dropdown"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false">
-                            <img src="/assets/default/svgs/dots-three.svg" alt="" aria-hidden="true">
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="rurera-tasks-dropdown">
-                            <button type="button" class="rurera-tasks-close-btn">
-                                <img src="/assets/default/svgs/delete.svg" alt="" aria-hidden="true">
-                                <span>Delete</span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+        @endforeach
+    @endif
+
 
     <div class="rurera-tasks-item">
         <div class="row align-items-center">
