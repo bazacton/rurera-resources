35bf0cf1bb41633539baa93f57242621d67e2c00
Baz <baz.chimpstudio@gmail.com>
2026-03-31 11:25:19 +0500

    setwork
    setwork

---
+++ resources/views/web/default/panel/set_work/topics_listings_tests.blade.php
@@ -0,0 +1,104 @@
+<div class="select-topics">
+    <h3 class="font-16 font-weight-bold">Select Topics</h3>
+    <div class="selected-topics rurera-hide" id="selectedTopics">
+        <span class="count selected-topics-count"><span>0</span> topics selected</span>
+        <div class="chips selected-topics-chips">
+        </div>
+    </div>
+    <!-- Top buttons -->
+
+
+    <!-- Subject filters -->
+    <div class="subject-filters">
+
+        @if(!empty( $parentData))
+            <button class="chip parent-filters active" data-id="all"><span class="active-tick">✓</span> All</button>
+            @foreach( $parentData as $parentObj)
+                <button type="button" class="chip parent-filters" data-id="{{isset($parentObj->id)? $parentObj->id : 0}}">{{isset($parentObj->title)? $parentObj->title : ''}}</button>
+            @endforeach
+        @endif
+
+        @if(!empty( $parentData))
+            <div class="select-holder">
+                <select class="select performance-level-selection">
+                    <option value="all">All performance levels</option>
+                    <option value="Not Started">Not Started</option>
+                    <option value="Practice Needed">Practice Needed</option>
+                    <option value="Good">Good</option>
+                    <option value="Very Good">Very Good</option>
+                    <option value="Excellent">Excellent</option>
+                </select>
+            </div>
+        @endif
+    </div>
+    <div class="topics-table-holder lms-chapter-ul-outer table-sm panel-border bg-white rounded-sm">
+        <table class="topics-table mt-0">
+            <thead>
+            <tr>
+                <th>Mock Tests </th>
+                <th>Total Attempts</th>
+                <th>Recent Attempt</th>
+                <th>Score</th>
+                <th>SAS</th>
+            </tr>
+            </thead>
+            <tbody>
+            @if(!empty($listingData))
+                @foreach($listingData as $listingObj)
+                    @php $topic_completion = isset($listingObj->performance)? $listingObj->performance : 0;
+                        $last_activity = isset($listingObj->last_activity)? $listingObj->last_activity : '-';
+                        $no_of_attempts = isset($listingObj->no_of_attempts)? $listingObj->no_of_attempts : 0;
+                        $sas_accuracy = isset($listingObj->sas_accuracy)? $listingObj->sas_accuracy : 0;
+                        $completion_title = 'Not Started';
+                        $completion_title = ($topic_completion > 0)? 'Practice Needed' : $completion_title;
+                        $completion_title = ($topic_completion > 39)? 'Good' : $completion_title;
+                        $completion_title = ($topic_completion > 59)? 'Very Good' : $completion_title;
+                        $completion_title = ($topic_completion > 79)? 'Excellent' : $completion_title;
+                    @endphp
+                    <tr class="listing-data-row" data-level_type="{{$completion_title}}" data-parent_id="{{isset($listingObj->parent_id)? $listingObj->parent_id : 0}}">
+                        <td data-th="Topic"> <input type="checkbox" id="check_{{isset($listingObj->id)? $listingObj->id : 0}}" name="chapter_ids[]" value="{{isset($listingObj->id)? $listingObj->id : 0}}" class="topic-check topic-selection-check" data-topic="{{isset($listingObj->title)? $listingObj->title : '-'}}"> <label  for="check_{{isset($listingObj->id)? $listingObj->id : 0}}">{{isset($listingObj->title)? $listingObj->title : '-'}}</label></td>
+                        <td data-th="Last seen">{{$no_of_attempts}}</td>
+                        <td data-th="Last seen">
+                            @if(isset($last_activity) && $last_activity > 0)
+                                {{ dateTimeFormat($last_activity, 'j M Y') }}
+                            @else
+                                -
+                            @endif
+                        </td>
+                        <td data-th="Performance">
+                            {{$topic_completion}}
+                            <div class="percent-holder">
+                                <div class="chapter_percent circle-blue" data-percent="{{$topic_completion}}">
+                                    <div class="circle_inner">
+                                        <div class="round_per"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+
+                        <td data-th="Last seen">
+                            {{$sas_accuracy}}
+                            <div class="percent-holder">
+                                <div class="chapter_percent circle-blue" data-percent="{{$sas_accuracy}}">
+                                    <div class="circle_inner">
+                                        <div class="round_per"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            @else
+                <tr class="listing-data-row" data-parent_id="{{isset($listingObj->parent_id)? $listingObj->parent_id : 0}}">
+                    <td colspan="4">
+                        @include('web.default.default.list_no_record')
+                    </td>
+                </tr>
+            @endif
+
+
+            </tbody>
+        </table>
+    </div>
+
+</div>
