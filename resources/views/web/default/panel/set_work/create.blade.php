806a27509467a5c2ecefcc695d2f106386557e47
Baz <baz.chimpstudio@gmail.com>
2026-04-02 02:36:58 +0500

    setwork
    setwork

--- resources/views/web/default/panel/set_work/create.blade.php
+++ resources/views/web/default/panel/set_work/create.blade.php
@@ -549,7 +549,7 @@
 
 
 
-                <div class="row">
+                <div class="row1">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-12">
                         <div class="form-group rurera_common_hide_field timestables_heatmap"></div>
 
