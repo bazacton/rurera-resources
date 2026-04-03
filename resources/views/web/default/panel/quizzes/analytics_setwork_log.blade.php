f8b4f26686641b6b5f481bb58ccdec8a6e8b7898
Baz <baz.chimpstudio@gmail.com>
2026-04-03 17:08:10 +0500

    analytics
    analytics

--- resources/views/web/default/panel/quizzes/analytics_setwork_log.blade.php
+++ resources/views/web/default/panel/quizzes/analytics_setwork_log.blade.php
@@ -38,7 +38,7 @@ $rand_id = rand(99,9999);
                 @php $attempt_counter = 1; @endphp
                 @foreach($topicAssignmentResults as $topicAssignmentResultObj)
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button">
+                        <button class="nav-link {{($active_result_id == $topicAssignmentResultObj->id)? 'active' : ''}}" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button">
                             Attempt {{$attempt_counter}}
                         </button>
                     </li>
