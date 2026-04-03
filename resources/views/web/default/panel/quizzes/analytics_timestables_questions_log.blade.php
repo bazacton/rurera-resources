bd3223ee54ead9b4836879386e5987045837b171
Baz <baz.chimpstudio@gmail.com>
2026-04-03 11:26:45 +0500

    timestables summary
    timestables summary

--- resources/views/web/default/panel/quizzes/analytics_timestables_questions_log.blade.php
+++ resources/views/web/default/panel/quizzes/analytics_timestables_questions_log.blade.php
@@ -29,6 +29,284 @@
     <div class="section-title mb-15">
         <h2 class="font-22 mb-0">Skill Summary</h2>
     </div>
+
+
+    <div class="card mb-30">
+
+
+        <div class="card-header  mb-0 font-14 border-top-0 collapsed" role="button" data-toggle="collapse" data-target="#attempt_questions_5642" aria-expanded="false" aria-controls="attempt_questions_5642">
+
+            <span>SESSION </span>
+
+            <span class="analytics-timespend">
+                <img src="/assets/default/img/panel-sidebar/clock.svg" alt="clock">
+                <span>Thu, 02 Apr 26 - 14s</span>
+            </span>
+            <span class="analytics-cions-earned">
+                <span>&nbsp;</span>
+            </span>
+        </div>
+
+
+
+
+        <div id="attempt_questions_5642" class="collapse" data-parent="#analyticsAccordion" style="">
+
+            <div class="card-header  mb-0 font-14 border-top-0 collapsed" role="button" data-toggle="collapse" data-target="#attempt_questions_5642" aria-expanded="false" aria-controls="attempt_questions_5642">
+
+                <span>Table 2 </span>
+
+                <span class="analytics-timespend">
+                <img src="/assets/default/img/panel-sidebar/clock.svg" alt="clock">
+                <span>Thu, 02 Apr 26 - 14s</span>
+            </span>
+                <span class="analytics-cions-earned">
+                <span>&nbsp;</span>
+            </span>
+            </div>
+
+            <div class="card-body pb-20">
+
+                <div class="question-result-layout-holder">
+
+                    <div class="question-result-layout question-status-correct mb-10">
+                        <div class="status-badge font-14">
+                            <i>✓</i>
+                            <span class="time-text">33s</span>
+                        </div>
+                        <div class="question-counts mb-10 font-14">
+                            <span>Question 10 of 10</span>
+                        </div>
+
+                        <div class="question-area">
+                            <div class="question-step">
+                                <div class="lms-radio-lists">
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Correct answer:</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="question-result-layout question-status-incorrect mb-10">
+                        <div class="status-badge font-14">
+                            <i>×</i>
+                            <span class="time-text">04s</span>
+                        </div>
+                        <div class="question-counts mb-10 font-14">
+                            <span>Question 9 of 10</span>
+                        </div>
+
+                        <div class="question-area">
+                            <div class="question-step">
+                                <div class="lms-radio-lists">
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Correct answer:</span>
+                                    </div>
+
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Red Redington answered:</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="question-result-layout question-status-correct mb-10">
+                        <div class="status-badge font-14">
+                            <i>✓</i>
+                            <span class="time-text">12s</span>
+                        </div>
+                        <div class="question-counts mb-10 font-14">
+                            <span>Question 8 of 10</span>
+                        </div>
+
+                        <div class="question-area">
+                            <div class="question-step">
+                                <div class="lms-radio-lists">
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Correct answer:</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="question-result-layout question-status-correct mb-10">
+                        <div class="status-badge font-14">
+                            <i>✓</i>
+                            <span class="time-text">12s</span>
+                        </div>
+                        <div class="question-counts mb-10 font-14">
+                            <span>Question 7 of 10</span>
+                        </div>
+
+                        <div class="question-area">
+                            <div class="question-step">
+                                <div class="lms-radio-lists">
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Correct answer:</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="question-result-layout question-status-incorrect mb-10">
+                        <div class="status-badge font-14">
+                            <i>×</i>
+                            <span class="time-text">04s</span>
+                        </div>
+                        <div class="question-counts mb-10 font-14">
+                            <span>Question 6 of 10</span>
+                        </div>
+
+                        <div class="question-area">
+                            <div class="question-step">
+                                <div class="lms-radio-lists">
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Correct answer:</span>
+                                    </div>
+
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Red Redington answered:</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="question-result-layout question-status-correct mb-10">
+                        <div class="status-badge font-14">
+                            <i>✓</i>
+                            <span class="time-text">14s</span>
+                        </div>
+                        <div class="question-counts mb-10 font-14">
+                            <span>Question 5 of 10</span>
+                        </div>
+
+                        <div class="question-area">
+                            <div class="question-step">
+                                <div class="lms-radio-lists">
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Correct answer:</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="question-result-layout question-status-correct mb-10">
+                        <div class="status-badge font-14">
+                            <i>✓</i>
+                            <span class="time-text">15s</span>
+                        </div>
+                        <div class="question-counts mb-10 font-14">
+                            <span>Question 4 of 10</span>
+                        </div>
+
+                        <div class="question-area">
+                            <div class="question-step">
+                                <div class="lms-radio-lists">
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Correct answer:</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="question-result-layout question-status-correct mb-10">
+                        <div class="status-badge font-14">
+                            <i>✓</i>
+                            <span class="time-text">23s</span>
+                        </div>
+                        <div class="question-counts mb-10 font-14">
+                            <span>Question 3 of 10</span>
+                        </div>
+
+                        <div class="question-area">
+                            <div class="question-step">
+                                <div class="lms-radio-lists">
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Correct answer:</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="question-result-layout question-status-incorrect mb-10">
+                        <div class="status-badge font-14">
+                            <i>×</i>
+                            <span class="time-text">09s</span>
+                        </div>
+                        <div class="question-counts mb-10 font-14">
+                            <span>Question 2 of 10</span>
+                        </div>
+
+                        <div class="question-area">
+                            <div class="question-step">
+                                <div class="lms-radio-lists">
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Correct answer:</span>
+                                    </div>
+
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Red Redington answered:</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="question-result-layout question-status-correct mb-10">
+                        <div class="status-badge font-14">
+                            <i>✓</i>
+                            <span class="time-text">14s</span>
+                        </div>
+                        <div class="question-counts mb-10 font-14">
+                            <span>Question 1 of 10</span>
+                        </div>
+
+                        <div class="question-area">
+                            <div class="question-step">
+                                <div class="lms-radio-lists">
+                                    <div class="lms-user-answer-block">
+                                        <span class="list-title">Correct answer:</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+
     @include('web.default.panel.quizzes.analytics_timestables_questions_log_inner',
     [
     'question'               => $question,
