a2f82c429deaffcb90a91eccf08760b790c3bb50
Baz <baz.chimpstudio@gmail.com>
2026-04-03 15:32:45 +0500

    analytics
    analytics

--- resources/views/web/default/panel/analytics/setwork_analytics.blade.php
+++ resources/views/web/default/panel/analytics/setwork_analytics.blade.php
@@ -33,7 +33,7 @@
                             @if($assignedTopics->count() > 0)
                                 @foreach($assignedTopics as $assignedTopicObj)
                                     @php $quizObj = $assignedTopicObj->quizData;
- pre($assignedTopicObj);
+ pre($assignedTopicObj->id);
 
                                         $analytics_detail_link = '';
                                         $analytics_detail_link = '/analytics/set_work/'.$studentAssignmentObj->id.'/'.$assignedTopicObj->id;
