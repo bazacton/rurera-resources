aaa81db57db136bad732d180438b6a9e444f69ea
Baz <baz.chimpstudio@gmail.com>
2026-04-07 00:14:12 +0500

    setwork
    setwork

--- resources/views/web/default/timestables/heatmap_layout.blade.php
+++ resources/views/web/default/timestables/heatmap_layout.blade.php
@@ -1,11 +1,1 @@
-<div class="col-12">
-    <div class="heatmap-select-option">
-        <div class="select-field">
-            <input checked type="radio" id="select-one" name="heatmap">
-            <label for="select-one" class="heatmap-type" data-type_limit="12"> 2-12<span>&#215;</span> </label>
-            <input type="radio" id="select-two" name="heatmap">
-            <label for="select-two" class="border-right-0 heatmap-type" data-type_limit="20"> 2-20<span>&#215;</span> </label>
-        </div>
-    </div>
-    ssd
-</div>
+testing heat map layout
