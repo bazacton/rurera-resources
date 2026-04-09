1a63960fb213b078192fb8bd6cd11d6a2a290106
Baz <baz.chimpstudio@gmail.com>
2026-04-09 19:05:11 +0500

    setwork
    setwork

--- resources/views/web/default/panel/set_work/set_work_listing.blade.php
+++ resources/views/web/default/panel/set_work/set_work_listing.blade.php
@@ -27,12 +27,31 @@
                 <!-- List -->
                 @if($studentAssignmentData->userAssignedTopics->count() > 0)
                     <ul class="assignment-list">
-                    @foreach($studentAssignmentData->userAssignedTopics->count() as $assignmentObj)
+                    @foreach($studentAssignmentData->userAssignedTopics as $assignmentObj)
+                            @php
+                                if(!isset($assignmentObj->StudentAssignmentData->id)){
+                                    continue;
+                                }
+                                $quizObj = $assignmentObj->quizData;
+                                $assignmentTitle = isset($quizObj->id)? $quizObj->getTitleAttribute() : '';
+                                $assignmentTitle = ($assignmentTitle == '')? ucfirst($assignmentObj->StudentAssignmentData->assignment_type) : $assignmentTitle;
+                                $assignmentLink = '/assignment/'.$assignmentObj->id;
+                                $time_difference = TimeDifference(time(), $assignmentObj->deadline_date , 'minutes');
+                                $assignmentLink = getAssignmentLink($assignmentObj, $assignmentObj->StudentAssignmentData);
+                                $due_label = '';
+                                $due_class = '';
+                                $due_label = ($time_difference <= 4000)? '- Due Soon' : $due_label;
+                                $due_label = ($time_difference <= 5)? '- Overdue' : $due_label;
+
+                                $due_label = ($time_difference <= 4000)? 'rurera-tasks-tag-due-soon' : $due_class;
+                                $due_label = ($time_difference <= 5)? 'rurera-tasks-tag-overdue' : $due_class;
+
+                            @endphp
                             <li class="assignment-item">
                                 <div class="assignment-left">
                                     <div class="icon-box">🎓</div>
                                     <div>
-                                        <div class="assignment-title">Working Like a Scientist</div>
+                                        <div class="assignment-title">{{$assignmentTitle}}</div>
                                         <div class="assignment-meta">
                                             Attempts : 2/10 &nbsp;&nbsp; SmartScore : 50/100
                                         </div>
