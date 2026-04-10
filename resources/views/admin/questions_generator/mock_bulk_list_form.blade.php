101836dde0cccc908861c7e41b5dfc204e62b03c
Baz <baz.chimpstudio@gmail.com>
2026-04-10 17:38:20 +0500

    mock test
    mock test

--- resources/views/admin/questions_generator/mock_bulk_list_form.blade.php
+++ resources/views/admin/questions_generator/mock_bulk_list_form.blade.php
@@ -81,7 +81,7 @@
                             @php $selected = (isset($quizObj->id) && $quizObj->mock_type == 'mock_practice')? 'selected' : ''; @endphp
                             <option data-target_common_class="practice_type_fields" data-target_field_class="mock_practice_fields" value="mock_practice" {{$selected}}>Yes</option>
                             @php $selected = (isset($quizObj->id) && $quizObj->mock_type == 'mock_exam')? 'selected' : ''; @endphp
-                            <option data-target_common_class="practice_type_fields" data-target_field_class="mock_practice_fields" value="mock_exam" {{$selected}}>No</option>
+                            <option data-target_common_class="practice_type_fields" data-target_field_class="mock_test_fields" value="mock_exam" {{$selected}}>No</option>
                         </select>
                     </div>
                 </div>
@@ -221,7 +221,7 @@
                                         </div>
                                     </div>
 
-                                    <div class="col-md-12 col-lg-12 subjects-listing-data practice_type_fields mock_practice_fields">
+                                    <div class="col-md-12 col-lg-12 subjects-listing-data practice_type_fields mock_practice_fields mock_test_fields">
 
                                     </div>
                                     <div class="col-12">
